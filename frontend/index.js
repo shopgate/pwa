bc2559cfc421015b7e977488469fecd436bd3178
Carsten Schneider <carsten.schneider@shopgate.com>
2017-08-14 12:49:01 +0200

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+import {
+  getRelevantConfigs,
+} from 'Library/helpers/tracking';
+import Plugin from './Plugin';
+import config from './config';
+
+/**
+ * Read the config and create plugin instances
+ * @param {Object} clientInformation Information about the current client
+ */
+export default function init(clientInformation) {
+  getRelevantConfigs(config, clientInformation).forEach(tracker => new Plugin(tracker.config));
+}
