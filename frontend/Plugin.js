6dd4a69b14b4acf4f36cedc135d42d7e5eabeded
Carsten <carsten.schneider@shopgate.com>
2017-08-22 07:43:12 +0200

    CON-2128 added license

--- frontend/Plugin.js
+++ frontend/Plugin.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import SgTrackingPlugin from '@shopgate/tracking-core/plugins/Base';
 // eslint-disable-next-line import/no-unresolved
 import { SGLink } from 'tracking-helper';
