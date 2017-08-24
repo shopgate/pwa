3d789cedaad01e1cb003116fca6d7be5287e606b
Florian Müller <florian.mueller81@gmx.de>
2017-08-24 14:38:55 +0200

    Fixed issues

--- components/Headline/style.js
+++ components/Headline/style.js
@@ -1,11 +1,18 @@
-import cxs from 'cxs';
-import { variables } from 'Templates/styles';
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-const headline = cxs({
+import css from 'glamor';
+import variables from 'Styles/variables';
+
+const headline = css({
   fontSize: 18,
   margin: `${variables.gap.big * 2}px 0 ${variables.gap.big}px`,
   textAlign: 'center',
-});
+}).toString();
 
 export default {
   headline,
--- components/Price/style.js
+++ components/Price/style.js
@@ -1,10 +1,17 @@
-import cxs from 'cxs';
-import { colors } from 'Templates/styles';
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-export const discounted = cxs({
+import css from 'glamor';
+import colors from 'Styles/colors';
+
+export const discounted = css({
   color: colors.primary,
-});
+}).toString();
 
-export default cxs({
+export default css({
   whiteSpace: 'nowrap',
-});
+}).toString();
