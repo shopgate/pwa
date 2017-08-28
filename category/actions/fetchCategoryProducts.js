0c466ae765714393757ebb24575eff822ede5b87
Richard Gorman <richardgorman@outlook.com>
2017-08-28 10:47:41 +0200

    Removed parameter from action

--- category/actions/fetchCategoryProducts.js
+++ category/actions/fetchCategoryProducts.js
@@ -1,23 +1,25 @@
 import logger from '@shopgate/pwa-core/classes/Logger';
 import { DEFAULT_SORT } from '@shopgate/pwa-common/constants/DisplayOptions';
 import getProducts from '../../product/actions/getProducts';
+import { getCurrentCategoryId } from '../selectors';
 
 /**
  * Retrieves category products for a certain category by ID.
- * @param {string} categoryId The ID of the category to request products for.
  * @param {number} offset The offset for the products to request.
  * @param {number} limit The amount of products to request.
  * @param {string} sort The sort order of the products.
  * @return {Function} The dispatched action.
  */
 const fetchCategoryProducts =
-  (categoryId, offset = 0, limit = 30, sort = DEFAULT_SORT) =>
+  (offset = 0, limit = 30, sort = DEFAULT_SORT) =>
     (dispatch, getState) => {
       const state = getState();
+      const categoryId = getCurrentCategoryId(state);
       const category = state.category.categoriesById[categoryId];
 
       if (!category) {
         logger.error(`Category '${categoryId}' doesn't exist in the state! No products fetched!`);
+        return;
       }
 
       dispatch(getProducts({
