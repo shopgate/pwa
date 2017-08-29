73da750813f77e9c4a4b39bb76d91958e1504d3e
Florian Müller <florian.mueller81@gmx.de>
2017-08-29 16:23:07 +0200

    Added Sheet

---
+++ components/Sheet/components/Header/index.jsx
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@shopgate/pwa-common/components/Grid';
+import Ripple from 'Components/Ripple';
+import CrossIcon from 'Components/icons/CrossIcon';
+import styles from './style';
+
+/**
+ * Header component.
+ */
+class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onToggleClose: PropTypes.func,
+  };
+
+  /**
+   * The component's default props.
+   * @type {Object}
+   */
+  static defaultProps = {
+    onToggleClose: () => {},
+  };
+
+  /**
+   * Only re-render when the title changes.
+   * @param {Object} nextProps Next Props
+   * @returns {boolean}
+   */
+  shouldComponentUpdate(nextProps) {
+    return (this.props.title !== nextProps.title);
+  }
+
+  /**
+   * Renders the component.
+   * @returns {JSX}
+   */
+  render() {
+    return (
+      <Grid component="div" wrap={false}>
+        <button className={styles.closeButton} onClick={this.props.onToggleClose}>
+          <Ripple className={styles.closeIcon}>
+            <CrossIcon size={24} />
+          </Ripple>
+        </button>
+        <Grid.Item className={styles.title} component="div" grow={1}>
+          {this.props.title}
+        </Grid.Item>
+      </Grid>
+    );
+  }
+}
+
+export default Header;
---
+++ components/Sheet/components/Header/style.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { css } from 'glamor';
+import variables from 'Styles/variables';
+
+const closeButton = css({
+  lineHeight: 1,
+  outline: 0,
+  padding: 0,
+  minWidth: variables.navigator.height,
+  height: variables.navigator.height,
+  position: 'relative',
+  zIndex: 2,
+}).toString();
+
+const closeIcon = css({
+  display: 'flex',
+  height: '100%',
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+}).toString();
+
+const title = css({
+  fontSize: '1.25rem',
+  fontWeight: 500,
+  position: 'relative',
+  alignItems: 'center',
+  padding: `0 ${variables.gap.big}px`,
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  alignSelf: 'center',
+}).toString();
+
+export default {
+  closeButton,
+  closeIcon,
+  title,
+};
