5396f6ac683dbf19873b19c06a92083cea81e970
Florian Müller <florian.mueller81@gmx.de>
2017-08-31 15:09:54 +0200

    Clean ups

---
+++ pages/Product/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Action Types
+ */
+
+export const OPEN_PRODUCT_GALLERY = 'OPEN_PRODUCT_GALLERY';
+export const REQUEST_PRODUCT_DATA = 'REQUEST_PRODUCT_DATA';
