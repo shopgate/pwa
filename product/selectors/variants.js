a016911f1571327e8c378b12f27fc8c4621770c5
Florian Müller <florian.mueller81@gmx.de>
2017-08-31 17:16:05 +0200

    Added missing selectors

--- product/selectors/variants.js
+++ product/selectors/variants.js
@@ -1,4 +1,14 @@
-import { getCurrentBaseProduct } from '../selectors/product';
+import {
+  getCurrentBaseProduct,
+  getCurrentBaseProductId,
+} from '../selectors/product';
+
+/**
+ * Selects collection of all stored product variants from the store.
+ * @param {Object} state The current application state.
+ * @return {Object} The collection of product variants.
+ */
+const getVariants = state => state.product.variantsByProductId;
 
 /**
  * Gets the id of the currently selected product variant.
@@ -27,3 +37,29 @@ export const hasCurrentProductVariants = (state) => {
  * @returns {boolean}
  */
 export const isProductChildrenSelected = state => !!getCurrentProductVariantId(state);
+
+/**
+ * Retrieves product variants by product ID from state.
+ * @param {Object} state The current application state.
+ * @param {string} id The product ID.
+ * @return {Object|null} The dedicated variants. Or null when the requested data is unavailable.
+ */
+export const getVariantsByProductId = (state, id) => {
+  const variants = getVariants(state);
+
+  if (!variants || !variants[id] || variants[id].isFetching === true) {
+    return null;
+  }
+
+  return variants;
+};
+
+/**
+ * Retrieves product variants for the currently selected base product from state.
+ * @param {Object} state The current application state.
+ * @return {Object|null} The dedicated variants. Or null when the requested data is unavailable.
+ */
+export const getCurrentBaseProductVariants = (state) => {
+  const baseProductId = getCurrentBaseProductId(state);
+  return getVariantsByProductId(state, baseProductId);
+};
