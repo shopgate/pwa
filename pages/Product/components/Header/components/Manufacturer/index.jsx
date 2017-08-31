56f2231b6fc5bf340a87ac64180b4fb77a7c3069
Florian Müller <florian.mueller81@gmx.de>
2017-08-31 13:21:30 +0200

    Added Manufacturer

---
+++ pages/Product/components/Header/components/Manufacturer/index.jsx
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@shopgate/pwa-common/components/Grid';
+import PlaceholderLabel from 'Components/PlaceholderLabel';
+import { Manufacturer as BaseManufacturer } from 'Components/Manufacturer';
+import styles from './style';
+
+/**
+ * The Manufacturer component.
+ * @param {Object} props The component props.
+ * @return {JSX}
+ */
+const Manufacturer = ({ manufacturer }) => (
+  <Grid component="div">
+    <Grid.Item component="div" shrink={0} className={styles.infoContainer}>
+      <PlaceholderLabel className={styles.placeholder.info} ready={(manufacturer !== null)}>
+        <BaseManufacturer text={manufacturer} />
+      </PlaceholderLabel>
+    </Grid.Item>
+  </Grid>
+);
+
+Manufacturer.propTypes = {
+  manufacturer: PropTypes.string,
+};
+
+Manufacturer.defaultProps = {
+  manufacturer: null,
+};
+
+export default Manufacturer;
--- pages/Product/components/Header/index.jsx
+++ pages/Product/components/Header/index.jsx
@@ -6,10 +6,10 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import AddToCartButton from './components/AddToCartButton';
 import Rating from './components/Rating';
 import Name from './components/Name';
+import Manufacturer from './components/Manufacturer';
 import styles from './style';
 
 /**
@@ -20,57 +20,15 @@ import styles from './style';
  * - price
  * - price info
  * If not available or animating it will display placeholders
- * @param {Object} props The component props.
  * @returns {JSX}
  */
-const ProductHeader = (props) => {
-  // If we don't have data yet OR if the view is currently animating show placeholder only
-  const placeholderProductReady = (!props.isAnimating && (props.product !== null));
-
-  /**
-   * Required to display placeholders.
-   * If we don't have the product we don't know if it has discount, manufacturer, etc.
-   * this object will decide what placeholders are visible
-   * @type {Object}
-   */
-  const defaultData = {
-    // White space because empty string is false. Needed to show placeholder.
-    manufacturer: ' ',
-    price: {},
-    availability: {
-      // White space because empty string is false. Needed to show placeholder.
-      text: ' ',
-    },
-  };
-  const product = (placeholderProductReady && props.product) ? props.product : defaultData;
-
-  /**
-   * Identical logic for shipping, like for the product. The data is requested with a separate
-   * pipeline request, so it's placeholder needs separate handling.
-   * @type {boolean}
-   */
-  const placeholderShippingReady = (!props.isAnimating && (props.shipping !== null));
-
-  return (
-    <div className={styles.content}>
-      <AddToCartButton />
-      <Rating />
-      <Name />
-    </div>
-  );
-};
-
-ProductHeader.propTypes = {
-  product: PropTypes.shape(),
-  shipping: PropTypes.shape({
-    currency: PropTypes.string,
-    price: PropTypes.number,
-  }),
-};
-
-ProductHeader.defaultProps = {
-  product: null,
-  shipping: {},
-};
+const ProductHeader = () => (
+  <div className={styles.content}>
+    <AddToCartButton />
+    <Rating />
+    <Name />
+    <Manufacturer />
+  </div>
+);
 
 export default ProductHeader;
