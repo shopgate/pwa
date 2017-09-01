5831a5c8f8f031e5210c7d8052ff96278cea0aea
Florian Müller <florian.mueller81@gmx.de>
2017-09-01 17:06:03 +0200

    Bug fixes

--- components/Navigator/components/CartButton/style.js
+++ components/Navigator/components/CartButton/style.js
@@ -5,11 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import cxs from 'cxs';
+import { css } from 'glamor';
 import colors from 'Styles/colors';
 import variables from 'Styles/variables';
 
-const button = cxs({
+const button = css({
   background: colors.primary,
   fontSize: '1.5rem',
   lineHeight: 1,
@@ -17,24 +17,24 @@ const button = cxs({
   padding: 0,
   minWidth: 0,
   width: 0,
-  height: navigator.height,
+  height: variables.navigator.height,
   overflow: 'hidden',
   position: 'relative',
   zIndex: 1,
   transform: 'translate3d(100%, 0, 0)',
   transition: 'transform 300ms cubic-bezier(0.25, 0.1, 0.25, 1), min-width 300ms cubic-bezier(0.25, 0.1, 0.25, 1)',
-});
+}).toString();
 
-const buttonContent = cxs({
+const buttonContent = css({
   display: 'flex',
   height: '100%',
   width: '100%',
   justifyContent: 'center',
   alignItems: 'center',
   color: colors.light,
-});
+}).toString();
 
-const badge = cxs({
+const badge = css({
   position: 'absolute',
   fontSize: '0.75rem',
   lineHeight: 1.4,
@@ -48,7 +48,7 @@ const badge = cxs({
   paddingRight: (variables.gap.small / 2),
   transform: 'translate3d(7px, -10px, 0)',
   boxShadow: '0 1px 1px rgba(0, 0, 0, 0.25)',
-});
+}).toString();
 
 export default {
   button,
