dd948c8f23daf3f8909690866f6322db74393226
Richard Gorman <richardgorman@outlook.com>
2017-09-06 15:18:44 +0200

    Added missing cart related streams

--- cart/streams/index.js
+++ cart/streams/index.js
@@ -1,13 +1,87 @@
 import { main$ } from '@shopgate/pwa-common/streams/main';
 import {
+  REQUEST_CART,
+  RECEIVE_CART,
+  ERROR_CART,
+
+  ADD_PRODUCTS_TO_CART,
   SUCCESS_ADD_PRODUCTS_TO_CART,
-  SUCCESS_DELETE_PRODUCTS_FROM_CART,
+  ERROR_ADD_PRODUCTS_TO_CART,
+
+  UPDATE_PRODUCTS_IN_CART,
   SUCCESS_UPDATE_PRODUCTS_IN_CART,
   ERROR_UPDATE_PRODUCTS_IN_CART,
+
+  DELETE_PRODUCTS_FROM_CART,
+  SUCCESS_DELETE_PRODUCTS_FROM_CART,
+  ERROR_DELETE_PRODUCTS_FROM_CART,
+
+  ADD_COUPONS_TO_CART,
   SUCCESS_ADD_COUPONS_TO_CART,
   ERROR_ADD_COUPONS_TO_CART,
+
+  DELETE_COUPONS_FROM_CART,
+  ERROR_DELETE_COUPONS_FROM_CART,
   SUCCESS_DELETE_COUPONS_FROM_CART,
 } from '../constants';
+
+/**
+ * Gets triggered when the user tried to add a coupon to the cart.
+ * @type {Observable}
+ */
+export const cartRequesting$ = main$.filter(
+  ({ action }) => action.type === REQUEST_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a coupon to the cart.
+ * @type {Observable}
+ */
+export const cartReceived$ = main$.filter(
+  ({ action }) =>
+    action.type === RECEIVE_CART ||
+    action.type === ERROR_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a coupon to the cart.
+ * @type {Observable}
+ */
+export const couponsAdded$ = main$.filter(
+  ({ action }) => action.type === ADD_COUPONS_TO_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a coupon to the cart.
+ * @type {Observable}
+ */
+export const couponsDeleted$ = main$.filter(
+  ({ action }) => action.type === DELETE_COUPONS_FROM_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a product to the cart.
+ * @type {Observable}
+ */
+export const productsAdded$ = main$.filter(
+  ({ action }) => action.type === ADD_PRODUCTS_TO_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a product to the cart.
+ * @type {Observable}
+ */
+export const productsUpdated$ = main$.filter(
+  ({ action }) => action.type === UPDATE_PRODUCTS_IN_CART
+);
+
+/**
+ * Gets triggered when the user tried to add a product to the cart.
+ * @type {Observable}
+ */
+export const productsDeleted$ = main$.filter(
+  ({ action }) => action.type === DELETE_PRODUCTS_FROM_CART
+);
 
 /**
  * Gets triggered when the remote cart was updated.
@@ -16,19 +90,13 @@ import {
 export const remoteCartDidUpdate$ = main$.filter(
   ({ action }) =>
     action.type === SUCCESS_ADD_PRODUCTS_TO_CART ||
-    action.type === SUCCESS_DELETE_PRODUCTS_FROM_CART ||
+    action.type === ERROR_ADD_PRODUCTS_TO_CART ||
     action.type === SUCCESS_UPDATE_PRODUCTS_IN_CART ||
     action.type === ERROR_UPDATE_PRODUCTS_IN_CART ||
+    action.type === SUCCESS_DELETE_PRODUCTS_FROM_CART ||
+    action.type === ERROR_DELETE_PRODUCTS_FROM_CART ||
     action.type === SUCCESS_ADD_COUPONS_TO_CART ||
-    action.type === SUCCESS_DELETE_COUPONS_FROM_CART
+    action.type === ERROR_ADD_COUPONS_TO_CART ||
+    action.type === SUCCESS_DELETE_COUPONS_FROM_CART ||
+    action.type === ERROR_DELETE_COUPONS_FROM_CART
   );
-
-/**
- * Gets triggered when the user tried to add a coupon to the cart.
- * @type {Observable}
- */
-export const couponsDidUpdate$ = main$.filter(
-  ({ action }) =>
-    action.type === SUCCESS_ADD_COUPONS_TO_CART ||
-    action.type === ERROR_ADD_COUPONS_TO_CART
-);
