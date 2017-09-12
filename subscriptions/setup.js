0c64c311eddd95c8d8ee60e6a5fe10c9c78d95cc
Carsten Schneider <carsten.schneider@shopgate.com>
2017-09-12 08:27:33 +0200

    CON-2206 added dummy tracking calls

--- subscriptions/setup.js
+++ subscriptions/setup.js
@@ -5,9 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import tracking from '@shopgate/tracking-core/core/Core';
+import get from 'lodash/get';
+
+// TODO: how to do this?
+import extensions from 'Extensions';
+
+import { TYPE_PHONE, OS_ALL } from '@shopgate/pwa-common/constants/Device';
+import { getWebStorageEntry } from '@shopgate/pwa-common/commands';
+import core from '@shopgate/tracking-core/core/Core';
 import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
-import UnifiedPlugin from '@shopgate/tracking-core/plugins/trackers/Unified';
+
+const UnifiedPlugin = extensions['core-tracking/plugin-unified'];
+const GaNativePlugin = extensions['shopgate-tracking-ga-native/init'];
 
 /**
  * Setup tracking subscriptions.
@@ -18,8 +27,24 @@ export default function setup(subscribe) {
    * Gets triggered when the app starts.
    */
   subscribe(appDidStart$, () => {
-    new UnifiedPlugin(); // eslint-disable-line no-new
+    getWebStorageEntry({ name: 'clientInformation' }).then((response) => {
+      const clientInformation = {
+        type: get(response, 'value.device.type', TYPE_PHONE),
+        os: get(response, 'value.device.os.platform', OS_ALL),
+      };
 
-    tracking.registerFinished();
+      // TODO: instantiate the UnifiedPlugin only if a native tracker is configured (FB, AppsFlyer)
+      // eslint-disable-next-line no-new
+      new UnifiedPlugin();
+
+      // TODO: later we have to loop through all tracking extensions here
+      /**
+       * Call the init function of the GA plugin.
+       * This init function will create the actual instances based on the config
+       */
+      GaNativePlugin(clientInformation);
+
+      core.registerFinished();
+    });
   });
 }
