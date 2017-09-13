86d6aa847b757fe4fe354d32d1303e9e14f7eacc
Felix Laukel <f.laukel@ecentral.de>
2017-09-13 14:19:22 +0200

    CON-2206 Remove unused code

--- subscriptions/setup.js
+++ subscriptions/setup.js
@@ -5,16 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import get from 'lodash/get';
-
-// TODO: how to do this? import extensions from 'Extensions'
-
-import { TYPE_PHONE, OS_ALL } from '@shopgate/pwa-common/constants/Device';
-import { getWebStorageEntry } from '@shopgate/pwa-common/commands';
+import { getWebStorageEntry } from '@shopgate/pwa-core/commands';
 import core from '@shopgate/tracking-core/core/Core';
 import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
 import UnifiedPlugin from '@shopgate/tracking-core/plugins/trackers/Unified';
-
 
 /**
  * Setup tracking subscriptions.
@@ -24,25 +18,13 @@ export default function setup(subscribe) {
   /**
    * Gets triggered when the app starts.
    */
-  subscribe(appDidStart$, () => {
-    getWebStorageEntry({ name: 'clientInformation' }).then((response) => {
-      const clientInformation = {
-        type: get(response, 'value.device.type', TYPE_PHONE),
-        os: get(response, 'value.device.os.platform', OS_ALL),
-      };
+  subscribe(appDidStart$, async () => {
+    await getWebStorageEntry({ name: 'clientInformation' });
 
-      // TODO: instantiate the UnifiedPlugin only if a native tracker is configured (FB, AppsFlyer)
-      // eslint-disable-next-line no-new
-      new UnifiedPlugin();
+    // TODO: instantiate the UnifiedPlugin only if a native tracker is configured (FB, AppsFlyer)
+    // eslint-disable-next-line no-new
+    new UnifiedPlugin();
 
-      // TODO: later we have to loop through all tracking extensions here
-      /**
-       * Call the init function of the GA plugin.
-       * This init function will create the actual instances based on the config
-       */
-      //GaNativePlugin(clientInformation);
-
-      core.registerFinished();
-    });
+    core.registerFinished();
   });
 }
