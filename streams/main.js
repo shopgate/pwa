3ec249dfac601bc04876df14f6a3228fc4fa22bb
Felix Laukel <f.laukel@ecentral.de>
2017-09-15 12:49:42 +0200

    CON-2206 Add rxjs zip operator

--- streams/main.js
+++ streams/main.js
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/zip';
 import { mainSubject } from '../store/observable-middleware';
 
 /**
