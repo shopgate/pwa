b24ab9f93675af5b9937b681ef8e63e3963ed265
Florian Müller <florian.mueller81@gmx.de>
2017-09-19 13:26:31 +0200

    Prepared release

---
+++ frontend/index.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { getRelevantConfigs } from '@shopgate/pwa-common/helpers/tracking';
+import Plugin from './Plugin';
+import config from './config';
+
+/**
+ * Read the config and create plugin instances
+ * @param {Object} clientInformation Information about the current client
+ */
+export default function init(clientInformation) {
+  getRelevantConfigs(config, clientInformation).forEach(tracker => new Plugin(tracker.config));
+}
