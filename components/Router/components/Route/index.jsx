fc689da51a298933960a4d21b14c0e4186bbd526
Richard Gorman <richardgorman@outlook.com>
2017-09-20 17:10:27 +0200

    Revert "The router's lifecycle methods are now called on direct children of the route."
    This reverts commit c416f448b5eb94d48ce1e2863f8d90a10078966c.

--- components/Router/components/Route/index.jsx
+++ components/Router/components/Route/index.jsx
@@ -9,7 +9,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { isFunction } from '../../../../helpers/validation';
 import RouteContent from './components/Content';
-import getChildInstance from '../../helpers/getChildInstance';
 
 /**
  * Updates the hosted component of the route.
@@ -162,16 +161,9 @@ class Route extends Component {
 
         newHostedComponent.componentInstance = this.findComponentRef(component);
 
-        const childInstance = getChildInstance(newHostedComponent.componentInstance);
-
         // Notify component that it is active now.
         if (isFunction(newHostedComponent.componentInstance.routeWillEnter)) {
           newHostedComponent.componentInstance.routeWillEnter();
-        }
-
-        // Notify first child that it is active now.
-        if (childInstance && isFunction(childInstance.routeWillLeave)) {
-          childInstance.routeWillLeave();
         }
 
         // Save a reference of the original componentDidUpdate lifecycle function.
@@ -187,9 +179,13 @@ class Route extends Component {
         newHostedComponent.componentInstance.componentDidUpdate =
           function componentDidUpdate(prevProps, prevState) {
             originalComponentDidUpdate(prevProps, prevState);
+            // @TODO: track(route, this);
           };
 
         newHostedComponent.componentInstance.enableTracking = this.enableTracking;
+
+        // @TODO: this.enableTracking();
+        // @TODO: track(route, newHostedComponent.componentInstance);
       },
       params,
     };
@@ -251,17 +247,9 @@ class Route extends Component {
     const { dom, componentInstance } = this.state.hostedComponents[index];
     dom.style.display = 'none';
 
-    // Select the instance of the first child component.
-    const childInstance = getChildInstance(componentInstance);
-
     // Notify component that it is inactive now.
     if (componentInstance && isFunction(componentInstance.routeWillLeave)) {
       componentInstance.routeWillLeave();
-    }
-
-    // Notify first child that it is active now.
-    if (childInstance && isFunction(childInstance.routeWillLeave)) {
-      childInstance.routeWillLeave();
     }
   }
 
@@ -273,18 +261,13 @@ class Route extends Component {
     const { dom, componentInstance } = this.state.hostedComponents[index];
     dom.style.display = '';
 
-    // Select the instance of the first child component.
-    const childInstance = getChildInstance(componentInstance);
-
     // Notify component that it is active now.
     if (componentInstance && isFunction(componentInstance.routeWillEnter)) {
       componentInstance.routeWillEnter();
     }
 
-    // Notify first child that it is active now.
-    if (childInstance && isFunction(childInstance.routeWillEnter)) {
-      childInstance.routeWillEnter();
-    }
+    // @TODO: this.enableTracking();
+    // @TODO: this.track(this, componentInstance);
   }
 
   /**
