6d8faf8db1b798a5abda9f5c4f78b94f10e52126
Carsten Schneider <carsten.schneider@shopgate.com>
2017-09-25 09:55:32 +0200

    CON-2206 implemented initiatedCheckout event

---
+++ subscriptions/checkout.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import core from '@shopgate/tracking-core/core/Core';
+import { routeDidEnter } from '@shopgate/pwa-common/streams/history';
+import { CHECKOUT_PATH } from '@shopgate/pwa-common-commerce/checkout/constants';
+import getCart from '../selectors/cart';
+
+/**
+ * Emits when the checkout route was entered.
+ */
+export const checkoutDidEnter$ = routeDidEnter(CHECKOUT_PATH);
+
+/**
+ * Checkout tracking subscriptions.
+ * @param {Function} subscribe The subscribe function.
+ */
+export default function checkout(subscribe) {
+  subscribe(checkoutDidEnter$, ({ getState }) => core.track.initiatedCheckout(getCart(getState())));
+}
