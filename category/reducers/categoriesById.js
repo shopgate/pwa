8b518520e397b4bc6150ad34d7eb7bbfd411b62e
Frithjof Kloes <frithjof.kloes@shopgate.com>
2017-09-25 16:41:09 +0200

    CON-2384: moved helper functions from reducers into separate modules

--- category/reducers/categoriesById.js
+++ category/reducers/categoriesById.js
@@ -13,33 +13,7 @@ import {
   RECEIVE_CATEGORY_CHILDREN,
   ERROR_CATEGORY,
 } from '../constants';
-
-/**
- * Builds state entries from a collection of categories.
- * @param {Array} collection A category collection.
- * @return {Object} The category entries for the state.
- */
-const handleCategoryCollection = (collection) => {
-  const categoryObjects = {};
-  // Is used to set them all to the same date because they arrived at the same time.
-  const expires = Date.now() + CATEGORY_LIFETIME;
-
-  if (!collection) {
-    return categoryObjects;
-  }
-
-  collection.forEach((category) => {
-    const { children, ...categoryData } = category;
-
-    categoryObjects[category.id] = {
-      ...categoryData,
-      isFetching: false,
-      expires,
-    };
-  });
-
-  return categoryObjects;
-};
+import handleCategoryCollection from './helpers/handleCategoryCollection';
 
 /**
  * Stores categories by their ID.
--- filter/reducers/helpers/enrichFilters.js
+++ filter/reducers/helpers/enrichFilters.js
@@ -6,15 +6,17 @@
  */
 
 import kebabCase from 'lodash/kebabCase';
-import { FILTER_PATH } from '../constants';
+import { FILTER_PATH } from '../../constants';
 
 /**
  * Enriches a filter object with additional properties.
  * @param {Array} filters The filters collection.
  * @return {Array} The new collection of enriched filters.
  */
-export const enrichFilters = filters => filters.map(filter => ({
+const enrichFilters = filters => filters.map(filter => ({
   ...filter,
   // The route url of this filter.
   url: `${FILTER_PATH}/${kebabCase(filter.id)}`,
 }));
+
+export default enrichFilters;
