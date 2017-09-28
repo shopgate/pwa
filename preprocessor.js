65087b2cd9fe0c9ff7c063ba687fc7aa53266d8b
Florian Müller <florian.mueller81@gmx.de>
2017-09-28 15:34:27 +0200

    Use path instead of require

--- preprocessor.js
+++ preprocessor.js
@@ -6,11 +6,12 @@
  */
 
 const { readFileSync } = require('fs');
+const { resolve } = require('path');
 const { createTransformer } = require('babel-jest');
 const { safeLoad } = require('js-yaml');
 const stripComments = require('strip-json-comments');
 
-const babelConfigString = readFileSync(require.resolve('./.babelrc'), 'utf8');
+const babelConfigString = readFileSync(resolve(process.cwd(), './.babelrc'), 'utf8');
 const babelConfig = safeLoad(stripComments(babelConfigString));
 
 module.exports = createTransformer(babelConfig);
