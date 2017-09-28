7e19c09b442876934c344b2d30a00011ef3039c6
Marcus von Keil <mvonkeil@ecentral.de>
2017-09-28 10:22:51 +0200

    CON-2376 Reverted history action imports

--- pages/Register/connector.js
+++ pages/Register/connector.js
@@ -6,7 +6,7 @@
  */
 
 import { connect } from 'react-redux';
-import { goBackHistory } from '@shopgate/pwa-common/actions/history/changeHistory';
+import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
 import fetchRegisterUrl from '@shopgate/pwa-common/actions/user/fetchRegisterUrl';
 import { getRegisterUrl } from '@shopgate/pwa-common/selectors/user';
 
