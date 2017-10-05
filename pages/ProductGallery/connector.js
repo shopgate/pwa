658f11faf48614d3f3242a0b3a8f4f23a3714ef6
Florian Müller <florian.mueller81@gmx.de>
2017-10-05 15:42:44 +0200

    CON-2411 Use 'connect' from router helpers

--- pages/ProductGallery/connector.js
+++ pages/ProductGallery/connector.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { connect } from 'react-redux';
+import connect from '@shopgate/pwa-common/components/Router/helpers/connect';
 import { hex2bin } from '@shopgate/pwa-common/helpers/data';
 import disableNavigator from 'Components/Navigator/actions/disableNavigator';
 import enableNavigator from 'Components/Navigator/actions/enableNavigator';
