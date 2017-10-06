16ea82db7beef65fe14571b6988df0c26a86bb51
Szymon Nowicki <szymon.nowicki@shopgate.com>
2017-10-06 11:58:32 +0200

    CON-1082 - missing period

--- reviews/action-creators/requestReviews.js
+++ reviews/action-creators/requestReviews.js
@@ -10,7 +10,7 @@ import { REQUEST_REVIEWS } from '../constants';
 /**
  * Dispatches the REQUEST_REVIEWS action.
  * @param {string} hash Generated hash.
- * @returns {Object} The REQUEST_PRODUCT_REVIEWS action
+ * @returns {Object} The REQUEST_PRODUCT_REVIEWS action.
  */
 const requestReviews = hash => ({
   type: REQUEST_REVIEWS,
