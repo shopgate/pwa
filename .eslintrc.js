b2afff073ac474e29522fd3c34b92799268d3ebb
Richard Gorman <richardgorman@outlook.com>
2017-10-09 11:05:59 +0200

    Environment setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  extends: '@shopgate/eslint-config',
+};
