549830ed0e8c087eb3e7fdbd27c6cd3a5634be25
Frithjof Kloes <frithjof.kloes@shopgate.com>
2017-10-11 16:41:09 +0200

    CON-2206: Changed the import of the former routeIsActive steam to routeDidNotChange

--- streams/search.js
+++ streams/search.js
@@ -10,13 +10,13 @@ import {
   SEARCH_PATH,
   RECEIVE_SEARCH_RESULTS,
 } from '@shopgate/pwa-common-commerce/search/constants';
-import { routeIsActive } from '@shopgate/pwa-common/streams/history';
+import { routeDidNotChange } from '@shopgate/pwa-common/streams/history';
 import { main$ } from '@shopgate/pwa-common/streams/main';
 
 /**
  * Emits when the search route is active.
  */
-const searchIsActive$ = routeIsActive(SEARCH_PATH);
+const searchIsActive$ = routeDidNotChange(SEARCH_PATH);
 
 /**
  * Emits when search results are received.
