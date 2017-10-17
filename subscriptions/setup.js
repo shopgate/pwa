66b473265396f23b2b01565b8975c56f83d9a158
Carsten Schneider <carsten.schneider@shopgate.com>
2017-10-17 15:51:11 +0200

    CON-2288 hessnatur econda track pages

--- subscriptions/setup.js
+++ subscriptions/setup.js
@@ -5,6 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import get from 'lodash/get';
+import { TYPE_PHONE, OS_ALL } from '@shopgate/pwa-common/constants/Device';
+import { componentsConfig } from '@shopgate/pwa-common/helpers/config';
 import { getWebStorageEntry } from '@shopgate/pwa-core/commands';
 import core from '@shopgate/tracking-core/core/Core';
 import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
@@ -19,13 +22,37 @@ export default function setup(subscribe) {
    * Gets triggered when the app starts.
    */
   subscribe(appDidStart$, async () => {
-    await getWebStorageEntry({ name: 'clientInformation' });
+    const clientInformationResponse = await getWebStorageEntry({ name: 'clientInformation' });
+
+    const clientInformation = {
+      type: get(clientInformationResponse, 'value.device.type', TYPE_PHONE),
+      os: get(clientInformationResponse, 'value.device.os.platform', OS_ALL),
+    };
 
     // TODO: instantiate the UnifiedPlugin only if a native tracker is configured (FB, AppsFlyer)
     // eslint-disable-next-line no-new
     new UnifiedPlugin();
 
-    // TODO: loop through all extensions/components with type=tracking an create a instance
+    /*
+     * TODO: This is just a intermediate solution until the extensions are not longer located in
+     * the theme. __PROJECT_PATH__ is replaced by the StringReplacePlugin with project root path
+     * during the build.
+     */
+    // eslint-disable-next-line no-undef
+    const extensionsIndex = (await import(`${__PROJECT_PATH__}/extensions/index`)).default;
+    const trackingExtensions = componentsConfig.tracking;
+
+    Object.keys(trackingExtensions).forEach((key) => {
+      const pluginInit = extensionsIndex[key];
+
+      if (pluginInit) {
+        /**
+         * Call the init function of the plugin.
+         * This init function will create the actual instances
+         */
+        pluginInit(clientInformation);
+      }
+    });
 
     core.registerFinished();
   });
