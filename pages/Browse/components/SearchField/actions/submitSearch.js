d00385a644e003d660c03552aeb15785ea83e7c4
Frithjof Kloes <frithjof.kloes@shopgate.com>
2017-10-24 15:17:42 +0200

    CON-2475 refactored direct state access to selectors

--- pages/Browse/components/SearchField/actions/submitSearch.js
+++ pages/Browse/components/SearchField/actions/submitSearch.js
@@ -6,7 +6,9 @@
  */
 
 import pushHistory from '@shopgate/pwa-common/actions/history/pushHistory';
+import { getQueryParams } from '@shopgate/pwa-common/selectors/history';
 import { SEARCH_PATH } from '@shopgate/pwa-common-commerce/search/constants';
+import { getSearchPhrase } from 'Components/Navigator/selectors';
 
 /**
  * Performs appropriate action(s) when UI search gets triggered.
@@ -14,22 +16,18 @@ import { SEARCH_PATH } from '@shopgate/pwa-common-commerce/search/constants';
  */
 const submitSearch = () => (dispatch, getState) => {
   const state = getState();
-  const { searchPhrase } = state.navigator;
-
-  // Perform search by entering/updating search route
-  const { history } = state;
-  const { sort, ...otherParams } = history.queryParams;
+  const { sort, ...otherParams } = getQueryParams(state);
 
   // Set up next history location.
   const historyLocation = {
     pathname: SEARCH_PATH,
     params: {
       ...otherParams,
-      s: searchPhrase,
+      s: getSearchPhrase(state),
     },
   };
 
-  // Move to the search location
+  // Move to the search location.
   dispatch(pushHistory(historyLocation));
 };
 
