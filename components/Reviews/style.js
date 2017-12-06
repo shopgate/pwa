89e096c76c140f4874b2bbb1b5cc7c00657a563d
Carina Hoffmann <carina.hoffmann@shopgate.com>
2017-12-06 16:18:19 +0100

    CON-2555: GMD Reviews visual updates
    - adjust PDP bottom spacing

---
+++ components/Reviews/style.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { css } from 'glamor';
+
+const container = css({
+  marginBottom: '35px',
+}).toString();
+
+export default {
+  container,
+};
