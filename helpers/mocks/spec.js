f3b9072b81d0c534e7e790e3fa9850b2e8ca53b2
Szymon Nowicki <szymon.nowicki@shopgate.com>
2017-12-07 11:59:10 +0100

    CON-2555 - test for mockedRenderOptions

---
+++ helpers/mocks/spec.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import mockRenderOptions from './mockRenderOptions';
+
+describe('MockRenderOptions', () => {
+  it('should return object', () => {
+    expect(typeof mockRenderOptions).toEqual('object');
+  });
+});
