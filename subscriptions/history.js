7a6edde3d1c24e867e015638b459efd393a7191a
Frithjof Kloes <frithjof.kloes@shopgate.com>
2017-12-07 10:59:21 +0100

    PI-8126: removed legacy checkout related app event logic

--- subscriptions/history.js
+++ subscriptions/history.js
@@ -5,27 +5,16 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import event from '@shopgate/pwa-core/classes/Event';
-import registerEvents from '@shopgate/pwa-core/commands/registerEvents';
 import appConfig from '../helpers/config';
 import redirectRoute from '../actions/history/redirectRoute';
 import resetHistory from '../actions/history/resetHistory';
 import { userDidLogin$, userDidLogout$ } from '../streams/user';
-import { appDidStart$ } from '../streams/app';
 
 /**
  * History subscriptions.
  * @param {Function} subscribe The subscribe function.
  */
 export default function history(subscribe) {
-  /**
-   * Gets triggered when the app starts.
-   */
-  subscribe(appDidStart$, ({ dispatch }) => {
-    registerEvents(['checkoutSuccess']);
-    event.addCallback('checkoutSuccess', () => dispatch(resetHistory()));
-  });
-
   /**
    * For the moment, we need to explicitly check for the Shopify webcheckout.
    * If it's there then we let that module handle the user login.
