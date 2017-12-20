6555003dea6697d2d65ab5c6b553ba73208b7bcd
Carsten Schneider <carsten.schneider@shopgate.com>
2017-12-20 15:59:39 +0100

    CON-2760 implemented event and commands to close the inAppBrowser

--- selectors/client.js
+++ selectors/client.js
@@ -11,3 +11,14 @@
  * @returns {Object}
  */
 export const getClientInformation = state => state.client;
+
+/**
+ * Check if the platform is android.
+ * @param {Object} state The application state.
+ * @return {boolean}
+ */
+export const isAndroid = (state) => {
+  const client = getClientInformation(state);
+
+  return client.device.os.platform === 'android';
+};
