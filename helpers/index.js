07509c505d5e59541b8dad3e6c0e23c7e1140c71
Carsten Schneider <carsten.schneider@shopgate.com>
2018-01-08 10:47:02 +0100

    CON-2744 E2E error logs

--- helpers/index.js
+++ helpers/index.js
@@ -6,10 +6,32 @@
  */
 
 /**
+ * Converts arguments into a selenium readable format if test mode is set.
+ * @param {Array} args Arguments that should be converted
+ * @return {Array} converted arguments
+ */
+const convertLogArgs = (args) => {
+  if (window.e2eTestLogs) {
+    return [JSON.stringify(args)];
+  }
+
+  return args;
+};
+
+/**
  * The logging wrapper for the console.
  * @type {Object}
  */
-export const logger = console;
+export const logger = {
+  ...console,
+  debug: (...args) => console.debug(...convertLogArgs(args)),
+  dir: (...args) => console.dir(...convertLogArgs(args)),
+  dirxml: (...args) => console.dirxml(...convertLogArgs(args)),
+  error: (...args) => console.error(...convertLogArgs(args)),
+  info: (...args) => console.info(...convertLogArgs(args)),
+  log: (...args) => console.log(...convertLogArgs(args)),
+  warn: (...args) => console.warn(...convertLogArgs(args)),
+};
 
 /**
  * Returns a URL for performing XHR Requests.
