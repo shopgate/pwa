851c4f44728d9b8807536ae6232da8f80253202d
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-01-09 16:55:09 +0100

    PI-8297: Feedback
    - Remove multiple set_product_id and set_variant_id action calls.
    - Create a selector for retrieving the product id from the history path.

--- product/selectors/product.js
+++ product/selectors/product.js
@@ -7,8 +7,10 @@
 
 import { createSelector } from 'reselect';
 import { generateResultHash } from '@shopgate/pwa-common/helpers/redux';
+import { hex2bin } from '@shopgate/pwa-common/helpers/data';
 import { isUndefined } from '@shopgate/pwa-common/helpers/validation';
 import { getSortOrder, getSearchPhrase } from '@shopgate/pwa-common/selectors/history';
+import { ITEM_PATH } from '../constants';
 import { getActiveFilters } from '../../filter/selectors';
 import { getCurrentCategoryId } from '../../category/selectors';
 
@@ -26,6 +28,24 @@ export const getProducts = state => state.product.productsById;
  */
 export const getCurrentProductId = state =>
   state.product.currentProduct.productVariantId || state.product.currentProduct.productId;
+
+/**
+ * Gets the product id from the current history state pathname.
+ * @param {Object} state The current application state.
+ * @return {null|string} Product id or null
+ */
+export const getHistoryPathProductId = (state) => {
+  if (!state.history.pathname.startsWith(ITEM_PATH)) {
+    return null;
+  }
+
+  const pathSegments = state.history.pathname.split('/');
+  if (!pathSegments[2]) {
+    return null;
+  }
+
+  return hex2bin(pathSegments[2]);
+};
 
 /**
  * Retrieves a product by ID from state.
--- product/subscriptions/index.js
+++ product/subscriptions/index.js
@@ -4,19 +4,16 @@
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { hex2bin } from '@shopgate/pwa-common/helpers/data';
 import {
   routeDidEnter,
   routeDidLeave,
 } from '@shopgate/pwa-common/streams/history';
-
-import {
-  ITEM_PATH,
-} from '../constants';
-
+import { getRedirectLocation } from '@shopgate/pwa-common/selectors/history';
+import { ITEM_PATH } from '../constants';
 import setProductId from '../action-creators/setProductId';
 import setProductVariantId from '../action-creators/setProductVariantId';
 import { getCurrentProductVariantId } from '../selectors/variants';
+import { getHistoryPathProductId } from '../selectors/product';
 
 /**
  * Product subscriptions.
@@ -30,24 +27,23 @@ function product(subscribe) {
 
   subscribe(itemRouteDidEnter$, ({ dispatch, getState }) => {
     const state = getState();
-    const baseProductId = hex2bin(state.history.pathname.split('/')[2]);
+    const historyProductId = getHistoryPathProductId(state);
     const selectedVariantId = getCurrentProductVariantId(state);
 
     if (selectedVariantId) {
-      dispatch(setProductVariantId(selectedVariantId));
       return;
     }
 
-    dispatch(setProductId(baseProductId));
+    dispatch(setProductId(historyProductId));
   });
 
   subscribe(itemRouteDidLeave$, ({ dispatch, getState }) => {
-    const state = getState();
-    /*
-     If there is a redirect to login we don't want the state to be mutated,
-     only if we leave product route by navigation.
+    /**
+     * If there is a redirect to login, e.g. when a user wants to give a review on a product and
+     * he is not logged in, we don't want the state to be mutated and to reset the current product.
+     * We only reset the current product if the user leaves the product route by navigation.
      */
-    if (!state.history.redirectLocation) {
+    if (!getRedirectLocation(getState())) {
       dispatch(setProductId(null));
       dispatch(setProductVariantId(null));
     }
