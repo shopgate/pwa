48812305274da7138eb43f274238b0919857e214
Carsten Schneider <carsten.schneider@shopgate.com>
2018-01-16 11:26:51 +0100

    CON-2836 removed sg wiki link

--- commands/analyticsSetCustomValues.js
+++ commands/analyticsSetCustomValues.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ * Copyright (c) 2018, Shopgate, Inc. All rights reserved.
  *
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -12,7 +12,6 @@ import AppCommand from '../classes/AppCommand';
  * @param {Object} params The command parameters
  * @param {Array} [params.customDimensions] Array of customDimensions
  * @param {Array} [params.customMetrics] Array of customMetrics
- * @see https://wiki.shopgate.guru/display/SPEC/analyticsSetCustomValues
  */
 export default (params) => {
   const command = new AppCommand();
