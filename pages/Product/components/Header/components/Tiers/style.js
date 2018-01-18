080072713ad0c4aa70a22497b7f356099cb2b849
Carsten Schneider <carsten.schneider@shopgate.com>
2018-01-18 11:52:57 +0100

    CON-2647 added tier price tests

--- pages/Product/components/Header/components/Tiers/style.js
+++ pages/Product/components/Header/components/Tiers/style.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import { css } from 'glamor';
 
 const wrapper = css({
