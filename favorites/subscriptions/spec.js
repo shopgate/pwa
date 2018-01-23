96c4a1b8e408fda762b6ddc3542f9305231e6acf
Szymon Nowicki <szymon.nowicki@shopgate.com>
2018-01-23 14:55:28 +0100

    CON-2766 - licence note update + unit test 100% coverage for favorites

--- favorites/subscriptions/spec.js
+++ favorites/subscriptions/spec.js
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2018, Shopgate, Inc. All rights reserved.
+ * Copyright (c) 2017 - present, Shopgate, Inc. All rights reserved.
  *
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  *
  */
 import favorites from './index';
+
+jest.mock('../actions/getFavorites', () => withCache => withCache);
 
 describe('Favorites - subscriptions', () => {
   it('should register to streams', () => {
@@ -14,7 +16,9 @@ describe('Favorites - subscriptions', () => {
     favorites(mockedSubscribe);
     expect(mockedSubscribe.mock.calls.length).toBe(2);
     mockedSubscribe.mock.calls[0][1]({ dispatch: mockedDispatch });
-    expect(mockedDispatch.mock.calls.length).toBe(1);
-    expect(typeof mockedDispatch.mock.calls[0][0]).toBe('function');
+    mockedSubscribe.mock.calls[1][1]({ dispatch: mockedDispatch });
+    expect(mockedDispatch.mock.calls.length).toBe(2);
+    expect(mockedDispatch.mock.calls[0][0]).toBe(undefined);
+    expect(mockedDispatch.mock.calls[1][0]).toBe(true);
   });
 });
