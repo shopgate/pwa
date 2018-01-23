2ebb80bf82f25da80c15c42df85bc7f19dd89a35
Carsten Schneider <carsten.schneider@shopgate.com>
2018-01-23 09:50:36 +0100

    PI-8236 fixed getVariantsByProductId selector

--- product/selectors/variants.js
+++ product/selectors/variants.js
@@ -25,17 +25,17 @@ const getVariantsState = state => state.product.variantsByProductId;
  * @returns {string}
  */
 export const getCurrentProductVariantId = createSelector(
-  state => state,
-  (state) => {
+  state => state.product,
+  (productState) => {
     if (
-      !state.product
-      || !state.product.currentProduct
-      || !state.product.currentProduct.productVariantId
+      !productState
+      || !productState.currentProduct
+      || !productState.currentProduct.productVariantId
     ) {
       return null;
     }
 
-    return state.product.currentProduct.productVariantId;
+    return productState.currentProduct.productVariantId;
   }
 );
 
@@ -68,9 +68,10 @@ export const isProductChildrenSelected = state => !!getCurrentProductVariantId(s
  * @param {string} productId The product ID.
  * @return {Object|null} The dedicated variants. Or null when the requested data is unavailable.
  */
-export const getVariantsByProductId = (state, productId) => createSelector(
+export const getVariantsByProductId = createSelector(
   getVariantsState,
-  (variants) => {
+  (state, props, productId) => productId,
+  (variants, productId) => {
     if (!variants || !variants[productId] || variants[productId].isFetching === true) {
       return null;
     }
