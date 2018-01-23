6c87022217f8aa486ff163fc46073ebc43489722
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-01-23 17:17:59 +0100

    CON-1076 Product can be added/removed to/from favorite list from product page
    - unit tests

---
+++ pages/Product/components/Header/components/CTAButtons/spec.jsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import CTAButtons from './index';
+import FavoritesButton from 'Components/FavoritesButton';
+import AddToCartButton from './components/AddToCartButton';
+
+describe('CTAs (product header)', () => {
+
+  describe('Rendering', () => {
+    it('should render rating when data is available', () => {
+      const component = shallow(<CTAButtons/>);
+      expect(component).toMatchSnapshot();
+      expect(component.find(FavoritesButton).exists()).toBe(true);
+      expect(component.find(AddToCartButton).exists()).toBe(true);
+    });
+  });
+});
+
