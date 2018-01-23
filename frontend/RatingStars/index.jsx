d580998e1c13e225d08d29423fd4268d8c281221
devbucket <florian.mueller81@gmx.de>
2018-01-23 01:30:43 +0100

    CON-2831 Added RatingStars portal component.

---
+++ frontend/RatingStars/index.jsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import appConfig from '@shopgate/pwa-common/helpers/config';
+import Stars from './components/Stars';
+import connect from './connector';
+
+const { hasReviews } = appConfig;
+
+/**
+ * The RatingStars component.
+ * @return {JSX}
+ */
+const RatingStars = ({ product }) => {
+  if (!hasReviews || !product || !product.rating || product.rating.count === 0) {
+    return null;
+  }
+
+  console.warn(product);
+
+  return <Stars value={product.rating.average} />;
+};
+
+RatingStars.propTypes = {
+  product: PropTypes.shape().isRequired,
+};
+
+export default connect(RatingStars);
