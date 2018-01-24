9d76e67edf63c0e397dea59d070adae178cb86bc
devbucket <florian.mueller81@gmx.de>
2018-01-24 17:28:06 +0100

    CON-2831 Changed the handling of themes styles.

--- styles/fonts.js
+++ styles/fonts.js
@@ -6,12 +6,9 @@
  */
 
 import { css } from 'glamor';
+import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
-const fonts = {
-  family: 'system, -apple-system, "SF Pro Display", "Helvetica Neue", "Lucida Grande"',
-  rootSize: 17,
-  lineHeight: 1.43,
-};
+const { fonts } = themeConfig;
 
 css.global('body', {
   font: `${fonts.rootSize}px/${fonts.lineHeight} ${fonts.family}`,
