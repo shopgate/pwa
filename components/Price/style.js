21322f3218892ababff2ca6802334048f6ef60f2
Richard Gorman <richardgorman@outlook.com>
2018-01-29 10:20:34 +0100

    CON-2782 Add tax disclaimer to product page and cart

--- components/Price/style.js
+++ components/Price/style.js
@@ -8,10 +8,26 @@
 import { css } from 'glamor';
 import colors from 'Styles/colors';
 
-export const discounted = css({
+const container = css({
+  display: 'flex',
+  position: 'relative',
+  whiteSpace: 'nowrap',
+}).toString();
+
+const disclaimer = css({
+  color: 'initial',
+  fontSize: 14,
+  position: 'absolute',
+  right: -10,
+  top: 0,
+}).toString();
+
+const discounted = css({
   color: colors.primary,
 }).toString();
 
-export default css({
-  whiteSpace: 'nowrap',
-}).toString();
+export default {
+  container,
+  disclaimer,
+  discounted,
+};
