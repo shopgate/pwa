53aa2b228a66698b09f47b22b25b517371938124
Richard Gorman <richardgorman@outlook.com>
2018-01-29 10:26:56 +0100

    CON-2782 Added check for if tax disclaimer is visible

---
+++ market/helper/showTaxDisclaimer.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import appConfig from '@shopgate/pwa-common/helpers/config';
+import { MARKET_GERMANY } from '../constants';
+
+const isVisible = [
+  MARKET_GERMANY,
+].includes(appConfig.marketId);
+
+export default isVisible;
