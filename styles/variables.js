9cdc59211a9bb752f8f2277affb9e1fae3a9958c
Richard Gorman <richardgorman@outlook.com>
2018-01-31 13:59:35 +0100

    CON-2898 Added default style variables to restore unit tests

--- styles/variables.js
+++ styles/variables.js
@@ -7,4 +7,36 @@
 
 import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
-export default themeConfig.variables;
+const materialShadow = 'rgba(0, 0, 0, .117647) 0 1px 6px, rgba(0, 0, 0, .117647) 0 1px 4px';
+
+export default {
+  materialShadow,
+  gap: {
+    xsmall: 4,
+    small: 8,
+    big: 16,
+    bigger: 20,
+    xbig: 32,
+    xxbig: 64,
+  },
+  emptyPage: {
+    icon: 216,
+    titleTopGap: 36,
+    buttonVerticalGap: 24,
+  },
+  navigator: {
+    height: 56,
+    shadow: materialShadow,
+  },
+  filterbar: {
+    height: 48,
+  },
+  loadingIndicator: {
+    size: 32,
+    strokeWidth: 3,
+  },
+  paymentBar: {
+    height: 78,
+  },
+  ...themeConfig.variables,
+};
