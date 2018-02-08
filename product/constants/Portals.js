b705cf6259a65a2298e94049f498b59d2536ddb1
Florian Müller <florian.mueller@shopgate.com>
2018-02-08 13:17:42 +0100

    CON-2764 Re-added constants uppercased.

---
+++ product/constants/Portals.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// FEATURES
+const PRODUCT_ITEM = 'product-item';
+
+// CONTENTS
+const NAME = 'name';
+
+// POSITIONS
+const BEFORE = 'before';
+
+export const PRODUCT_ITEM_NAME_BEFORE = `${PRODUCT_ITEM}.${NAME}.${BEFORE}`;
