eba7535bc486f0b47ba345449262ec773a7d629a
Carsten Schneider <carsten.schneider@shopgate.com>
2018-02-15 15:17:22 +0100

    CON-2911 added error code mapping

--- components/Dialog/actions/pipelineErrorDialog.js
+++ components/Dialog/actions/pipelineErrorDialog.js
@@ -7,25 +7,18 @@
 
 import showModal from '@shopgate/pwa-common/actions/modal/showModal';
 import { MODAL_PIPELINE_ERROR } from '@shopgate/pwa-common/constants/ModalTypes';
+import getErrorMessage from '@shopgate/pwa-common/helpers/i18n/getErrorMessage';
 
 /**
- * Checks if there's any specific message for given pipeline and error.
- * If yes, returns a translation string. If not, falls back to `error.message`.
- * @param {string} name Pipeline name.
- * @param {Object} error Error object.
- * @returns {string}
+ * Mapping between pipeline name/code and translation key
+ * @type {Object}
  */
-const getErrorMessage = ({ name, error }) => {
-  switch (name) {
-    case 'addFavorites_v1':
-      return 'favorites.error_add';
-    case 'deleteFavorites_v1':
-      return 'favorites.error_remove';
-    case 'login_v1':
-      return 'login.error';
-    default:
-      return error.message;
-  }
+const errorCodeMappings = {
+  addFavorites: 'favorites.error_add',
+  deleteFavorites: 'favorites.error_remove',
+  login: {
+    EINVALIDCREDENTIALS: 'login.error',
+  },
 };
 
 /**
@@ -41,10 +34,7 @@ const pipelineErrorDialog = ({ name, input, error }) => (dispatch) => {
     // Give the template a clue about how to show this modal.
     type: MODAL_PIPELINE_ERROR,
     // Set the message param if users shall see the error message
-    message: getErrorMessage({
-      name,
-      error,
-    }),
+    message: getErrorMessage(name, error, errorCodeMappings),
     params: {
       // Expose the error details as params.
       pipelineName: name,
