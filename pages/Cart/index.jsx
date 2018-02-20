a64080d9d42eab4999f097543f1146d9472f7013
Frithjof Kloes <frithjof.kloes@shopgate.com>
2018-02-20 10:43:23 +0100

    CON-2923: Added portals to the cart page

--- pages/Cart/index.jsx
+++ pages/Cart/index.jsx
@@ -7,6 +7,8 @@
 
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Portal from '@shopgate/pwa-common/components/Portal';
+import * as portals from '@shopgate/pwa-common-commerce/cart/constants/Portals';
 import View from 'Components/View';
 import CardList from 'Components/CardList';
 import MessageBar from 'Components/MessageBar';
@@ -94,16 +96,29 @@ class Cart extends Component {
           {messages.length > 0 && <MessageBar messages={messages} />}
           {cartItems.length > 0 && (
             <Fragment>
-              <CardList>
-                {cartItems.map(cartItem => (
-                  <Item
-                    key={cartItem.id}
-                    item={cartItem}
-                    togglePaymentBar={this.togglePaymentBar}
-                  />
-                ))}
-                <CouponField onToggleFocus={this.togglePaymentBar} />
-              </CardList>
+              <Portal name={portals.CART_ITEM_LIST_BEFORE} />
+              <Portal name={portals.CART_ITEM_LIST}>
+                <CardList>
+                  {cartItems.map(cartItem => (
+                    <Fragment key={cartItem.id}>
+                      <Portal name={portals.CART_ITEM_BEFORE} props={{ id: cartItem.id }} />
+                      <Portal name={portals.CART_ITEM} props={{ id: cartItem.id }}>
+                        <Item
+                          item={cartItem}
+                          togglePaymentBar={this.togglePaymentBar}
+                        />
+                      </Portal>
+                      <Portal name={portals.CART_ITEM_AFTER} props={{ id: cartItem.id }} />
+                    </Fragment>
+                  ))}
+                  <Portal name={portals.CART_COUPON_FIELD_BEFORE} />
+                  <Portal name={portals.CART_COUPON_FIELD} >
+                    <CouponField onToggleFocus={this.togglePaymentBar} />
+                  </Portal>
+                  <Portal name={portals.CART_COUPON_FIELD_AFTER} />
+                </CardList>
+              </Portal>
+              <Portal name={portals.CART_ITEM_LIST_AFTER} />
               <PaymentBar isVisible={!this.state.isPaymentBarHidden} onSize={this.onSize} />
             </Fragment>
           )}
