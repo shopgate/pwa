307c9486447d31c6e9833b18b323ceb578e09cf9
Frithjof Kloes <frithjof.kloes@shopgate.com>
2018-02-20 11:22:39 +0100

    CON-2923: Reimplemented portals after a merge conflict

--- pages/Cart/index.jsx
+++ pages/Cart/index.jsx
@@ -7,6 +7,8 @@
 
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Portal from '@shopgate/pwa-common/components/Portal';
+import * as portals from '@shopgate/pwa-common-commerce/cart/constants/Portals';
 import View from 'Components/View';
 import CardList from 'Components/CardList';
 import MessageBar from 'Components/MessageBar';
@@ -100,16 +102,25 @@ class Cart extends Component {
             {hasMessages && <MessageBar messages={messages} />}
             {hasItems && (
               <Fragment>
-                <CardList>
-                  {cartItems.map(cartItem => (
-                    <Item
-                      key={cartItem.id}
-                      item={cartItem}
-                      togglePaymentBar={this.togglePaymentBar}
-                    />
-                  ))}
-                  <CouponField onToggleFocus={this.togglePaymentBar} />
-                </CardList>
+                <Portal name={portals.CART_ITEM_LIST_BEFORE} />
+                <Portal name={portals.CART_ITEM_LIST}>
+                  <CardList>
+                    {cartItems.map(cartItem => (
+                      <Fragment key={cartItem.id}>
+                        <Portal name={portals.CART_ITEM_BEFORE} props={{ id: cartItem.id }} />
+                        <Portal name={portals.CART_ITEM} props={{ id: cartItem.id }}>
+                          <Item
+                            item={cartItem}
+                            togglePaymentBar={this.togglePaymentBar}
+                          />
+                        </Portal>
+                        <Portal name={portals.CART_ITEM_AFTER} props={{ id: cartItem.id }} />
+                      </Fragment>
+                    ))}
+                    <CouponField onToggleFocus={this.togglePaymentBar} />
+                  </CardList>
+                </Portal>
+                <Portal name={portals.CART_ITEM_LIST_AFTER} />
                 <PaymentBar
                   isVisible={!this.state.isPaymentBarHidden}
                   onSize={this.onSize}
