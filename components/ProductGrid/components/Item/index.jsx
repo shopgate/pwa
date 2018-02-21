1593788f0430b1b80e712b00405aa44ac3ce84a1
Richard Gorman <richardgorman@outlook.com>
2018-02-21 16:30:59 +0100

    CON-2922 Added first set of portals to the category page

--- components/ProductGrid/components/Item/index.jsx
+++ components/ProductGrid/components/Item/index.jsx
@@ -9,10 +9,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@shopgate/pwa-common/components/Grid';
 import Link from '@shopgate/pwa-common/components/Router/components/Link';
-import Portal from '@shopgate/pwa-common/components/Portal';
 import Ellipsis from '@shopgate/pwa-common/components/Ellipsis';
 import { bin2hex } from '@shopgate/pwa-common/helpers/data';
-import { PRODUCT_ITEM_NAME_BEFORE } from '@shopgate/pwa-common-commerce/product/constants/Portals';
+import Portal from '@shopgate/pwa-common/components/Portal';
+import * as portals from '@shopgate/pwa-common-commerce/category/constants/Portals';
 import ProductImage from 'Components/ProductImage';
 import DiscountBadge from 'Components/DiscountBadge';
 import Price from 'Components/Price';
@@ -36,7 +36,18 @@ const Item = ({ product, display, isFavorite }) => (
     itemScope
     itemType="http://schema.org/Product"
   >
-    <ProductImage itemProp="image" src={product.featuredImageUrl} alt={product.name} />
+    <Portal name={portals.PRODUCT_ITEM_IMAGE_BEFORE} props={{ productId: product.id }} />
+    <Portal
+      name={portals.PRODUCT_ITEM_IMAGE}
+      props={{ productId: product.id }}
+    >
+      <ProductImage
+        alt={product.name}
+        itemProp="image"
+        src={product.featuredImageUrl}
+      />
+    </Portal>
+    <Portal name={portals.PRODUCT_ITEM_IMAGE_AFTER} props={{ productId: product.id }} />
     {!!product.price.discount &&
       <div className={styles.badgeWrapper}>
         <DiscountBadge text={`-${product.price.discount}%`} />
@@ -47,40 +58,47 @@ const Item = ({ product, display, isFavorite }) => (
     </div>
     {(!display || display.name || display.price || display.reviews) && (
       <div className={styles.details}>
-        <Portal name={PRODUCT_ITEM_NAME_BEFORE} props={{ id: product.id }} />
+        <Portal name={portals.PRODUCT_ITEM_NAME_BEFORE} props={{ productId: product.id }} />
         {(!display || display.name) && (
-          <div className={styles.title} itemProp="name">
-            <Ellipsis>{product.name}</Ellipsis>
-          </div>
+          <Portal name={portals.PRODUCT_ITEM_NAME} props={{ productId: product.id }}>
+            <div className={styles.title} itemProp="name">
+              <Ellipsis>{product.name}</Ellipsis>
+            </div>
+          </Portal>
         )}
+        <Portal name={portals.PRODUCT_ITEM_NAME_AFTER} props={{ productId: product.id }} />
+        <Portal name={portals.PRODUCT_ITEM_PRICE_BEFORE} props={{ productId: product.id }} />
         {(!display || display.price) && (
-          <Grid className={styles.priceWrapper} wrap>
-            <Grid.Item grow={1}>
-              <Price
-                unitPrice={product.price.unitPrice}
-                unitPriceMin={product.price.unitPriceMin}
-                discounted={!!product.price.discount}
-                currency={product.price.currency}
-              />
-            </Grid.Item>
-            {product.price.msrp > 0 && (
-              <Grid.Item>
-                <PriceStriked
-                  value={product.price.msrp}
+          <Portal name={portals.PRODUCT_ITEM_PRICE} props={{ productId: product.id }}>
+            <Grid className={styles.priceWrapper} wrap>
+              <Grid.Item grow={1}>
+                <Price
+                  unitPrice={product.price.unitPrice}
+                  unitPriceMin={product.price.unitPriceMin}
+                  discounted={!!product.price.discount}
                   currency={product.price.currency}
                 />
               </Grid.Item>
-            )}
-            {(!product.price.msrp && product.price.unitPriceStriked > 0) && (
-              <Grid.Item>
-                <PriceStriked
-                  value={product.price.unitPriceStriked}
-                  currency={product.price.currency}
-                />
-              </Grid.Item>
-            )}
-          </Grid>
+              {product.price.msrp > 0 && (
+                <Grid.Item>
+                  <PriceStriked
+                    value={product.price.msrp}
+                    currency={product.price.currency}
+                  />
+                </Grid.Item>
+              )}
+              {(!product.price.msrp && product.price.unitPriceStriked > 0) && (
+                <Grid.Item>
+                  <PriceStriked
+                    value={product.price.unitPriceStriked}
+                    currency={product.price.currency}
+                  />
+                </Grid.Item>
+              )}
+            </Grid>
+          </Portal>
         )}
+        <Portal name={portals.PRODUCT_ITEM_PRICE_AFTER} props={{ productId: product.id }} />
         {(!display || display.price) && (
           <Grid>
             {product.price.info && (
@@ -96,6 +114,7 @@ const Item = ({ product, display, isFavorite }) => (
 );
 
 Item.propTypes = {
+  isFavorite: PropTypes.bool.isRequired,
   product: PropTypes.shape().isRequired,
   display: PropTypes.shape(),
 };
