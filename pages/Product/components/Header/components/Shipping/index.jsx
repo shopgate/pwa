281cbd2fde9eccfe82df5ba029155cf433551e00
Szymon Nowicki <szymon.nowicki@shopgate.com>
2018-02-21 15:37:46 +0100

    PI-8477 - bring back placeholder for shipping

--- pages/Product/components/Header/components/Shipping/index.jsx
+++ pages/Product/components/Header/components/Shipping/index.jsx
@@ -5,8 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import PlaceholderLabel from 'Components/PlaceholderLabel';
 import Label from './components/Label';
 import connect from './connector';
 import styles from './style';
@@ -17,11 +18,11 @@ import styles from './style';
  * @return {JSX}
  */
 const Shipping = ({ shipping }) => (
-  <Fragment>
+  <PlaceholderLabel className={styles.placeholder} ready={(shipping !== null)}>
     {shipping && shipping.price !== null && (
       <Label className={styles.shipping} price={shipping.price} currency={shipping.currency} />
     )}
-  </Fragment>
+  </PlaceholderLabel>
 );
 
 Shipping.propTypes = {
