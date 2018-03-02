e678015e9e02f2d0556706e203287469682acbfc
Florian Müller <florian.mueller@shopgate.com>
2018-03-02 17:03:24 +0100

    DTY-27 Added first chunk of navigation constants.

--- constants/Portals.js
+++ constants/Portals.js
@@ -13,7 +13,11 @@ const NAV_MENU = 'nav-menu';
 // CONTENTS
 const GLOBALS = 'globals';
 const ROUTES = 'routes';
+const HEADER = 'header';
 const CONTENT = 'content';
+const CATEGORIES = 'categories';
+const FAVORITES = 'favorites';
+const HOME = 'home';
 
 // POSITIONS
 const BEFORE = 'before';
@@ -25,5 +29,21 @@ export const PAGE_CONTENT_BEFORE = `${PAGE}.${CONTENT}.${BEFORE}`;
 export const PAGE_CONTENT = `${PAGE}.${CONTENT}`;
 export const PAGE_CONTENT_AFTER = `${PAGE}.${CONTENT}.${AFTER}`;
 
+export const NAV_MENU_HEADER_BEFORE = `${NAV_MENU}.${HEADER}.${BEFORE}`;
+export const NAV_MENU_HEADER = `${NAV_MENU}.${HEADER}`;
+export const NAV_MENU_HEADER_AFTER = `${NAV_MENU}.${HEADER}.${BEFORE}`;
+
 export const NAV_MENU_CONTENT_BEFORE = `${NAV_MENU}.${CONTENT}.${BEFORE}`;
 export const NAV_MENU_CONTENT_AFTER = `${NAV_MENU}.${CONTENT}.${AFTER}`;
+
+export const NAV_MENU_HOME_BEFORE = `${NAV_MENU}.${HOME}.${BEFORE}`;
+export const NAV_MENU_HOME = `${NAV_MENU}.${HOME}`;
+export const NAV_MENU_HOME_AFTER = `${NAV_MENU}.${HOME}.${BEFORE}`;
+
+export const NAV_MENU_CATEGORIES_BEFORE = `${NAV_MENU}.${CATEGORIES}.${BEFORE}`;
+export const NAV_MENU_CATEGORIES = `${NAV_MENU}.${CATEGORIES}`;
+export const NAV_MENU_CATEGORIES_AFTER = `${NAV_MENU}.${CATEGORIES}.${BEFORE}`;
+
+export const NAV_MENU_FAVORITES_BEFORE = `${NAV_MENU}.${FAVORITES}.${BEFORE}`;
+export const NAV_MENU_FAVORITES = `${NAV_MENU}.${FAVORITES}`;
+export const NAV_MENU_FAVORITES_AFTER = `${NAV_MENU}.${FAVORITES}.${BEFORE}`;
