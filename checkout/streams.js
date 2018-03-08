f18d926484e8c8f5e1053e8f9d835a95cde24170
Richard Gorman <richardgorman@outlook.com>
2018-03-08 14:57:54 +0100

    DTY-86 Added a logged in check inside the checkout url subscription
    A user cannot enter or fetch the checkout url until they ar elogged in.

--- checkout/streams.js
+++ checkout/streams.js
@@ -5,15 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { openedLink$ } from '@shopgate/pwa-common/streams/history';
+import { historyDidUpdate$ } from '@shopgate/pwa-common/streams/history';
 import { CHECKOUT_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
 
 /**
  * Gets triggered when the checkout link is opened.
  * @type {Observable}
  */
-export const openedCheckoutLink$ = openedLink$
-  .filter(({ action }) =>
-    action.options &&
-    action.options.url &&
-    action.options.url === CHECKOUT_PATH);
+export const openedCheckoutLink$ = historyDidUpdate$
+  .filter(({ action }) => (
+    action.historyProps &&
+    action.historyProps.pathname &&
+    action.historyProps.pathname === CHECKOUT_PATH
+  ));
+
