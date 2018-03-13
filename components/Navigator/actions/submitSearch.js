2ac31b78fd810b91f27be757cd747b909f49d784
Richard Gorman <richardgorman@outlook.com>
2018-03-13 17:52:32 +0100

    PWA-51 Removed license headers

--- components/Navigator/actions/submitSearch.js
+++ components/Navigator/actions/submitSearch.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
- *
- * This source code is licensed under the Apache 2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import { SEARCH_PATH } from '@shopgate/pwa-common-commerce/search/constants';
 import { getSearchPhrase } from '@shopgate/pwa-common/selectors/history';
 import pushHistory from '@shopgate/pwa-common/actions/history/pushHistory';
