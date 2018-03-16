fa7cccacf97e8f20dede05622d3eed9d3ad6f044
devbucket <florian.mueller81@gmx.de>
2018-03-16 11:27:25 +0100

    PWA-76 re-added class.

--- libraries/commerce/cart/actions/addProductsToCart.js
+++ libraries/commerce/cart/actions/addProductsToCart.js
@@ -18,7 +18,7 @@ const addToCart = productData => (dispatch, getState) => {
   dispatch(addProductsToCart(productData));
   dispatch(setCartProductPendingCount(pendingProductCount + 1));
 
-  const request = ('shopgate.cart.addProducts');
+  const request = new PipelineRequest('shopgate.cart.addProducts');
   request.setInput({ products: productData })
     .dispatch()
     .then(({ messages }) => {
