3516cf9396bda3d2e0e6eee029c2f1f57a5de905
devbucket <florian.mueller81@gmx.de>
2018-03-21 11:17:17 +0100

    PWA-32 Removed license blocks.

--- pages/Product/components/AddToCartBar/connector.js
+++ pages/Product/components/AddToCartBar/connector.js
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
- *
- * This source code is licensed under the Apache 2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import addCurrentProductToCart from '@shopgate/pwa-common-commerce/cart/actions/addCurrentProductToCart';
 import {
   isProductPageLoading,
--- pages/Product/components/AddToCartBar/subscriptions.js
+++ pages/Product/components/AddToCartBar/subscriptions.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
- *
- * This source code is licensed under the Apache 2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import { ITEM_PATH } from '@shopgate/pwa-common-commerce/product/constants';
 import { ERROR_ADD_PRODUCTS_TO_CART } from '@shopgate/pwa-common-commerce/cart/constants';
 import { productsUpdated$, productsAdded$ } from '@shopgate/pwa-common-commerce/cart/streams';
