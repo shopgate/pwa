6cc4c91da14c99c066ca14162f2696de48fe71de
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-04-05 09:39:39 +0200

    PWA-194: Checkout button doesn't work after canceled login
    - update snapshot and remove unnecessary import

--- pages/Cart/components/PaymentBar/components/Content/components/CheckoutButton/index.jsx
+++ pages/Cart/components/PaymentBar/components/Content/components/CheckoutButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import Link from '@shopgate/pwa-common/components/Router/components/Link';
