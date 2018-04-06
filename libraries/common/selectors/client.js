32715ecc4d2704cadae365a4bc216c1e30f77e73
Frithjof Kloes <frithjof.kloes@shopgate.com>
2018-04-06 18:22:02 +0200

    CCP-203: Extended the client selectors to deliver page insets

--- libraries/common/selectors/client.js
+++ libraries/common/selectors/client.js
@@ -1,3 +1,9 @@
+import { createSelector } from 'reselect';
+import {
+  OS_ANDROID,
+  OS_IOS,
+} from '../constants/Device';
+
 /**
  * Returns client state (state.client)
  * @param {Object} state The application state.
@@ -6,12 +12,98 @@
 export const getClientInformation = state => state.client;
 
 /**
- * Check if the platform is android.
+ * Gets the device information.
+ * @param {Object} state The application state.
+ * @return {Object|null}
+ */
+export const getDeviceInformation = createSelector(
+  getClientInformation,
+  (clientInformation) => {
+    const { device } = clientInformation;
+    return device || null;
+  }
+);
+
+/**
+ * Gets the platform of the device.
+ * @param {Object} state The application state.
+ * @return {string|null}
+ */
+export const getPlatform = createSelector(
+  getDeviceInformation,
+  (deviceInformation) => {
+    const { os: { platform = null } = {} } = deviceInformation || {};
+    return platform;
+  }
+);
+
+/**
+ * Check if the model of the device.
+ * @param {Object} state The application state.
+ * @return {string|null}
+ */
+export const getDeviceModel = createSelector(
+  getDeviceInformation,
+  (deviceInformation) => {
+    const { model = null } = deviceInformation || {};
+    return model;
+  }
+);
+
+/**
+ * Check if the platform is Android.
  * @param {Object} state The application state.
  * @return {boolean}
  */
-export const isAndroid = (state) => {
-  const client = getClientInformation(state);
+export const isAndroid = createSelector(
+  getPlatform,
+  platform => platform === OS_ANDROID
+);
 
-  return client.device.os.platform === 'android';
-};
+/**
+ * Check if the platform is iOS.
+ * @param {Object} state The application state.
+ * @return {boolean}
+ */
+export const isIos = createSelector(
+  getPlatform,
+  platform => platform === OS_IOS
+);
+
+/**
+ * Calculates page insets for the current device
+ * @param {Object} state The application state.
+ * @returns {Object}
+ */
+export const getPageInsets = createSelector(
+  getClientInformation,
+  getDeviceModel,
+  isIos,
+  (clientInformation, model, iOS) => {
+    const defaults = {
+      safeAreaInsetTop: iOS ? 20 : 0,
+      safeAreaInsetBottom: 0,
+      safeAreaInsetLeft: 0,
+      safeAreaInsetRight: 0,
+    };
+
+    let overrides = {};
+
+    if (iOS) {
+      const iphoneX = ['iPhone10,3', 'iPhone10,6'];
+
+      // Detect the iPhone X to apply special insets
+      if (iphoneX.includes(model)) {
+        overrides = {
+          safeAreaInsetTop: 40,
+          safeAreaInsetBottom: 30,
+        };
+      }
+    }
+
+    return {
+      ...defaults,
+      ...overrides,
+    };
+  }
+);
