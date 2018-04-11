49bf0dadf1ac4ddc9e42f003355e8f5df08c029f
Carsten Schneider <carsten.schneider@shopgate.com>
2018-04-11 09:13:25 +0200

    CCP-274 added portal for write a review link

--- components/Reviews/components/Header/index.jsx
+++ components/Reviews/components/Header/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { RATING_SCALE_DIVISOR } from 'Components/RatingStars/constants';
 import I18n from '@shopgate/pwa-common/components/I18n';
+import * as portals from '@shopgate/pwa-common-commerce/product/constants/Portals';
+import Portal from '@shopgate/pwa-common/components/Portal';
 import AverageRating from './components/AverageRating';
 import WriteReviewLink from './components/WriteReviewLink';
 import styles from './style';
@@ -23,10 +25,16 @@ const Header = ({ rating, withTopGap }) => {
     return (
       <div className={styles.container}>
         <AverageRating rating={rating} />
-        <div className={styles.noReviews}>
-          <I18n.Text string="reviews.no_reviews" />
-          <WriteReviewLink />
-        </div>
+
+          <div className={styles.noReviews}>
+            <I18n.Text string="reviews.no_reviews" />
+            <Portal name={portals.PRODUCT_WRITE_REVIEW_BEFORE} />
+            <Portal name={portals.PRODUCT_WRITE_REVIEW}>
+              <WriteReviewLink />
+            </Portal>
+            <Portal name={portals.PRODUCT_WRITE_REVIEW_AFTER} />
+          </div>
+        <Portal name={portals.PRODUCT_WRITE_REVIEW_BEFORE} />
       </div>
     );
   }
@@ -40,7 +48,11 @@ const Header = ({ rating, withTopGap }) => {
             {average / RATING_SCALE_DIVISOR}
           </span>
         </I18n.Text>
-        <WriteReviewLink />
+        <Portal name={portals.PRODUCT_WRITE_REVIEW_BEFORE} />
+        <Portal name={portals.PRODUCT_WRITE_REVIEW}>
+          <WriteReviewLink />
+        </Portal>
+        <Portal name={portals.PRODUCT_WRITE_REVIEW_AFTER} />
       </div>
     </div>
   );
