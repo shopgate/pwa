1ba297ebd9fdb466e4cf494268d9a80aba203203
Richard Gorman <richardgorman@outlook.com>
2018-04-19 11:53:26 +0200

    DTY-14 Removed unnecessary licence block

--- pages/routes.js
+++ pages/routes.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
- *
- * This source code is licensed under the Apache 2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import Loadable from 'react-loadable';
 
 export const Page = Loadable({
