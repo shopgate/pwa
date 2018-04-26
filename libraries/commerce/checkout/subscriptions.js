b769ac534295408166d00d9c6ec293e0f84f78a0
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-04-26 14:31:09 +0200

    PWA-152 Unsubscribe history from openedRegisterLink$ stream. Unsubscribe checkout from openedCheckoutLink$

--- libraries/commerce/checkout/subscriptions.js
+++ libraries/commerce/checkout/subscriptions.js
@@ -1,40 +1,5 @@
-import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
-import { isUserLoggedIn } from '@shopgate/pwa-common/selectors/user';
-import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link';
-import trackingCore from '@shopgate/tracking-core/core/Core';
-import { LEGACY_URL } from './constants';
-import { openedCheckoutLink$ } from './streams';
-import fetchCheckoutUrl from './actions/fetchCheckoutUrl';
-
 /**
  * Checkout subscriptions.
- * @param {Function} subscribe The subscribe function.
+ * @param {function} subscribe The subscribe function.
  */
-export default function checkout(subscribe) {
-  /**
-   * Gets triggered when the user enters the checkout.
-   */
-  subscribe(openedCheckoutLink$, ({ dispatch, getState }) => {
-    // Check if user is logged in.
-    if (!isUserLoggedIn(getState())) {
-      return;
-    }
-
-    dispatch(fetchCheckoutUrl())
-      .then((url) => {
-        /**
-         * Build the complete checkout url. Fallback to the
-         * legacy url if the Pipeline returns an invalid url.
-         * Add some tracking params for cross domain tracking.
-         */
-        const checkoutUrl = trackingCore.crossDomainTracking(url || LEGACY_URL);
-
-        // Open the checkout.
-        const link = new ParsedLink(checkoutUrl);
-        link.open();
-      })
-      .catch(e => e);
-
-    dispatch(goBackHistory(1));
-  });
-}
+export default function checkout(subscribe) {}
