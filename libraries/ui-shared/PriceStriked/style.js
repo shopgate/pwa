c26761d452efc93d6b7e955dfd06a3281661d078
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-05-04 11:16:08 +0200

    TD-660: Move shared global components out of the themes
    - move more and add specific style to  themeconfig
    - unit tests and codestyle

---
+++ libraries/ui-shared/PriceStriked/style.js
@@ -0,0 +1,37 @@
+import { css } from 'glamor';
+import { themeConfig } from '@shopgate/pwa-common/helpers/config';
+
+const basic = css({
+  whiteSpace: 'nowrap',
+  '& span': {
+    position: 'relative',
+    ':before': { // The strike-through effect is achieved by using the :before pseudo-class.
+      borderColor: 'currentColor',
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: '50%',
+    },
+  },
+  ...themeConfig.variables.priceStrikedBase,
+}).toString();
+
+/**
+ * Returns a class for the rotated strike-through line.
+ * @param {number} angle The calculated angle for the strike-through line.
+ * @returns {string} Classname
+ */
+const getAngleStyle = angle => css({
+  '& span': {
+    ':before': {
+      borderTop: '1px solid',
+      transform: `rotate(-${angle}deg)`,
+    },
+  },
+}).toString();
+
+export default {
+  basic,
+  getAngleStyle,
+};
