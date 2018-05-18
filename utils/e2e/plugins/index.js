626d0d80e40fc6e573712389d639e76f88f25535
devbucket <florian.mueller81@gmx.de>
2018-05-18 13:46:24 +0200

    PWA-332 Set up cypress config.

--- utils/e2e/plugins/index.js
+++ utils/e2e/plugins/index.js
@@ -1,17 +1,24 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const sdkConfig = require('../../../.sgcloud/frontend.json');
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  const ip = sdkConfig.ip || '127.0.0.1';
+  const port = sdkConfig.port || 8080;
+
+  // Build a new config object.
+  const newConfig = Object.assign({}, config, {
+    env: Object.assign({}, config.env, {
+      ENVIRONMENT: 'staging',
+    }),
+  });
+
+  // Set the base URL.
+  newConfig.baseUrl = `http://${ip}:${port}`;
+  // Set the default timeout.
+  newConfig.defaultCommandTimeout = 10000;
+  // Set the viewport width.
+  newConfig.viewportWidth = 411;
+  // Set the viewport height.
+  newConfig.viewportHeight = 731;
+
+  return newConfig;
+};
