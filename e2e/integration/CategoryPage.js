055075237565cfbe1c6a244df49347b11f798363
Dennis K <denniskraft87@googlemail.com>
2018-05-22 16:40:43 +0200

    PWA-333 added test ids and category test

--- e2e/integration/CategoryPage.js
+++ e2e/integration/CategoryPage.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 import els from '../elements/de';
 
-describe('AndroidGMDTest startPage', () => {
+describe('AndroidGMDTest CategoryPage', () => {
   it('should check title', () => {
     cy.visit('');
 
@@ -37,10 +37,54 @@ describe('AndroidGMDTest startPage', () => {
       .should('be.visible');
   });
 
-  it('should check for Product', () => {
-    cy.get(els)
+  it('should check for Product in grid view', () => {
+    cy.get(els.productWithManyProps4GridViewImage)
       .should('be.visible');
-    cy.get(els)
+    cy.get(els.productWithManyProps4GridViewName)
       .should('be.visible');
-  })
+    cy.get(els.productWithManyProps4GridViewFavButton)
+      .should('be.visible');
+    cy.get(els.productWithManyProps4GridViewPrice)
+      .should('be.visible');
+  });
+
+  it('should check for product in list view', () => {
+    cy.get(els.viewSwitch)
+      .click();
+    cy.get(els.productWithManyProps4ListViewImage)
+      .should('be.visible');
+    cy.get(els.productWithManyProps4ListViewName)
+      .should('be.visible');
+    cy.get(els.productWithManyProps4ListViewPrice)
+      .should('be.visible');
+  });
+
+  it('should check for strike price', () => {
+    cy.visit('/category/3630');
+    cy.get(els.productWithStrikePrice4GridViewStrikePrice)
+      .should('be.visible');
+    cy.get(els.productWithStrikePrice4GridViewDiscountBadge)
+      .should('be.visible');
+    cy.get(els.productWithStrikePrice4GridViewPrice);
+    // Switch to List view
+    cy.get(els.viewSwitch)
+      .click();
+    cy.get(els.productWithStrikePrice4listViewStrikePrice)
+      .should('be.visible');
+    cy.get(els.productWithStrikePrice4ListViewDiscountBadge)
+      .should('be.visible');
+    cy.get(els.productWithStrikePrice4ListViewPrice)
+      .should('be.visible');
+  });
+
+  it('should check for rating stars', () => {
+    cy.visit('/category/3633');
+    cy.get(els.productWithRating4GridViewRatingStars)
+      .should('be.visible');
+    // Switch to List View
+    cy.get(els.viewSwitch)
+      .click();
+    cy.get(els.productWithRating4ListViewRatingStars)
+      .should('be.visible');
+  });
 });
