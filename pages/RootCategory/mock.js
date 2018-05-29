99c27bda9d85555ff1ad8464c24287a5effa023b
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-05-29 13:51:06 +0200

    PWA-456: Test category page - root category test

---
+++ pages/RootCategory/mock.js
@@ -0,0 +1,106 @@
+const initialCategoryState = {
+  category: {
+    rootCategories: {},
+    categoriesById: {},
+    childrenByCategoryId: {},
+    currentCategoryId: null,
+  },
+};
+
+const categoryState = {
+  category: {
+    rootCategories: {
+      categories: [
+        'men',
+        'women',
+      ],
+      expires: 9999999999999,
+      isFetching: false,
+    },
+    categoriesById: {
+      men: {
+        id: 'men',
+        name: 'Men',
+        parent: {
+          id: null,
+          name: 'Default- category',
+        },
+        path: 'Men',
+        description: '',
+        imageUrl: 'https://shopgate-public.s3.amazonaws.com/categories/24453/1346609@2x.jpg',
+        externalUrl: 'http://material-fashion.myshopify.com/collections/men',
+        sort: 0,
+        productCount: 6,
+        childrenSort: 'import',
+        childrenCount: 0,
+        children: [],
+        isFetching: false,
+        expires: 9999999999999,
+      },
+      women: {
+        id: 'women',
+        name: 'Women',
+        parent: {
+          id: null,
+          name: 'Default- category',
+        },
+        path: 'Women',
+        description: '',
+        imageUrl: 'https://shopgate-public.s3.amazonaws.com/categories/24453/1346610@2x.jpg',
+        externalUrl: 'http://material-fashion.myshopify.com/collections/women',
+        sort: 0,
+        productCount: 6,
+        childrenSort: 'import',
+        childrenCount: 1,
+        children: [],
+        isFetching: false,
+        expires: 9999999999999,
+      },
+    },
+    childrenByCategoryId: {},
+    currentCategoryId: null,
+  },
+};
+
+const homeRoute = {
+  id: 'a4bf3a32-0ed8-4138-bb94-098f32f3aeb3',
+  params: {},
+  pathname: '/',
+  pattern: '/',
+  query: {},
+  state: {},
+};
+
+const categoryRouteMock = {
+  id: 'c8256921-3667-45d0-a0fe-a7a8bb5ed3b8',
+  params: {},
+  pathname: '/category',
+  pattern: '/category',
+  query: {},
+  state: {
+    title: 'Categories',
+  },
+};
+
+const routerState = {
+  router: {
+    routing: false,
+    stack: [
+      { ...homeRoute },
+    ],
+  },
+};
+
+const uiState = {
+  ui: {
+    general: {},
+  },
+};
+
+export {
+  initialCategoryState,
+  categoryState,
+  routerState,
+  categoryRouteMock,
+  uiState,
+};
---
+++ pages/RootCategory/spec.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { MockedView } from 'Components/View/mock';
+import { mount } from 'enzyme';
+import mockRenderOptions from '@shopgate/pwa-common/helpers/mocks/mockRenderOptions';
+import receiveRootCategories from '@shopgate/pwa-common-commerce/category/action-creators/receiveRootCategories';
+import { initialCategoryState, categoryState } from './mock';
+
+const mockedView = MockedView;
+const mockedStore = configureStore();
+jest.mock('Components/View', () => mockedView);
+let store;
+/**
+ * Creates component
+ * @param {boolean} state State that would be used for store.
+ * @return {ReactWrapper}
+ */
+const createComponent = (state) => {
+  store = mockedStore(state);
+  /* eslint-disable global-require */
+  const RootCategory = require('./index').default;
+  /* eslint-enable global-require */
+  return mount(
+    <Provider store={store} >
+      <RootCategory />
+    </Provider>,
+    mockRenderOptions
+  );
+};
+
+describe('<RootCategory> page', () => {
+  describe('Initial page', () => {
+    it('should render', () => {
+      const component = createComponent(categoryState);
+      expect(component).toMatchSnapshot();
+      expect(component.find('CategoryList').length).toEqual(1);
+      expect(component.find('List').length).toEqual(2);
+    });
+
+    it('should render empty', () => {
+      const component = createComponent(initialCategoryState);
+      expect(component).toMatchSnapshot();
+      expect(component.find('CategoryList').length).toEqual(1);
+      expect(component.find('List').length).toEqual(0);
+    });
+  });
+});
