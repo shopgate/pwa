98397b5cfefd0cda0d0077f2435d0e1faf52716a
Carina Hoffmann <carina.hoffmann@shopgate.com>
2018-06-04 16:21:25 +0200

    PWA-458: Test cart page
    - cart tests
    - fix paymentbar test

--- pages/Cart/spec.jsx
+++ pages/Cart/spec.jsx
@@ -8,7 +8,7 @@ import { ACTION_POP, ACTION_PUSH } from '@virtuous/conductor/constants';
 import { ITEM_PATH } from '@shopgate/pwa-common-commerce/product/constants';
 import { NAVIGATE } from '@shopgate/pwa-common/constants/ActionTypes';
 import mockRenderOptions from '@shopgate/pwa-common/helpers/mocks/mockRenderOptions';
-import { emptyState, cartState } from '@shopgate/pwa-common-commerce/cart/mock';
+import { emptyState, cartState, couponState } from '@shopgate/pwa-common-commerce/cart/mock';
 
 const mockedView = MockedView;
 jest.mock('Components/View', () => mockedView);
@@ -62,28 +62,28 @@ describe('<Cart> page', () => {
       const component = createComponent(cartState);
       expect(component).toMatchSnapshot();
       expect(component.find('Cart').length).toEqual(1);
-      expect(component.find('Item').length).toEqual(1);
+      expect(component.find('Connect(Product)').length).toEqual(1);
       expect(component.find('PaymentBar').length).toEqual(1);
+      expect(component.find('CouponField').length).toEqual(1);
       expect(component.find('Footer').length).toEqual(1);
     });
 
     it('should navigate to PDP on item tap', () => {
       const component = createComponent(cartState);
-      component.first('Item').find('Link')[0].simulate('click');
+      component.first('Connect(Product)').find('Link').simulate('click');
       component.update();
       const actions = store.getActions();
 
       expect(actions[0].type).toEqual(NAVIGATE);
       expect(actions[0].action).toEqual(ACTION_PUSH);
-      expect(actions[0].location).toEqual(ITEM_PATH);
-    });
-
-    it('should update quantity', () => {
-
+      expect(actions[0].location).toEqual(`${ITEM_PATH}/746573745f70726f64756374`);
     });
 
     it('should render coupon', () => {
-
+      const component = createComponent(couponState);
+      expect(component).toMatchSnapshot();
+      expect(component.find('CouponField').length).toEqual(1);
+      expect(component.debug());
     });
   });
 });
