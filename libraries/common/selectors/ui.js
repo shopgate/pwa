37e6f06e7ce30f51225b8acbd33c26929a4435af
devbucket <florian.mueller81@gmx.de>
2018-06-04 14:22:31 +0200

    PWA-440 Changed UI selectors.

--- libraries/common/selectors/ui.js
+++ libraries/common/selectors/ui.js
@@ -1,11 +1,22 @@
 import { createSelector } from 'reselect';
+import appConfig from '@shopgate/pwa-common/helpers/config';
+import { getCurrentState } from './router';
+
+/**
+ * @param {Object} state The current application state.
+ * @return {Object}
+ */
+export const getUiState = state => state.ui;
 
 /**
  * Selects the general UI state.
  * @param {Object} state The global state.
  * @return {Object}
  */
-export const getGeneralUI = state => state.ui.general;
+export const getGeneralUI = createSelector(
+  getUiState,
+  uiState => uiState.general
+);
 
 /**
  * Selects the title from the UI.
@@ -13,8 +24,14 @@ export const getGeneralUI = state => state.ui.general;
  * @return {Object}
  */
 export const getTitle = createSelector(
-  getGeneralUI,
-  general => general.title || ''
+  getCurrentState,
+  (state) => {
+    if (!state || !state.title) {
+      return appConfig.shopName;
+    }
+
+    return state.title;
+  }
 );
 
 /**
