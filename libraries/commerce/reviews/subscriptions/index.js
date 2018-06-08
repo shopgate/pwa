2ece080936955431514eaa3d8d93c7be265363f3
Richard Gorman <richardgorman@outlook.com>
2018-06-08 16:57:20 +0200

    PWA-472 Moved streams to ... streams

--- libraries/commerce/reviews/subscriptions/index.js
+++ libraries/commerce/reviews/subscriptions/index.js
@@ -1,11 +1,6 @@
-import { main$ } from '@shopgate/pwa-common/streams/main';
 import appConfig from '@shopgate/pwa-common/helpers/config';
-import {
-  RECEIVE_PRODUCT,
-  RECEIVE_PRODUCT_CACHED,
-} from '@shopgate/pwa-common-commerce/product/constants';
 import getProductReviews from '../actions/getProductReviews';
-import { successReviewSubmit$ } from '../streams';
+import { shouldFetchReviews$ } from '../streams';
 
 /**
  * Review subscriptions.
@@ -15,12 +10,6 @@ export default function product(subscribe) {
   if (!appConfig.hasReviews) {
     return;
   }
-
-  const shouldFetchReviews$ = main$
-    .filter(({ action }) => (
-      action.type === RECEIVE_PRODUCT || action.type === RECEIVE_PRODUCT_CACHED
-    ))
-    .merge(successReviewSubmit$);
 
   subscribe(shouldFetchReviews$, ({ action, dispatch }) => {
     const { id, baseProductId } = action.productData;
