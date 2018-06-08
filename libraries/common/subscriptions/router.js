f74d075fd033747470e2359eb7bb1ea70dda13df
Richard Gorman <richardgorman@outlook.com>
2018-06-08 09:40:38 +0200

    PWA-411 Implemented route authentication

--- libraries/common/subscriptions/router.js
+++ libraries/common/subscriptions/router.js
@@ -4,21 +4,48 @@ import {
   ACTION_PUSH,
   ACTION_REPLACE,
 } from '@virtuous/conductor/constants';
+import { navigate } from '../action-creators/router';
 import * as handler from './helpers/handleLinks';
 import { navigate$ } from '../streams/router';
+import { userDidLogin$ } from '../streams/user';
+import { isUserLoggedIn } from '../selectors/user';
+import appConfig from '../helpers/config';
 
 /**
  * Router subscriptions.
  * @param {Function} subscribe The subscribe function.
  */
 export default function router(subscribe) {
-  subscribe(navigate$, ({ action }) => {
-    const { action: historyAction, location, state } = action;
+  subscribe(navigate$, ({ action, dispatch, getState }) => {
+    const { action: historyAction, location, state: routeState } = action;
+    const state = getState();
+
+    // Route authentication.
+    if (!isUserLoggedIn(state)) {
+      // Determine whether or not this location is protected.
+      const protector = handler.getProtector(location);
+
+      // If protected then navigate to the protector instead.
+      if (protector) {
+        dispatch(navigate(historyAction, protector, {
+          redirect: {
+            location: action.location,
+            state: routeState,
+          },
+        }));
+
+        return;
+      }
+    }
 
     // If there is one of the known protocols in the url.
     if (handler.hasKnownProtocols(location)) {
-      if (handler.isExternalLink(location)) handler.openExternalLink(location);
-      else if (handler.isNativeLink(location)) handler.openNativeLink(location);
+      if (handler.isExternalLink(location)) {
+        handler.openExternalLink(location);
+      } else if (handler.isNativeLink(location)) {
+        handler.openNativeLink(location);
+      }
+
       return;
     }
 
@@ -38,15 +65,29 @@ export default function router(subscribe) {
         break;
       }
       case ACTION_PUSH: {
-        conductor.push(location, state);
+        conductor.push(location, routeState);
         break;
       }
       case ACTION_REPLACE: {
-        conductor.replace(location, state);
+        conductor.replace(location, routeState);
         break;
       }
       default:
         break;
+    }
+  });
+
+  /**
+   * Added a 100ms delay here to allow the Redux action to complete.
+   * Without it the store would show that the user is still not
+   * logged in during the upcoming navigate() action.
+   */
+  const redirectUser$ = userDidLogin$.delay(100);
+
+  subscribe(redirectUser$, ({ action, dispatch }) => {
+    if (appConfig.webCheckoutShopify === null) {
+      const { location, state } = action.redirect;
+      dispatch(navigate(ACTION_REPLACE, location, state));
     }
   });
 }
