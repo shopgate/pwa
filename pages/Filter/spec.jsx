b2fd8e2816d1f40b021d6b8c21b41a5c8541df98
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-13 13:39:39 +0200

    PWA-502 - tests for Filter page

--- pages/Filter/spec.jsx
+++ pages/Filter/spec.jsx
@@ -13,6 +13,25 @@ const mockedView = MockedView;
 jest.mock('Components/View', () => mockedView);
 const mockedStore = configureStore();
 
+const mockedMergeTemporaryFilters = jest.fn();
+jest.mock(
+  '@shopgate/pwa-common-commerce/filter/action-creators/mergeTemporaryFilters',
+  () => (...args) => {
+    mockedMergeTemporaryFilters(...args);
+    return { type: 'merge' };
+  }
+);
+const mockedRemoveTemporaryFilters = jest.fn();
+jest.mock(
+  '@shopgate/pwa-common-commerce/filter/action-creators/removeTemporaryFilter',
+  () => (...args) => {
+    mockedRemoveTemporaryFilters(...args);
+    return { type: 'remove' };
+  }
+);
+
+jest.mock('lodash/debounce', () => cb => (...args) => cb(...args));
+
 /**
  * Creates component
  * @return {ReactWrapper}
@@ -35,12 +54,28 @@ beforeEach(() => {
 });
 
 describe('<Filter> page', () => {
-
   it('should render', () => {
     const component = createComponent(mockedState);
     expect(component.find('Filter').exists()).toBe(true);
     expect(component.find('CardList').children().length).toBeTruthy();
     expect(component).toMatchSnapshot();
+
+    // Should change price range
+    component.find('PriceRangeSlider').at(0).instance().onChange([1251, 3000]);
+    expect(mockedRemoveTemporaryFilters).not.toHaveBeenCalled();
+    expect(mockedMergeTemporaryFilters.mock.calls[0][0]).toEqual({
+      display_amount: {
+        label: 'Price',
+        minimum: 1251,
+        maximum: 3000,
+        type: 'range',
+      },
+    });
+
+    // Should remove price range when beyond limits
+    component.find('PriceRangeSlider').at(0).instance().onChange([0, 99999]);
+    expect(mockedRemoveTemporaryFilters).toHaveBeenCalledTimes(1);
+    expect(mockedMergeTemporaryFilters).toHaveBeenCalledTimes(1);
   });
 
   it('should render empty', () => {
