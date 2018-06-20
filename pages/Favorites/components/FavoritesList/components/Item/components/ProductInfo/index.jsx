5e3c47e5269889f5c1d2c34ec0ed92934d863a82
Aaron Martin <aaron.martin@shopgate.com>
2018-06-20 06:48:55 -0500

    CCP-642: added const to hold prop value sent to favorites protal

--- pages/Favorites/components/FavoritesList/components/Item/components/ProductInfo/index.jsx
+++ pages/Favorites/components/FavoritesList/components/Item/components/ProductInfo/index.jsx
@@ -16,42 +16,45 @@ import styles from './style';
  * @param {Object} product Product data.
  * @constructor
  */
-const ProductInfo = ({ product }) => (
-  <Fragment>
-    <Portal name={portals.FAVORITES_PRODUCT_NAME_BEFORE} props={{ product }} />
-    <Portal name={portals.FAVORITES_PRODUCT_NAME} props={{ product }}>
-      <div className={styles.name}>
-        <Link
-          tagName="a"
-          href={`${ITEM_PATH}/${bin2hex(product.baseProductId || product.id)}`}
-          itemProp="item"
-          itemScope
-          itemType="http://schema.org/Product"
-        >
-          {product.name}
-        </Link>
-      </div>
-    </Portal>
-    <Portal name={portals.FAVORITES_PRODUCT_NAME_AFTER} props={{ product }} />
-    <Grid className={styles.detailsRow}>
-      <Grid.Item className={styles.propertiesContainer}>
-        <ProductCharacteristics characteristics={product.characteristics} />
-        <Portal name={portals.FAVORITES_AVAILABILITY_TEXT_BEFORE} props={{ product }} />
-        <Portal name={portals.FAVORITES_AVAILABILITY_TEXT} props={{ product }}>
-          <AvailableText
-            text={product.availability.text}
-            state={product.availability.state}
-            showWhenAvailable
-          />
-        </Portal>
-        <Portal name={portals.FAVORITES_AVAILABILITY_TEXT_AFTER} props={{ product }} />
-      </Grid.Item>
-      <Grid.Item className={styles.priceContainer}>
-        <Price price={product.price} />
-      </Grid.Item>
-    </Grid>
-  </Fragment>
-);
+const ProductInfo = ({ product }) => {
+  const props = { product };
+  return (
+    <Fragment>
+      <Portal name={portals.FAVORITES_PRODUCT_NAME_BEFORE} props={props} />
+      <Portal name={portals.FAVORITES_PRODUCT_NAME} props={props}>
+        <div className={styles.name}>
+          <Link
+            tagName="a"
+            href={`${ITEM_PATH}/${bin2hex(product.baseProductId || product.id)}`}
+            itemProp="item"
+            itemScope
+            itemType="http://schema.org/Product"
+          >
+            {product.name}
+          </Link>
+        </div>
+      </Portal>
+      <Portal name={portals.FAVORITES_PRODUCT_NAME_AFTER} props={props} />
+      <Grid className={styles.detailsRow}>
+        <Grid.Item className={styles.propertiesContainer}>
+          <ProductCharacteristics characteristics={product.characteristics} />
+          <Portal name={portals.FAVORITES_AVAILABILITY_TEXT_BEFORE} props={props} />
+          <Portal name={portals.FAVORITES_AVAILABILITY_TEXT} props={props}>
+            <AvailableText
+              text={product.availability.text}
+              state={product.availability.state}
+              showWhenAvailable
+            />
+          </Portal>
+          <Portal name={portals.FAVORITES_AVAILABILITY_TEXT_AFTER} props={props} />
+        </Grid.Item>
+        <Grid.Item className={styles.priceContainer}>
+          <Price price={product.price} />
+        </Grid.Item>
+      </Grid>
+    </Fragment>
+  );
+};
 
 ProductInfo.propTypes = {
   product: PropTypes.shape().isRequired,
