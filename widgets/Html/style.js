b264c1d0c920a8f82c0981c4ab01a307846f4700
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-28 16:38:45 +0200

    PWA-499 - basic elements typography fixes for CMS pages

--- widgets/Html/style.js
+++ widgets/Html/style.js
@@ -1,4 +1,7 @@
 import { css } from 'glamor';
+import { themeConfig } from '@shopgate/pwa-common/helpers/config';
+
+const { colors } = themeConfig;
 
 export default css({
   ' h1, h2, h3, h4, h5, h6, p, ul, ol': {
@@ -19,8 +22,26 @@ export default css({
   ' h4, h5, h6': {
     fontSize: '1rem',
   },
+  ' ol, ul': {
+    paddingLeft: '1rem',
+  },
+  ' ol > li': {
+    listStyle: 'decimal',
+  },
+  ' ul > li': {
+    listStyle: 'disc',
+  },
   ' img': {
     display: 'initial',
+  },
+  ' code, pre': {
+    whiteSpace: 'pre-wrap',
+  },
+  ' blockquote, quote': {
+    paddingLeft: '1rem',
+    margin: '2rem 0',
+    borderLeft: `.5rem solid ${colors.shade6}`,
+    fontStyle: 'italic',
   },
   // Clearfix for floated widget content
   ':after': {
