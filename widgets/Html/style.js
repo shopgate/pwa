458f94efa4616873efc9ff81d5c39a5ce0f37572
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-28 16:26:37 +0200

    PWA-499 - basic elements typography fixes for CMS pages

--- widgets/Html/style.js
+++ widgets/Html/style.js
@@ -1,6 +1,24 @@
 import { css } from 'glamor';
 
 export default css({
+  ' h1, h2, h3, h4, h5, h6, p, ul, ol': {
+    margin: '1rem 0',
+  },
+  ' h1, h2, h3, h4, h5, h6': {
+    fontWeight: 600,
+  },
+  ' h1': {
+    fontSize: '1.5rem',
+  },
+  ' h2': {
+    fontSize: '1.25rem',
+  },
+  ' h3': {
+    fontSize: '1.1rem',
+  },
+  ' h4, h5, h6': {
+    fontSize: '1rem',
+  },
   ' img': {
     display: 'initial',
   },
