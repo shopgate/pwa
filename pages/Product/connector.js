f903556b3070e30a9f996baee435e322fba96bc2
Carsten Schneider <carsten.schneider@shopgate.com>
2018-07-04 16:19:41 +0200

    CCP-370 removed unnecessary code

--- pages/Product/connector.js
+++ pages/Product/connector.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import { getProductName } from '@shopgate/pwa-common-commerce/product/selectors/product';
-import getProductData from './actions/getProductData';
 
 /**
  * Maps the contents of the state to the component props.
@@ -11,14 +10,4 @@ const mapStateToProps = state => ({
   name: getProductName(state),
 });
 
-/**
- * Connects the dispatch function to a callable function in the props.
- * @param {Function} dispatch The redux dispatch function.
- * @return {Object} The extended component props.
- */
-const mapDispatchToProps = dispatch => ({
-  getProductData: (selectedVariantId = null, baseProductId = null) =>
-    dispatch(getProductData(selectedVariantId, baseProductId)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps, null, { withRef: true });
+export default connect(mapStateToProps, null, null, { withRef: true });
