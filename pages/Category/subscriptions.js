3ee06caeeaac2054869a7ec1504a383d2ac4ba7d
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-05 14:58:02 +0200

    PWA-475 - error handling for category page errors - test FR, NL translations

--- pages/Category/subscriptions.js
+++ pages/Category/subscriptions.js
@@ -1,8 +1,9 @@
-import { CATEGORY_PATH } from '@shopgate/pwa-common-commerce/category/constants';
 import getCategory from '@shopgate/pwa-common-commerce/category/actions/getCategory';
 import { getCurrentCategoryId } from '@shopgate/pwa-common-commerce/category/selectors';
-import { routeDidEnter } from '@shopgate/pwa-common/streams/history';
-import { categoryError$ } from '@shopgate/pwa-common-commerce/category/streams';
+import {
+  categoryRouteDidEnter$,
+  categoryError$,
+} from '@shopgate/pwa-common-commerce/category/streams';
 import showModal from '@shopgate/pwa-common/actions/modal/showModal';
 import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
 
@@ -11,8 +12,6 @@ import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
  * @param {Function} subscribe The subscribe function.
  */
 export default function category(subscribe) {
-  const categoryRouteDidEnter$ = routeDidEnter(CATEGORY_PATH);
-
   /**
    * Gets triggered on entering the filter route.
    */
