378847c1358c7c94455d40d3904c84561ec9e4f0
Alexander Wesselburg <info@myportal.de>
2018-07-11 16:20:28 +0200

    MAGENTO-2044 - add setCookie App command

---
+++ libraries/core/commands/setCookie.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2017-present, Shopgate, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import AppCommand from '../classes/AppCommand';
+
+/**
+ * Sends an setCookie command to the app.
+ * @param {Object} params The command parameters
+ * @param {string} params.name The name of the cookie
+ * @param {string} params.value The value of the cookie
+ * @param {string} params.domain The domain for the cookie
+ * @param {int} params.expires The expires datetime
+ * @param {string} params.path The path for the cookie
+ * @param {Boolean} params.secure
+ * @param {Boolean} params.httpOnly
+ */
+export default (params) => {
+  const command = new AppCommand();
+
+  command
+    .setCommandName('setCookie')
+    .dispatch(params);
+};
