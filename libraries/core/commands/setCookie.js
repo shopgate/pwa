20044e3945d182fc8722cbec6ea93746b47caa30
Florian Müller <flomueller1981@gmail.com>
2018-07-12 11:01:50 +0200

    Update setCookie.js
    MAGENTO-2044 Fixed parameter types codestyle.

--- libraries/core/commands/setCookie.js
+++ libraries/core/commands/setCookie.js
@@ -2,14 +2,14 @@ import AppCommand from '../classes/AppCommand';
 
 /**
  * Sends an setCookie command to the app.
- * @param {Object} params The command parameters
- * @param {string} params.name The name of the cookie
- * @param {string} params.value The value of the cookie
- * @param {string} params.domain The domain for the cookie
- * @param {int} params.expires The expires datetime
- * @param {string} params.path The path for the cookie
- * @param {Boolean} params.secure
- * @param {Boolean} params.httpOnly
+ * @param {Object} params The command parameters.
+ * @param {string} params.name The name of the cookie.
+ * @param {string} params.value The value of the cookie.
+ * @param {string} params.domain The domain for the cookie.
+ * @param {number} params.expires The expiration datetime.
+ * @param {string} params.path The path for the cookie.
+ * @param {boolean} params.secure
+ * @param {boolean} params.httpOnly
  */
 export default (params) => {
   const command = new AppCommand();
