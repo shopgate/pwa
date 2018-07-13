5da3a0ee926f29b229d1c9ea02c97516a111422f
Alexander Wesselburg <info@myportal.de>
2018-07-13 09:33:29 +0200

    Update setCookie.js

--- libraries/core/commands/setCookie.js
+++ libraries/core/commands/setCookie.js
@@ -6,10 +6,10 @@ import AppCommand from '../classes/AppCommand';
  * @param {string} params.name The name of the cookie
  * @param {string} params.value The value of the cookie
  * @param {string} params.domain The domain for the cookie
- * @param {int} params.expires The expires datetime
+ * @param {number} params.expires The expires datetime
  * @param {string} params.path The path for the cookie
- * @param {Boolean} params.secure Set is secure
- * @param {Boolean} params.httpOnly Set is http only
+ * @param {boolean} params.secure Set is secure
+ * @param {boolean} params.httpOnly Set is http only
  */
 export default (params) => {
   const command = new AppCommand();
