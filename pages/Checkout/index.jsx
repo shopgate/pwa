743b4f30694a54bfdb1c5842f3bb4a27660274f9
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-17 09:52:34 +0200

    PWA-487 - delay loading spinner

--- pages/Checkout/index.jsx
+++ pages/Checkout/index.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import View from 'Components/View';
 import LoadingIndicator from '@shopgate/pwa-ui-shared/LoadingIndicator';
 import styles from './style';
 
 /**
  * Checkout dummy page.
- * @returns {JSX}
  */
-const Checkout = () => (
-  <View hasNavigator={false}>
-    <div className={styles.wrapper}>
-      <LoadingIndicator />
-    </div>
-  </View>
-);
+class Checkout extends Component {
+  /**
+   * Constructs.
+   * @param {Object} props Props.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      showIndicator: false,
+    };
+    this.timeout = undefined;
+  }
+
+  /**
+   * Sets a timeout for showing the indicator.
+   */
+  componentWillMount() {
+    this.timeout = setTimeout(() => {
+      this.setState({
+        showIndicator: true,
+      });
+    }, 3000);
+  }
+
+  /**
+   * Clears a timeout.
+   */
+  componentDidUnmount() {
+    clearTimeout(this.timeout);
+  }
+  /**
+   * Renders.
+   * @returns {JSX}
+   */
+  render() {
+    return (
+      <View hasNavigator={false}>
+        <div className={styles.wrapper}>
+          {this.state.showIndicator && <LoadingIndicator /> }
+        </div>
+      </View>
+    );
+  }
+}
 
 export default Checkout;
