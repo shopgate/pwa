69e84fa49b400977f9c53358617f2df0f1aed08f
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-18 10:02:05 +0200

    PWA-660 - removed comment

--- libraries/common/components/Image/style.js
+++ libraries/common/components/Image/style.js
@@ -19,15 +19,6 @@ const container = (background = '#f2f2f2', paddingTop = '100%') => css({
 
 const image = css({
   position: 'absolute',
-  /*
-   * Must be here in order to keep CCP-410 fixed.
-   * Before there was a left:50%, translateX(-50%) hack for centering the images if ratio is
-   * different, but it didn't work since height was not 'auto'.
-   * Adding this hack back would cause PWA-660 regression.
-   *
-   * To fix CCP-410 without shrinking the image and without making PWA-660 back, we should
-   * change how the image is rendered by not making it absolute positioned.
-   * */
   top: 0,
   left: 0,
   width: '100%',
