e082685778855a8b970e79e675c43eca31fb4b3f
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-20 09:50:08 +0200

    PWA-720 - fixed wrong alignment of price info on ios

--- pages/Cart/components/Item/components/Product/components/Layout/style.js
+++ pages/Cart/components/Item/components/Product/components/Layout/style.js
@@ -30,7 +30,7 @@ const info = css({
   marginTop: variables.gap.big * 0.875,
   marginBottom: variables.gap.small * 0.25,
   flexGrow: 1,
-  alignItems: 'flex-start',
+  alignItems: 'flex-end',
   justifyContent: 'space-between',
 }).toString();
 
