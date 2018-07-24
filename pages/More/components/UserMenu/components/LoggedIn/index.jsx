0b14ea9b06318532ca2b5ae9b32edad1d18d0340
rutkowski <waldemar.rutkowski@shopgate.com>
2018-07-24 11:46:40 +0200

    PWA-715: Removed submenu entries for features, that are yet missing (upon removal request).

--- pages/More/components/UserMenu/components/LoggedIn/index.jsx
+++ pages/More/components/UserMenu/components/LoggedIn/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Portal from '@shopgate/pwa-common/components/Portal';
 import * as commonPortals from '@shopgate/pwa-common/constants/Portals';
 import List from 'Components/List';
-import { USER_ADDRESSBOOK_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
+import { USER_ADDRESS_BOOK_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
 import appConfig from '@shopgate/pwa-common/helpers/config';
 
 /**
@@ -18,45 +18,15 @@ const LoggedIn = ({ logout }) => {
       {
         (appConfig.featureFlag.user_addresses &&
         <List>
-          <Portal name={commonPortals.NAV_MENU_MY_DETAILS_BEFORE} props={props} />
-          <Portal name={commonPortals.NAV_MENU_MY_DETAILS} props={props}>
-            <List.Item
-              title="navigation.my_details"
-              link="/user/details"
-              testId="myDetailsButton"
-            />
-          </Portal>
-          <Portal name={commonPortals.NAV_MENU_MY_DETAILS_AFTER} props={props} />
-
-          <Portal name={commonPortals.NAV_MENU_CHANGE_PASSWORD_BEFORE} props={props} />
-          <Portal name={commonPortals.NAV_MENU_CHANGE_PASSWORD} props={props}>
-            <List.Item
-              title="navigation.change_password"
-              link="/user/password"
-              testId="myChangePasswordButton"
-            />
-          </Portal>
-          <Portal name={commonPortals.NAV_MENU_CHANGE_PASSWORD_AFTER} props={props} />
-
           <Portal name={commonPortals.NAV_MENU_ADDRESS_BOOK_BEFORE} props={props} />
           <Portal name={commonPortals.NAV_MENU_ADDRESS_BOOK} props={props}>
             <List.Item
               title="navigation.address_book"
-              link={`${USER_ADDRESSBOOK_PATH}`}
+              link={`${USER_ADDRESS_BOOK_PATH}`}
               testId="addressBookButton"
             />
           </Portal>
           <Portal name={commonPortals.NAV_MENU_ADDRESS_BOOK_AFTER} props={props} />
-
-          <Portal name={commonPortals.NAV_MENU_PAYMENT_OPTIONS_BEFORE} props={props} />
-          <Portal name={commonPortals.NAV_MENU_PAYMENT_OPTIONS} props={props}>
-            <List.Item
-              title="navigation.payment_options"
-              link="/user/paymentOptions"
-              testId="paymentOptionsButton"
-            />
-          </Portal>
-          <Portal name={commonPortals.NAV_MENU_PAYMENT_OPTIONS_AFTER} props={props} />
 
           <Portal name={commonPortals.NAV_MENU_LOGOUT_BEFORE} props={props} />
           <Portal name={commonPortals.NAV_MENU_LOGOUT} props={props}>
