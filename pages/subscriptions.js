17560f230a92844c63ab671c426e20046f459da4
devbucket <florian.mueller81@gmx.de>
2018-07-24 16:34:51 +0200

    PWA-698 Fixed missing subscriptions and protected routes.

--- pages/subscriptions.js
+++ pages/subscriptions.js
@@ -1,8 +1,5 @@
-import event from '@shopgate/pwa-core/classes/Event';
-import { EVENT_PIPELINE_ERROR } from '@shopgate/pwa-core/constants/Pipeline';
 import authRoutes from '@shopgate/pwa-common/collections/AuthRoutes';
-import { appWillStart$, appDidStart$ } from '@shopgate/pwa-common/streams/app';
-import pipelineErrorDialog from '@shopgate/pwa-ui-shared/Dialog/actions/pipelineErrorDialog';
+import { appWillStart$ } from '@shopgate/pwa-common/streams/app';
 
 /**
  * App subscriptions.
@@ -13,10 +10,5 @@ export default function app(subscribe) {
     // TODO: use constants for these pathnames
     authRoutes.set('/checkout', '/login');
     authRoutes.set('/item/:productId/write_review', '/login');
-  });
-
-  subscribe(appDidStart$, ({ dispatch }) => {
-    // Add event callbacks.
-    event.addCallback(EVENT_PIPELINE_ERROR, params => dispatch(pipelineErrorDialog(params)));
   });
 }
