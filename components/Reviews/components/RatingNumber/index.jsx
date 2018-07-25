982aeea6ba45f37e2c75e961dcb0281b6e465a2b
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-25 13:53:35 +0200

    PWA-718 - return null only when rating is not a number

--- components/Reviews/components/RatingNumber/index.jsx
+++ components/Reviews/components/RatingNumber/index.jsx
@@ -11,15 +11,11 @@ import { RATING_SCALE_DIVISOR } from '@shopgate/pwa-ui-shared/RatingStars/consta
  * @returns {JSX}
  */
 const RatingNumber = ({ rating, className }) => {
-  if (!rating) {
+  if (Number.isNaN(rating)) {
     return null;
   }
 
   const number = rating / RATING_SCALE_DIVISOR;
-
-  if (Number.isNaN(number)) {
-    return null;
-  }
 
   return <I18n.Number number={number} className={className} fractions={2} />;
 };
