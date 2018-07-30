9a023fbe6eb41e2ec54480bcce9b2acb608a2ec7
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-30 15:24:09 +0200

    PWA-364 - physical pixel size css helper

--- libraries/common/helpers/style/index.js
+++ libraries/common/helpers/style/index.js
@@ -19,3 +19,35 @@ export const rem = (pixels) => {
 
   return result;
 };
+
+/**
+ * Returns an object ready to use with glamor with one pixel size property.
+ * For example physicalPixelSize('height', 1) would result in:
+ * - height: 1px for screen density 1,
+ * - height: 0.5px for screen density 2,
+ *
+ * @param {string} property Css property name.
+ * @param {number} [size=1] Requested physical pixel size.
+ * @returns {Object}
+ */
+export const physicalPixelSize = (property, size = 1) => {
+  const double = Math.round((size / 2) * 100) / 100;
+  const triple = Math.round((size / 3) * 100) / 100;
+
+  return {
+    [property]: size,
+    '@media (min-device-pixel-ratio: 2)': {
+      [property]: double,
+    },
+    '@media (-webkit-min-device-pixel-ratio: 2)': {
+      [property]: double,
+    },
+    '@media (min-device-pixel-ratio: 3)': {
+      [property]: triple,
+    },
+    '@media (-webkit-min-device-pixel-ratio: 3)': {
+      [property]: triple,
+    },
+  };
+};
+
--- libraries/common/helpers/style/spec.js
+++ libraries/common/helpers/style/spec.js
@@ -1,4 +1,4 @@
-import { rem } from './index';
+import { rem, physicalPixelSize } from './index';
 
 describe('style helper', () => {
   describe('rem helper with 16px rootSize', () => {
@@ -38,6 +38,60 @@ describe('style helper', () => {
     it('should output 1rem as fallback for wrong parameters', () => {
       expect(rem('rem')).toBe('1rem');
       expect(consoleSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('physicalPixelSize', () => {
+    it('should output declarations for 1px size', () => {
+      expect(physicalPixelSize('foo', 1)).toEqual({
+        foo: 1,
+        '@media (min-device-pixel-ratio: 2)': {
+          foo: 0.5,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 2)': {
+          foo: 0.5,
+        },
+        '@media (min-device-pixel-ratio: 3)': {
+          foo: 0.33,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 3)': {
+          foo: 0.33,
+        },
+      });
+    });
+    it('should output declarations for 2px size', () => {
+      expect(physicalPixelSize('foo', 2)).toEqual({
+        foo: 2,
+        '@media (min-device-pixel-ratio: 2)': {
+          foo: 1,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 2)': {
+          foo: 1,
+        },
+        '@media (min-device-pixel-ratio: 3)': {
+          foo: 0.67,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 3)': {
+          foo: 0.67,
+        },
+      });
+    });
+    it('should output declarations for 3px size', () => {
+      expect(physicalPixelSize('foo', 3)).toEqual({
+        foo: 3,
+        '@media (min-device-pixel-ratio: 2)': {
+          foo: 1.5,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 2)': {
+          foo: 1.5,
+        },
+        '@media (min-device-pixel-ratio: 3)': {
+          foo: 1,
+        },
+        '@media (-webkit-min-device-pixel-ratio: 3)': {
+          foo: 1,
+        },
+      });
     });
   });
 });
