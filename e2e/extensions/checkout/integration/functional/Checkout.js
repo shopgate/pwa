7cfd00d656f8735da55f998e9e6a4752ccba9f33
Dennis K <denniskraft87@googlemail.com>
2018-08-07 10:23:15 +0200

    PWA-723 added test for checkout extension

--- e2e/extensions/checkout/integration/functional/Checkout.js
+++ e2e/extensions/checkout/integration/functional/Checkout.js
@@ -2,10 +2,17 @@
 
 import els from '../../elements/de';
 import { clearProductFromCart } from '../../helper/cart';
-import{ logOutUser } from '../../helper/user';
+import { logOutUser, logInUser } from '../../helper/user';
 
 describe('Native Checkout functional', () => {
-  it('should check for wrong coupon', () => {
+
+  it('should check for user not logged in', () => {
+    logOutUser();
+  });
+
+  it('should check for coupon', () => {
+    logInUser();
+    cy.wait(2000);
     cy.visit('');
     cy.get(els.liveShoppingWidgetWithExpectedProduct)
       .should('be.visible')
@@ -23,10 +30,88 @@ describe('Native Checkout functional', () => {
       .should('be.visible');
     cy.get(els.basicDialogOkButton)
       .click();
-    cy.get(els.deleteCouponButton)
+    cy.get(els.checkoutButton)
       .should('be.visible')
       .click()
-      .wait(3000);
+      .wait(2000);
+    cy.get(els.totalComponentSpan)
+      .contains('-10')
+      .scrollIntoView()
+      .should('be.visible');
+  });
+
+  it('should check for DHL Shipping', () => {
+    cy.get(els.DHLShippingMethod)
+      .scrollIntoView()
+      .click();
+    cy.get(els.shippingTotalLabel)
+      .scrollIntoView()
+      .should('be.visible');
+    cy.get(els.shippingTotalAmountDHL)
+      .should('be.visible');
+  });
+
+  it('should check for paypal Payment', () => {
+    cy.get(els.PaypalPaymentMethod)
+      .click();
+    cy.get(els.paymentTotalLabel)
+      .should('be.visible');
+    cy.get(els.paymenttotalAmountPaypal)
+      .should('be.visible');
+  });
+
+  it('should check for add shpping adress button', () => {
+    cy.get(els.addShippingAdressButton)
+      .scrollIntoView()
+      .click();
+    cy.get(els.adressFormFirstName)
+      .should('be.visible')
+      .type('Mr');
+    cy.get(els.adressFormLastName)
+      .type('Example');
+    cy.get(els.adressFormStreet)
+      .type('teststreet 1');
+    cy.get(els.adressFormCity)
+      .type('Butzbach');
+    cy.get(els.adressFromCountry)
+      .select(els.adressFormCountryDEOption);
+    cy.get(els.adressFormZip)
+      .type('35510');
+    cy.go('back');
+  });
+
+  it('should check for add billing adress button', () => {
+    cy.get(els.addBillingAdressButton)
+      .scrollIntoView()
+      .click();
+    cy.get(els.adressFormFirstName)
+      .should('be.visible')
+      .type('Mr');
+    cy.get(els.adressFormLastName)
+      .type('Example');
+    cy.get(els.adressFormStreet)
+      .type('teststreet 1');
+    cy.get(els.adressFormCity)
+      .type('Butzbach');
+    cy.get(els.adressFromCountry)
+      .select(els.adressFormCountryDEOption);
+    cy.get(els.adressFormZip)
+      .type('35510');
+    cy.go('back');
+
+  });
+
+  it('should delete coupon and products from cart', () => {
+    cy.visit('/cart');
+    cy.get(els.deleteCouponButton)
+      .should('be.visible')
+      .click();
+    cy.get(els.deleteCouponButton)
+      .should('not.be.visible');
     clearProductFromCart();
+  });
+
+  it('should logout user', () => {
+    logOutUser();
   });
 });
