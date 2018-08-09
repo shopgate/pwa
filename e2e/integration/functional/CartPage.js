77dd0594d441fc6edd99095c9f9448fb361a308e
Dennis K <denniskraft87@googlemail.com>
2018-08-09 13:42:10 +0200

    PWA-749 wait for quantity picker

--- e2e/integration/functional/CartPage.js
+++ e2e/integration/functional/CartPage.js
@@ -20,10 +20,11 @@ describe('functional tests cart page', () => {
     cy.get(els.cartButton)
       .should('be.visible')
       .click();
-    cy.get(els.quantityPicker)
+      cy.get(els.quantityPicker)
       .should('be.visible')
       .click()
       .type(2)
+      .wait(100)
       .focus()
       .blur();
     cy.get('[data-test-id="minPrice: 0 price: 398 currency: EUR"]')
@@ -31,6 +32,7 @@ describe('functional tests cart page', () => {
     cy.get(els.quantityPicker)
       .clear()
       .type(1)
+      .wait(100)
       .focus()
       .blur();
     cy.get('[data-test-id="minPrice: 0 price: 199 currency: EUR"]')
