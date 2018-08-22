0f94e5425322f98b9da210d5eddf48db8f91ec5f
Dennis K <denniskraft87@googlemail.com>
2018-08-22 15:12:30 +0200

    PWA-821 change shopLogo selector for multi shop usage

--- e2e/elements/de.js
+++ e2e/elements/de.js
@@ -4,7 +4,7 @@ export default {
   tabBarCart: "[data-test-id='tabBar'] [data-test-id='tab_bar.cart']",
   tabBarBrowse: "[data-test-id='tabBar'] [data-test-id='tab_bar.browse']",
   tabBarHome: "[data-test-id='tabBar'] [data-test-id='tab_bar.home']",
-  shopLogo: "[data-test-id='Navigator'] img[alt='TestAutomation']",
+  shopLogo: "[data-test-id='Navigator'] img",
   imageSliderImage1: "[data-test-id='Slider']:nth-child(2)  img[data-test-id='withoutLink']",
   imageSliderImage2: "[data-test-id='Slider']:nth-child(3)  img[data-test-id='withoutLink']",
   imageWidgetWithLink1: "[data-test-id='imageWidget: /category/3537']",
