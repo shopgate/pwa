79f84000b87577716103b5faa0458d24ce25e282
devbucket <florian.mueller81@gmx.de>
2018-08-23 17:23:49 +0200

    PWA-830 Removed web-worker.

--- index.jsx
+++ index.jsx
@@ -12,22 +12,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { configureStore } from '@shopgate/pwa-common/store';
-import initSubscribers from '@shopgate/pwa-common/subscriptions';
 import { appWillStart } from '@shopgate/pwa-common/action-creators/app';
 import smoothscrollPolyfill from '@shopgate/pwa-common/helpers/scrollPolyfill';
 import fetchClientInformation from '@shopgate/pwa-common/actions/client/fetchClientInformation';
-import syncRouter from '@virtuous/redux-conductor';
 import Pages from './pages';
 import reducers from './pages/reducers';
-import Worker from './pages/worker';
 import subscribers from './pages/subscribers';
 
 injectTapEventPlugin();
 smoothscrollPolyfill();
-initSubscribers(subscribers);
 
-const store = configureStore(reducers, new Worker());
-syncRouter(store);
+const store = configureStore(reducers, subscribers);
 
 store.dispatch(appWillStart(`${window.location.pathname}${window.location.search}`));
 store.dispatch(fetchClientInformation());
--- pages/worker.js
+++
@@ -1,6 +0,0 @@
-import { createWorker } from '@virtuous/redux-web-worker';
-import reducer from './reducers';
-
-const worker = createWorker();
-
-worker.registerReducer(reducer);
