7fb02b040658100df6324388508d32b4e566b0e5
Richard Gorman <richardgorman@outlook.com>
2018-08-28 16:17:00 +0200

    PWA-858 Removed unnecessary filter store code

---
+++ libraries/common/components/Consume/helpers/buildParams.js
@@ -0,0 +1,20 @@
+import get from 'lodash/get';
+
+/**
+ * Searches an object for the given paths.
+ * Returns a new object with the values found at the end of each path.
+ * @param {Object} obj The object to look through.
+ * @param {Object} paths A set of paths to find inside the object.
+ * @returns {Object}
+ */
+const buildParams = (obj, paths) => {
+  const params = {};
+
+  Object.entries(paths).forEach(([prop, path]) => {
+    params[prop] = get(obj, path) || null;
+  });
+
+  return params;
+};
+
+export default buildParams;
