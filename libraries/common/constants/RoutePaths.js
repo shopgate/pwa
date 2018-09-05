00e86c3fd646294407f30f904239fbd88375de0b
Alexander Wesselburg <info@myportal.de>
2018-09-05 13:28:08 +0200

    SHOPIFY-537-create-functionality-and-add - Add new constant for guest checkout path

--- libraries/common/constants/RoutePaths.js
+++ libraries/common/constants/RoutePaths.js
@@ -3,6 +3,7 @@ export const PAGE_PATH = '/page';
 export const LOGIN_PATH = '/login';
 export const REGISTER_PATH = '/register';
 export const CHECKOUT_PATH = '/checkout';
+export const GUEST_CHECKOUT_PATH = '/checkout';
 
 /**
  * Our current existing Deeplinks and Pushs use '/index' for the homepage
