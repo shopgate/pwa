6c03fabe663573ab2042345e1e641b53a394c21c
Richard Gorman <richardgorman@outlook.com>
2018-09-05 22:26:03 +0200

    PWA-936 Corrected sheet item highlight style

--- pages/Product/components/Characteristics/Characteristic/components/SheetItem/style.js
+++ pages/Product/components/Characteristics/Characteristic/components/SheetItem/style.js
@@ -14,6 +14,7 @@ const buttonDisabled = css(button, {
 
 const buttonSelected = css(button, {
   background: colors.darkGray,
+  boxShadow: `-16px 0 0 ${colors.darkGray}`,
   margin: '-1px 0',
   paddingTop: 17,
   paddingBottom: 17,
