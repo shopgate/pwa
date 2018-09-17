a0861eec2b22e591b5e83ebbae3a420e13987e52
devbucket <florian.mueller81@gmx.de>
2018-09-17 12:08:23 +0200

    PWA-327 Added extension indexes.

---
+++ scripts/buildExtensionIndex.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd themes/gmd && mkdir extensions && cd extensions && touch reducers.js && cat 'export default null;' > reducers.js
+cd themes/ios11 && mkdir extensions && cd extensions && touch reducers.js && cat 'export default null;' > reducers.js
