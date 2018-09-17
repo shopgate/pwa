b774c9a07eca07292a6b7dfe85e984bdef811155
devbucket <florian.mueller81@gmx.de>
2018-09-17 12:14:18 +0200

    PWA-327 Changed sh script

--- scripts/buildExtensionIndex.sh
+++ scripts/buildExtensionIndex.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cd themes/gmd && mkdir extensions && cd extensions && touch reducers.js && cat 'export default null;' > reducers.js
-cd themes/ios11 && mkdir extensions && cd extensions && touch reducers.js && cat 'export default null;' > reducers.js
+cd themes/gmd
+/bin/mkdir extensions
+cd extensions
+/bin/touch reducers.js
+/bin/cat 'export default null;' > reducers.js
+cd ../..
+cd ios11
+/bin/mkdir extensions
+cd extensions
+/bin/touch reducers.js
+/bin/cat 'export default null;' > reducers.js
+cd ../../..
