7a2cb2471f9547c4217eb63556251b8c30bd89db
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-09-21 17:41:00 +0200

    PWA-765: Improved product tracking

--- libraries/tracking/constants/index.js
+++ libraries/tracking/constants/index.js
@@ -3,4 +3,4 @@ import { ITEM_PATH } from '@shopgate/pwa-common-commerce/product/constants';
 export const APP_EVENT_VIEW_DID_APPEAR = 'viewDidAppear';
 export const APP_EVENT_VIEW_DID_DISAPPEAR = 'viewDidDisappear';
 export const PWA_DID_APPEAR = 'PWA_DID_APPEAR';
-export const PATTERN_ITEM_PAGE = `${ITEM_PATH}/:productId`;
+export const ITEM_PATTERN = `${ITEM_PATH}/:productId`;
--- libraries/tracking/streams/product.js
+++ libraries/tracking/streams/product.js
@@ -2,12 +2,12 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
 import { Observable } from 'rxjs/Observable';
 import { main$ } from '@shopgate/pwa-common/streams/main';
-import { routeDidEnter$ } from '@shopgate/pwa-common/streams/router';
+import { routeWillEnter$ } from '@shopgate/pwa-common/streams/router';
 import { getBaseProduct } from '@shopgate/pwa-common-commerce/product/selectors/product';
 import { receivedVisibleProduct$ } from '@shopgate/pwa-common-commerce/product/streams';
 import { RECEIVE_PRODUCT, RECEIVE_PRODUCTS } from '@shopgate/pwa-common-commerce/product/constants';
-import { HISTORY_PUSH_ACTION, HISTORY_REPLACE_ACTION } from '@shopgate/pwa-common/constants/ActionTypes';
-import { PATTERN_ITEM_PAGE } from '../constants';
+import { HISTORY_REPLACE_ACTION } from '@shopgate/pwa-common/constants/ActionTypes';
+import { ITEM_PATTERN } from '../constants';
 import { pwaDidAppear$ } from './app';
 
 /**
@@ -26,20 +26,20 @@ export const productReceived$ = main$
  * Emits when the category route comes active again after a legacy page was active.
  */
 export const productRouteReappeared$ = pwaDidAppear$
-  .filter(({ action }) => action.route.pattern === PATTERN_ITEM_PAGE);
+  .filter(({ action }) => action.route.pattern === ITEM_PATTERN);
 
 /**
  * Emits when a product page was initially opened.
  */
-export const productDidEnter$ = routeDidEnter$.filter(({ action }) =>
-  action.route.pattern === PATTERN_ITEM_PAGE &&
-  action.historyAction === HISTORY_PUSH_ACTION);
+export const productDidEnter$ = routeWillEnter$.filter(({ action }) =>
+  action.route.pattern === ITEM_PATTERN &&
+  action.historyAction !== HISTORY_REPLACE_ACTION);
 
 /**
  * Emits when a product page was updated due to a variant product switch.
  */
-export const productDidUpdate$ = routeDidEnter$.filter(({ action }) =>
-  action.route.pattern === PATTERN_ITEM_PAGE &&
+export const productDidUpdate$ = routeWillEnter$.filter(({ action }) =>
+  action.route.pattern === ITEM_PATTERN &&
   action.historyAction === HISTORY_REPLACE_ACTION);
 
 /**
@@ -54,19 +54,20 @@ export const productIsReady$ = productDidEnter$
  * Emits when a variant product was changed.
  */
 export const variantDidChange$ = productDidUpdate$
-  .switchMap(() => receivedVisibleProduct$.switchMap((data) => {
-    const { id, baseProductId } = data.action.productData;
+  .switchMap(() => receivedVisibleProduct$
+    .switchMap((data) => {
+      const { id, baseProductId } = data.action.productData;
 
-    const variantId = baseProductId !== null ? id : null;
-    const baseProduct = getBaseProduct(data.getState(), { variantId });
+      const variantId = baseProductId !== null ? id : null;
+      const baseProduct = getBaseProduct(data.getState(), { variantId });
 
-    if (baseProduct === null) {
-      /**
-       * A PDP with a variant product was opened, but the base product is not fetched yet.
-       * So emitting of the stream is postponed till the data is present.
-       */
-      return productReceived$;
-    }
+      if (baseProduct === null) {
+        /**
+         * A PDP with a variant product was opened, but the base product is not fetched yet.
+         * So emitting of the stream is postponed till the data is present.
+         */
+        return productReceived$;
+      }
 
-    return Observable.of(data);
-  }));
+      return Observable.of(data);
+    }));
