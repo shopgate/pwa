329293c0dbe678d7c1c048a5198c644361b51238
devbucket <florian.mueller81@gmx.de>
2018-10-12 21:38:58 +0200

    PWA-327 Fixed iOS index.

--- index.jsx
+++ index.jsx
@@ -10,6 +10,19 @@ import 'Styles/fonts';
 import 'Extensions/portals';
 import React from 'react';
 import { render } from 'react-dom';
+import { configureStore } from '@shopgate/pwa-common/store';
+import { appWillStart } from '@shopgate/pwa-common/action-creators/app';
+import smoothscrollPolyfill from '@shopgate/pwa-common/helpers/scrollPolyfill';
+import fetchClientInformation from '@shopgate/pwa-common/actions/client/fetchClientInformation';
 import Pages from './pages';
+import reducers from './pages/reducers';
+import subscribers from './pages/subscribers';
 
-render(<Pages />, document.getElementById('root'));
+smoothscrollPolyfill();
+
+const store = configureStore(reducers, subscribers);
+
+store.dispatch(appWillStart(`${window.location.pathname}${window.location.search}`));
+store.dispatch(fetchClientInformation());
+
+render(<Pages store={store} />, document.getElementById('root'));
