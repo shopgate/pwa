9ef508d28482b158c69ce4b784ee79d574912820
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-10-24 17:35:13 +0200

    PWA-1143: Added browse and more tabs to the TabBar

---
+++ themes/theme-ios11/pages/Browse/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Consume from '@shopgate/pwa-common/components/Consume';
+import { RouteContext } from '@virtuous/react-conductor/Router/context';
+import View from 'Components/View';
+import colors from 'Styles/colors';
+import Content from './components/Content';
+
+const propsMap = {
+  visible: 'visible',
+};
+
+/**
+ * The Cart component.
+ * @returns {JSX}
+ */
+const Browse = () => (
+  <View background={colors.background}>
+    <Consume context={RouteContext} props={propsMap}>
+      {({ visible }) => visible && <Content />}
+    </Consume>
+  </View>
+);
+
+export default Browse;
