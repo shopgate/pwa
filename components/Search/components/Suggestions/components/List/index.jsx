5d5d9c4a8558a3b3c4829264d61188b04f8ada0a
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-10-30 15:35:36 +0100

    PWA-1170: Improved search suggestions in iOS and GMD - fixed and issue where the suggestions disappeared while fetching - increased suggestions request rate

--- components/Search/components/Suggestions/components/List/index.jsx
+++ components/Search/components/Suggestions/components/List/index.jsx
@@ -1,43 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import connect from './connector';
 import styles from './style';
 
 /**
- * @param {Object} props The component props.
- * @returns {JSX}
+ * The SuggestionList component.
  */
-function SuggestionList(props) {
-  const { onClick, suggestions } = props;
+class SuggestionList extends Component {
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    fetching: PropTypes.bool,
+    suggestions: PropTypes.arrayOf(PropTypes.string),
+  };
 
-  if (!suggestions) {
-    return null;
+  static defaultProps = {
+    fetching: false,
+    suggestions: [],
+  };
+  /**
+   * @param { Object } nextProps Next props.
+   * @return {boolean}
+   */
+  shouldComponentUpdate(nextProps) {
+    return nextProps.fetching === false && nextProps.suggestions;
   }
 
-  return (
-    <div className={styles.list}>
-      {suggestions.map(suggestion => (
-        <button
-          className={styles.item}
-          onClick={onClick}
-          key={suggestion}
-          value={suggestion}
-          data-test-id={`searchSuggestion ${suggestion}`}
-        >
-          {suggestion}
-        </button>
-      ))}
-    </div>
-  );
+  /**
+   * @return {JSX}
+   */
+  render() {
+    const { onClick, suggestions } = this.props;
+
+    if (!suggestions) {
+      return null;
+    }
+
+    return (
+      <div className={styles.list}>
+        {suggestions.map(suggestion => (
+          <button
+            className={styles.item}
+            onClick={onClick}
+            key={suggestion}
+            value={suggestion}
+            data-test-id={`searchSuggestion ${suggestion}`}
+          >
+            {suggestion}
+          </button>
+        ))}
+      </div>
+    );
+  }
 }
 
-SuggestionList.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  suggestions: PropTypes.arrayOf(PropTypes.string),
-};
-
-SuggestionList.defaultProps = {
-  suggestions: [],
-};
+export { SuggestionList as UnwrappedSuggestionList };
 
 export default connect(SuggestionList);
