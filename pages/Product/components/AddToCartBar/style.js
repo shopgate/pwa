44495dabf47a9732fbdfac4c7569bbd2358043aa
Richard Gorman <richardgorman@outlook.com>
2018-10-31 16:08:37 +0100

    PWA-1158 AddToCartBar is now rendered outside of the page content

--- pages/Product/components/AddToCartBar/style.js
+++ pages/Product/components/AddToCartBar/style.js
@@ -4,38 +4,28 @@ import variables from 'Styles/variables';
 const barHeight = 46;
 
 const container = css({
-  position: 'fixed',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  padding: variables.gap.small,
-  paddingBottom: `calc(${variables.gap.small}px + var(--safe-area-inset-bottom))`,
   background: 'rgba(255, 255, 255, 0.9)',
   backdropFilter: 'blur(20px)',
-  boxShadow: '0 0 30px rgba(0,0,0,0.1)',
+  boxShadow: '0 0 30px rgba(0, 0, 0, 0.1)',
+  padding: variables.gap.small,
+  paddingBottom: `calc(${variables.gap.small}px + var(--safe-area-inset-bottom))`,
 });
 
 const base = css({
-  position: 'relative',
   height: barHeight,
+  position: 'relative',
 });
 
 const statusBar = css({
+  alignItems: 'center',
   display: 'flex',
   height: '100%',
-  alignItems: 'center',
   maxWidth: '60%',
   padding: `0 ${variables.gap.small}px`,
-});
-
-const dummy = css({
-  display: 'block',
-  minHeight: `calc(${barHeight}px + ${variables.gap.small}px)`,
 });
 
 export default {
   container,
   base,
   statusBar,
-  dummy,
 };
