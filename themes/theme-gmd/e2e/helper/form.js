a541d194c2dcc1f30ccb9b09e367b3cca82bcfeb
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-10-31 15:42:22 +0100

    PWA-1141: Fixed some additional linter rules

--- themes/theme-gmd/e2e/helper/form.js
+++ themes/theme-gmd/e2e/helper/form.js
@@ -2,9 +2,9 @@ export function setValue(fieldSelector, value) {
   cy.get(fieldSelector)
     .should('be.visible')
     .clear()
-    .type(value)
+    .type(value);
 }
 
 export function selectValue(selectSelector, value) {
-  cy.get(selectSelector).select(value)
+  cy.get(selectSelector).select(value);
 }
