5fe9b4c547c069f246f937c17531f8ed700f1211
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-10-31 11:59:17 +0100

    PWA-1157: Fixed an issue where the page content was overlapped by footer components

--- components/View/components/Content/style.js
+++ components/View/components/Content/style.js
@@ -6,30 +6,35 @@ import variables from 'Styles/variables';
  * @param {boolean} isFullscreen Whether remove all offsets,
  *                  so that it's really fullscreen (including the notch).
  * @param {number} keyboardHeight The space that is taken by the keyboard.
+ * @param {boolean} noScroll Wheather the view should be scrollable or not.
  * @return {string} The content style class.
  */
-const content = (isFullscreen = false, keyboardHeight = 0) => css({
-  overflow: 'auto',
-  overflowScrolling: 'touch',
-  WebkitOverflowScrolling: 'touch',
-  width: '100%',
-  position: 'absolute',
-  top: 'var(--safe-area-inset-top)',
-  paddingBottom: `calc(var(--tabbar-height) + ${keyboardHeight}px + var(--safe-area-inset-bottom))`,
-  bottom: 0,
-  display: 'flex',
-  flexDirection: 'column',
-  ':before': {
-    position: 'fixed',
-    display: 'block',
-    top: 0,
+const content = (isFullscreen = false, keyboardHeight = 0, noScroll = false) => {
+  const overflow = noScroll ? 'hidden' : 'auto';
+
+  return css({
+    overflow,
+    overflowScrolling: 'touch',
+    WebkitOverflowScrolling: 'touch',
     width: '100%',
-    height: isFullscreen ? 0 : 'var(--safe-area-inset-top)',
-    zIndex: 3,
-    content: '""',
-    transition: 'box-shadow 100ms cubic-bezier(0.25, 0.1, 0.25, 1)',
-  },
-}).toString();
+    position: 'absolute',
+    top: 'var(--safe-area-inset-top)',
+    paddingBottom: `calc(var(--tabbar-height) + ${keyboardHeight}px + var(--safe-area-inset-bottom))`,
+    bottom: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    ':before': {
+      position: 'fixed',
+      display: 'block',
+      top: 0,
+      width: '100%',
+      height: isFullscreen ? 0 : 'var(--safe-area-inset-top)',
+      zIndex: 3,
+      content: '""',
+      transition: 'box-shadow 100ms cubic-bezier(0.25, 0.1, 0.25, 1)',
+    },
+  }).toString();
+};
 
 const contentShaded = css({
   ':before': {
