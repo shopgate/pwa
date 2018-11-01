384ac31da1c442b8559a8f9be360b4e213a7da76
Richard Gorman <richardgorman@outlook.com>
2018-11-01 12:39:44 +0100

    PWA-1165 CartButton style adjustments

--- components/AppBar/components/CartButton/index.jsx
+++ components/AppBar/components/CartButton/index.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { AppBar } from '@shopgate/pwa-ui-ios';
-import { CartIcon } from '@shopgate/pwa-ui-shared';
+import { AppBar, CartIcon } from '@shopgate/pwa-ui-ios';
 import Badge from './components/CartBadge';
 import connect from './connector';
 import styles from './style';
