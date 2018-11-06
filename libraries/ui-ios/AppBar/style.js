4179963fb1dced5134dbca8b01d0febe77a2795a
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-11-06 09:20:08 +0100

    PWA-1196: Added missing translations for the Filters page - fixed linter errors

--- libraries/ui-ios/AppBar/style.js
+++ libraries/ui-ios/AppBar/style.js
@@ -4,12 +4,10 @@ const outer = css({
   boxSizing: 'content-box',
   minHeight: 44,
   paddingTop: 'var(--safe-area-inset-top)',
-  position: 'relative',
 }).toString();
 
 const inner = css({
   display: 'flex',
-  flexBasis: 'auto',
   justifyContent: 'space-between',
   position: 'relative',
   zIndex: 1,
