670e6333420f92f46c68189cc1734242c85b70d6
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-11-14 14:35:40 +0100

    PWA-1232 Wrap sub tests into describe

--- themes/theme-gmd/e2e/integration/specFiles/consistency/shopify.js
+++ themes/theme-gmd/e2e/integration/specFiles/consistency/shopify.js
@@ -1,12 +1,13 @@
-describe('consistency test for shopify', () => {});
-require('../../consistency/CartPage.js');
-require('../../consistency/CategoryPage.js');
-require('../../consistency/FavoritesPage.js');
-require('../../consistency/FilterPage.js');
-require('../../consistency/LoginPage.js');
-require('../../consistency/NavDrawer.js');
-require('../../consistency/ProductPage.js');
-require('../../consistency/ReviewsPage.js');
-require('../../consistency/SearchPage.js');
-require('../../consistency/StartPage.js');
+describe('Consistency test for shopify', () => {
+  require('../../consistency/CartPage.js')
+  require('../../consistency/CategoryPage.js')
+  require('../../consistency/FavoritesPage.js')
+  require('../../consistency/FilterPage.js')
+  require('../../consistency/LoginPage.js')
+  require('../../consistency/NavDrawer.js')
+  require('../../consistency/ProductPage.js')
+  require('../../consistency/ReviewsPage.js')
+  require('../../consistency/SearchPage.js')
+  require('../../consistency/StartPage.js')
+})
 
