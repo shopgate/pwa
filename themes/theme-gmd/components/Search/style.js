791589279874259f0092d5e430de554228aed23f
Richard Gorman <richardgorman@outlook.com>
2018-11-14 15:09:42 +0100

    PWA-1254 Tapping on blank area now closes Search

--- themes/theme-gmd/components/Search/style.js
+++ themes/theme-gmd/components/Search/style.js
@@ -1,15 +1,9 @@
 import { css } from 'glamor';
-import colors from 'Styles/colors';
 
-const view = css({
-  background: colors.background,
+export default css({
   bottom: 0,
   position: 'fixed',
   top: 0,
   width: '100%',
   zIndex: 4,
 });
-
-export default {
-  view,
-};
