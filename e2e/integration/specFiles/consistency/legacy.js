52f4dd444138330c40ed197a5989d6b4684387e6
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-11-14 14:35:40 +0100

    PWA-1232 Wrap sub tests into describe

--- e2e/integration/specFiles/consistency/legacy.js
+++ e2e/integration/specFiles/consistency/legacy.js
@@ -1,13 +1,14 @@
-describe('consistency test for legacy', () => {});
-require('../../consistency/CartPage.js');
-require('../../consistency/CategoryPage.js');
-require('../../consistency/FavoritesPage.js');
-require('../../consistency/FilterPage.js');
-require('../../consistency/LoginPage.js');
-require('../../consistency/NavDrawer.js');
-require('../../consistency/ProductPage.js');
-require('../../consistency/ProductPageOptions.js');
-require('../../consistency/ProductPageBasePrice.js');
-require('../../consistency/ReviewsPage.js');
-require('../../consistency/SearchPage.js');
-require('../../consistency/StartPage.js');
+describe('Consistency test for legacy', () => {
+  require('../../consistency/CartPage.js')
+  require('../../consistency/CategoryPage.js')
+  require('../../consistency/FavoritesPage.js')
+  require('../../consistency/FilterPage.js')
+  require('../../consistency/LoginPage.js')
+  require('../../consistency/NavDrawer.js')
+  require('../../consistency/ProductPage.js')
+  require('../../consistency/ProductPageOptions.js')
+  require('../../consistency/ProductPageBasePrice.js')
+  require('../../consistency/ReviewsPage.js')
+  require('../../consistency/SearchPage.js')
+  require('../../consistency/StartPage.js')
+})
