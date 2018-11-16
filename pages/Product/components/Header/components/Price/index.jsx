271e87e45ca099a9a14fcd49255c2b949ba7a794
Frithjof Klös <frithjof.kloes@shopgate.com>
2018-11-16 17:50:50 +0100

    PWA-1260: Fixed wrong displayed tier price products

--- pages/Product/components/Header/components/Price/index.jsx
+++ pages/Product/components/Header/components/Price/index.jsx
@@ -17,7 +17,7 @@ import styles from './style';
  * @param {Object} props The component props.
  * @return {JSX}
  */
-const Price = ({ hasVariants, price }) => (
+const Price = ({ recalculatePrice, price }) => (
   <Fragment>
     <Portal name={PRODUCT_PRICE_BEFORE} />
     <Portal name={PRODUCT_PRICE}>
@@ -28,9 +28,8 @@ const Price = ({ hasVariants, price }) => (
             currency={price.currency}
             discounted={!!price.discount}
             taxDisclaimer
-            fromPrice={hasVariants}
-            unitPrice={hasVariants ? price.unitPrice : price.totalPrice}
-            unitPriceMin={price.unitPriceMin}
+            unitPrice={!recalculatePrice ? price.unitPrice : price.totalPrice}
+            unitPriceMin={!recalculatePrice ? price.unitPriceMin : 0}
           />
         )}
       </PlaceholderLabel>
@@ -40,13 +39,13 @@ const Price = ({ hasVariants, price }) => (
 );
 
 Price.propTypes = {
-  hasVariants: PropTypes.bool,
   price: PropTypes.shape(),
+  recalculatePrice: PropTypes.bool,
 };
 
 Price.defaultProps = {
   price: null,
-  hasVariants: false,
+  recalculatePrice: false,
 };
 
 export default connect(pure(Price));
