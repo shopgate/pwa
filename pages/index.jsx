e43c10a45805dd8541965980da417d4658e46fe3
Richard Gorman <richardgorman@outlook.com>
2018-11-22 08:55:37 +0100

    Installed conductor 2 beta

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import appConfig from '@shopgate/pwa-common/helpers/config';
 import { isDev } from '@shopgate/pwa-common/helpers/environment';
 import { history } from '@shopgate/pwa-common/helpers/router';
 import routePortals from '@shopgate/pwa-common/helpers/portals/routePortals';
-import { Router, Route, SpringRoute } from '@virtuous/react-conductor';
+import { Router, Route } from '@virtuous/react-conductor';
 import ModalContainer from '@shopgate/pwa-common/components/ModalContainer';
 import ToastProvider from '@shopgate/pwa-common/providers/toast';
 import App from '@shopgate/pwa-common/App';
@@ -53,23 +53,23 @@ const Pages = ({ store }) => (
             <ModalContainer component={Dialog} />
             <Toaster render={props => <SnackBar {...props} />} />
             <Router history={history}>
-              <SpringRoute pattern={INDEX_PATH} component={routes.StartPage} transition={transition} />
-              <SpringRoute pattern={PAGE_PATTERN} component={routes.Page} transition={transition} />
-              <SpringRoute pattern={ROOT_CATEGORY_PATTERN} component={routes.RootCategory} transition={transition} />
-              <SpringRoute pattern={CATEGORY_PATTERN} component={routes.Category} transition={transition} />
-              <SpringRoute pattern={CATEGORY_FILTER_PATTERN} component={routes.Filter} transition={transition} />
-              <SpringRoute pattern={ITEM_PATTERN} component={routes.Product} transition={transition} />
-              <SpringRoute pattern={ITEM_GALLERY_PATTERN} component={routes.ProductGallery} transition={transition} />
-              <SpringRoute pattern={ITEM_REVIEWS_PATTERN} component={routes.Reviews} transition={transition} />
-              <SpringRoute pattern={ITEM_WRITE_REVIEW_PATTERN} component={routes.WriteReview} transition={transition} />
-              <SpringRoute pattern={CART_PATH} component={routes.Cart} transition={transition} />
+              <Route pattern={INDEX_PATH} component={routes.StartPage} transition={transition} />
+              <Route pattern={PAGE_PATTERN} component={routes.Page} transition={transition} />
+              <Route pattern={ROOT_CATEGORY_PATTERN} component={routes.RootCategory} transition={transition} />
+              <Route pattern={CATEGORY_PATTERN} component={routes.Category} transition={transition} />
+              <Route pattern={CATEGORY_FILTER_PATTERN} component={routes.Filter} transition={transition} />
+              <Route pattern={ITEM_PATTERN} component={routes.Product} transition={transition} />
+              <Route pattern={ITEM_GALLERY_PATTERN} component={routes.ProductGallery} transition={transition} />
+              <Route pattern={ITEM_REVIEWS_PATTERN} component={routes.Reviews} transition={transition} />
+              <Route pattern={ITEM_WRITE_REVIEW_PATTERN} component={routes.WriteReview} transition={transition} />
+              <Route pattern={CART_PATH} component={routes.Cart} transition={transition} />
               {
                 appConfig.hasFavorites
-                && <SpringRoute pattern={FAVORITES_PATH} component={routes.Favorites} transition={transition} />
+                && <Route pattern={FAVORITES_PATH} component={routes.Favorites} transition={transition} />
               }
-              <SpringRoute pattern={LOGIN_PATH} component={routes.Login} transition={transition} />
-              <SpringRoute pattern={SEARCH_PATTERN} component={routes.Search} transition={transition} />
-              <SpringRoute pattern={SEARCH_FILTER_PATTERN} component={routes.Filter} transition={transition} />
+              <Route pattern={LOGIN_PATH} component={routes.Login} transition={transition} />
+              <Route pattern={SEARCH_PATTERN} component={routes.Search} transition={transition} />
+              <Route pattern={SEARCH_FILTER_PATTERN} component={routes.Filter} transition={transition} />
               {React.Children.map(routePortals, Component => Component)}
             </Router>
             {isDev && (
