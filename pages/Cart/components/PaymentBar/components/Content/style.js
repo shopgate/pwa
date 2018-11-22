804d9013d0602c6af7e67beb5e3b8861f5c2ad3d
Richard Gorman <richardgorman@outlook.com>
2018-11-22 20:34:12 +0100

    PWA-1314 Moved SnackBar to AppFooter

--- pages/Cart/components/PaymentBar/components/Content/style.js
+++ pages/Cart/components/PaymentBar/components/Content/style.js
@@ -5,8 +5,10 @@ import variables from 'Styles/variables';
 const container = css({
   background: colors.light,
   boxShadow: '0 -2px 4px rgba(0, 0, 0, 0.1)',
-  padding: `${variables.gap.big}px ${variables.gap.small}px`,
   lineHeight: 1.45,
+  padding: `${variables.gap.big}px ${variables.gap.small}px`,
+  position: 'relative',
+  zIndex: 2,
 }).toString();
 
 const column = css({
