2beb7c306623b35dd29f7c5cfe3e9729f4ff5f4e
Richard Gorman <richardgorman@outlook.com>
2018-11-22 14:55:54 +0100

    PWA-1310 Moved PaymentBar and SnackBar in iOS to AppFooter

--- pages/Cart/components/PaymentBar/components/Content/style.js
+++ pages/Cart/components/PaymentBar/components/Content/style.js
@@ -1,10 +1,15 @@
 import { css } from 'glamor';
+import colors from 'Styles/colors';
 import variables from 'Styles/variables';
 
 const container = css({
+  background: colors.light,
+  boxShadow: '0 -2px 4px rgba(0, 0, 0, 0.1)',
   padding: `${variables.gap.small}px`,
   lineHeight: 1.45,
   flexWrap: 'wrap',
+  position: 'relative',
+  zIndex: 2,
 }).toString();
 
 const column = css({
@@ -31,7 +36,10 @@ const checkoutButton = css({
 }).toString();
 
 const checkoutButtonContainer = css({
+  background: colors.light,
   padding: `0 ${variables.gap.big}px 10px ${variables.gap.big}px`,
+  position: 'relative',
+  zIndex: 2,
 }).toString();
 
 export default {
