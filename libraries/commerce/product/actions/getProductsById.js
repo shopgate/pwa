70477bb9c490a4c1d92a25c59ec3c3ed6c12a9d4
Aaron Martin <aaron.martin@shopgate.com>
2018-11-27 09:11:50 -0600

    PI-9198: Updated getPRoductsById so product hashes containing product IDs would not change based on which products were already cached and added another call to getProducts in product widget that responds to widget setting changes

--- libraries/commerce/product/actions/getProductsById.js
+++ libraries/commerce/product/actions/getProductsById.js
@@ -1,5 +1,23 @@
-import { shouldFetchData } from '@shopgate/pwa-common/helpers/redux';
-import getProducts from './getProducts';
+import PipelineRequest from '@shopgate/pwa-core/classes/PipelineRequest';
+import { logger } from '@shopgate/pwa-core/helpers';
+import { generateResultHash, shouldFetchData } from '@shopgate/pwa-common/helpers/redux';
+import { getSortOrder } from '@shopgate/pwa-common/selectors/history';
+import * as pipelines from '../constants/Pipelines';
+import requestProducts from '../action-creators/requestProducts';
+import receiveProducts from '../action-creators/receiveProducts';
+import errorProducts from '../action-creators/errorProducts';
+import { getActiveFilters } from '../../filter/selectors';
+
+/**
+ * Gets products that are already cached in state
+ * @param {Array} productIds The product id's that are already cached
+ * @param {Object} state application state
+ * @return {Array} Array of product objects
+ */
+const getProductsFromStateById = (productIds, state) => {
+  const stateProducts = state.product.productsById;
+  return productIds.map(id => stateProducts[id].productData);
+};
 
 /**
  * Retrieves products by id from the store.
@@ -9,24 +27,82 @@ import getProducts from './getProducts';
  */
 const getProductsById = (productIds, componentId = null) => (dispatch, getState) => {
   const state = getState();
-  const products = state.product.productsById;
+  const pipeline = pipelines.SHOPGATE_CATALOG_GET_PRODUCTS;
+  const sort = getSortOrder(state);
+  const filters = getActiveFilters(state);
 
-  // Filter out only the products that are not yet available in the store.
-  const missingIds = productIds.filter(id => shouldFetchData(products[id]));
+  const hash = generateResultHash({
+    pipeline,
+    sort,
+    productIds,
+    ...(filters && Object.keys(filters).length) && { filters },
+    ...componentId && { id: componentId },
+  }, false, false);
 
-  // Then only perform a pipeline request if there are products missing.
-  if (!missingIds.length) {
-    return;
+  const result = state.product.resultsByHash[hash];
+
+  // Stop if we don't need to get any data.
+  if (!shouldFetchData(result, 'products')) {
+    const { products } = result;
+
+    if (Array.isArray(products)) {
+      return Promise.resolve(result);
+    }
+
+    return null;
   }
 
-  dispatch(getProducts({
-    ...componentId && { id: componentId },
-    params: {
-      productIds: missingIds,
-    },
-    includeFilters: false,
-    includeSort: false,
+  // Filter out only the products that are not yet available in the store.
+  const missingIds = productIds.filter(id => shouldFetchData(state.product.productsById[id]));
+
+  dispatch(requestProducts({
+    hash,
+    productIds,
   }));
+
+  // Dispatch receiveProducts with updated hash and the cached products
+  if (!missingIds.length) {
+    const products = getProductsFromStateById(productIds, state);
+
+    return Promise.resolve({ products }).then((response) => {
+      dispatch(receiveProducts({
+        hash,
+        productIds,
+        products,
+        totalResultCount: products.length,
+        cached: true,
+      }));
+      return response;
+    });
+  }
+
+  // Then only perform a pipeline request if there are products missing.
+  const cachedIds = productIds.filter(id => !shouldFetchData(state.product.productsById[id]));
+  return new PipelineRequest(pipeline)
+    .setInput({ productIds: missingIds })
+    .dispatch()
+    .then((response) => {
+      let products = getProductsFromStateById(cachedIds, state);
+      products = products.concat(response.products);
+      dispatch(receiveProducts({
+        hash,
+        productIds,
+        products,
+        totalResultCount: products.length,
+        cached: true,
+      }));
+
+      return response;
+    })
+    .catch((error) => {
+      logger.error(error);
+      dispatch(errorProducts({
+        hash,
+        productIds,
+      }));
+
+      return error;
+    });
 };
 
 export default getProductsById;
