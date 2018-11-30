7a4fefd51e42ff2008aa54df20c236b12ae2f30f
Szymon Nowicki <szymon.nowicki@shopgate.com>
2018-11-30 11:15:13 +0100

    PWA-1388 - GMD Snackbar adjusts action color to white when accent color to black contrast is less than 4 (see https://www.w3.org/TR/WCAG20/)

--- themes/theme-gmd/components/SnackBar/components/ActionButton/style.js
+++ themes/theme-gmd/components/SnackBar/components/ActionButton/style.js
@@ -1,8 +1,11 @@
 import { css } from 'glamor';
+import color from 'color';
 import colors from 'Styles/colors';
 
+const textColorContrast = color(colors.accent).contrast(color('black'));
+
 export default css({
-  color: colors.accent,
+  color: textColorContrast > 4 ? colors.accent : 'white',
   fontWeight: 500,
   flexGrow: 1,
   textTransform: 'uppercase',
