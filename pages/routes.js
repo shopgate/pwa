ebeeddad44efad0103fc2d99479ee72f2bbda425
Richard Gorman <richardgorman@outlook.com>
2018-12-04 09:25:55 +0100

    Removed unnecessary diff

--- pages/routes.js
+++ pages/routes.js
@@ -1,13 +1,66 @@
-export { default as StartPage } from './StartPage';
-export { default as Page } from './Page';
-export { default as RootCategory } from './RootCategory';
-export { default as Category } from './Category';
-export { default as Filter } from './Filter';
-export { default as Product } from './Product';
-export { default as ProductGallery } from './ProductGallery';
-export { default as Reviews } from './Reviews';
-export { default as Cart } from './Cart';
-export { default as Favorites } from './Favorites';
-export { default as Search } from './Search';
-export { default as Login } from './Login';
-export { default as WriteReview } from './WriteReview';
+import Loadable from 'react-loadable';
+
+export const StartPage = Loadable({
+  loader: () => import('./StartPage'),
+  loading: () => null,
+});
+
+export const Page = Loadable({
+  loader: () => import('./Page'),
+  loading: () => null,
+});
+
+export const RootCategory = Loadable({
+  loader: () => import('./RootCategory'),
+  loading: () => null,
+});
+
+export const Category = Loadable({
+  loader: () => import('./Category'),
+  loading: () => null,
+});
+
+export const Filter = Loadable({
+  loader: () => import('./Filter'),
+  loading: () => null,
+});
+
+export const Product = Loadable({
+  loader: () => import('./Product'),
+  loading: () => null,
+});
+
+export const ProductGallery = Loadable({
+  loader: () => import('./ProductGallery'),
+  loading: () => null,
+});
+
+export const Reviews = Loadable({
+  loader: () => import('./Reviews'),
+  loading: () => null,
+});
+
+export const Cart = Loadable({
+  loader: () => import('./Cart'),
+  loading: () => null,
+});
+
+export const Favorites = Loadable({
+  loader: () => import('./Favorites'),
+  loading: () => null,
+});
+
+export const Search = Loadable({
+  loader: () => import('./Search'),
+  loading: () => null,
+});
+
+export const Login = Loadable({
+  loader: () => import('./Login'),
+  loading: () => null,
+});
+
+export const WriteReview = Loadable({
+  loader: () => import('./WriteReview'),
+  loading: () => null,
+});
