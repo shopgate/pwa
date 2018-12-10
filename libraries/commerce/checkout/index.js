2b59637038b1bf00a56a1bf7108555efea50db43
devbucket <florian.mueller81@gmx.de>
2018-12-10 09:46:57 -0600

    PWA-1456 Added index file to the checkout library

---
+++ libraries/commerce/checkout/index.js
@@ -0,0 +1,9 @@
+// ACTIONS
+export { default as fetchCheckoutUrl } from './actions/fetchCheckoutUrl';
+
+// CONSTANTS
+export * from './constants/index';
+export * from './constants/Pipelines';
+
+// SELECTORS
+export * from './selectors';
