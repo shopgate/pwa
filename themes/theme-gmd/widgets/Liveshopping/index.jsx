f488fed6fd0a92ad065bfca9ba712abfa05d31b3
Richard Gorman <richardgorman@outlook.com>
2019-01-03 14:02:31 +0100

    PWA-1251 Refactored Liveshopping widget

--- themes/theme-gmd/widgets/Liveshopping/index.jsx
+++ themes/theme-gmd/widgets/Liveshopping/index.jsx
@@ -1,178 +1,41 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Ellipsis from '@shopgate/pwa-common/components/Ellipsis';
-import Link from '@shopgate/pwa-common/components/Link';
-import Grid from '@shopgate/pwa-common/components/Grid';
-import { ITEM_PATH } from '@shopgate/pwa-common-commerce/product/constants/index';
-import { bin2hex } from '@shopgate/pwa-common/helpers/data';
-import Portal from '@shopgate/pwa-common/components/Portal';
-import * as portals from '@shopgate/pwa-common-commerce/category/constants/Portals';
 import ImageSlider from '@shopgate/pwa-ui-shared/ImageSlider';
-import Card from '@shopgate/pwa-ui-shared/Card';
-import DiscountBadge from '@shopgate/pwa-ui-shared/DiscountBadge';
-import Price from '@shopgate/pwa-ui-shared/Price';
-import PriceStriked from '@shopgate/pwa-ui-shared/PriceStriked';
-import ProductImage from 'Components/ProductImage';
+import Item from './components/Item';
 import connect from './connector';
-import Countdown from './components/Countdown';
 import styles from './style';
 
-/**
- * Calculates the timeout from a set of live-shopping settings. The highest 'to' timestamp
- * with an active time period (that is a time span with from <= current time <= to) is
- * considered to represent the current timeout.
- * @param {Object} liveshoppings The live-shopping settings.
- * @returns {number} the unix timestamp for the timeout or null if there is no active timeout.
- */
-const getLiveshoppingTimeout = (liveshoppings) => {
-  const now = Date.now();
-
-  const result = liveshoppings.reduce((best, item) => {
-    // Get the time span limits.
-    const from = new Date(item.from).getTime();
-    const to = new Date(item.to).getTime();
-
-    if (from <= now && to >= now) {
-      // This time span is currently active.
-      return Math.max(best, to);
-    }
-
-    return best;
-  }, null);
-
-  return result;
+const sliderStyle = {
+  indicator: {
+    bullets: styles.indicators,
+  },
 };
 
 /**
- * Creates a new product slider item.
- * @param {Object} product The product.
- * @param {string} product.id The product id.
- * @param {string} product.name The product name.
- * @param {string} product.featuredImageUrl The featured image url.
- * @param {Object} product.price The product price object.
- * @param {number} timeout The timeout of the live-shopping deal.
- * @returns {JSX}
+ * The LiveshoppingWidget component.
  */
-const createProductSliderItem = ({
-  id, name, featuredImageUrl, price,
-}, timeout) => {
-  const priceStriked = price.unitPriceStriked > 0 ? price.unitPriceStriked : price.msrp;
-
-  return (
-    <div key={id} className={styles.card} data-test-id="liveShoppingWidget">
-      <Card>
-        <Link
-          href={`${ITEM_PATH}/${bin2hex(id)}`}
-          state={{
-            title: name,
-          }}
-        >
-          <Grid>
-            <Grid.Item className={styles.imagePane}>
-              <ProductImage src={featuredImageUrl} alt={name} />
-            </Grid.Item>
-            <Grid.Item className={styles.infoPane}>
-              <div data-test-id={name}>
-                {price.discount > 0 ?
-                  <Fragment>
-                    <Portal name={portals.PRODUCT_ITEM_DISCOUNT_BEFORE} props={{ productId: id }} />
-                    <Portal name={portals.PRODUCT_ITEM_DISCOUNT} props={{ productId: id }}>
-                      <DiscountBadge
-                        text="liveshopping.discount_badge"
-                        discount={price.discount}
-                        display="big"
-                        className={styles.discountBadge}
-                      />
-                    </Portal>
-                    <Portal name={portals.PRODUCT_ITEM_DISCOUNT_AFTER} props={{ productId: id }} />
-                  </Fragment>
-                : null
-                }
-                <Ellipsis
-                  rows={2}
-                  className={styles.cardTitle}
-                >
-                  {name}
-                </Ellipsis>
-                {timeout ?
-                  <span className={styles.timer}>
-                    <Countdown to={timeout} />
-                  </span>
-                  : null
-                }
-              </div>
-              <Grid className={styles.priceGrid}>
-                {priceStriked > 0 ?
-                  <Grid.Item>
-                    <PriceStriked
-                      className={styles.priceStriked}
-                      value={priceStriked}
-                      currency={price.currency}
-                    />
-                  </Grid.Item>
-                  : null
-                }
-                <Grid.Item>
-                  <Price
-                    className={priceStriked > 0 ? styles.price : ''}
-                    unitPrice={price.unitPrice}
-                    unitPriceMin={price.unitPriceMin}
-                    currency={price.currency}
-                  />
-                </Grid.Item>
-              </Grid>
-            </Grid.Item>
-          </Grid>
-        </Link>
-      </Card>
-    </div>
-  );
-};
-
-createProductSliderItem.propTypes = {
-  featuredImageUrl: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.shape().isRequired,
-};
-
-/**
- * The live shopping (deal of the day) widget.
- */
-class LiveshoppingWidget extends Component {
+export class LiveshoppingWidget extends Component {
   /**
-   * Requests the liveshopping product data when the component is mounted.
+   * Request the product data.
    */
   componentDidMount() {
-    this.props.getLiveshoppingProducts();
+    this.props.fetchProducts();
   }
 
   /**
-   * Checks whether the component can update.
-   * @param {Object} nextProps The next component props.
-   * @return {JSX}
-   */
-  shouldComponentUpdate(nextProps) {
-    return (this.props.products.length !== nextProps.products.length);
-  }
-
-  /**
-   * Renders the component.
    * @return {JSX}
    */
   render() {
-    const items = this.props.products.slice().map((
-      product => createProductSliderItem(product, getLiveshoppingTimeout(product.liveshoppings))
-    ));
+    const { products } = this.props;
 
-    if (!items.length) {
+    if (!products.length) {
       return null;
     }
 
     return (
-      <div className={styles.cardsWrapper}>
-        <ImageSlider indicators loop classNames={{ indicator: styles.indicator }}>
-          {items}
+      <div className={styles.wrapper}>
+        <ImageSlider indicators loop classNames={sliderStyle}>
+          {products.map(id => <Item key={id} productId={id} />)}
         </ImageSlider>
       </div>
     );
@@ -180,9 +43,12 @@ class LiveshoppingWidget extends Component {
 }
 
 LiveshoppingWidget.propTypes = {
-  getLiveshoppingProducts: PropTypes.func.isRequired,
-  products: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  fetchProducts: PropTypes.func.isRequired,
+  products: PropTypes.arrayOf(PropTypes.string),
+};
+
+LiveshoppingWidget.defaultProps = {
+  products: [],
 };
 
 export default connect(LiveshoppingWidget);
-export { LiveshoppingWidget as UnwrappedLiveshoppingWidget };
---
+++ themes/theme-ios11/widgets/Liveshopping/components/Item/style.js
@@ -0,0 +1,70 @@
+import { css } from 'glamor';
+import colors from 'Styles/colors';
+import variables from 'Styles/variables';
+
+const wrapper = css({
+  padding: '0px 10px',
+});
+
+const card = css({
+  borderRadius: 10,
+}).toString();
+
+const pane = {
+  width: '50%',
+  background: colors.light,
+};
+
+const imagePane = css({
+  ...pane,
+}).toString();
+
+const infoPane = css({
+  ...pane,
+  padding: 16,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+}).toString();
+
+const priceGrid = css({
+  alignItems: 'flex-end',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+  marginTop: variables.gap.small,
+}).toString();
+
+const priceStriked = css({
+  fontSize: '0.875rem',
+}).toString();
+
+const price = css({
+  color: colors.primary,
+  fontSize: '1.25rem',
+  lineHeight: 1,
+}).toString();
+
+const cardTitle = css({
+  fontWeight: 500,
+  lineHeight: 1.25,
+  marginBottom: variables.gap.small * 0.5,
+}).toString();
+
+const timer = css({
+  fontSize: '0.875rem',
+  color: colors.primary,
+  fontStyle: 'italic',
+  fontWeight: 500,
+}).toString();
+
+export default {
+  wrapper,
+  card,
+  imagePane,
+  infoPane,
+  priceGrid,
+  priceStriked,
+  price,
+  cardTitle,
+  timer,
+};
