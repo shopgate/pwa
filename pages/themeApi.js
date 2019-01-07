2c168c8a00d9c118dbfd6a588e77568580dea5ac
Richard Gorman <richardgorman@outlook.com>
2019-01-07 14:49:39 +0100

    PWA-1513 Added Drawer theme component to ThemeContext

---
+++ pages/themeApi.js
@@ -0,0 +1,9 @@
+import AppBar from 'Components/AppBar/presets/BackBar';
+import Drawer from 'Components/Drawer';
+import View from 'Components/View';
+
+export default {
+  AppBar,
+  Drawer,
+  View,
+};
