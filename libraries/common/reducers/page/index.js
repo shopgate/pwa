bb4bb9929d4ee824937e42963c3b8c313dde77d9
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-07 12:01:25 +0100

    PWA-1266: Changed cache time of pages

--- libraries/common/reducers/page/index.js
+++ libraries/common/reducers/page/index.js
@@ -39,7 +39,7 @@ export default function pageReducer(state = {}, action) {
           title: action.config.title,
           widgets: enrichWidgets(action),
           isFetching: false,
-          expires: Date.now() + 86400000, // One day
+          expires: Date.now() + 3600000, // One hour
         },
       };
     }
