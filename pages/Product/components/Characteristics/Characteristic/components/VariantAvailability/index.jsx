df33faf80d641f6eada9d65d7e48b9f14e2ce56c
Aaron Martin <aaron.martin@shopgate.com>
2019-01-08 10:48:06 -0600

    CCP-1184: added missing portals PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY

--- pages/Product/components/Characteristics/Characteristic/components/VariantAvailability/index.jsx
+++ pages/Product/components/Characteristics/Characteristic/components/VariantAvailability/index.jsx
@@ -1,6 +1,12 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Availability from '@shopgate/pwa-ui-shared/Availability';
+import Portal from '@shopgate/pwa-common/components/Portal';
+import {
+  PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY_BEFORE,
+  PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY,
+  PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY_AFTER,
+} from '@shopgate/pwa-common-commerce/product/constants/Portals';
 import connect from './connector';
 import styles from './style';
 
@@ -21,13 +27,37 @@ class VariantAvailability extends PureComponent {
    */
   render() {
     const { availability } = this.props;
-
+    const { state, text } = availability || {};
     if (!availability) {
       return null;
     }
 
     return (
-      <Availability className={styles} state={availability.state} text={availability.text} />
+      <Fragment>
+        <Portal
+          name={PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY_BEFORE}
+          props={{
+            text,
+            state,
+          }}
+        />
+        <Portal
+          name={PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY}
+          props={{
+            text,
+            state,
+          }}
+        >
+          <Availability className={styles} state={state} text={text} />
+        </Portal>
+        <Portal
+          name={PRODUCT_VARIANT_SELECT_PICKER_AVAILABILITY_AFTER}
+          props={{
+            text,
+            state,
+          }}
+        />
+      </Fragment>
     );
   }
 }
