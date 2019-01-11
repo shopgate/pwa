5f88ef11ff333f0ffb11a7778f128758e2f99625
Richard Gorman <richardgorman@outlook.com>
2019-01-11 13:29:02 +0100

    PWA-1426 Reverted Portal name change

--- themeApi/ProductCard/components/Badge/index.jsx
+++ themeApi/ProductCard/components/Badge/index.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { Portal } from '@shopgate/pwa-common/components';
 import DiscountBadge from '@shopgate/pwa-ui-shared/DiscountBadge';
 import {
-  PRODUCT_CARD_BADGE,
-  PRODUCT_CARD_BADGE_BEFORE,
-  PRODUCT_CARD_BADGE_AFTER,
-} from '@shopgate/pwa-common-commerce/product/constants/Portals';
+  PRODUCT_ITEM_DISCOUNT,
+  PRODUCT_ITEM_DISCOUNT_BEFORE,
+  PRODUCT_ITEM_DISCOUNT_AFTER,
+} from '@shopgate/pwa-common-commerce/category/constants/Portals';
 import styles from './style';
 
 /**
@@ -17,11 +17,11 @@ function ProductCardBadge({ productId, style, value }) {
 
   return (
     <div className={styles} style={style}>
-      <Portal name={PRODUCT_CARD_BADGE_BEFORE} props={props} />
-      <Portal name={PRODUCT_CARD_BADGE} props={props}>
+      <Portal name={PRODUCT_ITEM_DISCOUNT_BEFORE} props={props} />
+      <Portal name={PRODUCT_ITEM_DISCOUNT} props={props}>
         <DiscountBadge text={`${value}%`} />
       </Portal>
-      <Portal name={PRODUCT_CARD_BADGE_AFTER} props={props} />
+      <Portal name={PRODUCT_ITEM_DISCOUNT_AFTER} props={props} />
     </div>
   );
 }
