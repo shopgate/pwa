f1d268aa2be9d0b124d02c0f6aeb123a1abb027d
Richard Gorman <richardgorman@outlook.com>
2019-01-11 11:59:40 +0100

    PWA-1426 Removed duplicate export

--- libraries/common/components/index.js
+++ libraries/common/components/index.js
@@ -1,4 +1,3 @@
 export { default as Ellipsis } from './Ellipsis';
 export { default as Portal } from './Portal';
 export { Router, Route } from '@virtuous/react-conductor';
-export { default as Portal } from './Portal';
