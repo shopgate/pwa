5220e70a6e14ac9fb8d3c75f5536c609bb015d1e
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-14 15:06:32 +0100

    PWA-1560: Added action fallbacks for backwards compatibility - added fallback for the renamed getFavorites action (now fetchFavorites) - added fallback for the renamed getFilters action (now fetchFilters)

---
+++ libraries/commerce/filter/actions/getFilters.js
@@ -0,0 +1,10 @@
+import fetchFilters from './fetchFilters';
+
+/**
+ * Retrieves the available filters for a list of products.
+ * @returns {Function} A redux thunk
+ * @deprecated
+ */
+const getFilters = fetchFilters;
+
+export default getFilters;
