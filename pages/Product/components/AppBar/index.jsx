a92aa2e1ff63b9b587ceef893ff641e98a0698c2
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-01-16 07:49:58 -0600

    Updated right prop for AppBar on Product page for iOS

--- pages/Product/components/AppBar/index.jsx
+++ pages/Product/components/AppBar/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { BackBar } from 'Components/AppBar/presets';
 import CartButton from 'Components/AppBar/components/CartButton';
+import { Portal } from '@shopgate/pwa-common/components';
+import {
+  APP_BAR_RIGHT,
+  APP_BAR_RIGHT_BEFORE,
+  APP_BAR_RIGHT_AFTER,
+} from '@shopgate/pwa-common/constants/Portals';
 import connect from './connector';
 
 /**
@@ -9,8 +15,18 @@ import connect from './connector';
  * @returns {JSX}
  */
 function ProductAppBar({ title }) {
+  const right = (
+    <Fragment key="right">
+      <Portal name={APP_BAR_RIGHT_BEFORE} />
+      <Portal name={APP_BAR_RIGHT}>
+        <CartButton />
+      </Portal>
+      <Portal name={APP_BAR_RIGHT_AFTER} />
+    </Fragment>
+  );
+
   return (
-    <BackBar title={title} right={<CartButton key="right" />} />
+    <BackBar title={title} right={right} />
   );
 }
 
