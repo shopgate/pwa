59e1073a41905372502204e64195397b9813b0ba
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-17 15:14:02 +0100

    PWA-1477: Fixed filtering of categories and search - when products are selected from the store, the related filters are now directly taken from the route state - when products are selected from the store, the current sort order is now directly taken from the route state - introduced a scrollTop method to the ViewContext which can be used when a view is just updated with fresh data, and the scroll position needs to be reset

--- themes/theme-gmd/pages/Search/components/Products/connector.js
+++ themes/theme-gmd/pages/Search/components/Products/connector.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import isEqual from 'lodash/isEqual';
 import { getProductsResult } from '@shopgate/pwa-common-commerce/product/selectors/product';
-import fetchSearchResults from '@shopgate/pwa-common-commerce/search/actions/fetchSearchResults';
+import getProducts from './actions/getProducts';
 
 /**
  * Maps the contents of the state to the component props.
@@ -13,12 +13,14 @@ const mapStateToProps = (state, props) => ({
   ...getProductsResult(state, props),
 });
 
-const mapDispatchToProps = {
-  getProducts: (searchPhrase, offset) => fetchSearchResults({
-    searchPhrase,
-    offset,
-  }),
-};
+/**
+ * Connects the dispatch function to a callable function in the props.
+ * @param {Function} dispatch The redux dispatch function.
+ * @return {Object} The extended component props.
+ */
+const mapDispatchToProps = dispatch => ({
+  getProducts: (categoryId, sort, offset) => dispatch(getProducts(categoryId, sort, offset)),
+});
 
 /**
  * Check to see if the categories have arrived.
