b8330df1cf95bb8e55ed6871fd35bb9d7313a0eb
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-17 15:14:02 +0100

    PWA-1477: Fixed filtering of categories and search - when products are selected from the store, the related filters are now directly taken from the route state - when products are selected from the store, the current sort order is now directly taken from the route state - introduced a scrollTop method to the ViewContext which can be used when a view is just updated with fresh data, and the scroll position needs to be reset

---
+++ pages/Search/components/Products/actions/getProducts.js
@@ -0,0 +1,21 @@
+import { getActiveFilters } from '@shopgate/pwa-common-commerce/filter/selectors';
+import fetchSearchResults from '@shopgate/pwa-common-commerce/search/actions/fetchSearchResults';
+
+/**
+ * @param {string} searchPhrase The search phrase to requests products for.
+ * @param {string} sort The sort order for the products to request.
+ * @param {number} offset The offset for the products to request.
+ * @return {Function} A redux thunk.
+ */
+const getProducts = (searchPhrase, sort, offset) => (dispatch, getState) => {
+  const filters = getActiveFilters(getState(), {});
+
+  dispatch(fetchSearchResults({
+    searchPhrase,
+    offset,
+    filters,
+    sort,
+  }));
+};
+
+export default getProducts;
