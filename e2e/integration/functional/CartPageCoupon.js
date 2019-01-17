f658e613598a7d213baed3e0841eaea730b2aebd
Dennis K <denniskraft87@googlemail.com>
2019-01-17 17:12:29 +0100

    adjust tests for PWA 6.1.0 and shopify

---
+++ e2e/integration/functional/CartPageCoupon.js
@@ -0,0 +1,58 @@
+import els from '../../elements/de';
+import { clearProductFromCart } from '../../helper/cart';
+import { checkForWrongCoupon } from '../../helper/coupon';
+
+describe('functional tests cart page', () => {
+  it('should add second product to cart', () => {
+    cy.visit('');
+    cy.get(els.basicCategory)
+      .last()
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+    cy.get(els.productsWithLongNamesCat)
+      .should('be.visible')
+      .last()
+      .click();
+    cy.get(els.loadingIndicator)
+      .should('not.be.visible');
+    cy.get(els.productWithVeryLongName5Name)
+      .last()
+      .should('be.visible')
+      .click();
+    cy.get(els.addToCartButton)
+      .click();
+    cy.get(els.cartButtonProductPage)
+      .last()
+      .click();
+  });
+
+  it('should check for wrong coupon', () => {
+    cy.get(els.couponFieldInput)
+      .type('wrongCoupon {enter}');
+    checkForWrongCoupon();
+  });
+
+  it('should check for right coupon', () => {
+    cy.get(els.couponFieldInput)
+      .clear()
+      .type('test1');
+    cy.get(els.couponSubmitButton)
+      .click();
+    cy.get(els.basicDialogOkButton)
+      .click();
+    cy.get('[data-test-id="subTotal: 84"]')
+      .should('be.visible');
+  });
+
+  it('should delete coupon from cart', () => {
+    cy.get(els.deleteCouponButton)
+      .click();
+    cy.get(els.deleteCouponButton)
+      .should('not.exist');
+  });
+
+  it('should check for delete product from cart', () => {
+    clearProductFromCart();
+  });
+});
