b52b4349ec871f596988c8d8ea6c678fc217bfe3
devbucket <florian.mueller81@gmx.de>
2019-01-18 08:35:22 -0600

    PWA-1518 Removed unnecessary center portals from content.

--- pages/Page/components/Content/index.jsx
+++ pages/Page/components/Content/index.jsx
@@ -5,9 +5,6 @@ import {
   PAGE_CONTENT_BEFORE,
   PAGE_CONTENT,
   PAGE_CONTENT_AFTER,
-  APP_BAR_CENTER,
-  APP_BAR_CENTER_BEFORE,
-  APP_BAR_CENTER_AFTER,
 } from '@shopgate/pwa-common/constants/Portals';
 import { PAGE_ID_INDEX } from '@shopgate/pwa-common/constants/PageIDs';
 import Widgets from '@shopgate/pwa-common/components/Widgets';
@@ -24,26 +21,10 @@ import connect from './connector';
  * @return {JSX}
  */
 function PageContent({ configs, pageId }) {
-  let center = (
-    <Fragment key="center">
-      <Portal name={APP_BAR_CENTER_BEFORE} />
-      <Portal name={APP_BAR_CENTER}>
-        <Logo key="center" />
-      </Portal>
-      <Portal name={APP_BAR_CENTER_AFTER} />
-    </Fragment>
-  );
+  let center = <Logo key="center" />;
 
   if (pageId !== PAGE_ID_INDEX) {
-    center = (
-      <Fragment key="center">
-        <Portal name={APP_BAR_CENTER_BEFORE} />
-        <Portal name={APP_BAR_CENTER}>
-          <AppBar.Title key="center" title={configs.title || ''} />
-        </Portal>
-        <Portal name={APP_BAR_CENTER_AFTER} />
-      </Fragment>
-    );
+    center = <AppBar.Title key="center" title={configs.title || ''} />;
   }
 
   return (
