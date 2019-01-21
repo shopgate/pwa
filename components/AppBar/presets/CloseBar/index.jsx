a1ae5835d5fd799619a04f7e9bb621102923df4d
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-21 11:11:44 +0100

    PWA-1518: Added additional AppBar portals

--- components/AppBar/presets/CloseBar/index.jsx
+++ components/AppBar/presets/CloseBar/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Portal } from '@shopgate/pwa-common/components';
-import { APP_BAR_CLOSE } from '@shopgate/pwa-common/constants/Portals';
+import {
+  APP_BAR_CLOSE_BEFORE,
+  APP_BAR_CLOSE,
+  APP_BAR_CLOSE_AFTER,
+} from '@shopgate/pwa-common/constants/Portals';
 import { AppBar } from '@shopgate/pwa-ui-material';
 import { CrossIcon } from '@shopgate/pwa-ui-shared';
 import DefaultBar from '../DefaultBar';
@@ -15,9 +19,13 @@ function CloseBar({ goBack, ...props }) {
   const left = <AppBar.Icon icon={CrossIcon} onClick={goBack} />;
 
   return (
-    <Portal name={APP_BAR_CLOSE}>
-      <DefaultBar left={left} right={null} {...props} />
-    </Portal>
+    <Fragment>
+      <Portal name={APP_BAR_CLOSE_BEFORE} />
+      <Portal name={APP_BAR_CLOSE}>
+        <DefaultBar left={left} right={null} {...props} />
+      </Portal>
+      <Portal name={APP_BAR_CLOSE_AFTER} />
+    </Fragment>
   );
 }
 
