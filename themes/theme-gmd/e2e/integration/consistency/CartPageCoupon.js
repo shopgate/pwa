b725efc29b9186b320f68cb5f47b6fe28711af97
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-01-30 17:36:41 +0100

    PWA-1372: Fixed GMD coupon test

--- themes/theme-gmd/e2e/integration/consistency/CartPageCoupon.js
+++ themes/theme-gmd/e2e/integration/consistency/CartPageCoupon.js
@@ -1,6 +1,30 @@
 import els from '../../elements/de';
 
 describe('AndroidGMDTest CartPageCoupons', () => {
+  it('should add second product to cart', () => {
+    cy.visit('');
+    cy.get(els.basicCategory)
+      .last()
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+    cy.get(els.productsWithLongNamesCat)
+      .should('be.visible')
+      .last()
+      .click();
+    cy.get(els.loadingIndicator)
+      .should('not.be.visible');
+    cy.get(els.productWithVeryLongName5Name)
+      .last()
+      .should('be.visible')
+      .click();
+    cy.get(els.addToCartButton)
+      .click();
+    cy.get(els.cartButtonProductPage)
+      .last()
+      .click();
+  });
+
   it('should check for couponField', () => {
     cy.get(els.couponFieldInput)
       .should('be.visible');
