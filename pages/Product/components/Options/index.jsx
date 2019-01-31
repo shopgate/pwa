4587311aec59c4bb9515e3072d5f7ea18b3823e5
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-01-31 12:12:25 +0100

    PWA-1607 Add fucntion eslint rule to force Function. Use switch instead of if statements

--- pages/Product/components/Options/index.jsx
+++ pages/Product/components/Options/index.jsx
@@ -75,33 +75,34 @@ class Options extends PureComponent {
           {(options !== null) && (
             <div data-test-id="optionsPicker">
               {options.map((option) => {
-                if (option.type === 'text') {
-                  return (
-                    <TextOption
-                      key={option.id}
-                      id={option.id}
-                      label={option.label}
-                      value={currentOptions[option.id]}
-                      info={option.info}
-                      onChange={storeSelection}
-                      required={option.required}
-                      price={option.price}
-                    />
-                  );
-                } else if (option.type === 'select') {
-                return (
-                  <Option
-                    key={option.id}
-                    label={option.label}
-                    id={option.id}
-                    items={option.items}
-                    value={currentOptions[option.id]}
-                    onChange={storeSelection}
-                  />
-                );
+                switch (option.type) {
+                  case 'text':
+                    return (
+                      <TextOption
+                        key={option.id}
+                        id={option.id}
+                        label={option.label}
+                        value={currentOptions[option.id]}
+                        info={option.info}
+                        onChange={storeSelection}
+                        required={option.required}
+                        price={option.price}
+                      />
+                    );
+                  case 'select':
+                    return (
+                      <Option
+                        key={option.id}
+                        label={option.label}
+                        id={option.id}
+                        items={option.items}
+                        value={currentOptions[option.id]}
+                        onChange={storeSelection}
+                      />
+                    );
+                  default:
+                    return null;
                 }
-                // unknown type
-                return null;
               })}
             </div>
           )}
