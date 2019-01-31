48fd752129c6f328fd2956cfc3b05bc10183dd38
devbucket <florian.mueller81@gmx.de>
2019-01-31 15:10:17 -0600

    PWA-1658 Renamed getUser to fetchUser and added backwards compatibility.

---
+++ libraries/common/actions/user/fetchUser.js
@@ -0,0 +1,41 @@
+import { PipelineRequest, logger, EACCESS } from '@shopgate/pwa-core';
+import * as pipelines from '../../constants/Pipelines';
+import * as actions from '../../action-creators/user';
+import { isUserLoggedIn } from '../../selectors/user';
+
+/**
+ * Get the current user
+ * @return {Function} A redux thunk.
+ */
+export default function fetchUser() {
+  return (dispatch, getState) => {
+    dispatch(actions.requestUser());
+
+    return new PipelineRequest(pipelines.SHOPGATE_USER_GET_USER)
+      .setTrusted()
+      .setErrorBlacklist([EACCESS])
+      .dispatch()
+      .then((user) => {
+        dispatch(actions.receiveUser(user));
+
+        // If the user's login state was incorrectly set false then set to true.
+        if (!isUserLoggedIn(getState())) {
+          dispatch(actions.toggleLoggedIn(true));
+        }
+
+        return user;
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case EACCESS:
+            dispatch(actions.toggleLoggedIn(false));
+            break;
+          default:
+            logger.error(error);
+            break;
+        }
+
+        dispatch(actions.errorUser());
+      });
+  };
+}
