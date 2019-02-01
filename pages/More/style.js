d25abdb7df319e9a955db4b2046abc83190d9111
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-02-01 11:15:51 +0100

    PWA-1546: Added props to the store info portals of the iOS More page - added a headline to the logout section of the More page on iOS

--- pages/More/style.js
+++ pages/More/style.js
@@ -1,21 +1,7 @@
 import { css } from 'glamor';
-import colors from '../../styles/colors';
 
 const list = css({
   margin: '0 20px 8px',
-});
-
-const loggedInListItem = css({
-  '&:first-child': {
-    boxShadow: `0 -4px 0 0 ${colors.darkGray}`,
-  },
-  boxShadow: `0 -1px 0 0 ${colors.darkGray}`,
-  padding: '12px 0',
-  'button&': {
-    outline: 0,
-    textAlign: 'inherit',
-    width: '100%',
-  },
 });
 
 const headline = {
@@ -24,6 +10,5 @@ const headline = {
 
 export default {
   list,
-  loggedInListItem,
   headline,
 };
