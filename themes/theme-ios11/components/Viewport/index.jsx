a59f09dd7708c04233687609ba26f9d4fcfb79b3
devbucket <florian.mueller81@gmx.de>
2019-02-11 11:42:11 -0600

    PWA-1631 Changed the way how the AppHeader DOM element is created to stay conform with React's portal API.

--- themes/theme-ios11/components/Viewport/index.jsx
+++ themes/theme-ios11/components/Viewport/index.jsx
@@ -5,25 +5,58 @@ import TabBar from 'Components/TabBar';
 import styles from './style';
 
 /**
- * The Viewport component.
- * @param {Object} props The component props.
- * @returns {JSX}
+ * The view component.
  */
-const Viewport = props => (
-  <main className={styles.viewport} role="main" itemScope itemProp="http://schema.org/MobileApplication">
-    <header className={styles.header} id="AppHeader" />
-    <section className={styles.content}>
-      {props.children}
-    </section>
-    <footer className={styles.footer} id="AppFooter">
-      <TabBar />
-    </footer>
-  </main>
-);
+class Viewport extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
 
-Viewport.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  /**
+   * @param {Object} props The component props.
+   */
+  constructor(props) {
+    super(props);
+
+    this.ref = React.createRef();
+    // Build the app header DOM element (for attaching the AppBar portal)
+    this.headerEl = document.createElement('div');
+    this.headerEl.id = 'AppHeader';
+    this.headerEl.className = styles.header;
+  }
+
+  /**
+   * Adds the app header element as the first child node.
+   */
+  componentDidMount() {
+    this.ref.current.insertBefore(this.headerEl, this.ref.current.firstChild);
+  }
+
+  /**
+   * Removes the app header element if this component unmounts.
+   */
+  componentWillUnmount() {
+    this.ref.current.removeChild(this.el);
+  }
+
+  /**
+   * @returns {React.Node}
+   */
+  render() {
+    const { children } = this.props;
+
+    return (
+      <main ref={this.ref} className={styles.viewport} role="main" itemScope itemProp="http://schema.org/MobileApplication">
+        <section className={styles.content}>
+          {children}
+        </section>
+        <footer className={styles.footer} id="AppFooter">
+          <TabBar />
+        </footer>
+      </main>
+    );
+  }
+}
 
 /**
  * @param {Object} prev The previous component props.
