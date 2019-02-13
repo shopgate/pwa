97c3497766e21ad5145e1d3eb087148588a99acb
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-02-13 13:26:21 +0100

    PWA-1687 Check issues of Browser Connector with Cypress Tests

--- e2e/helper/cart.js
+++ e2e/helper/cart.js
@@ -3,21 +3,19 @@ import els from '../elements/de';
 /**
  * Helper function that clears the Cart
  */
-export function clearProductFromCart() {
+export function clearProductsFromCart() {
   try {
     // Delete single product from cart
-    cy.visit('Cart');
-    cy.wait(500);
-    cy.get(els.contextMenu).each(() => {
-      cy.get(els.contextMenu)
-        .should('be.visible')
-        .first()
-        .click();
-      cy.get(els.contextMenuButton)
-        .contains('Entfernen')
-        .click();
-      cy.wait(1000);
+    cy.visit('cart');
+    cy.get(els.contextMenu).each(($el) => {
+      cy.window().spyAction('RECEIVE_CART', () => {
+        cy.wrap($el).should('be.visible').click();
+        cy.get(els.contextMenuButton)
+          .contains('Entfernen')
+          .click();
+      });
     });
+
     // Check for empty cart
     cy.get(els.emptyCartPlaceHolderString)
       .scrollIntoView()
