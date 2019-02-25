fb0c28010aca01a6cf2151ada4fc537db4583ce3
devbucket <florian.mueller81@gmx.de>
2019-02-25 14:30:36 -0600

    PWA-1583 Adjusted tests.

--- libraries/commerce/product/streams/index.js
+++ libraries/commerce/product/streams/index.js
@@ -55,23 +55,24 @@ export const receivedVisibleProduct$ = productReceived$.merge(cachedProductRecei
 
 export const variantDidChange$ = variantWillUpdate$
   // Take care that the stream only emits when underlying streams emit within the correct order.
-  .switchMap(() => receivedVisibleProduct$.first()
-    .switchMap((data) => {
-      const { id, baseProductId } = data.action.productData;
+  .switchMap(() =>
+    receivedVisibleProduct$.first()
+      .switchMap((data) => {
+        const { id, baseProductId } = data.action.productData;
 
-      const variantId = baseProductId !== null ? id : null;
-      const baseProduct = getBaseProduct(data.getState(), { variantId });
+        const variantId = baseProductId !== null ? id : null;
+        const baseProduct = getBaseProduct(data.getState(), { variantId });
 
-      if (baseProduct === null) {
+        if (baseProduct === null) {
         /**
          * A PDP with a variant product was opened, but the base product is not fetched yet.
          * So emitting of the stream is postponed till the data is present.
          */
-        return productReceived$;
-      }
+          return productReceived$;
+        }
 
-      return Observable.of(data);
-    }));
+        return Observable.of(data);
+      }));
 
 export const productRelationsReceived$ =
   main$.filter(({ action }) => action.type === RECEIVE_PRODUCT_RELATIONS);
