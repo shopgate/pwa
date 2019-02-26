ccd851a51898af29b3d4d5dc0bace41c08f5a487
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-02-26 14:45:55 +0100

    PWA-1725 implement sentry.io for javascript error logging for engage app

--- libraries/common/actions/router/historyPop.js
+++ libraries/common/actions/router/historyPop.js
@@ -2,7 +2,7 @@ import { ACTION_POP } from '@virtuous/conductor';
 import { navigate } from '../../action-creators/router';
 
 /**
- * @param {?Object} params The history params.
+ * @param {Object} [params={}] The history params.
  * @return {Function} The dispatched action.
  */
 export function historyPop(params = {}) {
