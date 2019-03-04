66bd29802d8eeb060f4d40f8c8c46b58e0d28c94
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-03-04 09:51:11 +0100

    PWA-1731 App Start take forever if images are not loading

--- libraries/core/commands/hideSplashScreen.js
+++ libraries/core/commands/hideSplashScreen.js
@@ -4,7 +4,7 @@ import AppCommand from '../classes/AppCommand';
  * Sends hideSplashScreen command to the app.
  */
 export default function hideSplashScreen() {
-  const command = new AppCommand();
+  const command = new AppCommand(true, false);
 
   command
     .setCommandName('hideSplashScreen')
