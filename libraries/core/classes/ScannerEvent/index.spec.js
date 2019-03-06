c4d8ee2a1445ae7e06aa32427ea990ba0d5f0c78
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-03-06 15:18:59 +0100

    PWA-1739: Simplified `ScannerEvent` jest tests.

--- libraries/core/classes/ScannerEvent/index.spec.js
+++ libraries/core/classes/ScannerEvent/index.spec.js
@@ -2,27 +2,16 @@ import ScannerEvent from './index';
 
 describe('ScannerEvent', () => {
   it('should set and return all values correctly', () => {
-    const input = {
-      scope: 'scope',
-      type: 'type',
-      payload: {
-        format: 'format',
-        code: 'code',
-      },
+    const scope = 'scope';
+    const type = 'type';
+    const payload = {
+      format: 'format',
+      code: 'code',
     };
-    const expected = {
-      scope: 'scope',
-      type: 'type',
-      payload: {
-        format: 'format',
-        code: 'code',
-      },
-    };
+    const e = new ScannerEvent(scope, type, payload);
 
-    const e = new ScannerEvent(input.scope, input.type, input.payload);
-
-    expect(e.getScope()).toStrictEqual(expected.scope);
-    expect(e.getType()).toStrictEqual(expected.type);
-    expect(e.getPayload()).toStrictEqual(expected.payload);
+    expect(e.getScope()).toStrictEqual(scope);
+    expect(e.getType()).toStrictEqual(type);
+    expect(e.getPayload()).toStrictEqual(payload);
   });
 });
