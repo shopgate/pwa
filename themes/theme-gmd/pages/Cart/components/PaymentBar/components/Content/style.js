5396e42554371d535f93654c855e33e17fa54bd2
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-03-07 13:37:45 +0100

    PWA-1599: Refactored PaymentBar component - reworked totals layout to have multiple rows instead of a column with labels and one with amounts - added Portals around the totals section an each total row - unified the PaymentBar layout between the themes (gmd has now the ios layout)

--- themes/theme-gmd/pages/Cart/components/PaymentBar/components/Content/style.js
+++ themes/theme-gmd/pages/Cart/components/PaymentBar/components/Content/style.js
@@ -1,45 +1,38 @@
 import { css } from 'glamor';
-import colors from 'Styles/colors';
-import variables from 'Styles/variables';
+import { themeConfig } from '@shopgate/pwa-common/helpers/config';
+
+const { colors, variables } = themeConfig;
+
+const wrapper = css({
+  paddingBottom: 'var(--safe-area-inset-bottom)',
+}).toString();
 
 const container = css({
   background: colors.light,
   boxShadow: '0 -2px 4px rgba(0, 0, 0, 0.1)',
+  padding: `${variables.gap.big}px`,
   lineHeight: 1.45,
-  padding: `${variables.gap.big}px ${variables.gap.small}px calc(${variables.gap.big}px + var(--safe-area-inset-bottom))`,
+  flexWrap: 'wrap',
   position: 'relative',
+  flexDirection: 'column',
   zIndex: 2,
 }).toString();
-
-const column = css({
-  padding: `0 ${variables.gap.small}px`,
-}).toString();
-
-const labelColumn = css({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-}).toString();
-
-const costsColumn = css({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-}).toString();
-
-const buttonColumn = costsColumn;
 
 const checkoutButton = css({
   display: 'flex',
   justifyContent: 'flex-end',
-  marginTop: 1,
+}).toString();
+
+const checkoutButtonContainer = css({
+  background: colors.light,
+  padding: `0 ${variables.gap.big}px 10px ${variables.gap.big}px`,
+  position: 'relative',
+  zIndex: 2,
 }).toString();
 
 export default {
+  wrapper,
   container,
-  column,
-  labelColumn,
-  costsColumn,
-  buttonColumn,
   checkoutButton,
+  checkoutButtonContainer,
 };
--- themes/theme-ios11/pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/index.jsx
+++ themes/theme-ios11/pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/index.jsx
@@ -1,45 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { LoadingContext } from '@shopgate/pwa-common/providers/';
-import { CART_PATH } from '@shopgate/pwa-common-commerce/cart/constants';
-import I18n from '@shopgate/pwa-common/components/I18n';
-import connect from './connector';
-import styles from './style';
+import React, { Fragment } from 'react';
+import Portal from '@shopgate/pwa-common/components/Portal';
+import {
+  CART_PAYMENT_BAR_TOTALS_SHIPPING,
+  CART_PAYMENT_BAR_TOTALS_SHIPPING_BEFORE,
+  CART_PAYMENT_BAR_TOTALS_SHIPPING_AFTER,
+} from '@shopgate/pwa-common-commerce/cart/constants/Portals';
+import portalProps from '../../totalsPortalProps';
+import TotalRow from '../TotalRow';
+import Label from './components/Label';
+import Amount from './components/Amount';
 
 /**
- * The ShippingCosts component.
- * @param {Object} props The component props.
- * @return {JSX}
+ * @returns {JSX}
  */
-const ShippingCosts = ({ currency, isDisabled, value }) => {
-  if (value === null) {
-    return (
-      <div className={styles.shippingInfoValue} data-test-id="noShippingPrice">&nbsp;</div>
-    );
-  }
+const ShippingCosts = () => (
+  <Fragment>
+    <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_BEFORE} props={portalProps} />
+    <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING} props={portalProps}>
+      <TotalRow>
+        <Label />
+        <Amount />
+      </TotalRow>
+    </Portal>
+    <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_AFTER} props={portalProps} />
+  </Fragment>
+);
 
-  return (
-    <div className={`${styles.shippingInfoValue} ${isDisabled ? styles.disabled : ''}`}>
-      {!value && <I18n.Text string="shipping.free_short" />}
-      {!!value && <I18n.Price price={value} currency={currency} />}
-    </div>
-  );
-};
-
-ShippingCosts.propTypes = {
-  currency: PropTypes.string.isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-  value: PropTypes.number,
-};
-
-ShippingCosts.defaultProps = {
-  value: null,
-};
-
-export default connect(props => (
-  <LoadingContext.Consumer>
-    {({ isLoading }) => (
-      <ShippingCosts {...props} isDisabled={isLoading(CART_PATH)} />
-    )}
-  </LoadingContext.Consumer>
-));
+export default ShippingCosts;
