d23e36d4788ee5e755ff9ba769d8a89aa81b8873
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-03-15 10:06:03 +0100

    PWA-1739: WIP Scanner UI (GMD).

--- themes/theme-gmd/components/NavDrawer/components/Main/components/ScannerButton/index.jsx
+++ themes/theme-gmd/components/NavDrawer/components/Main/components/ScannerButton/index.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { isVersionAtLeast } from '@shopgate/pwa-core/helpers/version';
 import {
-  SCANNER_SCOPE_DEFAULT_BARCODE,
+  SCANNER_SCOPE_DEFAULT,
   SCANNER_TYPE_BARCODE,
 } from '@shopgate/pwa-core/constants/Scanner';
+import { SCANNER_MIN_APP_LIB_VERSION } from '@shopgate/pwa-core/classes/Scanner';
 import {
   NAV_MENU_SCANNER,
   NAV_MENU_SCANNER_AFTER,
@@ -11,27 +13,32 @@ import {
 } from '@shopgate/pwa-common/constants/Portals';
 import { scannerPath } from '@shopgate/pwa-common/constants/RoutePaths';
 import Portal from '@shopgate/pwa-common/components/Portal';
+// TODO SCANNER: replace BarcodeScannerIcon with new svg from ticket
 import BarcodeScannerIcon from '@shopgate/pwa-ui-shared/icons/BarcodeScannerIcon';
 import { NavDrawer } from '@shopgate/pwa-ui-material';
-import connect from '../../../../connector';
+import clientConnect from '@shopgate/pwa-ui-shared/ClientInformation/connector';
+import navDrawerConnect from '../../../../connector';
 import portalProps from '../../../../portalProps';
 
 const LABEL = 'navigation.scanner';
 
 /**
  * @param {Function} props.navigate The navigate action.
+ * @param {Function} props.libVersion The lib version the app is currently running on.
  * @returns {JSX}
  */
-const ScannerButton = ({ navigate }) => (
+const ScannerButton = ({ navigate, libVersion }) => (
   <Fragment>
     <Portal name={NAV_MENU_SCANNER_BEFORE} props={portalProps} />
     <Portal name={NAV_MENU_SCANNER} props={portalProps}>
-      <NavDrawer.Item
-        label={LABEL}
-        icon={BarcodeScannerIcon}
-        onClick={navigate(scannerPath(SCANNER_SCOPE_DEFAULT_BARCODE, SCANNER_TYPE_BARCODE), LABEL)}
-        testId="navDrawerScannerButton"
-      />
+      { isVersionAtLeast(SCANNER_MIN_APP_LIB_VERSION, libVersion) &&
+        <NavDrawer.Item
+          label={LABEL}
+          icon={BarcodeScannerIcon}
+          onClick={navigate(scannerPath(SCANNER_SCOPE_DEFAULT, SCANNER_TYPE_BARCODE), LABEL)}
+          testId="navDrawerScannerButton"
+        />
+      }
     </Portal>
     <Portal name={NAV_MENU_SCANNER_AFTER} props={portalProps} />
   </Fragment>
@@ -39,6 +46,11 @@ const ScannerButton = ({ navigate }) => (
 
 ScannerButton.propTypes = {
   navigate: PropTypes.func.isRequired,
+  libVersion: PropTypes.string,
 };
 
-export default connect(ScannerButton);
+ScannerButton.defaultProps = {
+  libVersion: '1.0',
+};
+
+export default clientConnect(navDrawerConnect(ScannerButton));
