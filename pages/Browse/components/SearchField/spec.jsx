0c0beab23d625b270e34110d0d8773906ad3b18c
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-03-19 15:22:18 +0100

    PWA-1739 Finished scanner page implementation on iOS theme

--- pages/Browse/components/SearchField/spec.jsx
+++ pages/Browse/components/SearchField/spec.jsx
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { createMockStore } from '@shopgate/pwa-common/store';
+import {
+  SCANNER_SCOPE_DEFAULT,
+  SCANNER_TYPE_BARCODE,
+} from '@shopgate/pwa-core/constants/Scanner';
+import { scannerPath } from '@shopgate/pwa-common/constants/RoutePaths';
 import { historyPush } from '@shopgate/pwa-common/actions/router';
+import { hasScannerSupport } from '@shopgate/pwa-common/selectors/client';
 import SuggestionList from './components/SuggestionList';
+import styles from './style';
 import SearchField from './index';
 
 jest.mock('@shopgate/pwa-core/classes/AppCommand');
@@ -12,6 +19,23 @@ jest.mock('@shopgate/pwa-common/actions/router', () => ({
   historyPush: jest.fn().mockReturnValue({ type: 'FOO' }),
 }));
 jest.mock('./components/SuggestionList');
+
+jest.mock('@shopgate/pwa-common/selectors/client', () => ({
+  hasScannerSupport: jest.fn().mockReturnValue(true),
+}));
+
+let mockedHasNoScanner = false;
+jest.mock('@shopgate/pwa-common/helpers/config', () => ({
+  themeConfig: {
+    colors: {
+      accent: '',
+    },
+    variables: {
+      gap: {},
+    },
+  },
+  get hasNoScanner() { return mockedHasNoScanner; },
+}));
 
 const store = createMockStore();
 
@@ -24,22 +48,46 @@ const mockContext = {
   },
 };
 
-describe('<Content />', () => {
-  it('should render with intial search query', () => {
-    const wrapper = mount((
-      <Provider store={store}>
-        <SearchField pageId="1234" query="foo" />
-      </Provider>), mockContext);
+/**
+ * @return {JSX}
+ */
+const createWrapper = () => mount((
+  <Provider store={store}>
+    <SearchField pageId="1234" query="foo" />
+  </Provider>), mockContext);
 
+describe('<Content />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockedHasNoScanner = false;
+  });
+
+  it('should render with initial search query', () => {
+    const wrapper = createWrapper();
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('input').prop('value')).toEqual('foo');
+    expect(wrapper.find(`button.${styles.scannerIcon}`)).toExist();
+  });
+
+  it('should render with the scanner icon when the scanner is enabled and supported', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.find(`button.${styles.scannerIcon}`)).toExist();
+  });
+
+  it('should not render with the scanner icon when the scanner is not supported', () => {
+    hasScannerSupport.mockReturnValueOnce(false);
+    const wrapper = createWrapper();
+    expect(wrapper.find(`button.${styles.scannerIcon}`)).not.toExist();
+  });
+
+  it('should not render with the scanner icon when the scanner is not enabled', () => {
+    mockedHasNoScanner = true;
+    const wrapper = createWrapper();
+    expect(wrapper.find(`button.${styles.scannerIcon}`)).not.toExist();
   });
 
   it('should show suggestions when focused', () => {
-    const wrapper = mount((
-      <Provider store={store}>
-        <SearchField pageId="1234" query="foo" />
-      </Provider>), mockContext);
+    const wrapper = createWrapper();
 
     // Suggestion should not be visible when blured.
     expect(wrapper.find(SuggestionList).length).toEqual(0);
@@ -52,13 +100,11 @@ describe('<Content />', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find(SuggestionList).length).toEqual(1);
     expect(wrapper.find(SuggestionList).prop('searchPhrase')).toEqual('foo');
+    expect(wrapper.find(`button.${styles.scannerIcon}`)).not.toExist();
   });
 
   it('should submit search', () => {
-    const wrapper = mount((
-      <Provider store={store}>
-        <SearchField pageId="1234" query="foo" />
-      </Provider>), mockContext);
+    const wrapper = createWrapper();
 
     // Change serach and submit.
     wrapper.find('input').simulate('change', { target: { value: 'foo bar' } });
@@ -67,6 +113,15 @@ describe('<Content />', () => {
 
     expect(historyPush).toHaveBeenCalledWith({
       pathname: '/search?s=foo%20bar',
+    });
+  });
+
+  it('should open the scanner', () => {
+    const wrapper = createWrapper();
+    wrapper.find(`button.${styles.scannerIcon}`).simulate('click');
+    expect(historyPush).toHaveBeenCalledWith({
+      pathname: scannerPath(SCANNER_SCOPE_DEFAULT, SCANNER_TYPE_BARCODE),
+      title: 'navigation.scanner',
     });
   });
 });
