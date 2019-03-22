745df72bc80f2da3f8c034c70f969b5e60a5b59b
devbucket <flomueller1981@gmail.com>
2019-03-22 13:13:10 -0500

    PWA-1690 Add shipping hint from configuration and apply all changes to the iOS theme.

--- themes/theme-ios11/pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/components/Label/index.jsx
+++ themes/theme-ios11/pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/components/Label/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import { LoadingContext } from '@shopgate/pwa-common/providers/';
 import { CART_PATH } from '@shopgate/pwa-common-commerce/cart/constants';
+import CartContext from 'Pages/Cart/context';
 import styles from './style';
 import connect from './connector';
 
@@ -13,8 +14,20 @@ import connect from './connector';
  */
 const ShippingCostsLabel = ({ isDisabled, shipping }) => (
   <div className={`${styles.shippingInfo} ${isDisabled ? styles.disabled : ''}`} data-test-id="shippingLabel">
-    <I18n.Text string={shipping === null ? 'shipping.unknown' : 'titles.shipping'} />
-    {shipping !== null ? ':' : ''}
+    <CartContext.Consumer>
+      {({ shipping: { unavailable } }) => {
+        if (shipping === null && unavailable) {
+          return <span>{unavailable}</span>;
+        }
+
+        return (
+          <React.Fragment>
+            <I18n.Text string={shipping === null ? 'shipping.unknown' : 'titles.shipping'} />
+            {shipping !== null ? ':' : ''}
+          </React.Fragment>
+        );
+      }}
+    </CartContext.Consumer>
   </div>
 );
 
