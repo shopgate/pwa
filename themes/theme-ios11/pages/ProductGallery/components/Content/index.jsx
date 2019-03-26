98a628dbb0457bf2a877ed261da3a3da0632cd7b
devbucket <flomueller1981@gmail.com>
2019-03-26 13:31:46 -0500

    PWA-1198 Added adaption of GMD to iOS.

--- themes/theme-ios11/pages/ProductGallery/components/Content/index.jsx
+++ themes/theme-ios11/pages/ProductGallery/components/Content/index.jsx
@@ -46,7 +46,8 @@ const ProductGalleryContent = ({ initialSlide, images }) => {
         className={styles.slider}
         initialSlide={initialSlide}
         indicators
-        loop
+        loop={images.length > 1}
+        disabled={images.length === 1}
         zoom={zoom}
       >
         {imagesByIndex.map(imagesInIndex => (
