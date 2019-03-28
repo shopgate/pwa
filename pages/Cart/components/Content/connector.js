d3c6b8813e332ab03eebbf978104488a66745220
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-03-28 18:16:02 +0100

    PWA-1690 Use better config and ui-shared TotalLine component

--- pages/Cart/components/Content/connector.js
+++ pages/Cart/components/Content/connector.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { isUserLoggedIn } from '@shopgate/pwa-common/selectors/user';
 import { getCartItems, getCartMessages } from '@shopgate/pwa-common-commerce/cart/selectors';
 
 /**
@@ -7,6 +8,7 @@ import { getCartItems, getCartMessages } from '@shopgate/pwa-common-commerce/car
  * @return {Object} The extended component props.
  */
 const mapStateToProps = state => ({
+  isUserLoggedIn: isUserLoggedIn(state),
   cartItems: getCartItems(state),
   messages: getCartMessages(state),
 });
--- pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/index.jsx
+++ pages/Cart/components/PaymentBar/components/Content/components/ShippingCosts/index.jsx
@@ -1,36 +1,45 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import Portal from '@shopgate/pwa-common/components/Portal';
 import {
   CART_PAYMENT_BAR_TOTALS_SHIPPING,
   CART_PAYMENT_BAR_TOTALS_SHIPPING_BEFORE,
   CART_PAYMENT_BAR_TOTALS_SHIPPING_AFTER,
+  getShippingLine,
 } from '@shopgate/pwa-common-commerce/cart';
 import CartContext from 'Pages/Cart/context';
 import portalProps from '../../totalsPortalProps';
 import TotalRow from '../TotalRow';
-import Label from './components/Label';
-import Amount from './components/Amount';
+import connect from './connector';
 
 /**
  * @returns {JSX}
  */
-const ShippingCosts = () => (
+const ShippingCosts = ({ shippingCost }) => (
   <CartContext.Consumer>
-    {({ shipping: { show } }) => (
-      <Fragment>
-        <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_BEFORE} props={portalProps} />
-        <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING} props={portalProps}>
-          {show !== false && (
-            <TotalRow>
-              <Label />
-              <Amount />
-            </TotalRow>
-          )}
-        </Portal>
-        <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_AFTER} props={portalProps} />
-      </Fragment>
-    )}
+    {({ isUserLoggedIn, config }) => {
+      const shippingLine = getShippingLine(config, isUserLoggedIn, shippingCost);
+
+      return (
+        <Fragment>
+          <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_BEFORE} props={portalProps} />
+          <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING} props={portalProps}>
+            {shippingLine && (
+              <TotalRow>
+                <TotalRow.Label label={shippingLine.label} hasAmount={!!shippingLine.amount} />
+                <TotalRow.Amount />
+              </TotalRow>
+            )}
+          </Portal>
+          <Portal name={CART_PAYMENT_BAR_TOTALS_SHIPPING_AFTER} props={portalProps} />
+        </Fragment>
+      );
+    }}
   </CartContext.Consumer>
 );
 
-export default ShippingCosts;
+ShippingCosts.propTypes = {
+  shippingCost: PropTypes.number.isRequired,
+};
+
+export default connect(ShippingCosts);
