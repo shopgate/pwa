8d1cbd0e013baac75b666fb2e108420c56c34e3b
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-04-02 12:04:05 +0200

    PWA-1786 Not possible to implement product options implementation from extension level

--- themes/theme-ios11/themeApi/index.js
+++ themes/theme-ios11/themeApi/index.js
@@ -4,6 +4,7 @@ import ProductSlider from 'Components/ProductSlider';
 import View from 'Components/View';
 import TabBar from 'Components/TabBar';
 import { ProductContext } from '../pages/Product/context';
+import { TextOption, SelectOption, PriceDifference } from '../pages/Product/components/Options';
 import ProductCard from './ProductCard';
 
 export default {
@@ -13,6 +14,9 @@ export default {
   ProductSlider,
   View,
   TabBar,
+  TextOption,
+  SelectOption,
+  PriceDifference,
   contexts: {
     ProductContext,
   },
