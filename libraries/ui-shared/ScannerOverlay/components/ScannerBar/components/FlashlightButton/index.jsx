019c8f35e8813aa5fe03fd92169cf8244da877f8
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-04-16 14:30:55 +0200

    PWA-1806: Replaced scanner flash icon for disabled flashlight

--- libraries/ui-shared/ScannerOverlay/components/ScannerBar/components/FlashlightButton/index.jsx
+++ libraries/ui-shared/ScannerOverlay/components/ScannerBar/components/FlashlightButton/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ToggleIcon from '@shopgate/pwa-ui-shared/ToggleIcon';
 import FlashOnIcon from '@shopgate/pwa-ui-shared/icons/FlashOnIcon';
-import FlashOffIcon from '@shopgate/pwa-ui-shared/icons/FlashOffIcon';
+import FlashDisabledIcon from '@shopgate/pwa-ui-shared/icons/FlashDisabledIcon';
 import { SCANNER_FLASH } from '@shopgate/pwa-common-commerce/scanner/constants/Portals';
 import SurroundPortals from '@shopgate/pwa-common/components/SurroundPortals';
 import styles from './style';
@@ -24,7 +24,7 @@ const FlashlightButton = ({
       <ToggleIcon
         on={flashlightState}
         onIcon={<FlashOnIcon className={styles.icon} />}
-        offIcon={<FlashOffIcon className={styles.icon} />}
+        offIcon={<FlashDisabledIcon className={styles.icon} />}
       />
     </button>
   </SurroundPortals>
