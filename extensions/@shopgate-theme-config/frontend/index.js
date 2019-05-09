d0a26901d8945866d06331fd30f5bce349c2682b
devbucket <flomueller1981@gmail.com>
2019-05-09 10:04:15 -0500

    PWA-1913 removed beta flag from theme extension config.

--- extensions/@shopgate-theme-config/frontend/index.js
+++ extensions/@shopgate-theme-config/frontend/index.js
@@ -1,13 +1,11 @@
 import defaultsDeep from 'lodash/defaultsDeep';
-import appConfig from '@shopgate/pwa-common/helpers/config';
+import appConfig, { themeName } from '@shopgate/pwa-common/helpers/config';
 import configGmd from './config-gmd';
 import configIos from './config-ios';
 
-const { userAgent } = window.navigator;
-
 appConfig.beta = true;
 
-if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+if (themeName.includes('ios')) {
   appConfig.theme = defaultsDeep(configIos, (appConfig.theme || {}));
 } else {
   appConfig.theme = defaultsDeep(configGmd, (appConfig.theme || {}));
