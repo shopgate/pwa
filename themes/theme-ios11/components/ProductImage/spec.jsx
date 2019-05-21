6caf7d562b2cd89d98674677fbd429102b48a815
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-05-21 18:03:49 +0200

    PWA-1950 Improved extensibility of ProductImages - added portals - added app config to turn off inner shadows of ProductImages within the iOS theme

--- themes/theme-ios11/components/ProductImage/spec.jsx
+++ themes/theme-ios11/components/ProductImage/spec.jsx
@@ -2,15 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Image from '@shopgate/pwa-common/components/Image';
 import Placeholder from '@shopgate/pwa-ui-shared/icons/PlaceholderIcon';
+import styles from './style';
 import ProductImage from './index';
 
+let mockProductImageShadow;
+jest.mock('@shopgate/pwa-common/helpers/config', () => ({
+  get productImageShadow() { return mockProductImageShadow; },
+  themeConfig: {
+    colors: {
+      light: '#fff',
+    },
+  },
+}));
+
 describe('<ProductImage />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockProductImageShadow = true;
+  });
+
   it('should render a placeholder if no src prop is provided', () => {
     const wrapper = shallow(<ProductImage />);
 
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find(Image).length).toBe(0);
     expect(wrapper.find(Placeholder).length).toBe(1);
+    expect(wrapper.find(`.${styles.innerShadow}`).length).toBe(1);
   });
 
   it('should render the image without a placeholder', () => {
@@ -19,5 +36,22 @@ describe('<ProductImage />', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find(Image).length).toBe(1);
     expect(wrapper.find(Placeholder).length).toBe(0);
+    expect(wrapper.find(`.${styles.innerShadow}`).length).toBe(2);
+  });
+
+  it('should not apply an inner shadow to the placeholder if turned off via the app config', () => {
+    mockProductImageShadow = false;
+    const wrapper = shallow(<ProductImage />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(`.${styles.innerShadow}`).length).toBe(0);
+  });
+
+  it('should not apply an inner shadow to the image if turned off via the app config', () => {
+    mockProductImageShadow = false;
+    const wrapper = shallow(<ProductImage src="http://placehold.it/300x300" />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(`.${styles.innerShadow}`).length).toBe(0);
   });
 });
