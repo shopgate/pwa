22750a961d9bc4cfca52d4842d3551487e4c391c
devbucket <flomueller1981@gmail.com>
2019-05-22 10:50:25 -0500

    PWA-1987 Removed not existing properties.

--- libraries/common/helpers/config/theme.js
+++ libraries/common/helpers/config/theme.js
@@ -20,14 +20,14 @@ export function getThemeConfig(appConfig) {
     return themeConfig;
   }
 
-  const { colors = {}, theme: { styles: { globals = {} } = {} } = {} } = appConfig;
+  const { colors = {}, theme = {} } = appConfig;
 
   const oldTheme = process.env.THEME_CONFIG || defaultConfig;
 
   return {
-    font: globals.font,
+    font: theme.font,
     colors: {
-      ...globals.colors,
+      ...theme.colors,
       ...colors,
       /**
        * The SDK creates some colors dynamically and populates them via the THEME_CONFIG.
@@ -37,7 +37,7 @@ export function getThemeConfig(appConfig) {
     },
     variables: {
       ...(oldTheme.variables || {}),
-      materialShadow: globals.variables.baseShadow,
+      materialShadow: theme.variables.baseShadow,
     },
   };
 }
