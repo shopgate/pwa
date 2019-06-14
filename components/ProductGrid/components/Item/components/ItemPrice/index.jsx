ac8f600696ea6a8c44d8c65d2d9452fdfca2607e
devbucket <flomueller1981@gmail.com>
2019-06-14 08:05:08 -0500

    PWA-1996 Added Relations Slider to the product feature in engage.

--- components/ProductGrid/components/Item/components/ItemPrice/index.jsx
+++ components/ProductGrid/components/Item/components/ItemPrice/index.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Portal from '@shopgate/pwa-common/components/Portal';
+import { Portal } from '@shopgate/engage/components';
 import {
   PRODUCT_ITEM_PRICE,
   PRODUCT_ITEM_PRICE_AFTER,
   PRODUCT_ITEM_PRICE_BEFORE,
-} from '@shopgate/pwa-common-commerce/category/constants/Portals';
-import ProductGridPrice from 'Components/ProductGridPrice';
+} from '@shopgate/engage/category';
+import { ProductGridPrice } from '@shopgate/engage/product';
 
 /**
  * The item price component.
