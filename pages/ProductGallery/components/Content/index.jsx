514bb345869b626b8edb5217045f16d5bdf7b6b3
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-06-18 16:05:01 +0200

    PWA-2051 Implement featuredMedia for Engage: PLP, PDP, Gallery

--- pages/ProductGallery/components/Content/index.jsx
+++ pages/ProductGallery/components/Content/index.jsx
@@ -1,31 +1,27 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import connect from './connector';
-
-const ImageSlider = lazy(() => import('./components/ImagesSlider'));
-const MediaSlider = lazy(() => import('./components/MediaSlider'));
+import { isBeta } from '@shopgate/engage/core';
+import MediaSlider from './components/MediaSlider';
+import ImageSlider from './components/ImagesSlider';
 
 /**
  * The product media component.
- * @param {boolean} hasMedia hasMedia
  * @param {string} productId productId
  * @param {number} initialSlide initialSlide
  * @returns {JSX}
  */
-const Content = ({ hasMedia, productId, initialSlide }) => (
-  <Suspense fallback={<div />}>
-    {
-      hasMedia
+const Content = ({ productId, initialSlide }) => (
+  <Fragment>
+    {isBeta()
       ? <MediaSlider productId={productId} initialSlide={initialSlide} />
       : <ImageSlider productId={productId} initialSlide={initialSlide} />
     }
-  </Suspense>
+  </Fragment>
 );
 
 Content.propTypes = {
-  hasMedia: PropTypes.bool.isRequired,
   initialSlide: PropTypes.number.isRequired,
   productId: PropTypes.string.isRequired,
 };
 
-export default connect(Content);
+export default Content;
