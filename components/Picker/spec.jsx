abf8e54d836964c50c1d7f3053a8b1e0b95c8b12
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-06-19 14:30:38 +0200

    PWA-1981 Improved accessibility of the GMD product pages

--- components/Picker/spec.jsx
+++ components/Picker/spec.jsx
@@ -11,9 +11,7 @@ jest.mock('Components/View/context', () => ({
 describe('<Picker />', () => {
   it('should render the picker', () => {
     const label = 'Picker label';
-
     const wrapper = mount(<Picker label={label} />);
-
     expect(wrapper).toMatchSnapshot();
   });
 });
