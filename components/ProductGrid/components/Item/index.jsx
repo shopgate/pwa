3aae7668d9bb94cce444c1febee40aafb64856e0
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-06-19 09:11:42 +0200

    PWA-1981 Fixed a screen reader navigation bug on product lists

--- components/ProductGrid/components/Item/index.jsx
+++ components/ProductGrid/components/Item/index.jsx
@@ -21,7 +21,6 @@ const Item = ({ product, display }) => (
     href={`${ITEM_PATH}/${bin2hex(product.id)}`}
     className={styles}
     state={{ title: product.name }}
-    tabIndex={-1}
   >
     <ItemImage
       productId={product.id}
