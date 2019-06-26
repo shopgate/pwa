e6af461945ee35264b0110ffbd99dcc06cc01964
Frithjof Klös <fkloes@googlemail.com>
2019-06-26 23:25:09 +0200

    PWA-2073 Fixed wrong page insets on Android devices

--- components/Viewport/style.js
+++ components/Viewport/style.js
@@ -1,4 +1,4 @@
-import { css } from 'glamor';
+import { css, insert } from 'glamor';
 import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
 const { colors } = themeConfig;
@@ -30,13 +30,30 @@ export const updatePageInsets = (pageInsets) => {
   /**
    * Try to use environment variables for the insets. On Android those variables are not available,
    * on iOS devices without notch their value is 0px. To cover these different environments,
-   * the usage of the max() function implements a fallback mechanism, which is based on the values
-   * from the device constants.
+   * we need to provide two different strategies with the help of media queries.
+   *
+   * Android doesn't support the max() function, but we can just use the insets from the params
+   * (they will always be 0px anyway). On iOS the page insets from the params will always contain
+   * values > 0px, but on notch devices the required insets might need to be bigger.
+   * Since all iOS versions which are shipped with notch iPhones support max(), we can use it to
+   * determine the appropriate value for the css variables which can be used in the Engage css
+   * when insets need to be considered.
    */
-  css.global(':root', {
-    '--safe-area-inset-top': `max(${safeAreaInsetTop}px, env(safe-area-inset-top))`,
-    '--safe-area-inset-bottom': `max(${safeAreaInsetBottom}px, env(safe-area-inset-bottom))`,
-  });
+  css.insert(`
+    @supports not (padding: max(0px)) {
+      :root {
+        --safe-area-inset-top: ${safeAreaInsetTop}px;
+        --safe-area-inset-bottom: ${safeAreaInsetBottom}px;
+      }
+    }
+
+    @supports(padding: max(0px)) {
+      :root {
+        --safe-area-inset-top: max(${safeAreaInsetTop}px, env(safe-area-inset-top));
+        --safe-area-inset-bottom: max(${safeAreaInsetBottom}px, env(safe-area-inset-bottom));
+      }
+    }
+  `);
 };
 
 /**
