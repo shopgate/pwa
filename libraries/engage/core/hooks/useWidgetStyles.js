2cd4d30c7d6e4a71f8bef1e8dfd716d91cb175d4
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-06-26 16:08:13 +0200

    Removed redundant 'useSettings' file and added an index parameter to the 'useWidgetStyles' helper

--- libraries/engage/core/hooks/useWidgetStyles.js
+++ libraries/engage/core/hooks/useWidgetStyles.js
@@ -3,9 +3,10 @@ import { useWidgetConfig } from './useWidgetConfig';
 /**
  * Retrieves the styles for a specific widget by its ID.
  * @param {string} widgetId The ID of the widget to look for.
+ * @param {number|undefined} [index] The optional index of the widget.
  * @returns {Object}
  */
-export function useWidgetStyles(widgetId) {
-  const { styles = {} } = useWidgetConfig(widgetId);
+export function useWidgetStyles(widgetId, index) {
+  const { styles = {} } = useWidgetConfig(widgetId, index);
   return styles;
 }
