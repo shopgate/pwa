1e261db30fdf9266c303ae4357254db3a5337db2
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-06-27 14:31:23 +0200

    PWA-2080: Changed shopify-webcheckout url logic to accept a full domain over the alias for the web-login.

--- libraries/webcheckout/selectors/index.js
+++ libraries/webcheckout/selectors/index.js
@@ -13,10 +13,23 @@ export const hasShopifyCheckout = () => appConfig.webCheckoutShopify !== null;
 export const getShopifyCheckout = () => appConfig.webCheckoutShopify;
 
 /**
- * Returns the aliased Shopify URL.
+ * Returns the Shopify URL based on the given domain or alias.
  * @returns {string}
  */
-export const getShopifyUrl = () => `https://${getShopifyCheckout().alias}.myshopify.com`;
+export const getShopifyUrl = () => {
+  // Prioritize the "domain" property over alias, if it exists.
+  let url = typeof getShopifyCheckout().domain === 'string'
+    ? getShopifyCheckout().domain.replace(/\/+$/g, '')
+    : `https://${getShopifyCheckout().alias}.myshopify.com`;
+
+  // Add https protocol if none is set, yet.
+  if (!url.match(/^https:\/\/.*/g) && !url.match(/^http:\/\/.*/g)) {
+    // Cut out "://" or "//" from the beginning. E.g.: "//www.myshop.com" or "://www.myshop.com"
+    url = `https://${url.replace(/^[:/]*/g, '')}`;
+  }
+
+  return url;
+};
 
 /**
  * Checks if a configuration for Shopify is available
