d2c8b44606ba928a22f72f56f0479bb505c3c670
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-07-01 20:41:08 +0200

    PWA-2079: Small JSDoc addition in `usePageSettings`

--- libraries/engage/core/hooks/usePageSettings.js
+++ libraries/engage/core/hooks/usePageSettings.js
@@ -3,7 +3,7 @@ import { getPageSettings } from '../config/getPageSettings';
 
 /**
  * Retrieves the settings for the current page. If a key is given, inheritance is enabled for it.
- * @param {string} [key] Optional key to read and inherit from theme settings
+ * @param {string} [key=null] Optional key to read and inherit from theme settings
  * @returns {Object}
  */
 export function usePageSettings(key = null) {
