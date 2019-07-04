b68b755e02b18e02e2b6086511b91964450a2e7c
Szymon Nowicki <sznowicki@eskalacja.com>
2019-07-04 10:26:34 +0200

    PWA-2042 - start page renders immediately when React app is ready, doesn't wait for window.onload

--- themes/theme-ios11/pages/StartPage/index.jsx
+++ themes/theme-ios11/pages/StartPage/index.jsx
@@ -1,44 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import View from 'Components/View';
 import { PAGE_ID_INDEX } from '@shopgate/pwa-common/constants/PageIDs';
 import PageContent from '../Page/components/Content';
 
-let windowLoaded = false;
-
 /**
  * @returns {JSX}
  */
-class StartPage extends Component {
-  state = { content: windowLoaded };
-
-  /**
-   * Component did mount callback
-   */
-  componentDidMount() {
-    if (!windowLoaded) {
-      window.addEventListener('load', this.onWindowLoad);
-    }
-  }
-
-  /**
-   * Listener for window.load event
-   */
-  onWindowLoad = () => {
-    // With small timeout to let window commit loaded state
-    setTimeout(() => this.setState({ content: true }), 50);
-    windowLoaded = true;
-  }
-
-  /**
-   * @returns {JSX}
-   */
-  render() {
-    return (
-      <View>
-        {this.state.content && <PageContent pageId={PAGE_ID_INDEX} />}
-      </View>
-    );
-  }
-}
+const StartPage = () => (
+  <View>
+    <PageContent pageId={PAGE_ID_INDEX} />
+  </View>
+);
 
 export default StartPage;
