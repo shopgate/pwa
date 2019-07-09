a93e0d1ab62f2a52407f99b283c0287569f36a0c
devbucket <flomueller1981@gmail.com>
2019-07-09 13:56:59 -0500

    PWA-2115 Added possibility for multiple characteristic selections and wrapped the image with a link to the image gallery.

--- pages/Product/components/Media/connector.js
+++ pages/Product/components/Media/connector.js
@@ -2,16 +2,19 @@ import { connect } from 'react-redux';
 import { makeGetCharacteristicFeaturedImage } from '@shopgate/engage/product';
 
 /**
- * @param {Object} state The application state.
- * @param {Object} props The component props.
- * @returns {Object}
+ * @returns {Function}
  */
-function mapStateToProps(state, props) {
+function makeMapStateToProps() {
   const getCharacteristicFeaturedImage = makeGetCharacteristicFeaturedImage();
 
-  return ({
+  /**
+  * @param {Object} state The application state.
+  * @param {Object} props The component props.
+  * @returns {Object}
+  */
+  return (state, props) => ({
     imageUrl: getCharacteristicFeaturedImage(state, props),
   });
 }
 
-export default connect(mapStateToProps);
+export default connect(makeMapStateToProps);
