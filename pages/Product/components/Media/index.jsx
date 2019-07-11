542e3ddd200d8682cd9d069655b92c720af99b27
devbucket <flomueller1981@gmail.com>
2019-07-11 11:46:30 -0500

    PWA-2115 Finished characteristic image. Show the sliders when the selection is done.

--- pages/Product/components/Media/index.jsx
+++ pages/Product/components/Media/index.jsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useCallback, memo } from 'react';
+import React, { Fragment, memo } from 'react';
 import PropTypes from 'prop-types';
-import { isBeta, useNavigation, bin2hex } from '@shopgate/engage/core';
-import { MediaSlider, ProductImage, ITEM_PATH } from '@shopgate/engage/product';
+import { isBeta } from '@shopgate/engage/core';
+import { MediaSlider, ProductImage } from '@shopgate/engage/product';
 import ImageSlider from './components/ImageSlider';
 import connect from './connector';
 
@@ -13,29 +13,17 @@ const Media = memo((props) => {
   const {
     'aria-hidden': ariaHidden, productId, variantId, imageUrl,
   } = props;
-  const { push } = useNavigation();
-  const handleClick = useCallback(() => {
-    push({ pathname: `${ITEM_PATH}/${bin2hex(productId)}/gallery/0` });
-  }, []);
 
   if (imageUrl) {
     return (
-      <div
-        onClick={handleClick}
-        onKeyDown={handleClick}
-        role="button"
-        tabIndex="0"
-        aria-hidden={ariaHidden}
-      >
-        <ProductImage src={imageUrl} />
-      </div>
+      <ProductImage src={imageUrl} />
     );
   }
 
   return (
     <Fragment>
       {/* MediaSlider feature is currently in BETA testing.
-            It should only be used for approved BETA Client Projects */}
+          It should only be used for approved BETA Client Projects */}
       {isBeta()
         ? <MediaSlider productId={variantId || productId} aria-hidden={ariaHidden} />
         : <ImageSlider productId={productId} variantId={variantId} aria-hidden={ariaHidden} />
