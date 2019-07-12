03a281fb8b359a3791d44ed26b2eb0863bf589ff
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2019-07-12 19:36:13 +0200

    PWA-2094: Changed add and remove pipeline actions to work with async/await instead of promises. Added unit tests for addFavorites pipeline call.

--- libraries/commerce/favorites/actions/addFavorites.js
+++ libraries/commerce/favorites/actions/addFavorites.js
@@ -1,27 +1,26 @@
-import { logger } from '@shopgate/pwa-core';
+import { logger } from '@shopgate/pwa-core/helpers';
 import PipelineRequest from '@shopgate/pwa-core/classes/PipelineRequest';
 import { SHOPGATE_USER_ADD_FAVORITES } from '../constants/Pipelines';
 import { successAddFavorites, errorAddFavorites } from '../action-creators';
 
 /**
- * Calls addFavorites pipeline and dispatches action based on the result.
+ * Calls addFavorites pipeline and dispatches an action based on the result.
  * @param {string} productId Id of the product to be added.
  * @returns {Promise} Dispatched PipelineRequest.
  */
-export default productId => (dispatch) => {
-  const promise = new PipelineRequest(SHOPGATE_USER_ADD_FAVORITES)
-    .setInput({ productId })
-    .setRetries(0)
-    .dispatch();
+export default productId => async (dispatch) => {
+  let result;
+  try {
+    result = await new PipelineRequest(SHOPGATE_USER_ADD_FAVORITES)
+      .setInput({ productId })
+      .setRetries(0)
+      .dispatch();
 
-  promise
-    .then(() => {
-      dispatch(successAddFavorites(productId));
-    })
-    .catch((err) => {
-      logger.error(err);
-      dispatch(errorAddFavorites(productId, err));
-    });
+    dispatch(successAddFavorites(productId));
+  } catch (err) {
+    logger.error(err);
+    dispatch(errorAddFavorites(productId, err));
+  }
 
-  return promise;
+  return result;
 };
--- libraries/commerce/favorites/actions/removeFavorites.js
+++ libraries/commerce/favorites/actions/removeFavorites.js
@@ -1,29 +1,28 @@
-import { logger } from '@shopgate/pwa-core';
+import { logger } from '@shopgate/pwa-core/helpers';
 import PipelineRequest from '@shopgate/pwa-core/classes/PipelineRequest';
 import { SHOPGATE_USER_DELETE_FAVORITES } from '../constants/Pipelines';
 import { successRemoveFavorites, errorRemoveFavorites } from '../action-creators';
 
 /**
- * Calls deleteFavorites pipeline and dispatches action based on the result.
+ * Calls deleteFavorites pipeline and dispatches an action based on the result.
  * @param {string} productId Id of the product to be added.
  * @returns {Promise} Dispatched PipelineRxequest.
  */
-export default productId => (dispatch) => {
-  // This is an exception to the rule of calling the action by the pipeline name,
-  // because of consistency reasons
-  const promise = new PipelineRequest(SHOPGATE_USER_DELETE_FAVORITES)
-    .setInput({ productId })
-    .setRetries(0)
-    .dispatch();
+export default productId => async (dispatch) => {
+  let result;
+  try {
+    // This is an exception to the rule of calling the action by the pipeline name,
+    // because of consistency reasons
+    result = await new PipelineRequest(SHOPGATE_USER_DELETE_FAVORITES)
+      .setInput({ productId })
+      .setRetries(0)
+      .dispatch();
 
-  promise
-    .then(() => {
-      dispatch(successRemoveFavorites(productId));
-    })
-    .catch((err) => {
-      logger.error(err);
-      dispatch(errorRemoveFavorites(productId, err));
-    });
+    dispatch(successRemoveFavorites(productId));
+  } catch (err) {
+    logger.error(err);
+    dispatch(errorRemoveFavorites(productId, err));
+  }
 
-  return promise;
+  return result;
 };
