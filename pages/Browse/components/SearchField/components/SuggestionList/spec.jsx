4540b26e3ccd59d790828abbdd788cd9e6632f99
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-08-06 10:33:34 +0200

    PWA-2173 Add portals to ios11 theme. Adjust jest snapshots

--- pages/Browse/components/SearchField/components/SuggestionList/spec.jsx
+++ pages/Browse/components/SearchField/components/SuggestionList/spec.jsx
@@ -8,6 +8,9 @@ import SuggestionList, { UnwrappedSuggestionList } from './index';
 const store = createMockStore();
 
 let mockedFetchingState;
+jest.mock('@shopgate/engage/components', () => ({
+  SurroundPortals: ({ children }) => children,
+}));
 jest.mock('@shopgate/pwa-common-commerce/search/selectors', () => ({
   getSuggestions: () => ([
     'foo',
