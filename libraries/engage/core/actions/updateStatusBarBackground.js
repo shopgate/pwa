897d78c5635d75d78cdd2649bb68796b8351e377
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-08-12 13:19:42 +0200

    PWA-2167 Renamed a parameter of the status bar styling update system

--- libraries/engage/core/actions/updateStatusBarBackground.js
+++ libraries/engage/core/actions/updateStatusBarBackground.js
@@ -4,10 +4,10 @@ import { updateLegacyNavigationBar } from '../helpers/updateLegacyNavigationBar'
 /**
  * Updates the status bar background on iOS devices.
  * @param {string} color The background color.
- * @param {boolean} setDefault When set, the status bar will init with the color on next app start.
+ * @param {boolean} isDefault When set, the status bar will init with the color on next app start.
  * @return {Function} A redux thunk.
  */
-export default function updateStatusBarBackground(color, setDefault = false) {
+export default function updateStatusBarBackground(color, isDefault = false) {
   return (dispatch, getState) => {
     if (!isIos(getState())) {
       return;
@@ -15,7 +15,7 @@ export default function updateStatusBarBackground(color, setDefault = false) {
 
     updateLegacyNavigationBar({
       ...color && ({ statusBarBackground: color }),
-      isDefault: setDefault,
+      isDefault,
     });
   };
 }
