787479aa2e1c8667b87fb3875d9a2b23e5fcc4da
Frithjof Klös <frithjof.kloes@shopgate.com>
2019-08-30 11:29:40 +0200

    PI-9747 Added test for the itemView formatter

---
+++ libraries/tracking-core/helpers/formatHelpers.spec.js
@@ -0,0 +1,59 @@
+import helpers from './formatHelpers';
+
+const { itemView } = helpers;
+
+const mockBaseProduct = {
+  uid: 'base-product',
+  name: 'Product name',
+  manufacturer: 'Product manufacturer',
+  quantity: 3,
+  tags: [],
+  amount: {
+    currency: 'EUR',
+    gross: '100.99',
+    net: '90.99',
+    striked: '0.00',
+  },
+};
+
+const mockMockVariantProduct = {
+  ...mockBaseProduct,
+  uid: 'variant-product',
+};
+
+describe('Format helpers', () => {
+  describe('itemView()', () => {
+    it('should format raw base product data', () => {
+      const raw = {
+        product: mockBaseProduct,
+      };
+
+      expect(itemView(raw)).toEqual({
+        id: 'base-product',
+        type: '',
+        name: 'Product name',
+        priceNet: 90.99,
+        priceGross: 100.99,
+        currency: 'EUR',
+        brand: 'Product manufacturer',
+      });
+    });
+
+    it('should format raw variant product data', () => {
+      const raw = {
+        baseProduct: mockBaseProduct,
+        variant: mockMockVariantProduct,
+      };
+
+      expect(itemView(raw)).toEqual({
+        id: 'variant-product',
+        type: '',
+        name: 'Product name',
+        priceNet: 90.99,
+        priceGross: 100.99,
+        currency: 'EUR',
+        brand: 'Product manufacturer',
+      });
+    });
+  });
+});
