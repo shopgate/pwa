fdf172b3dfa5e1533bd777fe76ab0125f00d5510
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-09-11 16:40:32 +0200

    PWA-2244 Make Shadows configurable in Themes for Product sliders

--- themeApi/ProductCard/style.js
+++ themeApi/ProductCard/style.js
@@ -1,6 +1,13 @@
 import { css } from 'glamor';
+import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
-export default css({
+const { shadows } = themeConfig;
+
+export const shadowStyle = {
+  boxShadow: shadows.productCard,
+};
+
+export const itemClass = css({
   background: '#fff',
   borderRadius: 2,
   overflow: 'hidden',
