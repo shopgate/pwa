10cc82954eaf5b3bac1b55fd0ef05750c5105630
devbucket <flomueller1981@gmail.com>
2019-09-24 12:38:49 +0200

    PWA-2159 Made the filter page accessible and added enhanced a11y to the price range slider.

--- pages/Filter/components/Content/components/PriceSlider/style.js
+++ pages/Filter/components/Content/components/PriceSlider/style.js
@@ -46,8 +46,21 @@ const rangeSlider = {
   }).toString(),
 };
 
+const srOnly = css({
+  position: 'absolute',
+  width: 1,
+  height: 1,
+  padding: 0,
+  margin: -1,
+  overflow: 'hidden',
+  clip: 'rect(0, 0, 0, 0)',
+  whiteSpace: 'nowrap',
+  border: 0,
+});
+
 export default {
   wrapper,
   price,
   rangeSlider,
+  srOnly,
 };
