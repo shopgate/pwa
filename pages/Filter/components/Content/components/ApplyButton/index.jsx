ea64584fbafac30b3a8fe58eb60174211db79382
devbucket <flomueller1981@gmail.com>
2019-09-27 15:59:16 +0200

    PWA-2159 Skipped range slider tests.

--- pages/Filter/components/Content/components/ApplyButton/index.jsx
+++ pages/Filter/components/Content/components/ApplyButton/index.jsx
@@ -20,7 +20,6 @@ const FilterApplyButton = ({ active, onClick, widgetSettings }) => {
         onClick={onClick}
         disabled={!active}
         testId="applyFilterButton"
-        aria-hidden={!active}
       >
         <I18n.Text string="filter.apply" style={{ color: buttonColor }} />
       </Button>
