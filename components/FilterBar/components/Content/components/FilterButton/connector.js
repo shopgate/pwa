09c95cdb0bc535c7a7860a3da4a4382422815e77
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-10-21 16:30:56 +0200

    PWA-2277 Use plain object in mapDispatchToProps for FilterBar components

--- components/FilterBar/components/Content/components/FilterButton/connector.js
+++ components/FilterBar/components/Content/components/FilterButton/connector.js
@@ -2,12 +2,10 @@ import { connect } from 'react-redux';
 import openFilterRoute from '../../actions/openFilterRoute';
 
 /**
- * @param {Function} dispatch The redux dispatch function.
- * @param {Object} props The components props.
  * @return {Object}
  */
-const mapDispatchToProps = dispatch => ({
-  navigate: () => dispatch(openFilterRoute()),
-});
+const mapDispatchToProps = {
+  navigate: () => openFilterRoute(),
+};
 
 export default connect(null, mapDispatchToProps);
