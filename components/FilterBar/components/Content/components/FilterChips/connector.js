5eefd444d6d1b4c7cf82152fec92b35e0506250c
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-10-21 16:30:56 +0200

    PWA-2277 Use plain object in mapDispatchToProps for FilterBar components

--- components/FilterBar/components/Content/components/FilterChips/connector.js
+++ components/FilterBar/components/Content/components/FilterChips/connector.js
@@ -17,9 +17,9 @@ const mapStateToProps = state => ({
  * @param {Object} props The components props.
  * @return {Object}
  */
-const mapDispatchToProps = dispatch => ({
-  openFilters: () => dispatch(openFilterRoute()),
+const mapDispatchToProps = {
+  openFilters: () => openFilterRoute(),
   updateFilters,
-});
+};
 
 export default connect(mapStateToProps, mapDispatchToProps);
