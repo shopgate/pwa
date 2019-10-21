1dfb8493539b95326c91e898a5585dcdde7eb181
O.Bilenko <oleksandr.bilenko@shopgate.com>
2019-10-21 16:30:56 +0200

    PWA-2277 Use plain object in mapDispatchToProps for FilterBar components

--- components/FilterBar/components/Content/components/Sort/connector.js
+++ components/FilterBar/components/Content/components/Sort/connector.js
@@ -10,7 +10,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  historyReplace: params => historyReplace(params),
+  historyReplace,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps);
