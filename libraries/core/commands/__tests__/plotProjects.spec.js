43075c108a0c6fd1a491cbfd23a848adb6fcc266
Carsten Schneider <carsten.schneider@shopgate.com>
2019-12-04 17:56:39 +0100

    PWA-2390 added plotProjectsSetSegmentationProperties command

--- libraries/core/commands/__tests__/plotProjects.spec.js
+++ libraries/core/commands/__tests__/plotProjects.spec.js
@@ -1,8 +1,20 @@
-import { plotProjectsEnable, plotProjectsDisable } from '../plotProjects';
-// eslint-disable-next-line import/named
-import { mockedSetCommandName, mockedSetLibVersion, mockedDispatch } from '../../classes/AppCommand';
+import { plotProjectsEnable, plotProjectsDisable, plotProjectsSetSegmentationProperties } from '../plotProjects';
+/* eslint-disable import/named */
+import {
+  mockedSetCommandName,
+  mockedSetLibVersion,
+  mockedDispatch,
+  mockedSetCommandParams,
+} from '../../classes/AppCommand';
+import { logger } from '../../helpers';
+/* eslint-enable import/named */
 
 jest.mock('../../classes/AppCommand');
+jest.mock('../../helpers', () => ({
+  logger: {
+    error: jest.fn(),
+  },
+}));
 
 describe('plotprojects commands', () => {
   beforeEach(() => {
@@ -39,6 +51,86 @@ describe('plotprojects commands', () => {
       expect(mockedSetLibVersion).toHaveBeenCalledWith('18.0');
 
       expect(mockedDispatch).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('plotProjectsSetSegmentationProperties()', () => {
+    it('should dispatch the correct command', () => {
+      const inputProperties = [
+        // valid
+        {
+          key: 'key1',
+          type: 'string',
+          value: 'value1',
+        },
+        {
+          key: 'key2',
+          type: 'integer',
+          value: 123,
+        },
+        {
+          key: 'key3',
+          type: null,
+        },
+        {
+          key: 'key4',
+        },
+        {
+          key: 'key5',
+          value: null,
+        },
+        // invalid
+        {
+          key: 1,
+          type: 'integer',
+          value: 123,
+        },
+        {
+          type: 'integer',
+          value: 123,
+        },
+      ];
+
+      const expected = {
+        properties: [
+          {
+            key: 'key1',
+            type: 'string',
+            value: 'value1',
+          },
+          {
+            key: 'key2',
+            type: 'integer',
+            value: 123,
+          },
+          {
+            key: 'key3',
+            type: null,
+          },
+          {
+            key: 'key4',
+          },
+          {
+            key: 'key5',
+            value: null,
+          },
+        ],
+      };
+
+      plotProjectsSetSegmentationProperties(inputProperties);
+
+      expect(mockedSetCommandName).toHaveBeenCalledTimes(1);
+      expect(mockedSetCommandName).toHaveBeenCalledWith('plotProjectsSetSegmentationProperties');
+
+      expect(mockedSetCommandParams).toHaveBeenCalledTimes(1);
+      expect(mockedSetCommandParams).toHaveBeenCalledWith(expected);
+
+      expect(mockedSetLibVersion).toHaveBeenCalledTimes(1);
+      expect(mockedSetLibVersion).toHaveBeenCalledWith('25.0');
+
+      expect(mockedDispatch).toHaveBeenCalledTimes(1);
+
+      expect(logger.error).toHaveBeenCalledTimes(2);
     });
   });
 });
