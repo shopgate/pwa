6a4fd1e3bf0d91c282face6a51434bce9980631a
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-02-12 15:25:03 +0100

    PWA-2124 Use SurroundPortals component

---
+++ pages/Cart/components/ItemsGroup/components/FulfillmentLocation/connector.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import { makeGetLocation } from '@shopgate/engage/locations';
+
+/**
+ * Creates the mapStateToProps connector function.
+ * @returns {Function}
+ */
+const makeMapStateToProps = () => {
+  const getLocation = makeGetLocation();
+  return (state, props) => ({
+    location: getLocation(state, props),
+  });
+};
+
+export default connect(makeMapStateToProps);
