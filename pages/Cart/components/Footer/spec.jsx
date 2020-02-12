09c63cbac95d5bc8b514cee3a3bc48bc8214d701
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-02-12 15:10:50 +0100

    PWA-2124 Use react context. Code changes

--- pages/Cart/components/Footer/spec.jsx
+++ pages/Cart/components/Footer/spec.jsx
@@ -3,30 +3,29 @@ import { shallow } from 'enzyme';
 import Footer from './index';
 
 jest.mock('./connector', () => obj => obj);
-jest.mock('../../context', () => ({
-  Consumer: ({ children }) => children({
-    flags: {},
-  }),
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useContext: jest.fn().mockReturnValue({ flags: {} }),
 }));
 
 describe('<Footer />', () => {
   it('should render as expected when all items are supposed to be shown', () => {
-    const wrapper = shallow(<Footer showCouponsHint showTaxDisclaimer />).dive();
+    const wrapper = shallow(<Footer showCouponsHint showTaxDisclaimer />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render as expected when no items are supposed to be shown', () => {
-    const wrapper = shallow(<Footer showCouponsHint={false} showTaxDisclaimer={false} />).dive();
+    const wrapper = shallow(<Footer showCouponsHint={false} showTaxDisclaimer={false} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render as expected when only the the coupons hint is supposed to be shown', () => {
-    const wrapper = shallow(<Footer showCouponsHint showTaxDisclaimer={false} />).dive();
+    const wrapper = shallow(<Footer showCouponsHint showTaxDisclaimer={false} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render as expected when only the tax disclaimer is supposed to be shown', () => {
-    const wrapper = shallow(<Footer showCouponsHint={false} showTaxDisclaimer />).dive();
+    const wrapper = shallow(<Footer showCouponsHint={false} showTaxDisclaimer />);
     expect(wrapper).toMatchSnapshot();
   });
 });
--- pages/Cart/components/PaymentBar/components/Content/components/ReserveButton/index.jsx
+++ pages/Cart/components/PaymentBar/components/Content/components/ReserveButton/index.jsx
@@ -1,10 +1,6 @@
-import React, { Fragment } from 'react';
-import { I18n, Portal, RippleButton } from '@shopgate/engage/components';
-import {
-  CART_CHECKOUT_BUTTON,
-  CART_CHECKOUT_BUTTON_AFTER,
-  CART_CHECKOUT_BUTTON_BEFORE,
-} from '@shopgate/engage/cart';
+import React, { useContext } from 'react';
+import { I18n, RippleButton, SurroundPortals } from '@shopgate/engage/components';
+import { CART_CHECKOUT_BUTTON } from '@shopgate/engage/cart';
 import CartContext from '../../../../../../context';
 import styles from '../CheckoutButton/style';
 
@@ -12,24 +8,20 @@ import styles from '../CheckoutButton/style';
  * The reserve button component.
  * @return {JSX}
  */
-const ReserveButton = () => (
-  <CartContext.Consumer>
-    {({ flags: { orderable } }) => (
-      <Fragment>
-        <Portal name={CART_CHECKOUT_BUTTON_BEFORE} props={{ isActive: orderable }} />
-        <Portal name={CART_CHECKOUT_BUTTON} props={{ isActive: orderable }}>
-          <RippleButton
-            disabled={!orderable}
-            type="regular"
-            className={orderable ? styles.button : styles.disabledButton}
-          >
-            <I18n.Text string="cart.reserve" />
-          </RippleButton>
-        </Portal>
-        <Portal name={CART_CHECKOUT_BUTTON_AFTER} props={{ isActive: orderable }} />
-      </Fragment>
-    )}
-  </CartContext.Consumer>
-);
+const ReserveButton = () => {
+  const { flags: { orderable } } = useContext(CartContext);
+
+  return (
+    <SurroundPortals portalName={CART_CHECKOUT_BUTTON} portalProps={{ isActive: orderable }}>
+      <RippleButton
+        disabled={!orderable}
+        type="regular"
+        className={orderable ? styles.button : styles.disabledButton}
+      >
+        <I18n.Text string="cart.reserve" />
+      </RippleButton>
+    </SurroundPortals>
+  );
+};
 
 export default ReserveButton;
