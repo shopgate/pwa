788a41c202165c7ee15d63bd1e01579679488d6c
devbucket <flomueller1981@gmail.com>
2020-03-10 11:29:31 +0100

    PWA-2534 Optimuzed component structure.

--- libraries/engage/cart/components/CartItems/CartItemGroupReservation.jsx
+++ libraries/engage/cart/components/CartItems/CartItemGroupReservation.jsx
@@ -1,40 +1,19 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { every, isEmpty } from 'lodash';
 import CardListItem from '@shopgate/pwa-ui-shared/CardList/components/Item';
-import { Accordion, LocationIcon } from '@shopgate/engage/components';
-import { i18n } from '@shopgate/engage/core';
+import { Accordion } from '@shopgate/engage/components';
 import { StoreAddressOpeningHours, StoreAddressPhoneNumber } from '@shopgate/engage/locations';
 import connect from './CartItemGroupReservation.connector';
-import {
-  address, addressIcon, title, accordionToggle, addressDetails, simpleLabel,
-} from './CartItemGroup.style';
+import { accordionToggle, addressDetails, simpleLabel } from './CartItemGroup.style';
+import { CartItemGroupReservationLabel } from './CartItemGroupReservationLabel';
 
 /**
  * Renders the product group.
  * @param {Object} props The component props.
  * @returns {JSX.Element}
  */
-const FulfillmentLocation = ({ location }) => {
-  const label = useMemo(() => {
-    if (!location) {
-      return null;
-    }
-    return (
-      <div className={address}>
-        <div className={addressIcon}>
-          <LocationIcon />
-        </div>
-        <div>
-          <div className={title}>
-            {i18n.text('locations.method.ropis')}
-          </div>
-          {location.name}
-        </div>
-      </div>
-    );
-  }, [location]);
-
+function CartItemGroupReservation({ location }) {
   if (!location) {
     return null;
   }
@@ -43,13 +22,18 @@ const FulfillmentLocation = ({ location }) => {
 
   if ((!operationHours || every(operationHours, isEmpty)) && !phoneNumber) {
     return (
-      <CardListItem className={simpleLabel.toString()}>{label}</CardListItem>
+      <CardListItem className={simpleLabel.toString()}>
+        <CartItemGroupReservationLabel loction={location} />
+      </CardListItem>
     );
   }
 
   return (
     <CardListItem>
-      <Accordion renderLabel={() => label} className={accordionToggle}>
+      <Accordion
+        renderLabel={() => <CartItemGroupReservationLabel location={location} />}
+        className={accordionToggle}
+      >
         <div className={addressDetails}>
           <StoreAddressOpeningHours hours={operationHours} />
           <StoreAddressPhoneNumber phone={phoneNumber} />
@@ -57,14 +41,14 @@ const FulfillmentLocation = ({ location }) => {
       </Accordion>
     </CardListItem>
   );
-};
+}
 
-FulfillmentLocation.propTypes = {
+CartItemGroupReservation.propTypes = {
   location: PropTypes.shape(),
 };
 
-FulfillmentLocation.defaultProps = {
+CartItemGroupReservation.defaultProps = {
   location: null,
 };
 
-export default connect(FulfillmentLocation);
+export default connect(CartItemGroupReservation);
