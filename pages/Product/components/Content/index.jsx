33b9b3559ee7c0a328b861fc446d01f4f9e38722
devbucket <flomueller1981@gmail.com>
2020-03-16 23:57:00 +0100

    PWA-2537 Change position of the fulfillment selector.

--- pages/Product/components/Content/index.jsx
+++ pages/Product/components/Content/index.jsx
@@ -161,12 +161,6 @@ class ProductContent extends PureComponent {
         <ProductContext.Provider value={contextValue}>
           <Media aria-hidden />
           <Header />
-          <Section title="product.sections.fulfillment">
-            <FulfillmentSelector
-              productId={variantId || productId}
-              conditioner={this.baseContextValue.conditioner}
-            />
-          </Section>
           {/*
             This feature is currently in BETA testing.
             It should only be used for approved BETA Client Projects
@@ -175,6 +169,12 @@ class ProductContent extends PureComponent {
           <Section title="product.sections.options">
             <Characteristics productId={productId} variantId={variantId} />
             <Options />
+          </Section>
+          <Section title="product.sections.fulfillment">
+            <FulfillmentSelector
+              productId={variantId || productId}
+              conditioner={this.baseContextValue.conditioner}
+            />
           </Section>
           <Section title="product.sections.description">
             <Description productId={productId} variantId={variantId} />
