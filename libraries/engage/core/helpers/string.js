a6266dd73ab8396cf526c805307f6ea791c9f3e6
devbucket <flomueller1981@gmail.com>
2020-03-17 13:47:17 +0100

    PWA-2537 Removed unneeded helper

--- libraries/engage/core/helpers/string.js
+++ libraries/engage/core/helpers/string.js
@@ -1,12 +1,4 @@
 /**
- * Takes a string and capitalizes the first letter of it.
- * @param {string} text The string to modify.
- * @returns {string}
- */
-export const capitalize = text =>
-  `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
-
-/**
  * Takes a string and transforms the string into upper case.
  * @param {string} text The string to modify.
  * @returns {string}
