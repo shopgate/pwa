9b6b74a53fca03742286e7b096502447d2fddf0f
Frithjos Klös <frithjof.kloes@shopgate.com>
2020-03-31 11:54:43 +0200

    PWA-2743 Small fix for the FulfillmentSelector connector to avoid React warnings

--- libraries/engage/locations/components/FulfillmentSelector/FulfillmentSelector.connector.js
+++ libraries/engage/locations/components/FulfillmentSelector/FulfillmentSelector.connector.js
@@ -41,7 +41,7 @@ function makeMapStateToProps() {
       userFulfillmentMethod: getUserLocationFulfillmentMethod(state, props),
       location: getProductLocation(state, props) || getUserLocation(state),
       disabled: isFulfillmentSelectorDisabled(state, props),
-      isOrderable: isProductOrderable(state, props) || hasVariants,
+      isOrderable: isProductOrderable(state, props) || !!hasVariants,
       isReady: hasVariants !== null && !hasVariants,
     };
   };
