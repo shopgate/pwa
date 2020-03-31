01eea1b3d949a36f5215bc615dd553e8e54b3bdd
devbucket <flomueller1981@gmail.com>
2020-03-31 13:59:54 +0200

    PWA-2540 Optimized accordion UI.

--- libraries/ui-material/Accordion/components/AccordionContent/index.jsx
+++ libraries/ui-material/Accordion/components/AccordionContent/index.jsx
@@ -1,6 +1,8 @@
 // @flow
 import * as React from 'react';
 import classnames from 'classnames';
+import { useSpring, animated } from 'react-spring';
+import useMeasure from 'react-use/lib/useMeasure';
 import * as styles from './style';
 
 export type Props = {
@@ -15,19 +17,38 @@ export type Props = {
  * @param {Object} props The component props.
  * @returns {JSX}
  */
-function AccordionContent({
-  children, open, id, className,
-}: Props) {
-  const style = {
-    height: !open ? 0 : 'auto',
-  };
+function AccordionContent(props: Props) {
+  const {
+    children, open, id, className,
+  } = props;
+  const [contentHeight, setContentHeight] = React.useState(0);
+  const [ref, { height }] = useMeasure();
+
+  React.useEffect(() => {
+    setContentHeight(height);
+    window.addEventListener('resize', setContentHeight(height));
+    return () => {
+      window.removeEventListener('resize', setContentHeight(height));
+    };
+  }, [height]);
+
+  const expand = useSpring({
+    config: {
+      duration: 175,
+      tension: 100,
+      friction: 20,
+    },
+    height: open ? contentHeight : 0,
+  });
 
   return (
-    <div className={styles.content} style={style} id={id} aria-hidden={!open}>
-      <div className={classnames(styles.contentInner, className)}>
-        {children}
+    <animated.div className={styles.content} style={expand} id={id} aria-hidden={!open}>
+      <div ref={ref}>
+        <div className={classnames(styles.contentInner, className)}>
+          {children}
+        </div>
       </div>
-    </div>
+    </animated.div>
   );
 }
 
