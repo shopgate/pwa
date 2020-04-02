8daef712a190fbdef21c2b576a920514388d78a9
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-04-02 13:45:18 +0200

    Small improvement for the internal logic of the user selectors

--- libraries/engage/user/selectors/__tests__/data.spec.js
+++ libraries/engage/user/selectors/__tests__/data.spec.js
@@ -1,43 +1,65 @@
 import { getUserId, getExternalCustomerNumber } from '../data';
 
 describe('engage > user > selectors > data', () => {
-  it('should return user id for logged in user', () => {
-    const result = getUserId({
-      user: {
-        login: {},
-        data: { id: 123456 },
-      },
+  describe('getUserId()', () => {
+    it('should return user id for logged in user', () => {
+      const result = getUserId({
+        user: {
+          login: {},
+          data: { id: 123456 },
+        },
+      });
+      expect(result).toBe(123456);
     });
-    expect(result).toBe(123456);
+
+    it('should return null if user is not logged in', () => {
+      const result = getUserId({
+        user: {
+          login: {},
+          data: {},
+        },
+      });
+      expect(result).toBe(null);
+    });
+
+    it('should return null if there is no user data', () => {
+      const result = getUserId({
+        user: {
+          login: {},
+        },
+      });
+      expect(result).toBe(null);
+    });
   });
 
-  it('should return null if user is not logged in', () => {
-    const result = getUserId({
-      user: {
-        login: {},
-        data: {},
-      },
+  describe('getExternalCustomerNumber', () => {
+    it('should return user external number for logged in user', () => {
+      const result = getExternalCustomerNumber({
+        user: {
+          login: {},
+          data: { externalCustomerNumber: '123456' },
+        },
+      });
+      expect(result).toBe('123456');
     });
-    expect(result).toBe(null);
-  });
 
-  it('should return user external number for logged in user', () => {
-    const result = getExternalCustomerNumber({
-      user: {
-        login: {},
-        data: { externalCustomerNumber: '123456' },
-      },
+    it('should return external number as null if user is not logged in', () => {
+      const result = getExternalCustomerNumber({
+        user: {
+          login: {},
+          data: {},
+        },
+      });
+      expect(result).toBe(null);
     });
-    expect(result).toBe('123456');
-  });
 
-  it('should return external number as null if user is not logged in', () => {
-    const result = getExternalCustomerNumber({
-      user: {
-        login: {},
-        data: {},
-      },
+    it('should return null if there is no user data', () => {
+      const result = getExternalCustomerNumber({
+        user: {
+          login: {},
+        },
+      });
+      expect(result).toBe(null);
     });
-    expect(result).toBe(null);
   });
 });
--- libraries/engage/user/selectors/data.js
+++ libraries/engage/user/selectors/data.js
@@ -5,7 +5,10 @@ import { getUserData } from '@shopgate/pwa-common/selectors/user';
  * @param {Object} state The application state.
  * @returns {[number]}
  */
-export const getUserId = state => getUserData(state).id || null;
+export const getUserId = (state) => {
+  const userData = getUserData(state) || {};
+  return userData.id || null;
+};
 
 /**
  * Selects the external customer number (ecp identifier)
@@ -13,4 +16,7 @@ export const getUserId = state => getUserData(state).id || null;
  * @param {Object} state The application state.
  * @returns {[number]}
  */
-export const getExternalCustomerNumber = state => getUserData(state).externalCustomerNumber || null;
+export const getExternalCustomerNumber = (state) => {
+  const userData = getUserData(state) || {};
+  return userData.externalCustomerNumber || null;
+};
