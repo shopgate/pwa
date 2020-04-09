1c525a1a145de04a5891ec09a964b6d1b2ba0a58
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-04-09 14:04:50 +0200

    Implemented "Continue Shopping" functionality for the checkout success page

--- pages/Checkout/CheckoutConfirmation.jsx
+++ pages/Checkout/CheckoutConfirmation.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from '@shopgate/engage/components';
 import { themeConfig } from '@shopgate/engage';
 import { AppBar } from '@shopgate/pwa-ui-material';
+import { CrossIcon } from '@shopgate/pwa-ui-shared';
 import { DefaultBar } from 'Components/AppBar/presets';
-import { CheckoutConfirmation } from '@shopgate/engage/checkout';
+import { CheckoutConfirmation as Content } from '@shopgate/engage/checkout';
 import { i18n } from '@shopgate/engage/core';
+import connect from './CheckoutConfirmation.connector';
 
 const { colors } = themeConfig;
 
@@ -12,16 +15,25 @@ const { colors } = themeConfig;
  * The Cart component.
  * @returns {JSX}
  */
-const Checkout = () => (
-  <View background={colors.background} aria-hidden={false}>
-    <DefaultBar
-      center={
-        <AppBar.Title title={i18n.text('titles.checkout_confirmation')} />
-      }
-    />
-    <CheckoutConfirmation />
-  </View>
-);
+const CheckoutConfirmation = ({ handleClose }) => {
+  const left = <DefaultBar.Icon aria-label={i18n.text('common.close')} icon={CrossIcon} onClick={handleClose} />;
+  return (
+    <View background={colors.background} aria-hidden={false}>
+      <DefaultBar
+        left={left}
+        center={
+          <AppBar.Title title={i18n.text('titles.checkout_confirmation')} />
+        }
+        right={null}
+      />
+      <Content onContinueShopping={handleClose} />
+    </View>
+  );
+};
 
-export default Checkout;
+CheckoutConfirmation.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+};
+
+export default connect(CheckoutConfirmation);
 
