dd00f0d5984be16e160213b038192948380060a4
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-04-27 15:54:53 +0200

    CCP-2120 category view on tablet: first and last item for each row is loaded first (pwa 6.12.2)

--- themes/theme-gmd/components/ProductGrid/components/Layout/style.js
+++ themes/theme-gmd/components/ProductGrid/components/Layout/style.js
@@ -3,10 +3,17 @@ import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
 const { colors } = themeConfig;
 
-export default css({
+/**
+ * Generate css grid
+ * @param {number} columns .
+ * @returns {string}
+ */
+export const grid = columns => css({
   ':not(:empty)': {
     paddingBottom: 2,
   },
   background: colors.background,
-  justifyContent: 'space-between',
+  display: 'grid',
+  gridGap: '2px',
+  gridTemplateColumns: `repeat(${columns}, 1fr)`,
 }).toString();
