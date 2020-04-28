328467c7910c912358ff40264b9aa185005e2508
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-04-28 11:23:47 +0200

    CCP-2120 Add extension config to use a grid for ProductGrid component

--- components/ProductGrid/components/Layout/index.jsx
+++ components/ProductGrid/components/Layout/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@shopgate/pwa-common/components/Grid';
-import { grid } from './style';
+import { styles } from './style';
 
 /**
  * The product grid layout component.
  * @param {Object} props The component props.
  * @returns {JSX}
  */
-const Layout = ({ children, columns }) => (
-  <Grid wrap className={grid(columns)} data-test-id="productGrid">
+const Layout = ({ children, columns, type }) => (
+  <Grid wrap className={styles(type, columns)} data-test-id="productGrid">
     {children}
   </Grid>
 );
 
 Layout.propTypes = {
   columns: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
   children: PropTypes.node,
 };
 
