48f92018795408e8c926826b8715576af186b67c
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-04-29 16:11:37 +0200

    CURB-1 Fixed SheetList stylings

--- widgets/CategoryList/style.js
+++ widgets/CategoryList/style.js
@@ -1,4 +1,5 @@
 import { css } from 'glamor';
+import { responsiveMediaQuery } from '@shopgate/engage/styles';
 import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 
 const { colors, variables } = themeConfig;
@@ -7,6 +8,24 @@ const container = css({
   background: colors.light,
 }).toString();
 
+const sheetList = css({
+  [responsiveMediaQuery('>xs', { webOnly: true })]: {
+    ' > li': {
+      boxShadow: 'none !important',
+      padding: `${variables.gap.small}px 0`,
+      '&:first-child': {
+        paddingTop: 0,
+      },
+      '&:last-child': {
+        paddingBottom: 0,
+      },
+      '> :first-child': {
+        border: `1px solid ${colors.shade7}`,
+      },
+    },
+  },
+});
+
 const headline = css({
   fontSize: 18,
   padding: `${variables.gap.big}px 0 0`,
@@ -14,7 +33,22 @@ const headline = css({
   textAlign: 'center',
 }).toString();
 
+const image = css({
+  display: 'none',
+  [responsiveMediaQuery('>xs', { webOnly: true })]: {
+    display: 'flex',
+    ' img': {
+      position: 'inherit',
+      width: 'inherit !important',
+      maxWidth: 'inherit !important',
+      height: variables.gap.xxbig,
+    },
+  },
+}).toString();
+
 export default {
   container,
+  sheetList,
   headline,
+  image,
 };
