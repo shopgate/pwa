5d14ebbc764a2cf3c89b1174ec2e75e656db43c1
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-05-15 13:30:50 +0200

    CURB-1 Checkout styling improvements

--- pages/Register/index.jsx
+++ pages/Register/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { View } from '@shopgate/engage/components';
-import { themeConfig } from '@shopgate/engage';
 import { AppBar } from '@shopgate/pwa-ui-material';
 import { BackBar } from 'Components/AppBar/presets';
 import { i18n } from '@shopgate/engage/core';
 import { Registration } from '@shopgate/engage/registration';
-
-const { colors } = themeConfig;
 
 /**
  * The Cart component.
  * @returns {JSX}
  */
 const RegisterPage = () => (
-  <View background={colors.background} aria-hidden={false}>
+  <View aria-hidden={false}>
     <BackBar
       right={null}
       center={
