7086e642b9daede0b36fe4b74ffc77e0132e1bf8
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-05-20 15:41:51 +0200

    CURB-99 Removed legacy pages from apps when the connect extension is attached

--- pages/More/components/StoreInfo/index.jsx
+++ pages/More/components/StoreInfo/index.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Portal from '@shopgate/pwa-common/components/Portal';
+import { configuration, IS_CONNECT_EXTENSION_ATTACHED } from '@shopgate/engage/core';
 import {
   NAV_MENU_STORE_INFORMATION_BEFORE,
   NAV_MENU_STORE_INFORMATION,
@@ -9,24 +10,46 @@ import portalProps from '../../portalProps';
 import Section from '../Section';
 import Shipping from './components/Shipping';
 import Payment from './components/Payment';
+import Terms from './components/Terms';
+import Privacy from './components/Privacy';
+import ReturnPolicy from './components/ReturnPolicy';
+import Imprint from './components/Imprint';
 import LegalPages from './components/LegalPages';
 
 /**
  * The StoreInfoComponent.
  * @returns {JSX}
  */
-const StoreInfo = () => (
-  <Fragment>
-    <Portal name={NAV_MENU_STORE_INFORMATION_BEFORE} props={portalProps} />
-    <Portal name={NAV_MENU_STORE_INFORMATION} props={portalProps}>
-      <Section title="navigation.store_information">
-        <Shipping />
-        <Payment />
-        <LegalPages />
-      </Section>
-    </Portal>
-    <Portal name={NAV_MENU_STORE_INFORMATION_AFTER} props={portalProps} />
-  </Fragment>
-);
+const StoreInfo = () => {
+  const hasConnectExtension = !!configuration.get(IS_CONNECT_EXTENSION_ATTACHED);
+
+  return (
+    <Fragment>
+      <Portal name={NAV_MENU_STORE_INFORMATION_BEFORE} props={portalProps} />
+      <Portal name={NAV_MENU_STORE_INFORMATION} props={portalProps}>
+        <Section title="navigation.store_information">
+          { !hasConnectExtension && (
+            <Fragment>
+              <Shipping />
+              <Payment />
+            </Fragment>
+          )}
+          { hasConnectExtension ? (
+            <LegalPages />
+          ) : (
+            <Fragment>
+              <Terms />
+              <Privacy />
+              <ReturnPolicy />
+              <Imprint />
+            </Fragment>
+          )}
+
+        </Section>
+      </Portal>
+      <Portal name={NAV_MENU_STORE_INFORMATION_AFTER} props={portalProps} />
+    </Fragment>
+  );
+};
 
 export default StoreInfo;
