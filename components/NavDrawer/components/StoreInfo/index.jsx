b41877cab63053c2bf1de027c7efa84375d50819
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-05-20 15:41:51 +0200

    CURB-99 Removed legacy pages from apps when the connect extension is attached

--- components/NavDrawer/components/StoreInfo/index.jsx
+++ components/NavDrawer/components/StoreInfo/index.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Portal from '@shopgate/pwa-common/components/Portal';
-import { hasWebBridge } from '@shopgate/engage/core';
+import showReturnPolicy from '@shopgate/pwa-common-commerce/market/helpers/showReturnPolicy';
+import { configuration, IS_CONNECT_EXTENSION_ATTACHED } from '@shopgate/engage/core';
 import {
   NAV_MENU_STORE_INFORMATION,
   NAV_MENU_STORE_INFORMATION_AFTER,
@@ -16,36 +17,53 @@ import NavDrawerSection from '../Section';
 import ShippingButton from './components/ShippingButton';
 import PaymentButton from './components/PaymentButton';
 import LegalButtons from './components/LegalButtons';
+import TermsButton from './components/TermsButton';
+import PrivacyButton from './components/PrivacyButton';
+import ReturnsButton from './components/ReturnsButton';
+import ImprintButton from './components/ImprintButton';
 import portalProps from '../../portalProps';
 
 /**
  * @returns {JSX}
  */
-const StoreInfo = () => (
-  <Fragment>
-    <Portal name={NAV_MENU_STORE_INFORMATION_BEFORE} props={portalProps} />
-    <Portal name={NAV_MENU_STORE_INFORMATION} props={portalProps}>
-      <Portal name={NAV_MENU_STORE_INFORMATION_MORE_BEFORE} props={portalProps} />
-      <Portal name={NAV_MENU_STORE_INFORMATION_MORE} props={portalProps}>
-        { !hasWebBridge() && (
-          <NavDrawerSection title="navigation.menuSubHeader.more">
-            <ShippingButton />
-            <PaymentButton />
-          </NavDrawerSection>
-        )}
-      </Portal>
-      <Portal name={NAV_MENU_STORE_INFORMATION_MORE_AFTER} props={portalProps} />
+const StoreInfo = () => {
+  const hasConnectExtension = !!configuration.get(IS_CONNECT_EXTENSION_ATTACHED);
 
-      <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT_BEFORE} props={portalProps} />
-      <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT} props={portalProps}>
-        <NavDrawerSection title="navigation.menuSubHeader.about">
-          <LegalButtons />
-        </NavDrawerSection>
+  return (
+    <Fragment>
+      <Portal name={NAV_MENU_STORE_INFORMATION_BEFORE} props={portalProps} />
+      <Portal name={NAV_MENU_STORE_INFORMATION} props={portalProps}>
+        <Portal name={NAV_MENU_STORE_INFORMATION_MORE_BEFORE} props={portalProps} />
+        <Portal name={NAV_MENU_STORE_INFORMATION_MORE} props={portalProps}>
+          { !hasConnectExtension && (
+            <NavDrawerSection title="navigation.menuSubHeader.more">
+              <ShippingButton />
+              <PaymentButton />
+            </NavDrawerSection>
+          )}
+        </Portal>
+        <Portal name={NAV_MENU_STORE_INFORMATION_MORE_AFTER} props={portalProps} />
+
+        <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT_BEFORE} props={portalProps} />
+        <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT} props={portalProps}>
+          <NavDrawerSection title="navigation.menuSubHeader.about">
+            { hasConnectExtension ? (
+              <LegalButtons />
+            ) : (
+              <Fragment>
+                <TermsButton />
+                <PrivacyButton />
+                {showReturnPolicy && <ReturnsButton />}
+                <ImprintButton />
+              </Fragment>
+            )}
+          </NavDrawerSection>
+        </Portal>
+        <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT_AFTER} props={portalProps} />
       </Portal>
-      <Portal name={NAV_MENU_STORE_INFORMATION_ABOUT_AFTER} props={portalProps} />
-    </Portal>
-    <Portal name={NAV_MENU_STORE_INFORMATION_AFTER} props={portalProps} />
-  </Fragment>
-);
+      <Portal name={NAV_MENU_STORE_INFORMATION_AFTER} props={portalProps} />
+    </Fragment>
+  );
+};
 
 export default StoreInfo;
