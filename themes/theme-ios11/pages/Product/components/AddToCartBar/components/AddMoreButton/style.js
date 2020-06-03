90105c98de10ebf124a2e209317619259b694c9e
Rene Eichhorn <rene.eichhorn@shopgate.com>
2020-06-03 16:24:21 +0200

    PI-10178 - Fix cart button

--- themes/theme-ios11/pages/Product/components/AddToCartBar/components/AddMoreButton/style.js
+++ themes/theme-ios11/pages/Product/components/AddToCartBar/components/AddMoreButton/style.js
@@ -1,10 +1,6 @@
 import { css } from 'glamor';
 
 const container = css({
-  display: 'flex',
-  justifyContent: 'center',
-  flexGrow: 0,
-  flexShrink: 0,
   fontSize: '1.75rem !important',
   background: '#f0f0f0',
   transform: 'opacity 300ms cubic-bezier(0.25, 0.1, 0.25, 1) !important',
