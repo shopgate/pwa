3ea30e307ff55cd1b64428e5f8cc0333b9fdccec
Frithjof Klös <frithjof.kloes@shopgate.com>
2020-07-19 17:27:56 +0200

    CURB-180 Basic functionality of the store location finder

---
+++ pages/StoreFinder/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from '@shopgate/engage/components';
+import { AppBar } from '@shopgate/pwa-ui-material';
+import { StoreLocationFinder as Content } from '@shopgate/engage/locations';
+import { BackBar } from 'Components/AppBar/presets';
+
+/**
+ * @returns {JSX}
+ */
+const StoreFinder = () => (
+  <View aria-hidden={false}>
+    <BackBar
+      right={null}
+      center={<AppBar.Title title="titles.store_finder" />}
+    />
+    <Content />
+  </View>
+);
+
+export default StoreFinder;
