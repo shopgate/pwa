09884bc8cffb7267b05a7c2f69d291a913285d41
Rene Eichhorn <rene.eichhorn@shopgate.com>
2020-08-05 12:53:13 +0200

    Added fav list to desktop to side view

--- libraries/engage/components/SideNavigation/SideNavigationLinks.jsx
+++ libraries/engage/components/SideNavigation/SideNavigationLinks.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
+import { FAVORITES_PATH } from '@shopgate/pwa-common-commerce/favorites/constants';
 import { CART_PATH } from '@shopgate/pwa-common-commerce/cart/constants';
 import { LOGIN_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
+import appConfig from '@shopgate/pwa-common/helpers/config';
 import { useSideNavigation } from './SideNavigation.hooks';
 import SideNavigationItem from './SideNavigationItem';
 import SideNavigationLinksQuicklinks from './SideNavigationLinksQuicklinks';
@@ -16,6 +18,9 @@ const SideNavigationLinks = () => {
   return (
     <Fragment>
       <SideNavigationItem href={CART_PATH} label="navigation.cart" />
+      {appConfig.hasFavorites ? (
+        <SideNavigationItem href={FAVORITES_PATH} label="navigation.favorites" />
+      ) : null}
       {isLoggedIn && <SideNavigationItem href={ACCOUNT_PATH} label="navigation.your_account" />}
       <SideNavigationLinksQuicklinks />
       <SideNavigationLinksLegal />
