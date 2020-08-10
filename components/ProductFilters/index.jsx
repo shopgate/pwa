2959d83454f8396ac88724bac449c1b250085e96
Kevin Heye <kevin.heye@shopgate.com>
2020-08-10 18:24:47 +0200

    CURB-267 Hide product filters on scroll

---
+++ components/ProductFilters/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useRoute } from '@shopgate/engage/core';
+import { ResponsiveContainer, ScrollHeader } from '@shopgate/engage/components';
+import { GlobalLocationSwitcher } from '@shopgate/engage/locations';
+import FilterBar from 'Components/FilterBar';
+import { filters } from './style';
+
+/**
+ * @param {Object} props.categoryId The category id.
+ * @returns {JSX}
+ */
+const CategoryContent = ({ categoryId, hasProducts }) => {
+  const { state } = useRoute();
+  return (
+    <ScrollHeader className={filters}>
+      <ResponsiveContainer appAlways breakpoint="<=xs">
+        <GlobalLocationSwitcher renderBar standalone />
+      </ResponsiveContainer>
+
+      {hasProducts && (
+        <FilterBar
+          categoryId={categoryId}
+          filters={state.filters}
+        />
+      )}
+    </ScrollHeader>
+  );
+};
+
+CategoryContent.propTypes = {
+  categoryId: PropTypes.string.isRequired,
+  hasProducts: PropTypes.bool,
+};
+
+CategoryContent.defaultProps = {
+  hasProducts: false,
+};
+
+export default CategoryContent;
--- pages/Category/components/Content/index.jsx
+++ pages/Category/components/Content/index.jsx
@@ -1,49 +1,30 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { useRoute } from '@shopgate/engage/core';
-import { ResponsiveContainer } from '@shopgate/engage/components';
-import { GlobalLocationSwitcher } from '@shopgate/engage/locations';
-import FilterBar from 'Components/FilterBar';
+import ProductFilters from 'Components/ProductFilters';
 import ProductsContent from '../ProductsContent';
 import Empty from '../Empty';
 import CategoryListContent from '../CategoryListContent';
 import connect from './connector';
 import AppBar from '../AppBar';
-import { filters } from './style';
 
 /**
  * @param {Object} props.categoryId The category id.
  * @returns {JSX}
  */
-const CategoryContent = ({ categoryId, hasChildren, hasProducts }) => {
-  const { state } = useRoute();
-  return (
-    <Fragment>
-      <AppBar hasProducts={hasProducts} hasChildren={hasChildren} />
-      <CategoryListContent categoryId={categoryId} />
+const CategoryContent = ({ categoryId, hasChildren, hasProducts }) => (
+  <Fragment>
+    <AppBar hasProducts={hasProducts} hasChildren={hasChildren} />
+    <CategoryListContent categoryId={categoryId} />
 
-      <div className={filters}>
-        <ResponsiveContainer appAlways breakpoint="<=xs">
-          <GlobalLocationSwitcher renderBar standalone />
-        </ResponsiveContainer>
-
-        {hasProducts && (
-          <FilterBar
-            categoryId={categoryId}
-            filters={state.filters}
-          />
-        )}
-
-      </div>
-      <ProductsContent categoryId={categoryId} hasProducts={hasProducts} />
-      <Empty
-        categoryId={categoryId}
-        headlineText="category.no_result.heading"
-        bodyText="category.no_result.body"
-      />
-    </Fragment>
-  );
-};
+    <ProductFilters categoryId={categoryId} hasProducts={hasProducts} />
+    <ProductsContent categoryId={categoryId} hasProducts={hasProducts} />
+    <Empty
+      categoryId={categoryId}
+      headlineText="category.no_result.heading"
+      bodyText="category.no_result.body"
+    />
+  </Fragment>
+);
 
 CategoryContent.propTypes = {
   categoryId: PropTypes.string.isRequired,
