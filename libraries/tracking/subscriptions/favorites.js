d969629eea78054c43b43cde51839d1d381622dd
Carsten Schneider <carsten.schneider@shopgate.com>
2020-12-03 12:18:33 +0100

    PWA-2764 implemented feedback

--- libraries/tracking/subscriptions/favorites.js
+++ libraries/tracking/subscriptions/favorites.js
@@ -4,10 +4,10 @@ import { track } from '../helpers/index';
 import { getProductFormatted } from '../selectors/product';
 
 /**
- * Favlist tracking subscriptions.
+ * Favorites tracking subscriptions.
  * @param {Function} subscribe The subscribe function.
  */
-export default function favlist(subscribe) {
+export default function favorites(subscribe) {
   subscribe(favoritesDidAddItem$, ({ getState, action }) => {
     const state = getState();
     const product = getProductFormatted(state, { productId: action.productId });
@@ -16,9 +16,9 @@ export default function favlist(subscribe) {
     const productFavlist = {
       ...product,
       // eslint-disable-next-line camelcase
-      unit_amount_net: product.amount.net,
+      unit_amount_net: product.amount.net * 100,
       // eslint-disable-next-line camelcase
-      unit_amount_with_tax: product.amount.gross,
+      unit_amount_with_tax: product.amount.gross * 100,
       // eslint-disable-next-line camelcase
       currency_id: product.amount.currency,
     };
