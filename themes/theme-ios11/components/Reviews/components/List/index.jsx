ae363e2b698f74d53b6068fbbf8665414207ff8b
Carsten Schneider <carsten.schneider@shopgate.com>
2021-01-07 13:07:34 +0100

    CCP-2347 added new portal position for each review entry

--- themes/theme-ios11/components/Reviews/components/List/index.jsx
+++ themes/theme-ios11/components/Reviews/components/List/index.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
+import { SurroundPortals } from '@shopgate/engage/components';
 import PropTypes from 'prop-types';
+import { PRODUCT_REVIEWS_ENTRY } from '@shopgate/pwa-common-commerce/product/constants/Portals';
 import Title from './components/Title';
 import Rating from './components/Rating';
 import Text from './components/Text';
@@ -32,10 +34,12 @@ class List extends PureComponent {
       <ul>
         { reviews.map(review => (
           <li key={review.id} className={styles} data-test-id={`reviewTitle: ${review.title}`}>
-            <Title title={review.title} />
-            <Rating rate={review.rate} />
-            <Text review={review.review} />
-            <Info review={review} />
+            <SurroundPortals portalName={PRODUCT_REVIEWS_ENTRY} portalProps={{ review }}>
+              <Title title={review.title} />
+              <Rating rate={review.rate} />
+              <Text review={review.review} />
+              <Info review={review} />
+            </SurroundPortals>
           </li>
         ))}
       </ul>
