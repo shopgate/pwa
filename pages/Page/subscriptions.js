d5d6182c45d25d5a9380f6bc0038278f3b083c93
Frithjof Klös <frithjof.kloes@shopgate.com>
2021-01-18 17:39:10 +0100

    CURB-777 Reduced product data caching to 59 seconds

--- pages/Page/subscriptions.js
+++ pages/Page/subscriptions.js
@@ -1,5 +1,9 @@
 import { fetchPageConfig } from '@shopgate/pwa-common/actions/page';
-import { pageWillEnter$ } from './streams';
+import { getPageConfigById } from '@shopgate/pwa-common/selectors/page';
+import { PAGE_ID_INDEX } from '@shopgate/pwa-common/constants/PageIDs';
+import expireProductsByHash from '@shopgate/pwa-common-commerce/product/action-creators/expireProductsByHash';
+import { pageWillEnter$, clearExpiredProducts$ } from './streams';
+import { getProductsResult } from '../../widgets/selectors';
 
 /**
  * Page subscriptions.
@@ -8,5 +12,30 @@ import { pageWillEnter$ } from './streams';
 export default function page(subscribe) {
   subscribe(pageWillEnter$, ({ action, dispatch }) => {
     dispatch(fetchPageConfig(action.route.params.pageId));
+  });
+
+  subscribe(clearExpiredProducts$, ({ action, dispatch, getState }) => {
+    const state = getState();
+
+    const pageData = getPageConfigById(
+      state,
+      { pageId: action.route.params.pageId || PAGE_ID_INDEX }
+    );
+
+    if (pageData?.widgets) {
+      pageData.widgets.forEach(({ id, settings }) => {
+        const { queryType, sortOrder: sort, queryParams: value } = settings;
+
+        if (!queryType) {
+          return;
+        }
+
+        const { hash, expired } = getProductsResult(state, queryType, { sort, value }, id);
+
+        if (expired) {
+          dispatch(expireProductsByHash(hash));
+        }
+      });
+    }
   });
 }
