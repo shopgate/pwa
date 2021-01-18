237462acd39158f3f7b6829f03168b6cc394cfba
Frithjof Klös <frithjof.kloes@shopgate.com>
2021-01-18 17:39:10 +0100

    CURB-777 Reduced product data caching to 59 seconds

--- pages/Page/streams.js
+++ pages/Page/streams.js
@@ -1,10 +1,17 @@
+import { ACTION_PUSH, ACTION_REPLACE } from '@virtuous/conductor';
 import { getCurrentRoute } from '@shopgate/pwa-common/selectors/router';
 import { routeWillEnter$ } from '@shopgate/pwa-common/streams/router';
 import { main$ } from '@shopgate/pwa-common/streams/main';
 import { RECEIVE_PAGE_CONFIG } from '@shopgate/pwa-common/constants/ActionTypes';
+import { startPageWillEnter$ } from '../StartPage/streams';
 
 export const pageWillEnter$ = routeWillEnter$
   .filter(({ action }) => !!action.route.params.pageId);
+
+export const clearExpiredProducts$ = pageWillEnter$
+  .merge(startPageWillEnter$)
+  .filter(({ action }) =>
+    [ACTION_PUSH, ACTION_REPLACE].includes(action?.historyAction));
 
 export const receivedVisiblePageConfig$ = main$
   .filter(({ action, getState }) => {
