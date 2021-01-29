e7fc239b144493de5f8f3b88ae9625b8b16d4731
O.Bilenko <oleksandr.bilenko@shopgate.com>
2021-01-29 11:36:00 +0100

    CCP-2362 Add default cms icons inside gmd navdrawer

--- themes/theme-gmd/components/NavDrawer/components/QuickLinks/index.jsx
+++ themes/theme-gmd/components/NavDrawer/components/QuickLinks/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { NavDrawer } from '@shopgate/pwa-ui-material';
+import camelCase from 'lodash/camelCase';
+import { themeConfig } from '@shopgate/engage';
+import { NavDrawer, DescriptionIcon, Icon } from '@shopgate/engage/components';
 import connect from './connector';
+
+const { icons } = themeConfig;
 
 /**
  * @param {Array} props.links An array of quicklinks.
@@ -10,14 +14,25 @@ import connect from './connector';
 const QuickLinks = ({ links, navigate }) => (
   (links && links.length > 0) && (
     <NavDrawer.Section>
-      {links.map(link => (
-        <NavDrawer.Item
-          aria-hidden
-          key={link.url}
-          label={link.label}
-          onClick={() => navigate(link.url, link.label)}
-        />
-      ))}
+      {links.map((link) => {
+        let icon = DescriptionIcon;
+
+        // Convert /page/some-link to pageSomeLink for custom icons
+        const path = camelCase(link.url);
+        if (icons[path]) {
+          icon = props => <Icon content={icons[path]} {...props} />;
+        }
+
+        return (
+          <NavDrawer.Item
+            aria-hidden
+            key={link.url}
+            label={link.label}
+            onClick={() => navigate(link.url, link.label)}
+            icon={icon}
+          />
+        );
+      })}
     </NavDrawer.Section>
   )
 );
