f534f2d54b0c3e537f6b682e0dbdb58d33e5f5b9
Carsten Schneider <carsten.schneider@shopgate.com>
2021-02-19 12:30:46 +0100

    CCP-2368 fixing tests

--- widgets/Products/ProductsWidget.spec.jsx
+++ widgets/Products/ProductsWidget.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { ProductList } from '@shopgate/engage/product';
 import ProductGrid from 'Components/ProductGrid';
-import ProductList from 'Components/ProductList';
 import { UnwrappedProductsWidget as ProductsWidget } from './ProductsWidget';
 
 describe('<ProductsWidget />', () => {
