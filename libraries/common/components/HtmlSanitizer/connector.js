096e690fd261f1ee2a3409e391562cfa5366a13a
O.Bilenko <oleksandr.bilenko@shopgate.com>
2021-02-26 14:55:25 +0100

    CCP-2382 Add navigation handling to HtmlSanitizer by default

---
+++ libraries/common/components/HtmlSanitizer/connector.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { historyPush } from '../../actions/router';
+
+/**
+ * Connects the dispatch function to a callable function in the props.
+ * @param  {Function} dispatch The redux dispatch function.
+ * @return {Object} The extended component props.
+ */
+const mapDispatchToProps = dispatch => ({
+  navigate: (pathname, target) => dispatch(historyPush({
+    pathname,
+    ...target && { state: { target } },
+  })),
+});
+
+export default connect(null, mapDispatchToProps);
