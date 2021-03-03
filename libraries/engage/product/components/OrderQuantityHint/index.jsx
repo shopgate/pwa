f460e701c866d0e30d253663809de8788afa81ba
Frithjof Klös <frithjof.kloes@shopgate.com>
2021-03-03 09:36:34 +0100

    CURB-898 Removed OrderQuantityHint dependency to theme-config extension

--- libraries/engage/product/components/OrderQuantityHint/index.jsx
+++ libraries/engage/product/components/OrderQuantityHint/index.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { I18n, SurroundPortals } from '@shopgate/engage/components';
-import { useWidgetSettings, i18n } from '@shopgate/engage/core';
+import { i18n } from '@shopgate/engage/core';
 import { PRODUCT_ORDER_QUANTITY } from '@shopgate/engage/product';
 import { formatFloat } from '@shopgate/engage/components/QuantityInput/helper';
 import withProductStock from '../../hocs/withProductStock';
@@ -13,8 +13,6 @@ import { hint } from './style';
  * @return {JSX}
  */
 const OrderQuantityHint = ({ stock, product, className }) => {
-  const settings = useWidgetSettings('@shopgate/engage/product/OrderQuantityHint');
-
   const { hasCatchWeight, unit: productUnit } = product || {};
 
   const unit = useMemo(() => {
@@ -35,7 +33,7 @@ const OrderQuantityHint = ({ stock, product, className }) => {
   return (
     <div className={className}>
       <SurroundPortals portalName={PRODUCT_ORDER_QUANTITY} portalProps={{ stock }}>
-        {settings.show && stock && !!stock.minOrderQuantity &&
+        {stock && !!stock.minOrderQuantity &&
           <div className={hint}>
             <I18n.Text
               string="product.minOrderQuantity"
@@ -46,7 +44,7 @@ const OrderQuantityHint = ({ stock, product, className }) => {
             />
           </div>
           }
-        {settings.show && stock && !!stock.maxOrderQuantity &&
+        {stock && !!stock.maxOrderQuantity &&
           <div className={hint}>
             <I18n.Text
               string="product.maxOrderQuantity"
