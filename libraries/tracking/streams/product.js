931391e55c1d2d80ce4410ae98150ce5a79a2733
Carsten Schneider <carsten.schneider@shopgate.com>
2021-04-28 08:22:13 +0200

    CURB-1028 resolve circular deps

--- libraries/tracking/streams/product.js
+++ libraries/tracking/streams/product.js
@@ -1,12 +1,12 @@
 import 'rxjs/add/operator/switchMap';
 import { main$ } from '@shopgate/pwa-common/streams/main';
 import { routeWillEnter$ } from '@shopgate/pwa-common/streams/router';
+import { pwaDidAppear$ } from '@shopgate/pwa-common/streams/app';
+import { receivedVisibleProduct$ } from '@shopgate/pwa-common-commerce/product/streams';
 import {
   RECEIVE_PRODUCTS,
   ITEM_PATTERN,
 } from '@shopgate/pwa-common-commerce/product/constants';
-
-export { productIsReady$, productRouteReappeared$ } from '@shopgate/engage/product';
 
 /**
  * Emits when product results has been received.
@@ -15,7 +15,21 @@ export const productsReceived$ = main$
   .filter(({ action }) => action.type === RECEIVE_PRODUCTS);
 
 /**
+ * Emits when the category route comes active again after a legacy page was active.
+ */
+export const productRouteReappeared$ = pwaDidAppear$
+  .filter(({ action }) => action.route.pattern === ITEM_PATTERN);
+
+/**
  * Emits when a product page was initially opened.
  */
 export const productWillEnter$ = routeWillEnter$
   .filter(({ action }) => action.route.pattern === ITEM_PATTERN);
+
+/**
+ * Emits when a product page was initially opened and its data is present.
+ */
+export const productIsReady$ = productWillEnter$
+  // Take care that the stream only emits when underlying streams emit within the correct order.
+  .switchMap(() => receivedVisibleProduct$.first())
+  .merge(productRouteReappeared$);
