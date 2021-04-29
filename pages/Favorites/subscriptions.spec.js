e9e6d1902ba86ab71d001e83ca91802f53b65967
O.Bilenko <oleksandr.bilenko@shopgate.com>
2021-04-29 16:13:57 +0200

    CCP-2401 Use lazy favorites loading

--- pages/Favorites/subscriptions.spec.js
+++ pages/Favorites/subscriptions.spec.js
@@ -1,13 +1,13 @@
 import { UIEvents } from '@shopgate/pwa-core';
 import { createMockStore } from '@shopgate/pwa-common/store';
-import { REQUEST_REMOVE_FAVORITES } from '@shopgate/pwa-common-commerce/favorites/constants';
+import { REMOVE_PRODUCT_FROM_FAVORITES } from '@shopgate/pwa-common-commerce/favorites/constants';
 import subscriber from './subscriptions';
 
 const store = createMockStore(() => {}, subscriber);
 
 describe('Favorites Subscriptions', () => {
   it('should display a toast message when an item was deleted', (done) => {
-    store.dispatch({ type: REQUEST_REMOVE_FAVORITES });
+    store.dispatch({ type: REMOVE_PRODUCT_FROM_FAVORITES });
     setTimeout(() => {
       expect(UIEvents.emit).toHaveBeenCalled();
       done();
