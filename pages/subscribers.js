73174523568dc4ac5b0ab18d6219498165190dd4
Carsten Schneider <carsten.schneider@shopgate.com>
2021-07-05 17:23:10 +0200

    CCP-2425 show inventory in product lists

--- pages/subscribers.js
+++ pages/subscribers.js
@@ -16,6 +16,7 @@ import orders from '@shopgate/engage/orders/subscriptions';
 import { subscriptions as coreConfig } from '@shopgate/engage/core/config';
 import commerceReviews from '@shopgate/pwa-common-commerce/reviews/subscriptions';
 import commerceSearch from '@shopgate/pwa-common-commerce/search/subscriptions';
+import commerceCategory from '@shopgate/pwa-common-commerce/category/subscriptions';
 import commerceScanner from '@shopgate/pwa-common-commerce/scanner/subscriptions';
 // Engage
 import engageCheckout from '@shopgate/engage/checkout/subscriptions';
@@ -34,7 +35,6 @@ import trackingScanner from '@shopgate/pwa-tracking/subscriptions/scanner';
 // Theme
 import app from 'Pages/subscriptions';
 import viewport from 'Components/Viewport/subscriptions';
-import category from 'Pages/Category/subscriptions';
 import coupon from 'Pages/Cart/components/CouponField/subscriptions';
 import browse from 'Pages/Browse/subscriptions';
 import addToCartBar from 'Pages/Product/components/AddToCartBar/subscriptions';
@@ -42,7 +42,6 @@ import favorites from 'Pages/Favorites/subscriptions';
 import login from 'Pages/Login/subscriptions';
 import startPage from 'Pages/StartPage/subscriptions';
 import page from 'Pages/Page/subscriptions';
-import search from 'Pages/Search/subscriptions';
 import reviews from 'Pages/Reviews/subscriptions';
 import writeReview from 'Pages/WriteReview/subscriptions';
 import tabBar from 'Components/TabBar/subscriptions';
@@ -79,6 +78,7 @@ const subscriptions = [
   coreConfig,
   commerceReviews,
   commerceSearch,
+  commerceCategory,
   commerceScanner,
   // Engage subscribers
   engageCheckout,
@@ -88,14 +88,12 @@ const subscriptions = [
   app,
   viewport,
   browse,
-  category,
   coupon,
   addToCartBar,
   favorites,
   login,
   startPage,
   page,
-  search,
   reviews,
   writeReview,
   tabBar,
