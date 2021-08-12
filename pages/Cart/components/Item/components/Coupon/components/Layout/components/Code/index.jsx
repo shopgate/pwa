a2220b544ee5314d32a8b9306c41e561fd36fb64
Aien <aien.saidi@gmail.com>
2021-08-12 16:21:45 +0200

    CCP-2475
    - wrap the coupon code component inside the Ellipsis of engage

--- pages/Cart/components/Item/components/Coupon/components/Layout/components/Code/index.jsx
+++ pages/Cart/components/Item/components/Coupon/components/Layout/components/Code/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import I18n from '@shopgate/pwa-common/components/I18n';
+import { Ellipsis } from '@shopgate/engage/components';
+import { css } from 'glamor';
+
+/**
+ * on iOS theme the maximum with the
+ * ellipsis wrapper can have is almost 179px
+ * @type {{ellipsis: string}}
+ */
+const styles = {
+  ellipsis: css({
+    width: '175px',
+    whiteSpace: 'nowrap',
+    display: 'inline-block !important',
+  }).toString(),
+};
 
 /**
 * The Coupon Code component.
@@ -8,11 +23,11 @@ import I18n from '@shopgate/pwa-common/components/I18n';
 * @returns {JSX}
 */
 const Code = ({ value }) => (
-  <div>
+  <Ellipsis className={styles.ellipsis}>
     <I18n.Text string="cart.coupon_code" />
     {': '}
     {value}
-  </div>
+  </Ellipsis>
 );
 
 Code.propTypes = {
