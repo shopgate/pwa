03513022b3e834ec3646da8481be8a140cc4da93
Aien <aien.saidi@gmail.com>
2021-08-13 14:44:10 +0200

    CCP-2475
    - fix the issue with two row and free shipping

--- pages/Cart/components/Item/components/Coupon/components/Layout/components/Code/index.jsx
+++ pages/Cart/components/Item/components/Coupon/components/Layout/components/Code/index.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import { Ellipsis } from '@shopgate/engage/components';
-import { css } from 'glamor';
-
-const styles = {
-  ellipsis: css({
-    whiteSpace: 'nowrap',
-    display: 'inline-block !important',
-  }).toString(),
-};
 
 /**
-* The Coupon Code component.
-* @param {Object} props The component props.
-* @returns {JSX}
-*/
+ * The Coupon Code component.
+ * @param {Object} props The component props.
+ * @returns {JSX}
+ */
 const Code = ({ value }) => (
-  <Ellipsis className={styles.ellipsis}>
+  <Ellipsis rows={2}>
     <I18n.Text string="cart.coupon_code" />
     {': '}
     {value}
