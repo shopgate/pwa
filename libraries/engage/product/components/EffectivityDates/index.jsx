6f1ccbc81632ea906f03ce96a74fd7e989b3f3a1
Aien <aien.saidi@gmail.com>
2021-08-19 16:05:16 +0200

    CURB-1388
    - wrap EffectivityDates in SurroundPortal

--- libraries/engage/product/components/EffectivityDates/index.jsx
+++ libraries/engage/product/components/EffectivityDates/index.jsx
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { css } from 'glamor';
-import { isBeta, useWidgetSettings, useWidgetStyles } from '@shopgate/engage/core';
-import { I18n, TimeBoundary } from '@shopgate/engage/components';
+import {
+  isBeta,
+  useWidgetSettings,
+  useWidgetStyles,
+} from '@shopgate/engage/core';
+import { I18n, TimeBoundary, SurroundPortals } from '@shopgate/engage/components';
+import { PRODUCT_EFFECTIVITY_DATES } from '@shopgate/pwa-common-commerce/product/constants/Portals';
 import { showExpiringLabel, showScheduledLabel } from './helpers';
 import { hint } from './style';
 import connect from './connector';
@@ -13,7 +18,7 @@ import connect from './connector';
  * @return {JSX}
  */
 const EffectivityDates = ({
-  dates, children, productNotAvailable,
+  dates, children, productNotAvailable, productId,
 }) => {
   if (!isBeta() || !dates) {
     return children;
@@ -29,35 +34,40 @@ const EffectivityDates = ({
   const hintClass = classNames(hint, hintAddClass);
 
   return (
-    <TimeBoundary start={startDate} end={endDate}>
-      {({ before, between, after }) => {
-        if (before) {
-          return showScheduledLabel(startDate, settings)
-            ? <I18n.Text string="product.available.at" params={{ startDate }} className={hintClass} />
-            : children;
-        }
+    <SurroundPortals
+      portalName={PRODUCT_EFFECTIVITY_DATES}
+      portalProps={{ dates, productNotAvailable, productId }}
+    >
+      <TimeBoundary start={startDate} end={endDate}>
+        {({ before, between, after }) => {
+          if (before) {
+            return showScheduledLabel(startDate, settings)
+              ? <I18n.Text string="product.available.at" params={{ startDate }} className={hintClass} />
+              : children;
+          }
 
-        if (between) {
-          return (
-            <Fragment>
-              {children}
-              {showExpiringLabel(endDate, settings) &&
+          if (between) {
+            return (
+              <Fragment>
+                {children}
+                {showExpiringLabel(endDate, settings) &&
                 <I18n.Text string="product.available.until" params={{ endDate }} className={hintClass} />
-              }
-            </Fragment>
-          );
-        }
+                }
+              </Fragment>
+            );
+          }
 
-        if (after) {
-          productNotAvailable();
+          if (after) {
+            productNotAvailable();
 
-          return showExpiringLabel(endDate, settings)
-            ? <I18n.Text string="product.available.not" className={hintClass} />
-            : children;
-        }
-        return children;
-      }}
-    </TimeBoundary>
+            return showExpiringLabel(endDate, settings)
+              ? <I18n.Text string="product.available.not" className={hintClass} />
+              : children;
+          }
+          return children;
+        }}
+      </TimeBoundary>
+    </SurroundPortals>
   );
 };
 
@@ -68,11 +78,13 @@ EffectivityDates.propTypes = {
     startDate: PropTypes.string.isRequired,
     endDate: PropTypes.string.isRequired,
   }),
+  productId: PropTypes.string,
 };
 
 EffectivityDates.defaultProps = {
   children: null,
   dates: null,
+  productId: null,
 };
 
 export default connect(EffectivityDates);
