6c11631d428cb48f5f94b6e1796170e1f86a55ce
SaschaKrist <sascha.krist@me.com>
2021-09-09 14:59:50 +0200

    CCP-2486 fix styling for FilterBar / ScrollHeader

--- components/FilterBar/style.js
+++ components/FilterBar/style.js
@@ -4,7 +4,8 @@ import { themeShadows } from '@shopgate/pwa-common/helpers/config';
 export default css({
   boxShadow: themeShadows.material,
   position: 'sticky',
-  top: '-2px', // behind header shadow
+  // todo: top has to be set dynamic depending on whether persistent-search-bar is installed or not
+  top: '56px', // top: '-2px', // behind header shadow
   left: 0,
   zIndex: 1,
 });
