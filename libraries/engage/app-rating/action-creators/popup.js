3c99f559860bc1a8eafa4d87b31f3806eead28a5
Aien Saidi <aien.saidi@gmail.com>
2021-09-22 15:16:52 +0200

    CURB-1449
    - fix the issue with already rated logic

--- libraries/engage/app-rating/action-creators/popup.js
+++ libraries/engage/app-rating/action-creators/popup.js
@@ -1,4 +1,8 @@
-import { INCREASE_REJECTION_COUNT, SET_LAST_POPUP_TIMESTAMP } from '../constants';
+import {
+  INCREASE_REJECTION_COUNT,
+  SET_ALREADY_RATED,
+  SET_LAST_POPUP_TIMESTAMP,
+} from '../constants';
 
 /**
  * Sets the last popup timestamp
@@ -14,5 +18,15 @@ export const setLastPopupTimestamp = () => ({
  */
 export const increaseRejectionCount = () => ({
   type: INCREASE_REJECTION_COUNT,
+});
+
+/**
+ * Sets the last popup timestamp
+ * @param {boolean} to the value to set the rated status
+ * @return {Object} The dispatched action object.
+ */
+export const setAlreadyRated = to => ({
+  type: SET_ALREADY_RATED,
+  to,
 });
 
