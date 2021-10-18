c829c57970db4d7638664d634a310c52b4a98c46
Carsten Schneider <carsten.schneider@shopgate.com>
2021-10-18 17:50:11 +0200

    CCP-2496 move portal children in own component to allow portal to override props

--- libraries/engage/product/components/Description/index.jsx
+++ libraries/engage/product/components/Description/index.jsx
@@ -10,32 +10,54 @@ import {
 import connect from './connector';
 
 /**
+ * @param {Object} props The component props.
+ * @returns {JSX}
+ */
+const Content = ({ html, navigate }) => {
+  if (html === '') {
+    return null;
+  }
+
+  return (
+    <div className={`${container} engage__product__description`}>
+      <div aria-hidden className={title}>
+        <I18n.Text string="product.description_heading" />
+      </div>
+      <PlaceholderParagraph className={placeholder} ready={!!html}>
+        <div className={content} data-test-id="description">
+          <HtmlSanitizer
+            settings={{
+              html,
+              handleClick: navigate,
+            }}
+          >
+            {html}
+          </HtmlSanitizer>
+        </div>
+      </PlaceholderParagraph>
+    </div>
+  );
+};
+
+Content.propTypes = {
+  html: PropTypes.string,
+  navigate: PropTypes.func,
+};
+
+Content.defaultProps = {
+  html: null,
+  navigate: () => { },
+};
+
+/**
  * The product description.
  * @param {Object} props The component props.
  * @returns {JSX}
  */
-function Description({ html, navigate }) {
+function Description(props) {
   return (
-    <SurroundPortals portalName={PRODUCT_DESCRIPTION}>
-      {(html !== '') && (
-        <div className={`${container} engage__product__description`}>
-          <div aria-hidden className={title}>
-            <I18n.Text string="product.description_heading" />
-          </div>
-          <PlaceholderParagraph className={placeholder} ready={!!html}>
-            <div className={content} data-test-id="description">
-              <HtmlSanitizer
-                settings={{
-                  html,
-                  handleClick: navigate,
-                }}
-              >
-                {html}
-              </HtmlSanitizer>
-            </div>
-          </PlaceholderParagraph>
-        </div>
-      )}
+    <SurroundPortals portalName={PRODUCT_DESCRIPTION} portalProps={props}>
+      <Content {...props} />
     </SurroundPortals>
   );
 }
--- themes/theme-gmd/pages/Product/components/Header/components/Name/index.jsx
+++ themes/theme-gmd/pages/Product/components/Header/components/Name/index.jsx
@@ -8,6 +8,42 @@ import connect from './connector';
 import styles from './style';
 
 /**
+ * @param {Object} props The component props.
+ * @return {JSX}
+ */
+const Content = ({ name, longName }) => (
+  <div className={`${styles.name} product-name`} role="heading">
+    {/* This feature is currently in BETA testing.
+        It should only be used for approved BETA Client Projects */}
+    { isBeta()
+      ? (
+        <PlaceholderLabel className={styles.placeholder} ready={(longName !== null)}>
+          <span data-test-id={`name: ${longName}`}>
+            { longName }
+          </span>
+        </PlaceholderLabel>
+      )
+      : (
+        <PlaceholderLabel className={styles.placeholder} ready={(name !== null)}>
+          <span data-test-id={`name: ${name}`}>
+            {name}
+          </span>
+        </PlaceholderLabel>
+      )
+    }
+  </div>
+);
+Content.propTypes = {
+  longName: PropTypes.string,
+  name: PropTypes.string,
+};
+
+Content.defaultProps = {
+  longName: null,
+  name: null,
+};
+
+/**
  * The Product Name component.
  * @param {Object} props The component props.
  * @return {JSX}
@@ -16,26 +52,7 @@ const Name = ({ longName, name }) => (
   <Fragment>
     <Portal name={PRODUCT_NAME_BEFORE} />
     <Portal name={PRODUCT_NAME}>
-      <div className={`${styles.name} product-name`} role="heading">
-        {/* This feature is currently in BETA testing.
-        It should only be used for approved BETA Client Projects */}
-        { isBeta()
-          ? (
-            <PlaceholderLabel className={styles.placeholder} ready={(longName !== null)}>
-              <span data-test-id={`name: ${longName}`}>
-                { longName }
-              </span>
-            </PlaceholderLabel>
-          )
-          : (
-            <PlaceholderLabel className={styles.placeholder} ready={(name !== null)}>
-              <span data-test-id={`name: ${name}`}>
-                {name}
-              </span>
-            </PlaceholderLabel>
-          )
-        }
-      </div>
+      <Content name={name} longName={longName} />
     </Portal>
     <Portal name={PRODUCT_NAME_AFTER} />
   </Fragment>
