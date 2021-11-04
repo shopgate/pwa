66f22f2942338832a507739d2484278407197fb7
Aien Saidi <aien.saidi@gmail.com>
2021-11-04 18:39:43 +0100

    CCP-2499 Update snapshopts and tests

--- libraries/engage/product/components/ProductDiscountBadge/spec.jsx
+++ libraries/engage/product/components/ProductDiscountBadge/spec.jsx
@@ -4,12 +4,12 @@ import ProductDiscountBadge from './index';
 
 describe('<ProductDiscountBadge />', () => {
   it('should render without discount', () => {
-    const wrapper = shallow(<ItemDiscount productId="1234" />);
+    const wrapper = shallow(<ProductDiscountBadge productId="1234" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render with discount', () => {
-    const wrapper = shallow(<ItemDiscount productId="1234" discount={12} />);
+    const wrapper = shallow(<ProductDiscountBadge productId="1234" discount={12} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
