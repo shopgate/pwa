bc416daa61b9708a5b89dc54764be54dc05ad729
Frithjof Klös <frithjof.kloes@shopgate.com>
2022-01-31 16:26:01 +0100

    CCP-2521 Refactored category and search pages to re-establish view.content portals
    - added prop to skip rendering of the view.content portal within the View component
    - surrounded actual page content in category and search with the view.content portal
    -- extensions which use this portal will now render below the filter bar again as originally intended
    - added new portal around filter-bar content
    -- extensions like the persistent search bar can now use this portal on category and search to put filter bar extensions there

--- components/FilterBar/style.js
+++ components/FilterBar/style.js
@@ -4,6 +4,7 @@ import { themeShadows } from '@shopgate/pwa-common/helpers/config';
 export default css({
   boxShadow: themeShadows.material,
   position: 'sticky',
+  top: '-2px', // behind header shadow
   left: 0,
   zIndex: 10,
 });
