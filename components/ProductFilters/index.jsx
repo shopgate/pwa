c2eb3c869372ec9385430a745fe8844ecc9f2e63
Frithjof Klös <frithjof.kloes@shopgate.com>
2022-01-31 19:50:28 +0100

    CCP-2521 Refactored category and search pages to re-establish view.content portals
    - added prop to skip rendering of the view.content portal within the View component
    - surrounded actual page content in category and search with the view.content portal
    -- extensions which use this portal will now render below the filter bar again as originally intended
    - added new portal around filter-bar content
    -- extensions like the persistent search bar can now use this portal on category and search to put filter bar extensions there

--- components/ProductFilters/index.jsx
+++ components/ProductFilters/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useRoute } from '@shopgate/engage/core';
-import { ResponsiveContainer, ScrollHeader } from '@shopgate/engage/components';
+import { ResponsiveContainer, ScrollHeader, SurroundPortals } from '@shopgate/engage/components';
 import { GlobalLocationSwitcher, FulfillmentSlotSwitcher } from '@shopgate/engage/locations';
 import FilterBar from 'Components/FilterBar';
 import { filters } from './style';
@@ -14,17 +14,19 @@ const ProductFilters = ({ categoryId, showFilters }) => {
   const { state } = useRoute();
   return (
     <ScrollHeader className={filters}>
-      <ResponsiveContainer appAlways breakpoint="<=xs">
-        <GlobalLocationSwitcher renderBar />
-        <FulfillmentSlotSwitcher renderBar />
-      </ResponsiveContainer>
+      <SurroundPortals portalName="filter-bar.content">
+        <ResponsiveContainer appAlways breakpoint="<=xs">
+          <GlobalLocationSwitcher renderBar />
+          <FulfillmentSlotSwitcher renderBar />
+        </ResponsiveContainer>
 
-      {showFilters && (
+        {showFilters && (
         <FilterBar
           categoryId={categoryId}
           filters={state.filters}
         />
-      )}
+        )}
+      </SurroundPortals>
     </ScrollHeader>
   );
 };
