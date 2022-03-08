69323990605a0cc79e5544ea199c45df03cf9907
rocknegi <rocknegi53@gmail.com>
2022-03-08 11:46:36 +0100

    PWA config to show empty rating stars

--- frontend/RatingStars/index.jsx
+++ frontend/RatingStars/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
 import appConfig from '@shopgate/pwa-common/helpers/config';
+import { useWidgetSettings } from '@shopgate/engage/core/hooks/useWidgetSettings';
 import Stars from './components/Stars';
 import connect from './connector';
 
@@ -12,13 +13,12 @@ const { hasReviews } = appConfig;
  * @return {JSX}
  */
 const RatingStars = ({ product }) => {
-  // TODO: adjust condition like we did on PDP
-
-  if (!hasReviews || !product || !product.rating) {
-    return null;
-  }
-
-  return <Stars value={product.rating.average} />;
+  const { showEmptyRatingStars = false } = useWidgetSettings('@shopgate/engage/rating');
+  const showRatings = showEmptyRatingStars ?
+    hasReviews && product.rating && product
+    :
+    hasReviews && product.rating && product.rating.count;
+  return (showRatings && <Stars value={product.rating.average} />);
 };
 
 RatingStars.propTypes = {
