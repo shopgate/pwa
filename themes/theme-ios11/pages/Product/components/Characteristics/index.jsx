4f162e5a605614e21e9874710a970a5e658200c1
Frithjof Klös <frithjof.kloes@shopgate.com>
2022-10-14 22:24:22 +0200

    Revert "CCP-2591 Added product ids to product.variant-select portals"

--- themes/theme-ios11/pages/Product/components/Characteristics/index.jsx
+++ themes/theme-ios11/pages/Product/components/Characteristics/index.jsx
@@ -64,13 +64,13 @@ class Characteristics extends PureComponent {
   render() {
     return (
       <Fragment>
-        <Portal name={PRODUCT_VARIANT_SELECT_BEFORE} props={this.props} />
-        <Portal name={PRODUCT_VARIANT_SELECT} props={this.props}>
+        <Portal name={PRODUCT_VARIANT_SELECT_BEFORE} />
+        <Portal name={PRODUCT_VARIANT_SELECT}>
           <ProductContext.Consumer>
             {this.consumeRenderer}
           </ProductContext.Consumer>
         </Portal>
-        <Portal name={PRODUCT_VARIANT_SELECT_AFTER} props={this.props} />
+        <Portal name={PRODUCT_VARIANT_SELECT_AFTER} />
       </Fragment>
     );
   }
