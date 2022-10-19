eb9c254f1d172e393dab7f15b2803d07ff7d80e9
rocknegi <rocknegi53@gmail.com>
2022-10-20 00:00:05 +0200

    CCP-2607 add new portal to avoid breaking changes in the more page

--- components/NavDrawer/components/QuickLinks/index.jsx
+++ components/NavDrawer/components/QuickLinks/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { NavDrawer } from '@shopgate/pwa-ui-material';
+import { SurroundPortals } from '@shopgate/engage/components';
+import {
+  QUICK_LINKS_CONTENT,
+} from '@shopgate/pwa-common/constants/Portals';
+import portalProps from '../../portalProps';
 import connect from './connector';
-
 /**
  * @param {Array} props.links An array of quicklinks.
  * @param {Function} props.navigate The navigate function.
@@ -9,15 +13,18 @@ import connect from './connector';
  */
 const QuickLinks = ({ links, navigate }) => (
   (links && links.length > 0) && (
-    <NavDrawer.Section>
-      {links.map(link => (
-        <NavDrawer.Item
-          key={link.url}
-          label={link.label}
-          onClick={() => navigate(link.url, link.label)}
-        />
-      ))}
-    </NavDrawer.Section>
+    <SurroundPortals portalName={QUICK_LINKS_CONTENT} portalProps={portalProps}>
+      <NavDrawer.Section>
+        {links.map(link => (
+          <NavDrawer.Item
+            key={link.url}
+            label={link.label}
+            onClick={() => navigate(link.url, link.label)}
+          />
+        ))}
+      </NavDrawer.Section>
+    </SurroundPortals>
+
   )
 );
 
