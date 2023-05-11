cfd3709f1881b4704284f4e30526528ee89aa91d
Sascha Krist <sascha.krist@shopgate.com>
2023-05-11 08:16:43 +0200

    CURB-3197 Small fixes.

--- libraries/commerce/favorites/subscriptions/index.js
+++ libraries/commerce/favorites/subscriptions/index.js
@@ -128,7 +128,7 @@ export default function favorites(subscribe) {
         );
 
         allOnList.forEach(listProduct =>
-          dispatch(requestRemoveFavorites(listProduct, action.listId)));
+          dispatch(requestRemoveFavorites(listProduct.id, action.listId)));
         return;
       }
 
