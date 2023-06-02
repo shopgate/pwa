28c3202733d3c551b0f8c47fd14eb850ab4223bf
Frithjof Klös <frithjof.kloes@shopgate.com>
2023-06-02 16:21:54 +0200

    CURB-3240 Removed wishlist quantity from aria-label of wishlist badges

--- themes/theme-ios11/components/TabBar/components/connector.js
+++ themes/theme-ios11/components/TabBar/components/connector.js
@@ -1,5 +1,15 @@
 import { connect } from 'react-redux';
+import { getShowWishlistItemsCountBadge } from '@shopgate/engage/core';
 import { navigate } from './actions';
+
+/**
+ * Maps the contents of the state to the component props.
+ * @param {Object} state The current application state.
+ * @return {Object} The extended component props.
+ */
+const mapStateToProps = state => ({
+  showWishlistItemsCountBadge: getShowWishlistItemsCountBadge(state),
+});
 
 /**
  * @return {Object} The extended component props.
@@ -8,5 +18,5 @@ const mapDispatchToProps = {
   historyPush: navigate,
 };
 
-export default connect(null, mapDispatchToProps);
+export default connect(mapStateToProps, mapDispatchToProps);
 
