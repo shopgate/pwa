950a4ee9ff91cc31d326658bccd887365304737c
Frithjof Klös <frithjof.kloes@shopgate.com>
2023-11-17 15:33:20 +0100

    CURB-3191 Migrated reviews refactoring from PWA6
    - added reviews components into @shopgate/engage library
    - removed reviews components from themes
    - added reviews info feature

---
+++ libraries/engage/reviews/components/Reviews/components/Header/components/WriteReviewLink/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import I18n from '@shopgate/pwa-common/components/I18n';
+import { i18n } from '@shopgate/engage/core';
+import { ITEM_PATH } from '@shopgate/pwa-common-commerce/product/constants';
+import { bin2hex } from '@shopgate/pwa-common/helpers/data';
+import ButtonLink from '@shopgate/pwa-ui-shared/ButtonLink';
+
+/**
+ * Link to add a review.
+ * @returns {JSX|null}
+ */
+const WriteReviewLink = ({ productId }) => (
+  <div data-test-id="writeReview" aria-hidden className="engage__reviews__write-review-link">
+    <ButtonLink href={`${ITEM_PATH}/${bin2hex(productId)}/write_review`} noGap aria-label={i18n.text('reviews.button_add')}>
+      <I18n.Text string="reviews.button_add" />
+    </ButtonLink>
+  </div>
+);
+
+WriteReviewLink.propTypes = {
+  productId: PropTypes.string.isRequired,
+};
+
+export default WriteReviewLink;
---
+++ libraries/engage/reviews/components/Reviews/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import appConfig from '@shopgate/pwa-common/helpers/config';
+import { SurroundPortals } from '@shopgate/engage/components';
+import { PRODUCT_REVIEWS } from '@shopgate/engage/product';
+import List from './components/List';
+import Header from './components/Header';
+import AllReviewsLink from './components/AllReviewsLink';
+import ReviewsInfo from './components/ReviewsInfo';
+import styles from './style';
+import connect from './connector';
+
+/**
+ * @param {Object} props The component props.
+ * @param {Object} props.productId The id of the product, the review belongs to.
+ * @param {Array} props.reviews Reviews which should be shown in the product page.
+ * @returns {JSX}
+ */
+function Reviews({ productId, productActive, reviews }) {
+  return (
+    <SurroundPortals
+      portalName={PRODUCT_REVIEWS}
+      portalProps={{ productId }}
+    >
+      {(appConfig.hasReviews && productActive) && (
+        <div className={`${styles.container} engage__reviews__reviews`} data-test-id="reviewSection">
+          <Header productId={productId} />
+          <List productId={productId} reviews={reviews} />
+          <AllReviewsLink productId={productId} />
+          <ReviewsInfo reviews={reviews} />
+        </div>
+      )}
+    </SurroundPortals>
+  );
+}
+
+Reviews.propTypes = {
+  productActive: PropTypes.bool,
+  productId: PropTypes.string,
+  reviews: PropTypes.arrayOf(PropTypes.shape()),
+};
+
+Reviews.defaultProps = {
+  productId: null,
+  productActive: true,
+  reviews: null,
+};
+
+export default connect(Reviews);
