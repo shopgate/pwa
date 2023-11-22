120913666a20c82eca3868ccbdfe585bb1650d18
Frithjof Klös <frithjof.kloes@shopgate.com>
2023-11-22 18:23:08 +0100

    CURB-3728 Extended types for useProductListType hook

--- libraries/engage/product/providers/ProductListType/context.js
+++ libraries/engage/product/providers/ProductListType/context.js
@@ -1,9 +1,21 @@
 import React from 'react';
+
+/* eslint-disable max-len */
+/**
+ * @typedef {"productSlider"|"productGrid"|"productList"|"favoritesList"|"cart"|"liveshopping"|"pdp"|"productGallery"} ProductListTypeContextType
+ */
+
+/**
+ * @typedef {"widgets"|"category"} ProductListTypeContextSubType
+ */
+
+/* eslint-enable max-len */
+
 /**
  * @typedef ProductListTypeContextValue
- * @property {string} [type=null] Type of the currently active ProductListTypeContext
+ * @property {ProductListTypeContextType} [type=null] Type of the active ProductListTypeContext
  * e.g. "productSlider" or "productGrid".
- * @property {string} [subType=null] Optional sub type of the currently active
+ * @property {ProductListTypeContextSubType} [subType=null] Optional sub type of the active
  * ProductListTypeContext. Depending on its usage it can make a statement about in which context
  * the product list is used e.g. "widgets".
  */
