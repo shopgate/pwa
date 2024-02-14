b12d9037c30566d6a3a4cf46628772a3d5749806
Sascha Krist <sascha.krist@shopgate.com>
2024-02-14 09:34:31 +0100

    CURB-3886 Refactor getProductType

--- libraries/engage/back-in-stock/components/BackInStockButtonPortal/connector.js
+++ libraries/engage/back-in-stock/components/BackInStockButtonPortal/connector.js
@@ -6,19 +6,22 @@ import {
 import { addBackInStockSubscription } from '@shopgate/engage/back-in-stock/actions';
 import {
   getProductAvailability,
-  getProductType,
+  makeGetProductType,
 } from '@shopgate/engage/product';
 import { grantPushPermissions } from '@shopgate/engage/core';
 
 /**
  * @return {Object} The extended component props.
  */
-const makeMapStateToProps = () => (state, props) => ({
-  subscription: getSubscriptionByVariant(state, props),
-  productType: getProductType(state, props),
-  stock: getProductAvailability(state, props),
-  isBackInStockEnabled: getIsBackInStockEnabled(state, props),
-});
+const makeMapStateToProps = () => {
+  const getProductType = makeGetProductType();
+  return (state, props) => ({
+    subscription: getSubscriptionByVariant(state, props),
+    productType: getProductType(state, props),
+    stock: getProductAvailability(state, props),
+    isBackInStockEnabled: getIsBackInStockEnabled(state, props),
+  });
+};
 
 const mapDispatchToProps = {
   addBackInStockSubscription,
