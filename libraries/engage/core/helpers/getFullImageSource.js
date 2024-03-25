4151d85a85a038a4baa4799f54f766bed5ddb7fa
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-03-25 14:44:41 +0100

    CURB-3951 Improved image handling
    - all image service urls are now created by the getFullImageSource helper
    - getFullImageSource image source helper takes care that image service query params are added in a specific order and that the url doesn't contain duplicates
    - removed the 50x50 default resolution from the Image component

--- libraries/engage/core/helpers/getFullImageSource.js
+++ libraries/engage/core/helpers/getFullImageSource.js
@@ -2,6 +2,40 @@ import { getProductImageSettings } from '../../product/helpers';
 import { getImageFormat } from './getImageFormat';
 
 /**
+ * Regex to detect the "fill" query parameter
+ */
+const fillParamRegex = /(fill|fillc)=(\w+)%2C(\d)($|&)/i;
+
+/**
+ * Appends image service parameters to an image service url
+ * @param {string} baseUrl The base url
+ * @param {Object} params Additional params for the url
+ * @returns {string}
+ */
+const buildUrl = (baseUrl, params = {}) => {
+  const parsedUrl = new URL(baseUrl);
+
+  /**
+   * To improve caching of images, we need to create urls with a consistent order of query params.
+   * Otherwise it can happen that different urls create equal images, just because of different
+   * ordered query params.
+   * To reach this goal we utilize the "append" method of the URLSearchParams object and apply
+   * the params in the order given by the "params" object. Since "append" could create duplicate
+   * query params, each param needs to be removed from the URLSearchParams before it's added again.
+   */
+  Object.keys(params).forEach((key) => {
+    parsedUrl.searchParams.delete(key);
+    return parsedUrl.searchParams.append(key, params[key]);
+  });
+
+  /**
+   * The "fill" parameter contains a ",". Since this would be url encoded by toString(), and the
+   * service will not recognize it anymore, we need to replace the encoding with a ",".
+   */
+  return parsedUrl.toString().replace(fillParamRegex, '$1=$2,$3$4');
+};
+
+/**
  * Returns the actual url to the image, by adding url parameters with the dimensions for img-cdn
  * @param {string} src Source to the image.
  * @param {Object} dimension Dimension of the requested image.
@@ -9,16 +43,28 @@ import { getImageFormat } from './getImageFormat';
  * @param {number} dimension.height Height in pixels.
  * @returns {string}
  */
-export const getFullImageSource = (src, { width, height }) => {
+export const getFullImageSource = (src, { width, height } = {}) => {
   if (src && src.includes('images.shopgate.services/v2/images')) {
     const { fillColor, quality } = getProductImageSettings();
     const format = getImageFormat();
 
-    return `${src}&format=${format}&width=${width}&height=${height}&quality=${quality}&fill=${fillColor.replace('#', '')}`;
+    return buildUrl(src, {
+      format,
+      width,
+      height,
+      quality,
+      fill: fillColor.replace('#', ''),
+    });
   }
 
   if (src && src.startsWith('https://img-cdn.shopgate.com') && !src.includes('?')) {
-    return `${src}?w=${width}&h=${height}&q=70&zc=resize&fillc=FFFFFF`;
+    return buildUrl(src, {
+      w: width,
+      h: height,
+      q: 70,
+      zd: 'resize',
+      fillc: 'FFFFFF',
+    });
   }
 
   return src;
