09b082c442781c8879490dc00bec9057850dc735
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-06-03 09:39:44 +0200

    CURB-4179 implemented analyticsSetConsent command

--- libraries/engage/tracking/subscriptions/analytics.js
+++ libraries/engage/tracking/subscriptions/analytics.js
@@ -1,12 +1,35 @@
 import { reloadApp } from '@shopgate/engage/core/action-creators';
-import { cookieConsentUpdated$ } from '../streams';
+import { analyticsSetConsent } from '@shopgate/engage/core/commands';
+import {
+  cookieConsentInitialized$,
+  cookieConsentUpdated$,
+} from '../streams';
 
 /**
  * Analytics subscriptions
  * @param {Function} subscribe The subscribe function
  */
 export default function analytics(subscribe) {
-  subscribe(cookieConsentUpdated$, ({ dispatch }) => {
+  /**
+   * Sends cookie consent decisions to the app
+   * @param {Object} action Cookie consent update action
+   */
+  const sendConsentToApp = async (action) => {
+    const { statisticsCookiesAccepted, comfortCookiesAccepted } = action;
+
+    await analyticsSetConsent({
+      statistics: statisticsCookiesAccepted,
+      comfort: comfortCookiesAccepted,
+    });
+  };
+
+  subscribe(cookieConsentInitialized$, async ({ action }) => {
+    await sendConsentToApp(action);
+  });
+
+  subscribe(cookieConsentUpdated$, async ({ dispatch, action }) => {
+    await sendConsentToApp(action);
+
     // The PWA is reloaded whenever cookie consent settings changed to guarantee that all trackers
     // are turned off
     dispatch(reloadApp());
