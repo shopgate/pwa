ce38e3e393a4e39d9b9a6948006c10a9ad098d2a
aylinuenal <aylin.uenal@shopgate.com>
2024-06-19 16:19:13 +0200

    CURB-4129 add safe area insets to modal component

--- libraries/common/components/Modal/style.js
+++ libraries/common/components/Modal/style.js
@@ -1,4 +1,5 @@
 import { css } from 'glamor';
+import { themeColors } from '@shopgate/pwa-common/helpers/config';
 
 const container = css({
   position: 'fixed',
@@ -8,6 +9,7 @@ const container = css({
   right: 0,
   overflow: 'hidden',
   zIndex: 2000,
+  backgroundColor: themeColors.lightOverlay,
 }).toString();
 
 const layout = css({
@@ -22,6 +24,9 @@ const content = css({
   position: 'relative',
   maxWidth: '100vw',
   maxHeight: '100vh',
+  paddingTop: 'var(--safe-area-inset-top)',
+  paddingBottom: 'var(--safe-area-inset-bottom)',
+  overflowY: 'scroll',
 }).toString();
 
 export default {
