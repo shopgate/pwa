7af53259c1612a02a37acce61cd4b688674790b9
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-06-21 17:44:58 +0200

    CURB-4135 Reworked Switch component and PrivacySettings screen to improve a11y

--- libraries/engage/components/Switch/index.jsx
+++ libraries/engage/components/Switch/index.jsx
@@ -1,47 +1,69 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import { VisuallyHidden } from '@shopgate/engage/a11y/components';
 import styles from './style';
 
 /**
  * The Switch component.
  * @param {Object} props The component props.
+ * @param {React.ReactNode} [children=null] Component children are rendered as the checkbox label
+ * @param {boolean} [props.checked=false] Whether the checkbox is checked
+ * @param {boolean} [props.disabled=false] Whether the checkbox is disabled
+ * @param {Function} [props.onChange] Callback invoked when the checkbox changes
+ * @param {string} [props.a11yFallbackText=null] Optional text to be presented to screen readers
+ * when issues with the regular label are expected e.g. due to problematic layout.
+ * @param {string} [props.id=null] Optional custom id for checkbox input and label
  * @returns {JSX.Element}
  */
 const Switch = ({
-  disabled, checked, onChange, id,
+  disabled,
+  checked,
+  onChange,
+  id,
+  a11yFallbackText,
+  children,
 }) => {
   const switchId = useMemo(() => id || Math.random(), [id]);
 
   return (
     <div className={styles.container}>
       <input
-        onChange={onChange}
-        className={styles.input}
+        type="checkbox"
         disabled={disabled}
         checked={checked}
-        type="checkbox"
+        onChange={onChange}
+        className={styles.input}
         id={switchId}
       />
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label
-        className={disabled
-          ? classNames(styles.disabled, styles.switchButton)
-          : styles.switchButton}
-        htmlFor={switchId}
-      />
+
+      { children && (
+        <label htmlFor={switchId}>
+          <span aria-hidden={!!a11yFallbackText}>
+            {children}
+          </span>
+          { a11yFallbackText && (
+          <VisuallyHidden>
+            { a11yFallbackText}
+          </VisuallyHidden>
+          )}
+        </label>
+      )}
     </div>
   );
 };
 
 Switch.propTypes = {
+  a11yFallbackText: PropTypes.string,
   checked: PropTypes.bool,
+  children: PropTypes.node,
   disabled: PropTypes.bool,
   id: PropTypes.string,
   onChange: PropTypes.func,
 };
 Switch.defaultProps = {
+  children: null,
   disabled: false,
+  a11yFallbackText: null,
   checked: false,
   onChange: null,
   id: null,
