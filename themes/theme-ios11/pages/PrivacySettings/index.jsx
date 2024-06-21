11e528a49e4fabdf37a34db6e3d53fb16415f76c
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-06-21 17:42:18 +0200

    CURB-4135 Privacy settings page improvements
    - disabled content portals for the View to avoid showing extensions that enable leaving the route in unexpected ways
    - added page title when initial cookie consent process is done

--- themes/theme-ios11/pages/PrivacySettings/index.jsx
+++ themes/theme-ios11/pages/PrivacySettings/index.jsx
@@ -1,17 +1,39 @@
-import { View } from '@shopgate/engage/components';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { BackBar } from 'Components/AppBar/presets';
+import { View } from '@shopgate/engage/components';
 import { PrivacySettings } from '@shopgate/engage/tracking/components';
+import { getIsCookieConsentHandled } from '@shopgate/engage/tracking/selectors/cookieConsent';
+
+/**
+ * Maps the contents of the state to the component props.
+ * @param {Object} state The current application state.
+ * @return {Object} The extended component props.
+ */
+const mapStateToProps = state => ({
+  showTitle: getIsCookieConsentHandled(state),
+});
 
 /**
  * The CookieConsentPage component.
+ * @param {Object} props The component props
+ * @param {boolean} props.showTitle Whether to show the page title
  * @returns {JSX.Element}
  */
-const PrivacySettingsPage = () => (
-  <View aria-hidden={false}>
-    <BackBar />
+const PrivacySettingsPage = ({ showTitle }) => (
+  <View noContentPortal>
+    <BackBar title={showTitle ? 'navigation.privacySettings' : undefined} />
     <PrivacySettings />
   </View>
 );
 
-export default PrivacySettingsPage;
+PrivacySettingsPage.propTypes = {
+  showTitle: PropTypes.bool,
+};
+
+PrivacySettingsPage.defaultProps = {
+  showTitle: false,
+};
+
+export default connect(mapStateToProps)(PrivacySettingsPage);
