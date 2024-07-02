9f33d870b1cf24080c9d09adb06d7c978f9ef182
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-02 14:13:54 +0200

    Migrated libraries/ui-ios changes from PWA6 to PWA7

--- libraries/ui-shared/Dialog/index.jsx
+++ libraries/ui-shared/Dialog/index.jsx
@@ -93,7 +93,7 @@ const Dialog = ({
 
   return (
     <Modal>
-      <Backdrop isVisible level={0} />
+      <Backdrop isVisible level={0} opacity={30} />
       <DialogComponent {...dialogProps} />
     </Modal>
   );
