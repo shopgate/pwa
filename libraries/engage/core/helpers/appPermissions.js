c062a5c648a5693791e5d00e7122250fda8cdebf
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-02 18:26:59 +0200

    Migrated libraries/core changes from PWA6 to PWA7

--- libraries/engage/core/helpers/appPermissions.js
+++ libraries/engage/core/helpers/appPermissions.js
@@ -4,21 +4,109 @@ import {
 } from '@shopgate/pwa-core/constants/AppCommands';
 
 import {
-  STATUS_NOT_DETERMINED,
-  STATUS_GRANTED,
-  STATUS_DENIED,
+  PERMISSION_STATUS_NOT_DETERMINED,
+  PERMISSION_STATUS_NOT_SUPPORTED,
+  PERMISSION_STATUS_GRANTED,
+  PERMISSION_STATUS_DENIED,
   PERMISSION_ID_LOCATION,
+  PERMISSION_ID_PUSH,
+  PERMISSION_ID_CAMERA,
 } from '@shopgate/pwa-core/constants/AppPermissions';
 
 import geolocationRequestBrowser from '../classes/GeolocationRequestBrowser';
+
+const MOCK_PERMISSIONS_KEY = 'sg-mocked-app-permissions';
+
+let getMockedAppPermissions;
+
+/**
+ * In development within the browser, app permissions can be mocked by calling a global functions
+ * on the MockedPermissions object. The values are persisted in local storage, so that they survive
+ * reload of the PWA.
+ *
+ * Permissions are stored "globally" and will affect every shop.
+ *
+ * Example:
+ * MockedAppPermissions.setGetAppPermissionsResponse('push', 'notDetermined')
+ * MockedAppPermissions.setRequestAppPermissionsResponse('push', 'denied')
+ *
+ * The object also provides access to the relevant constants.
+ *
+ * Example:
+ * MockedAppPermissions.ID_PUSH
+ * MockedAppPermissions.STATUS_DENIED
+ *
+ * Complete Example:
+ * MockedAppPermissions.setRequestAppPermissionsResponse(
+ *   MockedAppPermissions.ID_PUSH,
+ *   MockedAppPermissions.STATUS_DENIED
+ * )
+ */
+if (process.env.NODE_ENV === 'development') {
+  /**
+   * Retrieves mocked app permissions from the local storage
+   * @returns {Object|null}
+   */
+  getMockedAppPermissions = () => JSON.parse(localStorage.getItem(MOCK_PERMISSIONS_KEY));
+
+  /**
+   * Sets a single mocked app permission status from the local storage
+   * @param {string} commandType Command type for which the status is set (get / request)
+   * @param {string} permissionId The permissionId for which the status is set
+   * @param {string} status The mocked permission status
+   */
+  const setMockedAppPermission = (commandType, permissionId, status) => {
+    let current = getMockedAppPermissions();
+
+    if (current === null) {
+      current = {};
+    }
+
+    if (!current?.[permissionId]) {
+      // Add defaults for the permissionId when not set yet
+      current[permissionId] = {
+        get: PERMISSION_STATUS_GRANTED,
+        request: PERMISSION_STATUS_GRANTED,
+      };
+    }
+
+    // Add the status to the mocked app permissions
+    current[permissionId][commandType] = status;
+    // Update local storage
+    localStorage.setItem(MOCK_PERMISSIONS_KEY, JSON.stringify(current));
+  };
+
+  /**
+   * Prepare global object in DEV environment to mock app permissions
+   */
+  const obj = {
+    setGetAppPermissionsResponse: (permissionId, status = PERMISSION_STATUS_GRANTED) => {
+      setMockedAppPermission('get', permissionId, status);
+    },
+    setRequestAppPermissionsResponse: (permissionId, status = PERMISSION_STATUS_GRANTED) => {
+      setMockedAppPermission('request', permissionId, status);
+    },
+  };
+
+  // Populate useful constants
+  obj.STATUS_GRANTED = PERMISSION_STATUS_GRANTED;
+  obj.STATUS_DENIED = PERMISSION_STATUS_DENIED;
+  obj.STATUS_NOT_DETERMINED = PERMISSION_STATUS_NOT_DETERMINED;
+  obj.STATUS_NOT_SUPPORTED = PERMISSION_STATUS_NOT_SUPPORTED;
+  obj.ID_PUSH = PERMISSION_ID_PUSH;
+  obj.ID_CAMERA = PERMISSION_ID_CAMERA;
+  obj.ID_LOCATION = PERMISSION_ID_LOCATION;
+
+  window.MockedAppPermissions = obj;
+}
 
 /**
  * Mapping of states from the browser permissions API to app permissions.
  */
 const permissionsApiStateMapping = {
-  prompt: STATUS_NOT_DETERMINED,
-  granted: STATUS_GRANTED,
-  denied: STATUS_DENIED,
+  prompt: PERMISSION_STATUS_NOT_DETERMINED,
+  granted: PERMISSION_STATUS_GRANTED,
+  denied: PERMISSION_STATUS_DENIED,
 };
 
 /**
@@ -46,13 +134,13 @@ const getGeolocationPermissionsFromPermissionsApi = async () => {
  * @returns {string}
  */
 const getGeolocationPermissionsFromGeolocationApi = async () => {
-  let state = STATUS_GRANTED;
+  let state = PERMISSION_STATUS_GRANTED;
   let geolocation;
   try {
     geolocation = await geolocationRequestBrowser.dispatch();
   } catch ({ code = null }) {
     if (code !== null) {
-      state = STATUS_DENIED;
+      state = PERMISSION_STATUS_DENIED;
     }
   }
 
@@ -72,7 +160,7 @@ const getGeolocationPermissionsFromGeolocationApi = async () => {
 const mockedPermissions = async (
   commandName,
   commandParams = {},
-  fallbackStatus = STATUS_GRANTED
+  fallbackStatus = PERMISSION_STATUS_GRANTED
 ) => {
   let { permissionIds } = commandParams;
   const { permissions } = commandParams;
@@ -96,8 +184,25 @@ const mockedPermissions = async (
   const result = permissionIds.map((permissionId) => {
     let state = fallbackStatus;
     let geolocation;
+    let useLocalStorageMock = false;
 
-    if (permissionId === PERMISSION_ID_LOCATION && geolocationState?.state) {
+    // Use mocked permissions from local storage if available
+    if (typeof getMockedAppPermissions === 'function') {
+      const permissionMock = getMockedAppPermissions();
+      const type = this.commandName === COMMAND_GET_APP_PERMISSIONS ? 'get' : 'request';
+      const mockValue = permissionMock?.[permissionId]?.[type];
+
+      if (mockValue) {
+        useLocalStorageMock = true;
+        state = mockValue || PERMISSION_STATUS_GRANTED;
+      }
+    }
+
+    if (
+      !useLocalStorageMock &&
+      permissionId === PERMISSION_ID_LOCATION &&
+      geolocationState?.state
+    ) {
       ({ state, geolocation } = geolocationState);
     }
 
@@ -116,5 +221,5 @@ const mockedPermissions = async (
  * @param {string} [fallbackStatus] The status that's returned when no status could be determined.
  * @returns {Function}
  */
-export const createMockedPermissions = (fallbackStatus = STATUS_GRANTED) =>
+export const createMockedPermissions = (fallbackStatus = PERMISSION_STATUS_GRANTED) =>
   (commandName, commandParams) => mockedPermissions(commandName, commandParams, fallbackStatus);
