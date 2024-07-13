8d96db09ac4a1dcfd2c61dad3e17fdc5aab3f713
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-13 18:25:40 +0200

    CURB-4266 Migrated engage/reviews library from PWA6 to PWA7

---
+++ libraries/engage/reviews/actions/index.js
@@ -0,0 +1,5 @@
+export { default as fetchProductReviews } from '@shopgate/pwa-common-commerce/reviews/actions/fetchProductReviews';
+export { default as fetchReviews } from '@shopgate/pwa-common-commerce/reviews/actions/fetchReviews';
+export { default as fetchUserReview } from '@shopgate/pwa-common-commerce/reviews/actions/fetchUserReview';
+export { default as flushUserReview } from '@shopgate/pwa-common-commerce/reviews/actions/flushUserReview';
+export { default as submitReview } from '@shopgate/pwa-common-commerce/reviews/actions/submitReview';
