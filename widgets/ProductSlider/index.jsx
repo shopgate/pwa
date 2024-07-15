682553c6c5531745e16522f4c3b05e195ab5fde6
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-15 14:42:00 +0200

    CURB-4266 File cleanup during PWA6 and PWA7 merge

--- widgets/ProductSlider/index.jsx
+++ widgets/ProductSlider/index.jsx
@@ -9,7 +9,7 @@ import {
 import { transformDisplayOptions } from '@shopgate/pwa-common/helpers/data';
 import { withWidgetSettings } from '@shopgate/engage/core';
 import appConfig from '@shopgate/pwa-common/helpers/config';
-import { WIDGET_ID } from 'Components/ProductSlider';
+import { ProductSlider as EngageProductSlider } from '@shopgate/engage/product/components';
 import Headline from 'Components/Headline';
 import connect from './connector';
 import styles from './style';
@@ -165,6 +165,6 @@ class ProductSlider extends PureComponent {
   }
 }
 
-export default withWidgetSettings(connect(ProductSlider), WIDGET_ID);
+export default withWidgetSettings(connect(ProductSlider), EngageProductSlider.WIDGET_ID);
 
 export { ProductSlider as UnwrappedProductSlider };
