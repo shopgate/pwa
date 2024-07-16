6c706bf2e08a07577bf091b415d87ce74d81c719
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-16 12:46:15 +0200

    CURB-4266 Added widgetSettings check to rating components

--- libraries/engage/product/components/Rating/index.jsx
+++ libraries/engage/product/components/Rating/index.jsx
@@ -1,16 +1,17 @@
-import React, { Fragment, memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import Portal from '@shopgate/pwa-common/components/Portal';
-import {
-  PRODUCT_RATING,
-  PRODUCT_RATING_AFTER,
-  PRODUCT_RATING_BEFORE,
-} from '@shopgate/pwa-common-commerce/product/constants/Portals';
 import appConfig from '@shopgate/pwa-common/helpers/config';
-import RatingStars from '@shopgate/pwa-ui-shared/RatingStars';
+import {
+  RatingStars,
+  SurroundPortals,
+} from '@shopgate/engage/components';
+import { PRODUCT_RATING } from '@shopgate/engage/product/constants';
 import RatingCount from '@shopgate/engage/reviews/components/Reviews/components/RatingCount';
+import { useWidgetSettings } from '@shopgate/engage/core/hooks';
 import { container } from './style';
 import connect from './connector';
+
+const { hasReviews } = appConfig;
 
 /**
  * Scrolls page to reviews excerpt.
@@ -38,20 +39,32 @@ const scrollToRating = () => {
  * @param {Object} props The component props.
  * @return {JSX}
  */
-const Rating = ({ rating }) => (
-  <Fragment>
-    <Portal name={PRODUCT_RATING_BEFORE} />
-    <Portal name={PRODUCT_RATING}>
-      {appConfig.hasReviews && rating && rating.count &&
-        <div className={container} onClick={scrollToRating} role="link" aria-hidden>
-          <RatingStars value={rating.average} display="big" />
-          <RatingCount count={rating.count} prominent />
-        </div>
-      }
-    </Portal>
-    <Portal name={PRODUCT_RATING_AFTER} />
-  </Fragment>
-);
+const Rating = ({ rating }) => {
+  const { showEmptyRatingStars = false } = useWidgetSettings('@shopgate/engage/rating');
+
+  const showRatings = useMemo(() => {
+    if (hasReviews && rating?.average > 0) {
+      return true;
+    }
+
+    if (hasReviews && showEmptyRatingStars && rating) {
+      return true;
+    }
+
+    return false;
+  }, [rating, showEmptyRatingStars]);
+
+  return (
+    <SurroundPortals portalName={PRODUCT_RATING}>
+      {showRatings &&
+      <div className={container} onClick={scrollToRating} role="link" aria-hidden>
+        <RatingStars value={rating.average} display="big" />
+        <RatingCount count={rating.count} prominent />
+      </div>
+        }
+    </SurroundPortals>
+  );
+};
 
 Rating.propTypes = {
   rating: PropTypes.shape(),
