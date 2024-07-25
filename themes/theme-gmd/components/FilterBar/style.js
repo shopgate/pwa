64ee529e9a5d0f24b9805b26d91405a1f84f7a37
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-25 12:24:44 +0200

    CURB-4267 Small fixes

--- themes/theme-gmd/components/FilterBar/style.js
+++ themes/theme-gmd/components/FilterBar/style.js
@@ -1,8 +1,6 @@
 import { css } from 'glamor';
-import { themeShadows } from '@shopgate/pwa-common/helpers/config';
 
 export default css({
-  boxShadow: themeShadows.material,
   position: 'sticky',
   top: '-2px', // behind header shadow
   left: 0,
