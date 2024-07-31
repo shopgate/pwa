db1af10c7b5f5ce2db2900a6037a82403b2b97ba
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-07-31 16:43:25 +0200

    CURB-4267 Migrated Browser, Cart and More pages of iOS theme from PWA6

--- themes/theme-ios11/pages/More/constants.js
+++ themes/theme-ios11/pages/More/constants.js
@@ -1,9 +1,1 @@
-import { PAGE_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
-
 export const MORE_PATH = '/more';
-export const IMPRINT_PATH = `${PAGE_PATH}/imprint`;
-export const PAYMENT_PATH = `${PAGE_PATH}/payment`;
-export const PRIVACY_PATH = `${PAGE_PATH}/privacy`;
-export const RETURN_POLICY_PATH = `${PAGE_PATH}/return_policy`;
-export const SHIPPING_PATH = `${PAGE_PATH}/shipping`;
-export const TERMS_PATH = `${PAGE_PATH}/terms`;
