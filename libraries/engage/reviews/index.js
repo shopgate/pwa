69a8038a3d181bcf0346e18a2cca4d54263a9c62
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-08-22 17:09:43 +0200

    CURB-4263 Removed engage/reviews/mocks folder
    - is deleted by script inside "Makefile" before code is uploaded to github during release process
    - fixed error caused by invalid import

--- libraries/engage/reviews/index.js
+++ libraries/engage/reviews/index.js
@@ -1,9 +1,12 @@
 /** @module reviews */
+// Not moved into a "mocks" folder with index.js, since folders with "*mocks*" are deleted during
+// release before upload to github, which will cause errors because auf in invalid import.
+export * from '@shopgate/pwa-common-commerce/reviews/mock';
+
 /* eslint-disable import/export */
 export * from './actions';
 export * from './components';
 export * from './constants';
-export * from './mocks';
 export * from './selectors';
 export * from './streams';
 /* eslint-enable import/export */
