3b294741b2a17a5c4efa6edbb4196a89987d9de9
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-09-18 12:38:04 +0200

    CURB-4263 Added background color to GMD startpage view to match PWA6 background

--- themes/theme-gmd/pages/StartPage/index.jsx
+++ themes/theme-gmd/pages/StartPage/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View } from '@shopgate/engage/components';
 import { PAGE_ID_INDEX } from '@shopgate/pwa-common/constants/PageIDs';
+import { themeConfig } from '@shopgate/engage';
 import PageContent from '../Page/components/Content';
+
+const { colors } = themeConfig;
 
 /**
  * @returns {JSX}
  */
 const StartPage = () => (
-  <View>
+  <View background={colors.shade8}>
     <PageContent pageId={PAGE_ID_INDEX} />
   </View>
 );
