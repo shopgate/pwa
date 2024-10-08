f4a08681700fe57bfdc5f09a3ab77dd1d7570492
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-10-08 15:32:25 +0200

    CURB-4299 Updated scroll offset logic for ProductFilters in website mode
    - applied higher default offset to the filters when GMD theme is in website mode
    - different offset will cause that filter bar hides later than usual when users scroll
    - mimics scroll behaviour before refactoring of the ScrollHeader component

--- themes/theme-gmd/components/ProductFilters/index.jsx
+++ themes/theme-gmd/components/ProductFilters/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useWidgetSettings, useRoute } from '@shopgate/engage/core/hooks';
+import { useWidgetSettings, useRoute, useResponsiveValue } from '@shopgate/engage/core/hooks';
 import { ResponsiveContainer, ScrollHeader, SurroundPortals } from '@shopgate/engage/components';
 import { GlobalLocationSwitcher, FulfillmentSlotSwitcher } from '@shopgate/engage/locations';
 import { themeConfig } from '@shopgate/engage';
@@ -16,8 +16,17 @@ const { variables: { scroll: { offset = 100 } = {} } } = themeConfig || {};
 const ProductFilters = ({ categoryId, showFilters }) => {
   const { hideOnScroll } = useWidgetSettings('@shopgate/engage/components/FilterBar');
   const { state } = useRoute();
+
+  // When the PWA is in website mode, we apply a higher offset value than usual because the AppBar
+  // is larger.
+  const responsiveOffset = useResponsiveValue('>xs', {
+    webOnly: true,
+    valueMatch: 220,
+    valueMiss: offset,
+  });
+
   return (
-    <ScrollHeader className={filters} hideOnScroll={hideOnScroll} scrollOffset={offset}>
+    <ScrollHeader className={filters} hideOnScroll={hideOnScroll} scrollOffset={responsiveOffset}>
       <SurroundPortals portalName="filter-bar.content">
         <ResponsiveContainer appAlways breakpoint="<=xs">
           <GlobalLocationSwitcher renderBar />
