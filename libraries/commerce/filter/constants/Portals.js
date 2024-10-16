f2f1caedd4298a3ad628a62879f6d4dc08011cca
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-10-16 14:26:02 +0200

    CURB-4492 Added portal around FilterSelector component

--- libraries/commerce/filter/constants/Portals.js
+++ libraries/commerce/filter/constants/Portals.js
@@ -3,6 +3,7 @@ const FILTER = 'filter';
 
 // CONTENTS
 const PRICE_RANGE = 'price-range';
+const SELECTOR = 'selector';
 
 // POSITIONS
 const BEFORE = 'before';
@@ -11,3 +12,7 @@ const AFTER = 'after';
 export const FILTER_PRICE_RANGE = `${FILTER}.${PRICE_RANGE}`;
 export const FILTER_PRICE_RANGE_BEFORE = `${FILTER}.${PRICE_RANGE}.${BEFORE}`;
 export const FILTER_PRICE_RANGE_AFTER = `${FILTER}.${PRICE_RANGE}.${AFTER}`;
+
+export const FILTER_SELECTOR = `${FILTER}.${SELECTOR}`;
+export const FILTER_SELECTOR_BEFORE = `${FILTER}.${SELECTOR}.${BEFORE}`;
+export const FILTER_SELECTOR_AFTER = `${FILTER}.${SELECTOR}.${AFTER}`;
