1e68269d98d01d60c983bae8f41e03910b88a326
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-01-16 11:14:41 +0100

    CURB-4650 Migrated ProductGallery MediaSlider to the refactored Image component

--- themes/theme-ios11/pages/ProductGallery/components/Content/components/MediaSlider/index.jsx
+++ themes/theme-ios11/pages/ProductGallery/components/Content/components/MediaSlider/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useWidgetSettings, getFullImageSource } from '@shopgate/engage/core';
-import { Swiper } from '@shopgate/engage/components';
-import { GALLERY_SLIDER_ZOOM, GALLERY_SLIDER_IMAGE_FORMATS } from '../../../../constants';
+import { useWidgetSettings } from '@shopgate/engage/core/hooks';
+import { getProductImageSettings } from '@shopgate/engage/product/helpers';
+import { Swiper, Image } from '@shopgate/engage/components';
+import { GALLERY_SLIDER_ZOOM } from '../../../../constants';
 import styles from './style';
 import connect from './connector';
 
@@ -12,11 +13,13 @@ import connect from './connector';
  * @return {JSX}
  */
 const ProductGalleryMedia = ({ initialSlide, media }) => {
+  const settings = useWidgetSettings('@shopgate/engage/product/Gallery');
+
   if (!Array.isArray(media) || media.length === 0) {
     return <div className={styles.container} />;
   }
 
-  const settings = useWidgetSettings('@shopgate/engage/product/Gallery');
+  const { GalleryImage: galleryResolutions } = getProductImageSettings();
 
   return (
     <div className={styles.container}>
@@ -35,10 +38,12 @@ const ProductGalleryMedia = ({ initialSlide, media }) => {
         {media.map(singleMedia => (
           <Swiper.Item key={singleMedia.url}>
             <div className="swiper-zoom-container">
-              <img
-                src={getFullImageSource(singleMedia.url, GALLERY_SLIDER_IMAGE_FORMATS[1])}
+              <Image
+                src={singleMedia.url}
                 alt={singleMedia.altText}
-                className={styles.slide}
+                classNameImg={styles.slide}
+                resolutions={galleryResolutions}
+                unwrapped
               />
             </div>
           </Swiper.Item>
