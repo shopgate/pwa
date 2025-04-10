38d9da21e371bb347d92f449ba3c7fdc4fcf05c9
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-04-10 08:31:25 +0200

    CURB-4992 Refactored system to hide content from screen readers
    - switched to new a11y modalCount state

--- libraries/engage/core/providers/AppProvider.jsx
+++ libraries/engage/core/providers/AppProvider.jsx
@@ -1,28 +1,15 @@
 import React, {
   useEffect, useMemo, useState, useCallback,
 } from 'react';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import appConfig from '@shopgate/pwa-common/helpers/config';
 import {
   APP_EVENT_VIEW_WILL_APPEAR,
   APP_EVENT_VIEW_WILL_DISAPPEAR,
 } from '@shopgate/engage/core/constants';
-import { getModalCount } from '@shopgate/engage/a11y/selectors';
 import { event } from '@shopgate/engage/core/classes';
-import UIEvents from '@shopgate/pwa-core/emitters/ui';
-import { MODAL_EVENTS } from '@shopgate/engage/components';
 import AppContext from '../contexts/AppContext';
 
-/**
- * Helper function to dispatch an event that informs the app that a modal is shown.
- * In that case the main content will be hidden from screen readers.
- */
-const dispatchShowModal = () => {
-  if (document.querySelector('.engage__view')) {
-    UIEvents.emit(MODAL_EVENTS.SHOW);
-  }
-};
 /**
  * The AppProvider component.
  * @param {Object} props The component props.
@@ -57,41 +44,6 @@ const AppProvider = ({ children }) => {
       event.removeCallback(APP_EVENT_VIEW_WILL_DISAPPEAR, handleViewWillDisappear);
     };
   }, [handleViewWillDisappear, handleViewWillAppear]);
-
-  // Determine if currently modals are open
-  const openModalsCount = useSelector(getModalCount);
-
-  useEffect(() => {
-    if (openModalsCount <= 0) return undefined;
-
-    if (document.querySelector('.engage__view')) {
-      // Dispatch the event to hide the main content from screen readers when a view is present.
-      dispatchShowModal();
-    }
-
-    // Create a MutationObserver to watch for changes in the DOM. We need to dispatch an UI event
-    // when the modal is shown, so that the main content is hidden from screen readers.
-    const observer = new MutationObserver(() => {
-      if (document.querySelector('.engage__view')) {
-        // Dispatch the event to hide the main content from screen readers when new views are added.
-        dispatchShowModal();
-      }
-    });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-
-    return () => observer.disconnect();
-  }, [openModalsCount]);
-
-  useEffect(() => {
-    if (openModalsCount === 0) {
-      // Dispatch the event to show the main content when no modals are present anymore.
-      UIEvents.emit(MODAL_EVENTS.HIDE);
-    }
-  }, [openModalsCount]);
 
   const value = useMemo(() => ({
     isVisible,
