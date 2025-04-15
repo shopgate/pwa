a607abb87817f7ba3a4bae46d09a53b3f1ba226f
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-04-15 10:42:48 +0200

    CURB-4870 Fixed unit test

--- libraries/ui-shared/DiscountBadge/index.jsx
+++ libraries/ui-shared/DiscountBadge/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { I18n } from '@shopgate/engage/components';
-import { i18n } from '@shopgate/engage/core';
+import { i18n } from '@shopgate/engage/core/helpers';
 import styles from './style';
 
 /**
