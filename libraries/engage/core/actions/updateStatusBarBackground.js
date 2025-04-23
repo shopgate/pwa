ab5dd8d2e3762b23eb12549140bcdcaffd2b8568
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-04-23 14:43:08 +0200

    CURB-5006 Added support for upcoming Android app version that supports edge-to-edge mode
    - device status bar updates are now also dispatched to the app on app versions that support edge-to-edge mode

--- libraries/engage/core/actions/updateStatusBarBackground.js
+++ libraries/engage/core/actions/updateStatusBarBackground.js
@@ -1,15 +1,16 @@
 import { isIos } from '@shopgate/pwa-common/selectors/client';
+import { appSupportsAndroidEdgeToEdge } from '@shopgate/engage/core/helpers';
 import { updateLegacyNavigationBar } from '../helpers/updateLegacyNavigationBar';
 
 /**
- * Updates the status bar background on iOS devices.
+ * Updates the status bar background on iOS and Android devices that support edge-to-edge screens.
  * @param {string} color The background color.
  * @param {boolean} isDefault When set, the status bar will init with the color on next app start.
  * @return {Function} A redux thunk.
  */
 export default function updateStatusBarBackground(color, isDefault = false) {
   return (dispatch, getState) => {
-    if (!isIos(getState())) {
+    if (!isIos(getState()) && !appSupportsAndroidEdgeToEdge()) {
       return;
     }
 
