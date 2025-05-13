23375068380fa21cd6cf483ffc888deab717169b
aylinuenal <aylin.uenal@shopgate.com>
2025-05-13 10:39:13 +0200

    CURB-4937 make screen reader announce error text in review form

--- libraries/ui-shared/TextField/components/ErrorText/index.jsx
+++ libraries/ui-shared/TextField/components/ErrorText/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import I18n from '@shopgate/pwa-common/components/I18n';
+import { I18n } from '@shopgate/engage/components';
 import styles from './style';
 
 /**
  * Error message component.
  * @param {Object} props The component props.
- * @returns {JSX}
+ * @returns {JSX.Element}
  */
 const ErrorText = props => (
-  <div className={classNames(props.className, styles.error, 'errorText')}>
+  <div
+    className={classNames(props.className, styles.error, 'errorText')}
+    aria-live="assertive"
+    aria-atomic="true"
+  >
     { props.translate && <I18n.Text string={props.validationError || props.errorText} /> }
     { !props.translate && (props.validationError || props.errorText) }
   </div>
