01984167a5026d9aa7898f310130a1a209033c89
aylinuenal <aylin.uenal@shopgate.com>
2025-06-17 16:51:40 +0200

    CURB-5127 implement full SVG support for empty favorites image config

--- themes/theme-gmd/pages/Favorites/components/EmptyFavorites/index.jsx
+++ themes/theme-gmd/pages/Favorites/components/EmptyFavorites/index.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import { FAVORITES_EMPTY } from '@shopgate/pwa-common-commerce/favorites/constants/Portals';
 import SurroundPortals from '@shopgate/pwa-common/components/SurroundPortals';
 import { themeConfig } from '@shopgate/pwa-common/helpers/config';
-import Icon from '@shopgate/pwa-common/components/Icon';
+import { encodeSVG } from '@shopgate/engage/core/helpers';
 import styles from './style';
 import { EmptyFavoritesIcon } from './components/Icon';
 import ContinueButton from './components/ContinueButton';
 
-const { icons } = themeConfig;
+const { fullSVGs } = themeConfig;
 
 /**
  * The Empty Favorites component
  * @return {JSX.Element}
  */
-const EmptyFavorites = () => (
-  <div className={styles.container}>
-    <SurroundPortals portalName={FAVORITES_EMPTY}>
-      <div className={styles.iconContainer} data-test-id="emptyFavComponent">
-        {icons?.emptyFavorites
-          ? <Icon content={icons.emptyFavorites} />
-          : <EmptyFavoritesIcon />
+const EmptyFavorites = () => {
+  const { emptyFavorites } = fullSVGs;
+
+  const imageSRC = useMemo(() => encodeSVG(emptyFavorites),
+    [emptyFavorites]);
+
+  return (
+    <div className={styles.container}>
+      <SurroundPortals portalName={FAVORITES_EMPTY}>
+        <div className={styles.iconContainer} data-test-id="emptyFavComponent">
+          {emptyFavorites
+            ? <img src={imageSRC} alt="" />
+            : <EmptyFavoritesIcon />
           }
-        <I18n.Text string="favorites.empty" className={styles.title} />
-      </div>
-      <ContinueButton />
-    </SurroundPortals>
-  </div>
-);
+          <I18n.Text string="favorites.empty" className={styles.title} />
+        </div>
+        <ContinueButton />
+      </SurroundPortals>
+    </div>
+  );
+};
 
 export default EmptyFavorites;
--- themes/theme-ios11/pages/Favorites/components/EmptyFavorites/index.jsx
+++ themes/theme-ios11/pages/Favorites/components/EmptyFavorites/index.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import I18n from '@shopgate/pwa-common/components/I18n';
 import SurroundPortals from '@shopgate/pwa-common/components/SurroundPortals';
 import { FAVORITES_EMPTY } from '@shopgate/pwa-common-commerce/favorites/constants/Portals';
-import Icon from '@shopgate/pwa-common/components/Icon';
 import { themeConfig } from '@shopgate/pwa-common/helpers/config';
+import { encodeSVG } from '@shopgate/engage/core/helpers';
 import styles from './style';
 import { EmptyFavoritesIcon } from './components/Icon';
 
-const { icons } = themeConfig;
+const { fullSVGs } = themeConfig;
 
 /**
  * The Empty Favorites component
  * @return {JSX.Element}
  */
-const EmptyFavorites = () => (
-  <div className={styles.container}>
-    <SurroundPortals portalName={FAVORITES_EMPTY}>
-      <div className={styles.iconContainer} data-test-id="emptyFavComponent">
-        {icons?.emptyFavorites
-          ? <Icon content={icons.emptyFavorites} />
-          : <EmptyFavoritesIcon />
-        }
-        <I18n.Text string="favorites.empty" className={styles.title} />
-      </div>
-    </SurroundPortals>
-  </div>
-);
+const EmptyFavorites = () => {
+  const { emptyFavorites } = fullSVGs;
+
+  const imageSRC = useMemo(() => encodeSVG(emptyFavorites),
+    [emptyFavorites]);
+  return (
+    <div className={styles.container}>
+      <SurroundPortals portalName={FAVORITES_EMPTY}>
+        <div className={styles.iconContainer} data-test-id="emptyFavComponent">
+          {emptyFavorites
+            ? <img src={imageSRC} alt="" />
+            : <EmptyFavoritesIcon />
+          }
+          <I18n.Text string="favorites.empty" className={styles.title} />
+        </div>
+      </SurroundPortals>
+    </div>
+  );
+};
 
 export default EmptyFavorites;
