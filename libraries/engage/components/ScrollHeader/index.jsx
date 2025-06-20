dc569e64e101105742d3a76742c567a1a88b9bec
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-06-20 09:46:50 +0200

    CURB-5147 Extended ScrollHeader component
    - added ref support
    - added onChange handler to enable parent components to react on visibility changes

--- libraries/engage/components/ScrollHeader/index.jsx
+++ libraries/engage/components/ScrollHeader/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { useScrollDirectionChange } from '@shopgate/engage/core/hooks';
@@ -7,17 +7,31 @@ import {
 } from './style';
 
 /**
- * Scroll Header component
- * @param {Object} props props
- * @returns {JSX}
+ * A container component that hides its content on scroll down and shows it on scroll up.
+ *
+ * @param {Object} props The component props.
+ * @param {boolean}  [props.hideOnScroll] Toggle hide-on-scroll (default: true).
+ * @param {number} [props.scrollOffset] Pixels to scroll before toggling (default: 100).
+ * @param {Function} [props.onChange] Callback function that is called when the header changes
+ * @param {string} [props.className] Extra CSS classes on the root element.
+ * @param {Object} [props.classes] Override internal class names.
+ * @param {string} [props.classes.scrolledIn] Override class for the scrolled-in state
+ * (content is hidden).
+ * @param {string} [props.classes.scrolledOut] Override class for the scrolled-out state
+ * (content is visible).
+ * visibility. Contains a boolean indicating whether the header is visible.
+ * @param {React.ReactNode} props.children  Content of the header.
+ * @param {React.Ref<HTMLDivElement>} ref   Forwarded ref to the header `<div>`.
+ * @returns {JSX.Element}
  */
-function ScrollHeader({
+function ScrollHeaderBase({
   className,
   children,
   hideOnScroll,
   scrollOffset,
+  onChange,
   classes,
-}) {
+}, ref) {
   const [shouldHideHeader, setShouldHideHeader] = useState(false);
 
   useScrollDirectionChange({
@@ -31,18 +45,28 @@ function ScrollHeader({
     },
   });
 
+  useEffect(() => {
+    if (typeof onChange !== 'function') {
+      return;
+    }
+
+    onChange(!shouldHideHeader);
+  }, [onChange, shouldHideHeader]);
+
   return (
-    <div className={classNames(root, transition, className, {
-      [classNames(scrolledIn, classes.scrolledIn)]: !shouldHideHeader,
-      [classNames(scrolledOut, classes.scrolledOut)]: shouldHideHeader,
-    })}
+    <div
+      ref={ref}
+      className={classNames(root, transition, className, {
+        [classNames(scrolledIn, classes?.scrolledIn)]: !shouldHideHeader,
+        [classNames(scrolledOut, classes?.scrolledOut)]: shouldHideHeader,
+      })}
     >
       {children}
     </div>
   );
 }
 
-ScrollHeader.propTypes = {
+ScrollHeaderBase.propTypes = {
   children: PropTypes.node.isRequired,
   classes: PropTypes.shape({
     scrolledIn: PropTypes.object,
@@ -50,14 +74,19 @@ ScrollHeader.propTypes = {
   }),
   className: PropTypes.string,
   hideOnScroll: PropTypes.bool,
+  onChange: PropTypes.func,
   scrollOffset: PropTypes.number,
 };
 
-ScrollHeader.defaultProps = {
+ScrollHeaderBase.defaultProps = {
   className: null,
   hideOnScroll: true,
   scrollOffset: 100,
   classes: {},
+  onChange: null,
 };
+
+const ScrollHeader = forwardRef(ScrollHeaderBase);
+ScrollHeader.displayName = 'ScrollHeader';
 
 export default ScrollHeader;
