39db249cb174a700e7742afc15d15bc6ee453ac2
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-07-21 13:12:22 +0200

    CURB-4524 Fixed cms preview iframe communication

--- libraries/engage/page/components/Widgets/constants.js
+++ libraries/engage/page/components/Widgets/constants.js
@@ -2,10 +2,10 @@
  * List of allowed origins for cms page preview iFrame communication.
  */
 export const ALLOWED_PAGE_PREVIEW_ORIGINS = [
-  'next.admin.shopgatedev.com',
-  'next.admin.shopgatepg.com',
-  'next.admin.shopgate.com',
-  'next.us.admin.shopgate.com',
+  'https://next.admin.shopgatedev.com',
+  'https://next.admin.shopgatepg.com',
+  'https://next.admin.shopgate.com',
+  'https://next.us.admin.shopgate.com',
   'http://localhost:1337',
 ];
 
