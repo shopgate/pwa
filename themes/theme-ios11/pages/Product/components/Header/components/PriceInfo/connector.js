66ce43d146356763f3ce91d4894af3f1c963bc1c
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-08-10 10:26:04 +0200

    CURB-5276 Updated comment

--- themes/theme-ios11/pages/Product/components/Header/components/PriceInfo/connector.js
+++ themes/theme-ios11/pages/Product/components/Header/components/PriceInfo/connector.js
@@ -14,7 +14,7 @@ const mapStateToProps = (state, props) => ({
 
 /**
  * @param {Object} next The next component props.
- * @param {Object} prev The current component props.
+ * @param {Object} prev The previous component props.
  * @return {boolean}
  */
 const areStatePropsEqual = (next, prev) => {
