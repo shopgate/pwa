33c8c715acbc972ec05a49fc8616b8e3d11db5b3
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-09-09 14:21:18 +0200

    CURB-5335 Refactored ClientInformation component

---
+++ libraries/engage/development/components/ClientInformation/ClientInformation.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@shopgate/engage/styles';
+import { useLongPress } from '@shopgate/engage/core/hooks';
+import { getClientInformation } from '@shopgate/engage/core/selectors';
+import { enableDebugLogging } from './actions';
+
+const useStyles = makeStyles()(theme => ({
+  root: {
+    position: 'relative',
+    textAlign: 'center',
+    color: theme.palette.grey[500],
+    fontSize: 12,
+    paddingBottom: 20,
+  },
+  unselectable: {
+    WebkitTouchCallout: 'none',
+    WebkitUserSelect: 'none',
+    userSelect: 'none',
+  },
+  deviceId: {
+    wordBreak: ['keep-all', 'break-word'],
+    hyphens: 'auto',
+    padding: theme.spacing(0, 2),
+  },
+}));
+
+/**
+ * @param {Object} props The component props.
+ * @returns {JSX.Element}
+ */
+const ClientInformation = () => {
+  const { classes, cx } = useStyles();
+  const dispatch = useDispatch();
+  const [deviceIdVisible, setDeviceIdVisible] = useState(false);
+
+  // Press handler to show the device ID
+  const showDeviceIdAttrs = useLongPress(() => {
+    if (!deviceIdVisible) {
+      setDeviceIdVisible(true);
+      dispatch(enableDebugLogging());
+    }
+  }, { threshold: 5000 });
+
+  const {
+    appVersion,
+    codebaseVersion,
+    deviceId,
+    libVersion,
+  } = useSelector(getClientInformation);
+
+  if (!codebaseVersion) {
+    return null;
+  }
+
+  return (
+    <div
+      className={cx('ui-shared__client-information', classes.root)}
+      {...showDeviceIdAttrs}
+      aria-hidden
+    >
+      <p className={classes.unselectable}>
+        {`App Version: ${appVersion} (${codebaseVersion})`}
+        <br />
+        {`Lib Version: ${libVersion}`}
+      </p>
+      {deviceIdVisible &&
+        <p className={classes.deviceId}>
+          {`Device ID: ${deviceId ?? ''}`}
+        </p>
+      }
+    </div>
+  );
+};
+
+export default ClientInformation;
