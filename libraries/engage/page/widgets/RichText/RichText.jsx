7cd644023d0b56d961b0754453b7359eee4d22ae
Sascha Krist <sascha.krist@shopgate.com>
2025-09-11 08:29:33 +0200

    CURB-5357 adjust richText widget

--- libraries/engage/page/widgets/RichText/RichText.jsx
+++ libraries/engage/page/widgets/RichText/RichText.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@shopgate/engage/styles';
 import { HtmlSanitizer } from '@shopgate/engage/components';
 import { useRichTextWidget } from './hooks';
-
-const useStyles = makeStyles()(() => ({
-  root: {},
-}));
 
 /**
  * @returns {JSX.Element}
  */
 const RichText = () => {
-  const { cx, classes } = useStyles();
   const { richText } = useRichTextWidget();
 
   if (!richText) return null;
 
   return (
-    <div className={cx(classes.root)}>
-      <HtmlSanitizer>
-        {richText}
-      </HtmlSanitizer>
-    </div>
+    <HtmlSanitizer>
+      {richText}
+    </HtmlSanitizer>
   );
 };
 export default RichText;
