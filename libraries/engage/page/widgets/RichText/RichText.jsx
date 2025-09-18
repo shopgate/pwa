26345b0738c1d42f76cc409fc389275065f8c7dc
Sascha Krist <sascha.krist@shopgate.com>
2025-09-18 12:24:08 +0200

    CURB-5375 review feedback

--- libraries/engage/page/widgets/RichText/RichText.jsx
+++ libraries/engage/page/widgets/RichText/RichText.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { HtmlSanitizer } from '@shopgate/engage/components';
+import { makeStyles } from '@shopgate/engage/styles';
 import { useRichTextWidget } from './hooks';
+
+const useStyles = makeStyles()(() => ({
+  html: {
+    '& p': {
+      marginBottom: '17px',
+    },
+    'ul, ol': {
+      paddingLeft: '40px',
+    },
+    'ul li': {
+      listStyleType: 'disc',
+    },
+    'ol li': {
+      listStyleType: 'decimal',
+    },
+  },
+}));
 
 /**
  * @returns {JSX.Element}
  */
 const RichText = () => {
   const { richText } = useRichTextWidget();
+  const { cx, classes } = useStyles();
 
   if (!richText) return null;
 
   return (
-    <HtmlSanitizer>
+    <HtmlSanitizer
+      processStyles
+      settings={{ html: richText }}
+      className={cx(classes.html)}
+    >
       {richText}
     </HtmlSanitizer>
   );
