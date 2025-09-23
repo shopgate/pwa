b93776fb6d82dcb67f00e97f85a413b6a137b2b4
aylinuenal <aylin.uenal@shopgate.com>
2025-09-23 15:23:51 +0200

    CURB-5402 add support for imageSpacing to enable gaps between images

--- libraries/engage/page/widgets/ImageRow/hooks.js
+++ libraries/engage/page/widgets/ImageRow/hooks.js
@@ -2,30 +2,32 @@ import { useWidget } from '@shopgate/engage/page/hooks';
 import { useMemo } from 'react';
 
 /**
- * @typedef {Object} ImageWidgetConfig
+ * @typedef {Object} ImageRowWidgetConfig
  * @property {Array} images The images array.
  * @property {string} imageWrapping The imageWrapping setting,
  * one of: responsiveDefault | responsiveDense | responsiveNoWrap.
  * @property {string} [link] The optional link URL.
+ * @property {number} [imageSpacing] An optional gap between images (in pixels).
  */
 
 /**
  * @typedef {ReturnType< typeof import('@shopgate/engage/page/hooks')
- *   .useWidget<ImageWidgetConfig> >} UseWidgetReturnType
+ *   .useWidget<ImageRowWidgetConfig> >} UseWidgetReturnType
  */
 
 // eslint-disable-next-line valid-jsdoc
 /**
- * Hook to access the Image widget configuration and data.
+ * Hook to access the Image row widget configuration and data.
  * Handles responsive image URLs and alt texts.
  */
-export const useImageWidget = () => {
+export const useImageRowWidget = () => {
   /** @type {UseWidgetReturnType}  */
   const { config } = useWidget();
 
   const {
     images = [],
     imageWrapping,
+    imageSpacing,
   } = config || {};
 
   const mappedImages = useMemo(() => images.map(({ image, link }) => {
@@ -41,5 +43,6 @@ export const useImageWidget = () => {
   return {
     images: mappedImages,
     imageWrapping,
+    imageSpacing,
   };
 };
