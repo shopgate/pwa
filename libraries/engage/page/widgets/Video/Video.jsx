7c0dc7867798d3d85b46dbcf1fd9fceec48a486a
aylinuenal <aylin.uenal@shopgate.com>
2025-10-16 13:56:52 +0200

    CURB-5435 improve video widget for loading and reduced motion

--- libraries/engage/page/widgets/Video/Video.jsx
+++ libraries/engage/page/widgets/Video/Video.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@shopgate/engage/styles';
+import { useReduceMotion } from '@shopgate/engage/a11y/hooks';
 import { useVideoWidget } from './hooks';
+import { usePrevious } from '../../../core';
 
 const useStyles = makeStyles()(() => ({
   video: {
     maxWidth: '100%',
-    maxHeight: '100%',
+  },
+  videoLoading: {
+    width: '100%',
   },
 }));
 /**
@@ -14,24 +18,47 @@ const useStyles = makeStyles()(() => ({
  */
 const Video = () => {
   const { classes } = useStyles();
+  const reduceMotion = useReduceMotion();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const videoRef = React.useRef(null);
+
   const {
     url, muted, loop, controls, autoplay,
   } = useVideoWidget();
+
+  const prevAutoPlay = usePrevious(autoplay);
+  useEffect(() => {
+    if (videoRef.current && prevAutoPlay && !autoplay) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+
+    if (videoRef.current && !prevAutoPlay && autoplay) {
+      videoRef.current.play();
+    }
+
+    if (!url) {
+      setIsLoaded(false);
+    }
+  }, [autoplay, prevAutoPlay, url]);
 
   if (!url) return null;
 
   return (
     <video
+      ref={videoRef}
       src={url}
       muted={muted}
       controls={controls}
-      autoPlay={autoplay}
-      className={classes.video}
+      autoPlay={reduceMotion ? false : autoplay}
+      className={isLoaded ? classes.video : classes.videoLoading}
       preload="auto"
       playsInline
       loop={loop}
       aria-hidden
+      onLoadedData={() => setIsLoaded(true)}
     >
+      {/* for a11y reasons there needs to be a track file (but video is aria hidden) */}
       <track kind="captions" src="" srcLang="de" label="Deutsch" />
     </video>
   );
