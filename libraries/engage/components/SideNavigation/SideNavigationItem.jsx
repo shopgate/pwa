89e2e338eeb8dad87f83e8d42343dc5e423ac732
aylinuenal <aylin.uenal@shopgate.com>
2025-10-31 11:14:39 +0100

    CURB-4564 refactor types in side navigation components

--- libraries/engage/components/SideNavigation/SideNavigationItem.jsx
+++ libraries/engage/components/SideNavigation/SideNavigationItem.jsx
@@ -2,30 +2,37 @@ import React, { useMemo, memo } from 'react';
 import classNames from 'classnames';
 import Link from '@shopgate/pwa-common/components/Link';
 import { i18n } from '@shopgate/engage/core';
+import PropTypes from 'prop-types';
 import {
   getIndentation, item, itemActive, link, linkActive, list,
 } from './SideNavigationItem.style';
 import { useSideNavigation } from './SideNavigation.hooks';
 
-type Props = {
-  children?: any,
-  level?: number,
-  buttonRight?: any,
-  forceActive?: boolean,
-  forceInactive?: boolean,
-  className?: string,
-  onClick?: () => any,
-  href?: string,
-  label: string
-}
-
 /**
- * SideNavigationItem component
- * @returns {JSX}
+ * SideNavigationItem component.
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} [props.children] - The child components.
+ * @param {number} [props.level=0] - The indentation level of the item.
+ * @param {React.ReactNode} [props.buttonRight] - The button to display on the right side.
+ * @param {boolean} [props.forceActive=false] - Whether to force the item to be active.
+ * @param {boolean} [props.forceInactive=false] - Whether to force the item to be inactive.
+ * @param {string} [props.className] - Additional class names for the item.
+ * @param {Function} [props.onClick] - The click handler for the item.
+ * @param {string} [props.href] - The URL the item links to.
+ * @param {string} props.label - The label text for the item.
+ * @returns {JSX.Element} The rendered component.
  */
 const SideNavigationItem = ({
-  level, href, label, buttonRight, children, forceActive, forceInactive, className, onClick,
-}: Props) => {
+  children,
+  level,
+  buttonRight,
+  forceActive,
+  forceInactive,
+  className,
+  onClick,
+  href,
+  label,
+}) => {
   const { currentPathname } = useSideNavigation();
   const isActive = useMemo(() => !forceInactive && (currentPathname === href || forceActive), [
     currentPathname,
@@ -66,6 +73,18 @@ const SideNavigationItem = ({
       {children}
     </li>
   );
+};
+
+SideNavigationItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  buttonRight: PropTypes.node,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  forceActive: PropTypes.bool,
+  forceInactive: PropTypes.bool,
+  href: PropTypes.string,
+  level: PropTypes.number,
+  onClick: PropTypes.func,
 };
 
 SideNavigationItem.defaultProps = {
