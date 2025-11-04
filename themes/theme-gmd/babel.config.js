ecfa99a075e4d7430ab2e05df0441095b00419e5
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-11-04 17:00:21 +0100

    CURB-4562 Updated babel.config files
    - added browser targets

--- themes/theme-gmd/babel.config.js
+++ themes/theme-gmd/babel.config.js
@@ -1,3 +1,10 @@
+/** @typedef {import('@babel/core').ConfigAPI} ConfigAPI */
+/** @typedef {import('@babel/core').TransformOptions} TransformOptions */
+
+/**
+ * @param {ConfigAPI} api The Babel config API object
+ * @returns {TransformOptions}
+ */
 module.exports = (api) => {
   const isTest = api.env('test');
   const isProd = api.env('production');
@@ -5,39 +12,29 @@ module.exports = (api) => {
   api.cache(true);
 
   return {
-    // Keep ES modules for Webpack tree-shaking
-
     presets: [
       ['@babel/preset-env', {
         modules: false,
         bugfixes: true,
-        // If you need automatic polyfills for legacy browsers, uncomment:
-        // useBuiltIns: 'usage',
-        // corejs: 3,
+        targets: {
+          ios: '13.4',
+          chrome: '80',
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
       }],
       ['@babel/preset-react', {
-        // React 16 -> classic runtime (keep this). If you move to React 17+,
-        // switch to runtime: 'automatic'
         runtime: 'classic',
         development: !isProd && !isTest,
       }],
-
       '@babel/preset-flow',
     ],
 
     plugins: [
-      // Keep lodash optimization
       'lodash',
-
-      // Class fields (replace deprecated "proposal" plugin)
       ['@babel/plugin-transform-class-properties', { loose: true }],
-
-      // If you use private fields/methods with loose semantics, add:
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-
-      // Path aliases
-
       ['module-resolver', {
         cwd: 'packagejson',
         alias: {
@@ -47,19 +44,14 @@ module.exports = (api) => {
           Pages: './pages',
         },
       }],
-
-      // Runtime helpers (avoid inlining helpers everywhere)
       ['@babel/plugin-transform-runtime', {
         helpers: true,
         regenerator: true,
-        // Do not set corejs here unless you want runtime to polyfill globals.
-        // Prefer preset-env's useBuiltIns/corejs instead.
       }],
     ],
 
     env: {
       test: {
-        // Transpile everything for Node/Jest
         presets: [
           ['@babel/preset-env', { targets: { node: 'current' } }],
           ['@babel/preset-react', { runtime: 'classic' }],
@@ -68,13 +60,11 @@ module.exports = (api) => {
       },
       development: {
         plugins: [
-          // Flow to PropTypes is optional; keep if you still want it
           'flow-react-proptypes',
         ],
       },
       production: {
         plugins: [
-          // Strip PropTypes in prod bundles
           'transform-react-remove-prop-types',
         ],
       },
