32dcea6459496d86d2ca97a22b76343537057874
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-11-04 08:07:03 +0100

    Fixed unit tests

--- libraries/engage/cart/components/PaymentBar/__tests__/PaymentBarCheckoutButton.spec.jsx
+++ libraries/engage/cart/components/PaymentBar/__tests__/PaymentBarCheckoutButton.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Link from '@shopgate/pwa-common/components/Link';
 import PaymentBarCheckoutButton from '../PaymentBarCheckoutButton';
 
 jest.mock('react', () => ({
@@ -15,16 +14,16 @@ jest.mock('../PaymentBarCheckoutButton.connector', () => cmp => cmp);
 describe('<PaymentBarCheckoutButton />', () => {
   it('should render disabled button', () => {
     const wrapper = shallow(<PaymentBarCheckoutButton isOrderable={false} />).dive();
-    const childLink = wrapper.find(Link);
+    const childLink = wrapper.find('Link');
 
     expect(wrapper).toMatchSnapshot();
     expect(childLink.props().disabled).toBe(true);
-    expect(wrapper.find('Translate').props().string).toBe('cart.checkout');
+    expect(wrapper.find('Text').props().string).toBe('cart.checkout');
   });
 
   it('should render enabled button', () => {
     const wrapper = shallow(<PaymentBarCheckoutButton isOrderable />).dive();
-    const childLink = wrapper.find(Link);
+    const childLink = wrapper.find('Link');
 
     expect(wrapper).toMatchSnapshot();
     expect(childLink.props().disabled).toBe(false);
