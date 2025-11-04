05506f010155f09d270fb9b20b4beda6c3878f62
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-11-04 14:53:59 +0100

    CURB-4562 Updated babel to latest versions

--- themes/theme-ios11/babel.config.js
+++ themes/theme-ios11/babel.config.js
@@ -1,32 +1,43 @@
 module.exports = (api) => {
+  const isTest = api.env('test');
+  const isProd = api.env('production');
+
   api.cache(true);
 
   return {
-    compact: true,
-    sourceType: 'unambiguous',
+    // Keep ES modules for Webpack tree-shaking
+
     presets: [
       ['@babel/preset-env', {
         modules: false,
+        bugfixes: true,
+        // If you need automatic polyfills for legacy browsers, uncomment:
+        // useBuiltIns: 'usage',
+        // corejs: 3,
       }],
-      '@babel/preset-react',
+      ['@babel/preset-react', {
+        // React 16 -> classic runtime (keep this). If you move to React 17+,
+        // switch to runtime: 'automatic'
+        runtime: 'classic',
+        development: !isProd && !isTest,
+      }],
+
       '@babel/preset-flow',
     ],
+
     plugins: [
+      // Keep lodash optimization
       'lodash',
-      '@babel/plugin-proposal-class-properties',
-      ['@babel/plugin-proposal-object-rest-spread', {
-        loose: true,
-      }],
-      ['@babel/plugin-transform-spread', {
-        loose: true,
-      }],
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-export-default-from',
-      'transform-export-extensions',
-      ['@babel/plugin-syntax-dynamic-import', {
-        loose: true,
-      }],
+
+      // Class fields (replace deprecated "proposal" plugin)
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+
+      // If you use private fields/methods with loose semantics, add:
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+
+      // Path aliases
+
       ['module-resolver', {
         cwd: 'packagejson',
         alias: {
@@ -36,26 +47,34 @@ module.exports = (api) => {
           Pages: './pages',
         },
       }],
+
+      // Runtime helpers (avoid inlining helpers everywhere)
       ['@babel/plugin-transform-runtime', {
-        helpers: false,
+        helpers: true,
         regenerator: true,
+        // Do not set corejs here unless you want runtime to polyfill globals.
+        // Prefer preset-env's useBuiltIns/corejs instead.
       }],
     ],
+
     env: {
       test: {
+        // Transpile everything for Node/Jest
         presets: [
-          '@babel/preset-env',
-          '@babel/preset-react',
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          ['@babel/preset-react', { runtime: 'classic' }],
           '@babel/preset-flow',
         ],
       },
       development: {
         plugins: [
+          // Flow to PropTypes is optional; keep if you still want it
           'flow-react-proptypes',
         ],
       },
       production: {
         plugins: [
+          // Strip PropTypes in prod bundles
           'transform-react-remove-prop-types',
         ],
       },
--- utils/webpack/webpack.config.js
+++ utils/webpack/webpack.config.js
@@ -229,6 +229,21 @@ const config = {
         test: /\.mjs$/,
         type: 'javascript/auto',
       },
+      // Special treatment for PWA Extension Kit to ensure compatibility with current Babel setup
+      // https://github.com/shopgate-professional-services/pwa-extension-kit
+      {
+        test: /\.js$/,
+        include: /node_modules\/@shopgate-ps\/pwa-extension-kit/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { modules: 'commonjs' }],
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: new RegExp(`node_modules\\b(?!\\${path.sep}@shopgate)\\b.*`),
