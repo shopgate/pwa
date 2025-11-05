b68b1fd90d47ebd385b63cc02761dcdaa0b2c618
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-11-05 23:47:40 +0100

    CURB-4562 Added option to skip long running unit tests and removed unused packages

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,30 @@
 const jestConfig = require('@shopgate/pwa-unit-test/jest.config');
 
+const fs = require('fs');
+const path = require('path');
+
+const engageRoot = path.join(__dirname, 'libraries', 'engage');
+
+// Collect all immediate subfolders inside /libraries/engage
+let engagePackages = [];
+try {
+  engagePackages = fs
+    .readdirSync(engageRoot)
+    .filter(name => fs.statSync(path.join(engageRoot, name)).isDirectory());
+} catch {
+  engagePackages = [];
+}
+
+// Build a regex to match only: libraries/engage/<package>/index.spec.js(x)
+// Used to skip these tests when RUN_LONG is not set
+const skippedIndexSpecPattern =
+  `libraries[\\\\/]engage[\\\\/](?:${engagePackages.join('|')})[\\\\/]index\\.spec\\.(js|jsx)$`;
+
+// Only skip these tests when RUN_LONG is NOT set
+const skipPatterns = process.env.RUN_LONG === 'true'
+  ? [] // run everything
+  : [skippedIndexSpecPattern];
+
 const testedExtensions = [
   '@shopgate-tracking-ga-native',
   '@shopgate-product-reviews',
@@ -25,6 +50,7 @@ module.exports = Object.assign({}, jestConfig, {
     `/extensions/(?!(${testedExtensions.join('|')}))`,
     '/dist/',
     '.*/dist/.*',
+    ...skipPatterns,
   ],
   transformIgnorePatterns: [
     'node_modules/(?!(swiper|dom7)/)',
