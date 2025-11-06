e58cb24ebc9eedf8e85b523121a25efe67f87502
Frithjof Klös <frithjof.kloes@shopgate.com>
2025-11-06 08:31:58 +0100

    CURB-4562 Small improvement

--- themes/theme-ios11/jest.config.js
+++ themes/theme-ios11/jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     '^Config(.*)$': '<rootDir>/config',
     '^Extensions(.*)$': '<rootDir>/extensions',
     '^Pages(.*)$': '<rootDir>/pages',
-    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     // Fix issue with Swiper ES module imports that work via "exports" field in package.json
     '^swiper/react$': '<rootDir>/node_modules/swiper/swiper-react.mjs',
     // Mock Swiper styles since they are not needed for unit tests
