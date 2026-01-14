edf497c824283531b21169411a599c631f1329e5
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-01-14 13:16:53 +0100

    CURB-5626 Reordered ESLint rules

--- utils/eslint-config/ruleset/react.js
+++ utils/eslint-config/ruleset/react.js
@@ -58,83 +58,29 @@ module.exports = {
     ],
 
     // ## Fixes of Airbnb rules for our use cases
+
     'react/no-unknown-property': ['error', { ignore: ['test-id'] }],
-    'function-paren-newline': ['error', 'consistent'],
-    'function-call-argument-newline': ['warn', 'consistent'],
-    'no-restricted-exports': [
-      'warn',
-      {
-      // Allow export { default } from '...';
-        restrictDefaultExports: { defaultFrom: false },
-      },
-    ],
-    camelcase: 'warn',
-    'class-methods-use-this': 'warn',
-    // Warn about unexpected side effects by using async functions as Promise executors
-    'no-async-promise-executor': 'warn',
-    // Just warn about circular dependencies since they are hard to avoid completely
-    'import/no-cycle': 'warn',
     // Warn about unstable nested components, but allow them as props to other components
     'react/no-unstable-nested-components': ['warn', { allowAsProps: true }],
-    'arrow-parens': [
-      'error',
-      'as-needed',
-      { requireForBlockBody: true },
-    ],
     // Don't care about placement of static properties in React components
     'react/static-property-placement': 'off',
     // Allow prop spreading
     'react/jsx-props-no-spreading': 'off',
     // Allow function components to be defined using arrow functions
     'react/function-component-definition': 'off',
+    // Warn about usage of .bind() or array functions in JSX props
+    'react/jsx-no-bind': 'warn',
 
     // ## TBD
-    'react/jsx-curly-newline': 'off',
-    'react/jsx-no-bind': 'warn',
+    'react/jsx-curly-newline': ['warn', 'consistent'],
 
     // ## ESLint complain makes sense - revisit at the next refactoring
 
     'react/state-in-constructor': 'warn',
-    // In functions parameters with default values should be last
-    'default-param-last': 'warn',
-    // In switch statements, require 'default' case to be last
-    'default-case-last': 'warn',
-    // Return values of Promise executor functions don't make sense
-    'no-promise-executor-return': 'warn',
-    // Prefer Object spread over Object.assign
-    'prefer-object-spread': 'warn',
-    // Catch blocks that only rethrow the caught error are redundant
-    'no-useless-catch': 'warn',
-    // Prefer regex literals over RegExp constructor
-    'prefer-regex-literals': 'warn',
 
-    // ## Autofixable
+    // ## Autofixable - can be removed when ESLint update pull request is merged
     'react/jsx-curly-brace-presence': 'warn',
     'react/jsx-indent-props': 'warn',
     'react/jsx-fragments': 'warn',
-    indent: 'warn',
-    semi: ['warn', 'always'],
   },
-  overrides: [
-    {
-      files: [
-        '**/*.spec.js',
-        '**/*.spec.jsx',
-        '**/*.test.js',
-        '**/*.test.jsx',
-        '**/*.mock.js',
-        '**/*.mock.jsx',
-        '**/spec.js',
-        '**/spec.jsx',
-        '**/mock.js',
-        '**/mock.jsx',
-      ],
-      rules: {
-        // Allow more than one class per file in test files
-        'max-classes-per-file': 'off',
-        // Allow non-camelcase names in test files (e.g., snake_case from API)
-        camelcase: 'off',
-      },
-    },
-  ],
 };
