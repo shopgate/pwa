7a025e2e039f9a4711bc7fe0cded15dd962d7bbd
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-01-16 11:17:16 +0100

    CURB-5672 Replaced babel-plugin-lodash to get rid of deprecation warnings in webpack logs

--- themes/theme-ios11/babel.config.js
+++ themes/theme-ios11/babel.config.js
@@ -26,7 +26,7 @@ module.exports = (api) => {
     ],
 
     plugins: [
-      'lodash',
+      '@sigmacomputing/babel-plugin-lodash',
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
