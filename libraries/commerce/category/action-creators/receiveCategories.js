98de801c7760cd457788eeb2e091e35dccc03c29
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-03-03 14:42:08 +0100

    CURB-5759 Small improvement
    - receiveCategories can be imported from engage package

--- libraries/commerce/category/action-creators/receiveCategories.js
+++ libraries/commerce/category/action-creators/receiveCategories.js
@@ -1,8 +1,8 @@
 import { RECEIVE_CATEGORIES } from '../constants';
 
 /**
- * Dispatches an action to store multiple categories with a single action.
- * @param {Array} categories The received categories.
+ * Action to put multiple categories into the redux store with a single dispatch.
+ * @param {Object[]} categories The received categories.
  * @return {Object} The RECEIVE_CATEGORIES action.
  */
 const receiveCategories = categories => ({
