1998ab26a2c1169a33e92d73f2c15083131d750a
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-03-17 15:05:39 +0100

    CURB-5780 Improved Icon component types

--- libraries/common/components/Icon/index.tsx
+++ libraries/common/components/Icon/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { makeStyles } from '@shopgate/engage/styles';
 
 const useStyles = makeStyles()({
@@ -10,22 +9,45 @@ const useStyles = makeStyles()({
 });
 
 export interface IconProps {
+  /**
+   * Raw SVG markup string that will be injected into the <svg>.
+   */
   content: string;
+  /**
+   * Additional class name(s) applied to the root element.
+   */
   className?: string;
-  color?: string | null;
+  /**
+   * Overrides the icon color (maps to CSS `fill`).
+   */
+  color?: string;
+  /**
+   * Controls the icon size via `font-size`.
+   * Accepts any valid CSS size value.
+   * @default 'inherit'
+   */
   size?: string | number;
+  /**
+   * The SVG viewBox attribute.
+   * @default '0 0 24 24'
+   */
   viewBox?: string;
 }
 
 /**
  * The Icon component.
+ *
+ * Renders an inline SVG icon using injected SVG markup.
+ *
+ * Provides basic styling via `currentColor` and supports size and color
+ * overrides through props while allowing external class-based overrides.
  */
 const Icon = ({
   content,
-  className = '',
-  color = null,
+  className,
+  color,
   viewBox = '0 0 24 24',
-  size = 24,
+  size = 'inherit',
 }: IconProps) => {
   const { cx, classes } = useStyles();
 
@@ -34,10 +56,11 @@ const Icon = ({
       className={cx(classes.root, 'common__icon', className)}
       viewBox={viewBox}
       xmlns="http://www.w3.org/2000/svg"
+      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: content }}
       style={{
         fontSize: size,
-        fill: color ?? undefined,
+        fill: color,
       }}
     />
   );
