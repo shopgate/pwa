4c65eb2393b50bdcc420ad3eb52456d42ba36669
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-08-27 11:51:24 +0200

    CURB-5783 Add additional url to cms preview allow list

--- libraries/engage/admin-preview/constants/index.ts
+++ libraries/engage/admin-preview/constants/index.ts
@@ -11,5 +11,6 @@ export const ALLOWED_ADMIN_PREVIEW_ORIGINS = [
   'https://admin-mono.shopgatedev.com',
   'https://admin-mono.shopgatepg.com',
   'https://admin-mono.shopgate.com',
+  'http://localhost.localdev.cc',
   'http://localhost:1337',
 ];
