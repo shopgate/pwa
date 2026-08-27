e9560887daf59c6fc15df1cdfbb359089284897d
Frithjof Klös <frithjof.kloes@shopgate.com>
2026-08-27 10:54:00 +0200

    CURB-5783 Add cms / page config support for more domains

--- libraries/engage/admin-preview/constants/index.ts
+++ libraries/engage/admin-preview/constants/index.ts
@@ -8,5 +8,8 @@ export const ALLOWED_ADMIN_PREVIEW_ORIGINS = [
   'https://next.admin.shopgatepg.com',
   'https://next.admin.shopgate.com',
   'https://next.us.admin.shopgate.com',
+  'https://admin-mono.shopgatedev.com',
+  'https://admin-mono.shopgatepg.com',
+  'https://admin-mono.shopgate.com',
   'http://localhost:1337',
 ];
